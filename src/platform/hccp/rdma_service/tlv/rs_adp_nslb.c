447d4af0b6d32f2d7fdf6820693f4adcd0c0f669
yangxiaoyuan <yangxiaoyuan3@huawei.com>
2025-12-03 22:11:26 +0800

    change netco init fail log level from error to warn
    Co-authored-by: 杨小渊<yangxiaoyuan3@huawei.com>

--- src/platform/hccp/rdma_service/tlv/rs_adp_nslb.c
+++ src/platform/hccp/rdma_service/tlv/rs_adp_nslb.c
@@ -132,6 +132,7 @@ int RsNslbNetcoRequest(unsigned int phyId, struct RsNslbCb *nslbCb, unsigned int
     switch(type) {
         case NETCO_REQ_TYPE_INIT:
             ret = RsNslbNetcoInit(phyId, nslbCb);
+            CHK_PRT_RETURN(ret == -ENOTSUPP, hccp_warn("netco init unsuccessful ret(%d)", ret), 0);
             break;
         case NETCO_REQ_TYPE_DEINIT:
             RsNslbNetcoDeinit(nslbCb);
