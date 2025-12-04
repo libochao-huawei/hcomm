8d3c371fa388029ee37a6632ab6a4fbc6af88d68
杨小渊 <yangxiaoyuan3@huawei.com>
2025-12-04 20:57:34 +0800

    add description
    Signed-off-by: 杨小渊 <yangxiaoyuan3@huawei.com>

--- src/platform/hccp/external_depends/rdma-core/include/ccan/str.h
+++ src/platform/hccp/external_depends/rdma-core/include/ccan/str.h
@@ -1,7 +1,7 @@
 /* CC0 (Public domain) - see LICENSE.CC0 file for details */
 #ifndef CCAN_STR_H
 #define CCAN_STR_H
-#include "config.h"
+// #include "config.h"
 #include <string.h>
 #include <stdbool.h>
 #include <limits.h>
