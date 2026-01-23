103755111cffd92cf0e99341d9d49b9ab1951ab4
jiyuanhao <jiyuanhao1@huawei.com>
2026-01-23 09:51:33 +0800

    [Fix] Fix SetDispatcherCtx
    Co-authored-by: jiyuanhao<jiyuanhao1@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !919 merge fix/dispatcherctx into master
    
    [Fix] Fix SetDispatcherCtx
    
    Created-by: jiyuanhao
    Commit-by: jiyuanhao
    Merged-by: cann-robot
    Description: [Fix] Fix SetDispatcherCtx
    
    See merge request: cann/hcomm-dev!919

--- src/framework/device/framework/aicpu_communicator.cc
+++ src/framework/device/framework/aicpu_communicator.cc
@@ -5600,7 +5600,6 @@ HcclResult HcclCommAicpu::NotifyFree(NotifyMgrAicpuParam *param)
 
 HcclResult HcclCommAicpu::RegisterOpInfo(void* opInfo, u32 size)
 {
-    CHK_RET(SetDispatcherCtx(dispatcherCtx_));
     CHK_RET(taskExecption_.RegisterOpInfo(opInfo, size));
     u32 opIdx = taskExecption_.GetOpRingBufferIdx();
     CHK_RET(SetDispatcherCtxOpIdx(opIdx));
@@ -5611,6 +5610,13 @@ HcclResult HcclCommAicpu::RegisterOpInfo(void* opInfo, u32 size)
 HcclResult HcclCommAicpu::RegOpTaskException(HcommGetOpInfoCallback callback)
 {
     CHK_RET(taskExecption_.RegisterOpInfoCallback(callback));
+    return HCCL_SUCCESS;
+}
+
+HcclResult HcclCommAicpu::SetDispatcherCtxOnThread()
+{
+    // 设置 DispatcherCtx 到线程变量
+    CHK_RET(SetDispatcherCtx(dispatcherCtx_));
     return HCCL_SUCCESS;
 }
 }  // namespace hccl
