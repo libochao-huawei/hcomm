79a2ba12f96a49a774c1d9f2ea6610a52950515b
yuxjbiu <yuxuejiao3@h-partners.com>
2026-01-28 20:25:19 +0800

    add license
    Co-authored-by: ywx1202661<yuxuejiao3@h-partners.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !1039 merge master into master
    
    add license
    
    Created-by: yuxjbiu
    Commit-by: ywx1202661
    Merged-by: cann-robot
    Description: test llt
    Merge branch 'master' of https://gitcode.com/yuxjbiu/hcomm-dev into master
    add license
    
    See merge request: cann/hcomm-dev!1039

--- test/llt/aicpu_kfc/stub/llt_aicpu_kfc_stub.cc
+++ test/llt/aicpu_kfc/stub/llt_aicpu_kfc_stub.cc
@@ -1,25 +1,12 @@
-/******************************************************************************
-
-                  版权所有 (C), 2001-2011, 华为技术有限公司
-
- ******************************************************************************
-  文 件 名   : llt_aicpu_kfc_stub.cc
-  版 本 号   : 初稿
-  作    者   : p00335137
-  生成日期   : 2017年12月7日
-  最近修改   : 2024年11月13日
-  功能描述   : hccl llt stub函数
-  函数列表   :
-  修改历史   :
-  1.日    期   : 2017年12月7日
-    作    者   : p00335137
-    修改内容   : 创建文件
-
-  2.日    期   : 2024年11月13日
-    作    者   : l30061817
-    修改内容   : 修改文件
-
-******************************************************************************/
+/**
+ * Copyright (c) 2025 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
 
 #include <driver/ascend_hal.h>
 #include <runtime/rt.h>
