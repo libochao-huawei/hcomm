cb652367e0e52789aba597340d8e780cba5449e1
jiyuanhao <jiyuanhao1@huawei.com>
2026-01-30 16:55:02 +0800

    license

--- src/legacy/framework/service/aicpu/alg_topo_package_helper.cc
+++ src/legacy/framework/service/aicpu/alg_topo_package_helper.cc
@@ -7,6 +7,7 @@
  * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
  * See LICENSE in the root of the software repository for the full text of the License.
  */
+
 #include "alg_topo_package_helper.h"
 #include "binary_stream.h"
 
