46d6a50f229771d9a5e4aa620ddcd015f3295c6f
jiyuanhao <jiyuanhao1@huawei.com>
2026-01-30 16:55:02 +0800

    license

--- src/legacy/framework/resource_manager/transport/ub_memory_transport_mgr.cc
+++ src/legacy/framework/resource_manager/transport/ub_memory_transport_mgr.cc
@@ -7,6 +7,7 @@
  * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
  * See LICENSE in the root of the software repository for the full text of the License.
  */
+
 #include "ub_memory_transport_mgr.h"
 #include "timeout_exception.h"
 #include "communicator_impl.h"
