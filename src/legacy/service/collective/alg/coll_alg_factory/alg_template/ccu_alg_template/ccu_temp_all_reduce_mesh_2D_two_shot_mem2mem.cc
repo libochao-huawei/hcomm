755720603f70418a3d63b7e165828e9c6d877141
jiyuanhao <jiyuanhao1@huawei.com>
2026-02-02 11:39:22 +0800

    update license

--- src/legacy/service/collective/alg/coll_alg_factory/alg_template/ccu_alg_template/ccu_temp_all_reduce_mesh_2D_two_shot_mem2mem.cc
+++ src/legacy/service/collective/alg/coll_alg_factory/alg_template/ccu_alg_template/ccu_temp_all_reduce_mesh_2D_two_shot_mem2mem.cc
@@ -1,10 +1,13 @@
-
-/*
- * Copyright (c) Huawei Technologies Co., Ltd. 2025-2025. All rights reserved.
- * Description: 算法模板 CcuTempAllReduceMeshTwoShotMem2Mem2D 类实现
- * Author: lijiangquan
- * Create: 2025-10-14
+/**
+ * Copyright (c) 2025 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
  */
+
 #include "ccu_temp_all_reduce_mesh_2D_two_shot_mem2mem.h"
 
 #include <ios>
