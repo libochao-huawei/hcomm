f2ab93429138f688a494f93bbb1387a87a3788d9
jiyuanhao <jiyuanhao1@huawei.com>
2026-02-02 11:32:37 +0800

    update license

--- src/legacy/service/collective/alg/coll_alg_factory/alg_ccu_context/ccu_context_alg_base.h
+++ src/legacy/service/collective/alg/coll_alg_factory/alg_ccu_context/ccu_context_alg_base.h
@@ -1,11 +1,13 @@
-/*
- * Copyright (c) Huawei Technologies Co., Ltd. 2025-2025. All rights reserved.
- * Description: CcuContextAlgBase类的头文件
- * Author: hulinkang
- * Create: 2025-08-21
+/**
+ * Copyright (c) 2025 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
  */
- 
-// 算法层CcuContext基类，包含V2版本方法的实现
+
 #ifndef HCCLV2_CCU_CONTEXT_ALG_BASE_H_
 #define HCCLV2_CCU_CONTEXT_ALG_BASE_H_
  
