23baa2649e29eca4928f9fb63b3668e5b79b287b
mumu233 <lirui345@h-partners.com>
2026-02-02 16:18:33 +0800

    HCCP LLT INIT

---
+++ test/ut/platform/hccp/rdma_service_stub/tc_ut_rs_ub.h
@@ -0,0 +1,20 @@
+#ifndef _TC_UT_RS_UB_H
+#define _TC_UT_RS_UB_H
+
+#include <stdio.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void tc_rs_ub_ctx_ext_jetty_create();
+void tc_rs_ub_ctx_ext_jetty_delete();
+void tc_rs_ub_stars_va_munmap_batch();
+
+void tc_rs_ub_ctx_jfc_create_ext();
+void tc_rs_ub_delete_jfc_ext();
+
+#ifdef __cplusplus
+}
+#endif
+#endif
