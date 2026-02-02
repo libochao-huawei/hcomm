506eca59a56a537320ae81b9adffbd3de565a756
jiyuanhao <jiyuanhao1@huawei.com>
2026-02-02 19:24:01 +0800

    [Fix] Update license [2/2]
    Co-authored-by: jiyuanhao<jiyuanhao1@huawei.com>

--- src/platform/hccp/external_depends/ubengine/udma_u_ctl.h
+++ src/platform/hccp/external_depends/ubengine/udma_u_ctl.h
@@ -1,5 +1,12 @@
-/* SPDX-License-Identifier: MulanPSL-2.0 */
-/* Copyright (c) 2025 HiSilicon Technologies Co., Ltd. All rights reserved. */
+/**
+ * Copyright (c) 2025 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
 
 #ifndef __UDMA_U_CTL_H__
 #define __UDMA_U_CTL_H__
