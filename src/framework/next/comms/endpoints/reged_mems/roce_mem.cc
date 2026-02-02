9843771278d687c537548c05689365c15c8e7930
liguicheng8888 <liguicheng7@huawei.com>
2026-02-02 17:28:48 +0800

    HcclBuffer重复注册变更

--- src/framework/next/comms/endpoints/reged_mems/roce_mem.cc
+++ src/framework/next/comms/endpoints/reged_mems/roce_mem.cc
@@ -29,7 +29,6 @@ HcclResult RoceRegedMemMgr::RegisterMemory(HcommMem mem, const char *memTag, voi
     HCCL_INFO("[%s] Begin", __FUNCTION__);
     CHK_PTR_NULL(this->localRdmaRmaBufferMgr_);
     CHK_PTR_NULL(memHandle);
-    CHK_PTR_NULL(memTag);
 
     // 构造LocalRdmaRmaBuffer
     std::shared_ptr<Hccl::Buffer> localBufferPtr = nullptr;
@@ -50,7 +49,7 @@ HcclResult RoceRegedMemMgr::RegisterMemory(HcommMem mem, const char *memTag, voi
     }
     
     std::shared_ptr<Hccl::LocalRdmaRmaBuffer> localRdmaRmaBuffer = nullptr;
-    EXECEPTION_CATCH((localRdmaRmaBuffer = std::make_shared<Hccl::LocalRdmaRmaBuffer>(localBufferPtr, this->rdmaHandle_)),
+    EXECEPTION_CATCH((localRdmaRmaBuffer = std::make_shared<Hccl::localRdmaRmaBuffer>(localBufferPtr, this->rdmaHandle_)),
         return HCCL_E_PTR);
     
     // 注册到LocalRdmaRmaBuffer计数器
