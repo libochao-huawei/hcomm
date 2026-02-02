55d6cda8ec55b3e2201fddc551f05770e67e20b8
jiyuanhao <jiyuanhao1@huawei.com>
2026-02-02 19:24:01 +0800

    [Fix] Update license [2/2]
    Co-authored-by: jiyuanhao<jiyuanhao1@huawei.com>

--- src/legacy/service/collective/alg/coll_alg_factory/alg_ccu_context/ccu_context_broadcast_mesh2d.h
+++ src/legacy/service/collective/alg/coll_alg_factory/alg_ccu_context/ccu_context_broadcast_mesh2d.h
@@ -1,10 +1,12 @@
-/*
- * Copyright (c) Huawei Technologies Co., Ltd. 2025-2025. All rights reserved.
- * Description: CcuContextBroadcastMesh2D的头文件
- * Author: ningshuqi
- * Create: 2025-02-19
+/**
+ * Copyright (c) 2025 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
  */
-
 
 #ifndef HCCLV2_CCU_CONTEXT_BROADCAST_MESH_2D_H_
 #define HCCLV2_CCU_CONTEXT_BROADCAST_MESH_2D_H_
