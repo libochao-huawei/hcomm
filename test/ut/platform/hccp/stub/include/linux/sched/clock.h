7524519bb9764c948af000d7bb20c78a86da6ea9
mumu233 <lirui345@h-partners.com>
2026-02-02 16:18:33 +0800

    HCCP LLT INIT

---
+++ test/ut/platform/hccp/stub/include/linux/sched/clock.h
@@ -0,0 +1,12 @@
+#ifndef _LINUX_SCHED_CLOCK_H
+#define _LINUX_SCHED_CLOCK_H
+ 
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+ 
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+ 
+#endif /* _LINUX_SCHED_CLOCK_H */
