875d18a0b04b2e40db006aa6196e675b0b1086d0
ren-mengguang <renmengguang@h-partners.com>
2026-02-03 21:07:11 +0800

    Add DFX

--- src/framework/communicator/impl/resource_manager/transport_manager.cc
+++ src/framework/communicator/impl/resource_manager/transport_manager.cc
@@ -932,8 +932,8 @@ HcclResult TransportManager::CreateLink(const std::string &tag, const ErrContext
         retOut = ret;
         CHK_PRT_BREAK(ret != HCCL_SUCCESS, HCCL_ERROR("[%s]errNo[0x%016llx]TransportInit error.", __func__, HCCL_ERROR_CODE(ret)),);
     } while(0);
-
-    if (ret != HCCL_SUCCESS) {
+    // 临时硬编码出包验证
+    if (ret == HCCL_SUCCESS) {
         link = nullptr;
         retOut = ret;
         if (ret == HCCL_E_MEMORY) {
@@ -958,8 +958,14 @@ HcclResult TransportManager::CreateLink(const std::string &tag, const ErrContext
         CHK_PRT_CONT(stringRet == -1, HCCL_ERROR("[Create][DestLink]Transport init error! Failed to build log info"));
         std::string tmpErrInfo = ret == HCCL_E_TIMEOUT ? LOG_KEYWORDS_TIMEOUT : LOG_KEYWORDS_RUN_FAILED;
         HCCL_ERROR("[%s][%s]Transport init error! %s", LOG_KEYWORDS_INIT_CHANNEL.c_str(), tmpErrInfo.c_str(), errorLogBuffer);
-
-        return ret;
+        std::string logicSuperPodId = GetExternalInputLogicSuperPodId();
+        logicSuperPodId = logicSuperPodId.empty() ? "not set" : logicSuperPodId;
+        s64 phySuperPodId;
+        CHK_RET(hrtGetDeviceInfo(deviceLogicId_, HcclRtDeviceModuleType::HCCL_RT_MODULE_TYPE_SYSTEM,
+                                 HcclRtDeviceInfoType::HCCL_INFO_TYPE_SUPER_POD_ID, phySuperPodId));
+        HCCL_ERROR("[TransportManager][%s]nicType[%d], logicSuperPodId[%s], phySuperPodId[%lld].Cannot assign ranks from different physical nodes to the same logical node.", __func__, nicType, logicSuperPodId.c_str(), phySuperPodId);
+        // 临时硬编码出包验证
+        return HCCL_E_PARA;
     }
     HCCL_INFO("[createLink success]:rank[%u]-localUserrank[%u]-localIpAddr[%s], "
         "dst_rank[%u]-remoteUserrank[%u]-remote_ip_addr[%s], tag[%s]", userRank_, rankInfoList_[userRank_].worldRank,
