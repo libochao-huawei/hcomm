1e5fd88c6ad674939331fdd1a7ec98e6ac256a55
xz00xz <xiongzhe5@huawei.com>
2026-02-04 17:00:52 +0800

    HCCP support get jetty context

--- src/platform/hccp/rdma_service/ctx/rs_ctx.h
+++ src/platform/hccp/rdma_service/ctx/rs_ctx.h
@@ -87,4 +87,6 @@ RS_ATTRI_VISI_DEF int RsCtxGetAuxInfo(struct RaRsDevInfo *devInfo, struct HccpAu
     struct HccpAuxInfoOut *infoOut);
 RS_ATTRI_VISI_DEF int RsCtxGetCrErrInfoList(struct RaRsDevInfo *devInfo, struct CrErrInfo infoList[],
     unsigned int *num);
+RS_ATTRI_VISI_DEF int RsCtxGetJettyContext(struct RaRsDevInfo *devInfo, unsigned int id, char context[],
+    unsigned int *len)
 #endif // RS_CTX_H
