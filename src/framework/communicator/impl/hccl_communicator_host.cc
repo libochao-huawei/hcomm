5adb5d8b0ed26c7c354117b7cde4916f7081abdc
zwx1212220 <zhangjianning3@h-partners.com>
2026-02-05 10:04:37 +0800

    [zjn] DFX

--- src/framework/communicator/impl/hccl_communicator_host.cc
+++ src/framework/communicator/impl/hccl_communicator_host.cc
@@ -4342,8 +4342,8 @@ namespace hccl
         if (isOnlyAiv_ && !algDesc.isAivMode) {
             std::string opTypeName = GetCMDTypeEnumStr(opType);
             HCCL_ERROR("[HcclCommunicator][ExecOp] opType[%s] currently do not select aiv mode,"
-                "aiv only not support, if support is required,"
-                "ensure the number of cards is greater than one", opTypeName.c_str());
+                "aiv only not support, please ensure rankNum is greater than one",
+                opTypeName.c_str());
             return HCCL_E_NOT_SUPPORT;
         }
         CHK_RET(PrepareZeroCopy(algName, algDesc, opParam));
