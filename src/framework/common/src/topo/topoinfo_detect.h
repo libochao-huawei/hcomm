1aa99d608c6532664853185e34d3744edba08bbc
leironghao1 <leironghao1@h-partners.com>
2026-02-05 09:57:39 +0800

    HCCL支持通信域级别的AICPU展开模式配置

--- src/framework/common/src/topo/topoinfo_detect.h
+++ src/framework/common/src/topo/topoinfo_detect.h
@@ -31,7 +31,7 @@ public:
     HcclResult SetupAgent(u32 rankSize, u32 myrank, const HcclRootHandle &rootInfo, const HcclRankHandle &rankHandle, const CommConfig &commConfig);  // 分层建链时使用的SetupAgent
     HcclResult PrepareHandle (HcclRankHandle &rankHandle, std::vector<HcclIpAddress> &whitelist); //准备要发送的agent
     HcclResult SetupRank(std::shared_ptr<HcclSocket> &agentConnRoot); //分层建链时获得每个GroupLeader的监听端口
-    HcclResult SetupAgentByMasterInfo(HcclIpAddress &localHostIp, const HcclRootHandle &rootInfo, const CommConfig &commConfig);
+    HcclResult SetupAgentByMasterInfo(HcclIpAddress &localHostIp, const HcclRootHandle &rootInfo);
     HcclResult SetupServer(HcclRootHandle &rootInfo);
     HcclResult GroupLeaderAccept(HcclRankHandle &grpLeaderInfo, std::vector<HcclIpAddress> whitelist,
         std::shared_ptr<HcclSocket> grpLeaderToRoot);
