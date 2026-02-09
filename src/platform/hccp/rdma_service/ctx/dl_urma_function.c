e97fcabbf2e4d432f2bfeecb9f453a47520aa6f4
mumu233 <lirui345@h-partners.com>
2026-02-09 14:14:48 +0800

    头文件版权说明修改

--- src/platform/hccp/rdma_service/ctx/dl_urma_function.c
+++ src/platform/hccp/rdma_service/ctx/dl_urma_function.c
@@ -1,9 +1,13 @@
-/*
- * Copyright (c) Huawei Technologies Co., Ltd. 2023. All rights reserved.
- * Description: 获取动态库中urma接口函数的适配接口
- * Create: 2023-11-15
+/**
+ * Copyright (c) 2025 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
  */
- 
+
 #include <errno.h>
 #include <urma_api.h>
 #include "hccp_dl.h"
