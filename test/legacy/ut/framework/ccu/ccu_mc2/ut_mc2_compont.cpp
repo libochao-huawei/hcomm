924c7da36df7d0fd4d3c0432fc8fe24da8c4dee4
linyixin4 <linyixin4@huawei.com>
2026-02-10 09:19:04 +0800

    Bugfix for MC2 ccu scheduel mode.

--- test/legacy/ut/framework/ccu/ccu_mc2/ut_mc2_compont.cpp
+++ test/legacy/ut/framework/ccu/ccu_mc2/ut_mc2_compont.cpp
@@ -111,8 +111,7 @@ TEST_F(Mc2CompontTest, should_return_success_when_calling_Alloc)
 
     void *commContext;
     // check
-    HcclCombinOpParam combinOpParam{0};
-    EXPECT_NO_THROW(mc2Compont.Alloc(combinOpParam));
+    EXPECT_NO_THROW(mc2Compont.Alloc());
 }
 
 TEST_F(Mc2CompontTest, should_return_success_when_calling_AllocV2)
@@ -142,8 +141,7 @@ TEST_F(Mc2CompontTest, should_return_success_when_calling_AllocV2)
     commConfigPtr->srcDataType = HcclDataType::HCCL_DATA_TYPE_FP32;
     commConfigPtr->dstDataType = HcclDataType::HCCL_DATA_TYPE_FP32;
 
-    HcclCombinOpParam combinOpParam{0};
-    EXPECT_NO_THROW(mc2Compont.AllocV2(combinOpParam));
+    EXPECT_NO_THROW(mc2Compont.AllocV2());
 
     free(mc2TilingPtr);
 }
