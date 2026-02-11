0c8772267c3712164c92f816d00f422f112b75ec
WujingChen <chenwujing@h-partners.com>
2026-02-11 19:46:29 +0800

    aaa

--- src/legacy/framework/resource_manager/socket/socket_manager.cc
+++ src/legacy/framework/resource_manager/socket/socket_manager.cc
@@ -92,6 +92,7 @@ void SocketManager::ServerInit(PortData &localPort)
 {
     std::lock_guard<std::mutex> lock(socketLock);
     auto &serverSocketMap = SocketManager::GetServerSocketMap();
+    auto &rankListenPortMap = SocketManager::GetDeviceServerListenPortMap();
     auto serverSocketInMap = serverSocketMap.find(localPort);
     if (serverSocketInMap != serverSocketMap.end()) {
         HCCL_INFO("[%s] find localPort in serverSocketMap, localPort [%s]", __func__, localPort.Describe().c_str());
@@ -101,8 +102,8 @@ void SocketManager::ServerInit(PortData &localPort)
     SocketHandle hccpSocketHandle = SocketHandleManager::GetInstance().Create(devicePhyId, localPort);
     IpAddress    ipAddress        = localPort.GetAddr();
     u32 serverListenPort          = DEFAULT_VALUE_DEVICEPORT;
-    auto iter = rankListenPortMap_.find(devicePhyId);
-    if (iter != rankListenPortMap_.end()) {
+    auto iter = rankListenPortMap.find(devicePhyId);
+    if (iter != rankListenPortMap.end()) {
         serverListenPort = iter->second;
     }
     auto         serverSocket     = socketProducer(ipAddress, ipAddress, serverListenPort, hccpSocketHandle, "server",
@@ -197,20 +198,20 @@ Socket *SocketManager::CreateConnectedSocket(SocketConfig &socketConfig)
         THROW<NullPtrException>(StringFormat("socketHandle of is nullptr, devicePhyId=%d, port=%s", devicePhyId,
                                              socketConfig.link.GetLocalPort().Describe().c_str()));
     }
-
+    auto &rankListenPortMap = SocketManager::GetDeviceServerListenPortMap();
     IpAddress  localIpAddress  = socketConfig.link.GetLocalAddr();
     IpAddress  remoteIpAddress = socketConfig.link.GetRemoteAddr();
     SocketRole socketRole      = socketConfig.GetRole();
     string     hccpSocketTag   = socketConfig.GetHccpTag();
     u32 serverListenPort = DEFAULT_VALUE_DEVICEPORT;
-    auto iter = rankListenPortMap_.find(static_cast<u32>(socketConfig.remoteRank));
-    if (iter != rankListenPortMap_.end()) {
+    auto iter = rankListenPortMap.find(static_cast<u32>(socketConfig.remoteRank));
+    if (iter != rankListenPortMap.end()) {
         serverListenPort = iter->second;
     }
 
     auto tmpSocket = socketProducer(localIpAddress, remoteIpAddress, serverListenPort, socketHandle, hccpSocketTag,
                                     socketRole, NicType::DEVICE_NIC_TYPE);
-    HCCL_INFO("[SocketManager::%s] Device %u ConnectAsync port %u.", __func__, deviceLogicId_, serverListenPort);
+    HCCL_INFO("[SocketManager::%s] Device %u connect async the remote rank %u port %u.", __func__, devicePhyId, socketConfig.remoteRank, serverListenPort);
     tmpSocket->ConnectAsync();
     connectedSocketMap[socketConfig] = std::move(tmpSocket);
     return connectedSocketMap[socketConfig].get();
@@ -305,13 +306,18 @@ bool SocketManager::DelWhiteList(PortData &localPort, vector<RaSocketWhitelist> 
 
 void SocketManager::SetDeviceServerListenPortMap(const std::unordered_map<u32, u32> &rankListenPortMap)
 {
-    rankListenPortMap_ = rankListenPortMap;
+    std::unordered_map<u32, u32> staticMap = SocketManager::GetDeviceServerListenPortMap();
+    staticMap.clear();
+    for (auto &iter : rankListenPortMap) {
+        staticMap[iter->first] = iter->second;
+    }
     return;
 }
 
-std::unordered_map<u32, u32> SocketManager::GetDeviceServerListenPortMap() const
+std::unordered_map<u32, u32> &SocketManager::GetDeviceServerListenPortMap()
 {
-    return rankListenPortMap_;
+    static std::unordered_map<u32, u32> rankListenPortMap;
+    return rankListenPortMap;
 }
 
 
