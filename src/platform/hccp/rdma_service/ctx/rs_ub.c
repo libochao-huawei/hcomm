e882b01a62e80893eafaa33b1ee00d248c1b9703
YeZZzzz1 <yezhenni1@huawei.com>
2026-02-11 17:07:05 +0800

    fix

--- src/platform/hccp/rdma_service/ctx/rs_ub.c
+++ src/platform/hccp/rdma_service/ctx/rs_ub.c
@@ -1511,10 +1511,10 @@ int RsUbCtxJfcCreate(struct RsUbDevCb *devCb, struct CtxCqAttr *attr, struct Ctx
         jfcCb->swdbAddr = dbVa;
         hccp_warn("@@@RsUbCtxJfcCreate after RsUrmaGetJfcOpt cqBuffVa: %ld, dbVa: %ld", cqBuffVa, dbVa);
 
-        ret = RsMmapJfcVa(jfcCb);
-        CHK_PRT_RETURN(ret != 0, hccp_err("rs_mmap_jfc_va failed, ret:%d", ret), ret);
-        hccp_warn("@@@RsUbCtxJfcCreate after RsMmapJfcVa jfcCb->bufAddr: %ld, jfcCb->swdbAddr: %ld",
-            jfcCb->bufAddr, jfcCb->swdbAddr);
+        // ret = RsMmapJfcVaCopy(jfcCb);
+        // CHK_PRT_RETURN(ret != 0, hccp_err("rs_mmap_jfc_va failed, ret:%d", ret), ret);
+        // hccp_warn("@@@RsUbCtxJfcCreate after RsMmapJfcVa jfcCb->bufAddr: %ld, jfcCb->swdbAddr: %ld",
+        //     jfcCb->bufAddr, jfcCb->swdbAddr);
 
     } else {
         hccp_err("jfc_type %d is invalid, not support!", attr->ub.mode);
