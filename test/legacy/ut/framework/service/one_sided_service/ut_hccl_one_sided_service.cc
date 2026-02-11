6b48c580bc1437237e0bd46ff2efdce4f922ec72
lhdlhdlhd1234569 <lihaodong19@h-partners.com>
2026-02-11 10:05:52 +0800

    update

--- test/legacy/ut/framework/service/one_sided_service/ut_hccl_one_sided_service.cc
+++ test/legacy/ut/framework/service/one_sided_service/ut_hccl_one_sided_service.cc
@@ -201,8 +201,8 @@ TEST_F(HcclOneSidedServiceTest, test_BatchGet_BatchPut)
 {
     MOCKER(HrtGetStreamId).stubs().will(returnValue(0));
     MOCKER(HrtStreamCreateWithFlags).stubs().will(returnValue(static_cast<void*>(0)));    
-    MOCKER(HrtStreamGetSqId).stubs().will(returnValue(u32)(0));
-    MOCKER(HrtStreamGetCqId).stubs().will(returnValue(u32)(0));
+    MOCKER(HrtStreamGetSqId).stubs().will(returnValue(static_cast<u32>(0)));
+    MOCKER(HrtStreamGetCqId).stubs().will(returnValue(static_cast<u32>(0)));
     MOCKER(HrtNotifyRecord).stubs().will(returnValue(static_cast<void*>(0)));
     MOCKER(HrtAicpuKernelLaunchExWithArgs).stubs().will(returnValue(static_cast<void*>(0)));
     MOCKER(HrtNotifyWaitWithTimeOut).stubs().will(returnValue(static_cast<void*>(0)));
