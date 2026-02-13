cb951cae8c446c3b15188889197bf6bbd8e8a0dd
ren-mengguang <renmengguang@h-partners.com>
2026-02-13 10:12:30 +0800

    add A5 aicpu communicator

--- src/framework/next/coll_comms/communicator/aicpu/coll_comm_aicpu_mgr.h
+++ src/framework/next/coll_comms/communicator/aicpu/coll_comm_aicpu_mgr.h
@@ -8,20 +8,19 @@
  * See LICENSE in the root of the software repository for the full text of the License.
  */
 
-#include "independent_op_aicpu_interface.h"
-#include "framework/aicpu_hccl_process.h"
-#include "aicpu_indop_process.h"
+#ifndef __COLL_COMM_AICPU_MGR_H__
+#define __COLL_COMM_AICPU_MGR_H__
 
-extern "C" {
-__attribute__((visibility("default"))) uint32_t RunAicpuIndOpCommInit(void *args)
-{
-    CHK_PRT_RET(args == nullptr, HCCL_ERROR("[%s]args is null.", __func__), HCCL_E_PARA);
+#include "coll_comm_aicpu.h"
 
-    CommAicpuParam *commAicpuParam = reinterpret_cast<CommAicpuParam *>(args);
-    DevType devType = static_cast<DevType>(commAicpuParam->deviceType);
-    if (devType == DevType::DEV_TYPE_910_95) {
-        return AicpuIndopProcess::AicpuIndOpCommInit(commAicpuParam);
-    }
-    return AicpuHcclProcess::AicpuIndOpCommInit(commAicpuParam);
-}
-}
+class CollCommAicpuMgr {
+public:
+    HcclResult AcquireCollCommAicpu();
+    HcclResult InitAicpuIndOp(CommAicpuParam *commAicpuParam);
+private:
+    void* oldA5Comm_{nullptr};
+    std::shared_ptr<CollCommAicpu> collCommAicpu_{nullptr};
+    bool isUsed_{false};
+};
+
+#endif // __COLL_COMM_AICPU_MGR_H__
