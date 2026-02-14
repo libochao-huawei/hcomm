bd0145d711e18842d0b08df9b9ab7d1edfc3e706
xz00xz <xiongzhe5@huawei.com>
2026-02-14 17:22:20 +0800

    fix CCUA hang issue

--- src/platform/hccp/rdma_agent/adapter/tlv/ra_adp_tlv.c
+++ src/platform/hccp/rdma_agent/adapter/tlv/ra_adp_tlv.c
@@ -20,7 +20,7 @@
 struct RsTlvOps {
     int (*tlvInit)(unsigned int phyId, unsigned int *bufferSize);
     int (*tlvDeinit)(unsigned int phyId);
-    int (*tlvRequest)(struct TlvRequestMsgHead *head, char *data);
+    int (*tlvRequest)(struct TlvRequestMsgHead *head, char *dataIn, char *dataOut, unsigned int *bufferSize);
 };
 
 struct RsTlvOps gRaRsTlvOps = {
@@ -68,11 +68,13 @@ int RaRsTlvDeinit(char *inBuf, char *outBuf, int *outLen, int *opResult, int rcv
 
 int RaRsTlvRequest(char *inBuf, char *outBuf, int *outLen, int *opResult, int rcvBufLen)
 {
+    union OpTlvRequestData *dataOut = (union OpTlvRequestData *)(outBuf + sizeof(struct MsgHead));
     union OpTlvRequestData *dataIn = (union OpTlvRequestData *)(inBuf + sizeof(struct MsgHead));
 
     HCCP_CHECK_PARAM_LEN_RET_HOST(sizeof(union OpTlvRequestData), sizeof(struct MsgHead), rcvBufLen, opResult);
 
-    *opResult = gRaRsTlvOps.tlvRequest(&dataIn->txData.head, dataIn->txData.data);
+    *opResult = gRaRsTlvOps.tlvRequest(&dataIn->txData.head, dataIn->txData.data, dataOut->rxData.recvData,
+            &dataOut->rxData.recvBytes);
 
     CHK_PRT_RETURN(*opResult == -EUSERS, hccp_warn("tlv request unsuccessful"), 0);
     if (*opResult != 0) {
