6e6e0c46e3c0dea0be5ecde31269c8251c63bcb2
linyixin4 <linyixin4@huawei.com>
2026-02-24 16:33:28 +0800

    dfx plus

--- src/legacy/framework/dfx/task_exception/task_exception_handler.cpp
+++ src/legacy/framework/dfx/task_exception/task_exception_handler.cpp
@@ -346,7 +346,7 @@ void TaskExceptionHandler::ProcessException(rtExceptionInfo_t* exceptionInfo, co
     HCCL_ERROR("[TaskExceptionHandler]Task run failed, groupRank information is %s.",
         GetGroupRankInfo(taskInfo).c_str());
     auto count = GetOpCounter(taskInfo);
-    HCCL_ERROR("[[TaskExceptionHandler]Task run failed, headOpCounter[%u] tailOpCounter[%u]]", static_cast<u32>(count.first), static_cast<u32>(count.second));
+    HCCL_ERROR("[TaskExceptionHandler]Task run failed, headOpCounter[%u] tailOpCounter[%u].", static_cast<u32>(count.first), static_cast<u32>(count.second));
     HCCL_ERROR("[TaskExceptionHandler]Task run failed, opData information is %s.", taskInfo.GetOpInfo().c_str());
 }
 
