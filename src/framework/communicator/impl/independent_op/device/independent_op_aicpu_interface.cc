456f3b54ed5c5fd6b418456b71301511d6361088
ren-mengguang <renmengguang@h-partners.com>
2026-02-24 11:44:52 +0800

    add A5 aicpu communicator

--- src/framework/communicator/impl/independent_op/device/independent_op_aicpu_interface.cc
+++ src/framework/communicator/impl/independent_op/device/independent_op_aicpu_interface.cc
@@ -10,6 +10,7 @@
 
 #include "independent_op_aicpu_interface.h"
 #include "framework/aicpu_hccl_process.h"
+#include "aicpu_indop_process.h"
 
 extern "C" {
 __attribute__((visibility("default"))) uint32_t RunAicpuIndOpCommInit(void *args)
@@ -17,6 +18,10 @@ __attribute__((visibility("default"))) uint32_t RunAicpuIndOpCommInit(void *args
     CHK_PRT_RET(args == nullptr, HCCL_ERROR("[%s]args is null.", __func__), HCCL_E_PARA);
 
     CommAicpuParam *commAicpuParam = reinterpret_cast<CommAicpuParam *>(args);
+    DevType devType = static_cast<DevType>(commAicpuParam->deviceType);
+    if (devType == DevType::DEV_TYPE_910_95) {
+        return AicpuIndopProcess::AicpuIndOpCommInit(commAicpuParam);
+    }
     return AicpuHcclProcess::AicpuIndOpCommInit(commAicpuParam);
 }
 }
