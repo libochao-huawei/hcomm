dd1ae872246a0039600873bcbd094c9c4717e427
ren-mengguang <renmengguang@h-partners.com>
2026-02-24 11:44:52 +0800

    add A5 aicpu communicator

--- src/legacy/framework/communicator/aicpu/kernel_entrance.cc
+++ src/legacy/framework/communicator/aicpu/kernel_entrance.cc
@@ -18,6 +18,9 @@
 #include "task_exception_handler_lite.h"
 #include "log.h"
 #include "inc/aicpu_utils.h"
+#ifdef CCL_KERNEL_AICPU
+#include "aicpu_indop_process.h"
+#endif
 extern "C" {
 using namespace Hccl;
 
@@ -42,6 +45,13 @@ uint32_t HcclKernelEntrance(void *args)
         HCCL_ERROR("HcclKernelEntrance communicatorImplLite is null.");
         return 1;
     }
+
+#ifdef CCL_KERNEL_AICPU
+    std::string group = kernelParam->comm.commId;
+    CollCommAicpuMgr *collCommAicpuMgr = AicpuIndopProcess::AicpuGetCommMgrbyGroup(group);
+    CHK_PRT_RET(!collCommAicpuMgr, HCCL_ERROR("%s collCommAicpuMgr is null, group[%s]", __func__, group.c_str()), 1);
+    collCommAicpuMgr->SetOldA5Comm(communicatorImplLite);
+#endif
 
     CHK_RET(AicpuUtils::GetInstance().WaitCommFree(communicatorImplLite, __func__));
     if (communicatorImplLite->LoadWithOpBasedMode(kernelParam) != 0) {
@@ -72,6 +82,14 @@ uint32_t HcclUpdateCommKernelEntrance(void *args)
         HCCL_ERROR("HcclUpdateCommKernelEntrance communicatorImplLite is null.");
         return 1;
     }
+
+#ifdef CCL_KERNEL_AICPU
+    std::string group = kernelParam->comm.commId;
+    CollCommAicpuMgr *collCommAicpuMgr = AicpuIndopProcess::AicpuGetCommMgrbyGroup(group);
+    CHK_PRT_RET(!collCommAicpuMgr, HCCL_ERROR("%s collCommAicpuMgr is null, group[%s]", __func__, group.c_str()), 1);
+    collCommAicpuMgr->SetOldA5Comm(communicatorImplLite);
+#endif
+
     CHK_RET(AicpuUtils::GetInstance().WaitCommFree(communicatorImplLite, __func__));
     communicatorImplLite->UpdateComm(kernelParam);
     unique_lock<std::mutex> aicpuLock(communicatorImplLite->GetAicpuMc2Mutex());
