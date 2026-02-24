8e43ae0a786d1715e6cbca09594b2a0d38356c96
lhdlhdlhd1234569 <lihaodong19@h-partners.com>
2026-02-24 16:30:14 +0800

    update

--- src/legacy/unified_platform/ccu/dfx/ccu_error_handler.cpp
+++ src/legacy/unified_platform/ccu/dfx/ccu_error_handler.cpp
@@ -418,7 +418,7 @@ void CcuErrorHandler::GenErrorInfoRemWaitGroup(const ErrorInfoBase &baseInfo, sh
 
     const auto rep                           = static_pointer_cast<CcuRepWaitGroup>(repBase);
     u32 cntCkeId;
-    HcclResult ret = ACL_SUCCESS;
+    HcclResult ret = HcclResult::ACL_SUCCESS;
     ret = (rep->transportGroup.GetCntCkeId(rep->semIndex, cntCkeId));
     if (ret != ACL_SUCCESS) {
         string msg = StringFormat("[GenErrorInfoRemWaitGroup]rt get CntCkeId failed. "
