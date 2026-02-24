70c867e3d2f5a81947ead512f07ebc2691a65219
lhdlhdlhd1234569 <lihaodong19@h-partners.com>
2026-02-24 09:13:45 +0800

    update

--- src/legacy/unified_platform/resource/ccu_transport/ccu_transport_group.cpp
+++ src/legacy/unified_platform/resource/ccu_transport/ccu_transport_group.cpp
@@ -82,7 +82,7 @@ CcuTransportGroup::CcuTransportGroup(const vector<CcuTransport*> &transports, u3
 
 TransportGrpStatus CcuTransportGroup::GetGrpStatus() const
 {
-    HCCL_INFO("[CcuTransportGroup] current group status [%s].", grpStatus);
+    HCCL_INFO("[CcuTransportGroup] current group status [%s].", grpStatus.c_str());
     return grpStatus;
 }
 
