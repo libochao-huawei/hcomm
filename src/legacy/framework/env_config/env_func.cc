d02bb4c87e8a21e2c8b1a912739078e2d1bd6c9c
acjr0011 <chenjiarong6@huawei.com>
2026-02-25 12:10:23 +0800

    fix error code to pta/tf

--- src/legacy/framework/env_config/env_func.cc
+++ src/legacy/framework/env_config/env_func.cc
@@ -250,6 +250,8 @@ constexpr u32 HCCL_RDMA_TC_BASE = 4;    // RDMATrafficClass需要是4的整数�
 void CheckRDMATrafficClass(const u32 &rdmaTrafficClass)
 {
     if (rdmaTrafficClass % HCCL_RDMA_TC_BASE != 0) {
+        RPT_ENV_ERR(true, "EI0001", std::vector<std::string>({"env", "tips"}),
+                            std::vector<std::string>({"HCCL_RDMA_TC", "Please check whether the value is multiple of four."}));
         HCCL_ERROR("rdmaTrafficClass[%u] is not a multiple of [%u]", rdmaTrafficClass, HCCL_RDMA_TC_BASE);
         THROW<InvalidParamsException>(
             StringFormat("rdmaTrafficClass[%u] is not a multiple of [%u]", rdmaTrafficClass, HCCL_RDMA_TC_BASE));
