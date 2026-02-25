719ffb00ef631d35154003f0dd5e4f2153a47e0c
lhdlhdlhd1234569 <lihaodong19@h-partners.com>
2026-02-25 17:41:04 +0800

    update

--- test/legacy/ut/framework/resource_manager/stream/ut_aicpu_stream_manager.cc
+++ test/legacy/ut/framework/resource_manager/stream/ut_aicpu_stream_manager.cc
@@ -38,9 +38,9 @@ protected:
     virtual void SetUp()
     {
         streamManager = new AicpuStreamManager();
-
-        MOCKER(HrtStreamCreateWithFlags).stubs().will(returnValue(static_cast<u32>(0)));
-        MOCKER(HrtStreamGetCqId).stubs().will(returnValue(pid));
+        
+        MOCKER(HrtStreamCreateWithFlags).stubs().will(returnValue(static_cast<void*>(0)));
+        MOCKER(HrtStreamGetCqId).stubs().will(returnValue(static_cast<u32>(0)));
         MOCKER(HrtGetStreamId).stubs().will(returnValue(fakeDevLogId));
         MOCKER(HrtStreamGetSqId).stubs().will(returnValue(fakeSqId));
         MOCKER(HrtStreamDestroy).stubs();
