d076d378e12f2dc6c83970d0a915c591029d974e
shirokawa_ <jinliuyi@huawei.com>
2026-02-25 11:09:50 +0800

    alltoall symmetric memory

--- src/framework/communicator/impl/hccl_communicator_host.cc
+++ src/framework/communicator/impl/hccl_communicator_host.cc
@@ -601,6 +601,7 @@ namespace hccl
         // 目前只支持allgather, allreduce, reducescatter
         CHK_PRT_RET(opType != HcclCMDType::HCCL_CMD_ALLGATHER && 
                     opType != HcclCMDType::HCCL_CMD_ALLREDUCE &&
+                    opType != HcclCMDType::HCCL_CMD_ALLTOALL &&
                     opType != HcclCMDType::HCCL_CMD_REDUCE_SCATTER,
                     HCCL_INFO("[%s] opType[%d] not support symmetric memory", 
                             __func__, opType),
@@ -4462,6 +4463,8 @@ namespace hccl
         ForceProf(opParam.isCapture);
         bool isInGraphCaptureZeroCopy = false;
         zeroCopyAclGraph_->SetRetryEnable(retryEnable_);
+        opParam.supportSymmetricMemory = IsSupportSymmetricMemory(opType, opParam);
+        opParam.supportZeroCopy = !opParam.supportSymmetricMemory && IsSupportZeroCopy(opParam);
         opParam.aclGraphZeroCopyEnable = GetConfigAclGraphZeroCopyEnable();
         isInGraphCaptureZeroCopy = zeroCopyAclGraph_->SetAclGraphZeroCopyMode(
             deviceType_, opType, opParam, implAlg_.get(), cclBufferManager_.GetOutCCLbufferSize());
@@ -4518,6 +4521,7 @@ namespace hccl
                 "aiv only not support, please ensure rankNum is greater than one", opTypeName.c_str());
             return HCCL_E_NOT_SUPPORT;
         }
+        CHK_RET(PrepareZeroCopy(algName, algDesc, opParam));
 
         newTag += !opParam.isCapture ? "" : "_Capture";
         auto isSupportAlg = [](const std::string &algName, bool aicpuUnfoldMode) -> bool {
@@ -4571,6 +4575,7 @@ namespace hccl
                 }
                 CHK_RET(RegisterToHeartBeat());
             }
+            CHK_RET(UpdateZeroCopy(opParam, resMap_[newTag]));
         }
         else
         {
