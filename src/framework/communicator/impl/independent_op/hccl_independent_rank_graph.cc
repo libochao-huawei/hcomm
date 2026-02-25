47f900a36dc59d5bb08e3794154fc8922a6daabc
wangkl <wangkailang1@huawei.com>
2026-02-25 14:24:16 +0800

    fix rankGraph

--- src/framework/communicator/impl/independent_op/hccl_independent_rank_graph.cc
+++ src/framework/communicator/impl/independent_op/hccl_independent_rank_graph.cc
@@ -42,7 +42,7 @@ HcclResult HcclRankGraphGetLinks(HcclComm comm, uint32_t netLayer, uint32_t srcR
     CHK_PTR_NULL(links);
     CHK_PTR_NULL(linkNum);
     hccl::hcclComm *hcclComm = static_cast<hccl::hcclComm *>(comm);
-    HCCL_RUN_INFO("Entry-%s: comm[%s], netLayer%u], srcRank[%u], dstRank[%u]", __func__,
+    HCCL_RUN_INFO("Entry-%s: comm[%s], netLayer[%u], srcRank[%u], dstRank[%u]", __func__,
         hcclComm->GetIdentifier().c_str(), netLayer, srcRank, dstRank);
     if (srcRank == dstRank) {
         HCCL_ERROR("[%s] srcRank[%u] and dstRank[%u] is same", __func__, srcRank, dstRank);
@@ -50,11 +50,11 @@ HcclResult HcclRankGraphGetLinks(HcclComm comm, uint32_t netLayer, uint32_t srcR
     }
     HcclResult ret = hcclComm->GetLinks(netLayer, srcRank, dstRank, links, linkNum);
     if (ret != HCCL_SUCCESS) {
-        HCCL_ERROR("[%s] Failed to get links for netLayer[%d], srcRank[%u], dstRank[%u]] ret[%d]",
+        HCCL_ERROR("[%s] Failed to get links for netLayer[%d], srcRank[%u], dstRank[%u] ret[%d]",
             __func__, netLayer, srcRank, dstRank, ret);
         return ret;
     }
-    HCCL_INFO("[%s] success: comm[%s] linkNum[%u]",  __func__, hcclComm->GetIdentifier().c_str(), *linkNum);
+    HCCL_RUN_INFO("[%s] success: comm[%s] linkNum[%u]",  __func__, hcclComm->GetIdentifier().c_str(), *linkNum);
     return HCCL_SUCCESS;
 }
 
