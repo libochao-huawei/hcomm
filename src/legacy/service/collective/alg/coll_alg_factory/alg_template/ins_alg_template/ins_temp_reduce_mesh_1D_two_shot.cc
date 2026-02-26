9ae77754480ee8a7f46a71eaf960a16a0755c011
liuhaoyu35 <liuhaoyu35@huawei.com>
2026-02-26 19:37:40 +0800

    clean code

--- src/legacy/service/collective/alg/coll_alg_factory/alg_template/ins_alg_template/ins_temp_reduce_mesh_1D_two_shot.cc
+++ src/legacy/service/collective/alg/coll_alg_factory/alg_template/ins_alg_template/ins_temp_reduce_mesh_1D_two_shot.cc
@@ -217,8 +217,7 @@ HcclResult InsTempReduceMesh1DTwoShot::RunGatherToRoot(const RankSliceInfo &slic
         u64 curSize = sliceInfoVec[rankIdx][0].size;
         
         if (curSize != 0) {
-            u64 srcOffset = static_cast<u64>(rankIdx) * curSize + scOff;
-            DataSlice ssrc(tempAlgParams.buffInfo.scratBuffType, srcOffset, curSize);
+            DataSlice ssrc(tempAlgParams.buffInfo.scratBuffType, static_cast<u64>(rankIdx) * curSize + scOff, curSize);
             DataSlice sdest(tempAlgParams.buffInfo.outBuffType, sliceInfoVec[rankIdx][0].offset + outOff, curSize);
 
             if (tempLinks.find(root_) == tempLinks.end()) {
@@ -229,14 +228,13 @@ HcclResult InsTempReduceMesh1DTwoShot::RunGatherToRoot(const RankSliceInfo &slic
             const auto &link = tempLinks.at(root_)[0];
             SlicesList sliceList({ssrc}, {sdest});
 
-            auto it = tempVirtRankMap_.find(root_);
-            if (it == tempVirtRankMap_.end()) {
+            auto rootIt = tempVirtRankMap_.find(root_);
+            if (rootIt == tempVirtRankMap_.end()) {
                 HCCL_ERROR("[InsTempReduceMesh1DTwoShot] root_ [%d] not found in tempVirtRankMap.", root_);
                 return HcclResult::HCCL_E_INTERNAL;
             }
-            u32 rootIdx = it->second;
 
-            CHK_RET(Send(DataInfo(link, sliceList), tempInsQues[rootIdx], 1, true, DmaMode::GET));
+            CHK_RET(Send(DataInfo(link, sliceList), tempInsQues[rootIt->second], 1, true, DmaMode::GET));
         }
     }
 
