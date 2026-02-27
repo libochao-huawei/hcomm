4c8fc7f7618c7561e726c657ac3560ae256f5d5d
lhdlhdlhd1234569 <lihaodong19@h-partners.com>
2026-02-27 10:08:34 +0800

    [Update] rts update
    Co-authored-by: lhdlhdlhd1234569<lihaodong19@h-partners.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !546 merge master into master
    
    [Update] rts update
    
    Created-by: lhdlhdlhd1234569
    Commit-by: lhdlhdlhd1234569
    Merged-by: cann-robot
    Description: ## 描述
    上下游协同的配置项边界值不匹配问题：
    1、AicpuAclKernelLaunch u16的超时时间传入s32/u64的宏定义，代码规范问题
    2、AicpuAclKernelLaunchV2 tilingDataSize定义u32，传入u64，代码规范问题
    3、aicpu里写sqe的形式下task，u64的notify超时时间赋值给u32
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #123-->
    <!-- 如果这个PR不涉及Issue，可填写"NA"。-->
    
    ## 测试
    修改完成后进行了蓝区线上compile、黄区的构建和冒烟以及本地llt编译均成功
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    
    
    See merge request: cann/hcomm!546

--- src/framework/communicator/impl/one_sided_service/hccl_one_sided_service.cc
+++ src/framework/communicator/impl/one_sided_service/hccl_one_sided_service.cc
@@ -1203,7 +1203,8 @@ HcclResult HcclOneSidedService::AicpuUnfoldKernelLaunchV2(const std::string &ker
     u64 tilingDataSize, const rtStream_t stream)
 {
     u64 commContext = 0ULL;
-    u16 timeOut = NOTIFY_DEFAULT_WAIT_TIME;
+    u16 timeOut = NOTIFY_DEFAULT_WAIT_TIME > std::numeric_limits<uint16_t>::max() ? 
+                    std::numeric_limits<uint16_t>::max() : NOTIFY_DEFAULT_WAIT_TIME;
     if (GetExternalInputHcclExecTimeoutSet() !=
         HcclExecTimeoutSet::HCCL_EXEC_TIMEOUT_NOT_SET ||
         CommConfiger::GetInstance().GetCommConfigExecTimeOutSet(identifier_)) {
@@ -1213,6 +1214,12 @@ HcclResult HcclOneSidedService::AicpuUnfoldKernelLaunchV2(const std::string &ker
             } else {
                 timeOut = execTimeOut;
             }
+    }
+
+    if (tilingDataSize > std::numeric_limits<uint32_t>::max()) {
+        HCCL_ERROR("[AicpuUnfoldKernelLaunchV2] tilingDataSize[%llu] exceeds the "
+                    "maximum allowed value for u32 [%u].", tilingDataSize, std::numeric_limits<uint32_t>::max());
+        return HCCL_E_RUNTIME;
     }
 
     CHK_RET(AicpuAclKernelLaunchV2(stream, reinterpret_cast<void *>(&commContext),
