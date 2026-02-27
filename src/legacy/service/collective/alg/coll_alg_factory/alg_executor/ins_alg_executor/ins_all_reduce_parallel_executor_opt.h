0bb13d294e5eeb9fd46ab7075aad2b104358c1e5
liguicheng8888 <liguicheng7@huawei.com>
2026-02-27 09:39:45 +0800

    allReduce跨框算法优化

---
+++ src/legacy/service/collective/alg/coll_alg_factory/alg_executor/ins_alg_executor/ins_all_reduce_parallel_executor_opt.h
@@ -0,0 +1,90 @@
+/**
+ * Copyright (c) 2025 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+#ifndef HCCLV2_INS_ALL_REDUCE_PARALLEL_EXECUTOR_OPT_H
+#define HCCLV2_INS_ALL_REDUCE_PARALLEL_EXECUTOR_OPT_H
+#include "ins_coll_alg_base.h"
+
+namespace Hccl {
+
+
+template <typename AlgTopoMatch, typename InsAlgTemplate0, typename InsAlgTemplate1, typename InsAlgTemplate2, typename InsAlgTemplate3>
+class InsAllReduceParallelExecutorV2 : public InsCollAlgBase {
+public:
+    explicit InsAllReduceParallelExecutorV2();
+    ~InsAllReduceParallelExecutorV2() override;
+
+    std::string Describe() const override
+    {
+        return "Instruction based All Reduce Parallel Executor.";
+    }
+
+    // HOST 接口
+    HcclResult Orchestrate(const RankGraph *rankGraph, const CollAlgOperator &op, const CollAlgParams &params,
+                          InsQuePtr insQue) override;
+    // AICPU 接口
+    HcclResult Orchestrate(const AlgTopoInfo &topoInfo, const CollAlgOperator &op, const CollAlgParams &params,
+                             ConnectedLinkMgr *linkMgr, InsQuePtr insQue) override;
+
+    HcclResult CalcResOffload(const RankGraph *rankGraph, const u64 &dataSize,
+                              CollOffloadOpResReq &resReq) override;
+
+    HcclResult CalcRes(const RankGraph *rankGraph, CollAlgResReq &algResReq) override;
+
+private:
+    HcclResult CalcLocalRankSize();
+    HcclResult GenInsQues(InsAlgTemplate0 &tempAlgIntraRS, InsAlgTemplate1 &tempAlgInterRS, InsAlgTemplate2 &tempAlgIntraAG, InsAlgTemplate3 &tempAlgInterAG);
+    void GenTemplateAlgParams0(const u64 dataOffset, const u64 dataCount, const u64 scratchOffset,TemplateDataParams &tempAlgParams) const;
+    void GenTemplateAlgParams1(const u64 dataOffset, const u64 dataCount, const u64 scratchOffset,TemplateDataParams &tempAlgParams) const;
+    void GenTemplateAlgParams2(const u64 dataOffset, const u64 dataCount, const u64 scratchOffset,TemplateDataParams &tempAlgParams) const;
+    HcclResult CalcSendDataSize(u64 &memBlockSize, float &SplitRate, u32 &multipleIntraRS, u32 &multipleInterRS, u32 &multipleIntraAG, u32 &multipleInterAG);
+    
+    void GetParallelDataSplitRate(std::vector<float> &splitDataSize) const;
+    HcclResult PrepareResForTemplate(const RankGraph *rankGraph, InsAlgTemplate0 &tempAlgIntraRS, InsAlgTemplate1 &tempAlgInterRS, InsAlgTemplate2 &tempAlgIntraAG, InsAlgTemplate3 &tempAlgInterAG);
+    HcclResult PrepareResForTemplate(ConnectedLinkMgr *linkMgr, InsAlgTemplate0 &tempAlgIntraRS, InsAlgTemplate1 &tempAlgInterRS, InsAlgTemplate2 &tempAlgIntraAG, InsAlgTemplate3 &tempAlgInterAG);
+    
+    uint64_t rankSizeLevel0_{0};
+    uint64_t rankSizeLevel1_{0};
+
+    std::vector<std::vector<RankId>>              virtRanks_;
+    std::vector<std::map<RankId, u32>>            virtRankMap_; // map<virtRank, virtRankOrder>
+    std::vector<std::vector<std::vector<RankId>>> vTopo_;
+
+    std::vector<InsQuePtr> requiredQue_;
+    std::vector<InsQuePtr> intraQue_;
+    std::vector<InsQuePtr> interQue_;
+    std::vector<InsQuePtr> syncQueues_;
+
+    ResLinks               intraRSLinks_;
+    ResLinks               interRSLinks_;
+    ResLinks               intraAGLinks_;
+    ResLinks               interAGLinks_;
+
+    u64 interScratchOffset0{0};
+    u64 interScratchOffset1{0};
+    u64 interScratchOffset2{0};
+    u64 interScratchOffset3{0};
+
+    u64 Intra0ScratchSize{0};
+    u64 Intra1ScratchSize{0};
+    u64 Intra2ScratchSize{0};
+    u64 Intra3ScratchSize{0};
+
+    u64 Inter0ScratchSize{0};
+    u64 Inter1ScratchSize{0};
+    u64 Inter2ScratchSize{0};
+    u64 Inter3ScratchSize{0};
+
+    float dataSplitRate{0.5};
+};
+
+} // namespace Hccl
+
+#endif // HCCLV2_INS_ALL_REDUCE_PARALLEL_EXECUTOR_H
