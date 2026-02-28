2dd5a59ae9a1d3413e544623fcb3c11760ca1007
weixin_58244yso <suimingcong@huawei.com>
2026-02-28 11:11:27 +0800

    add_globalmitask

--- src/framework/next/coll_comms/dfx/hcclCommDfx.cc
+++ src/framework/next/coll_comms/dfx/hcclCommDfx.cc
@@ -13,14 +13,14 @@ namespace hccl {
 
 HcclCommDfx::HcclCommDfx(uint32_t deviceId) {
     deviceId_ = deviceId;
-    mirrorTaskManager_ = new Hccl::MirrorTaskManager(deviceId_, &GlobalMirrorTasks::Instance(), false);
+    mirrorTaskManager_ = new Hccl::MirrorTaskManager(deviceId_, &Hccl::GlobalMirrorTasks::Instance(), false);
 }
 
 void HcclCommDfx::Init() {
     // 1. 如果mirrorTaskManager_为空，则创建新的MirrorTaskManager
     if (mirrorTaskManager_ == nullptr) {
         // 注意：实际实现中应该避免这种情况，CommunicatorImpl应该传入已经存在的MirrorTaskManager
-        mirrorTaskManager_ = new Hccl::MirrorTaskManager(deviceId_, &GlobalMirrorTasks::Instance(), false);
+        mirrorTaskManager_ = new Hccl::MirrorTaskManager(deviceId_, &Hccl::GlobalMirrorTasks::Instance(), false);
     }
     
     // 2. 创建Profiling管理类
