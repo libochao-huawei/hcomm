195576d33ea1614f447efbbcc2c18fa387ad20df
c15029001705 <caiyifan2@huawei.com>
2026-03-03 14:37:35 +0800

    修改格式

--- src/framework/next/coll_comms/dfx/profiling/host/hcclCommProfiling.cc
+++ src/framework/next/coll_comms/dfx/profiling/host/hcclCommProfiling.cc
@@ -11,7 +11,7 @@
 #include "profiling_reporter.h"
 namespace hccl {
     
-explicit HcclCommProfiling(DevId deviceId){
+explicit HcclCommProfiling(DevId deviceId) {
     mirrorTaskManager_ = std::make_unique<MirrorTaskManager>(deviceId, &GlobalMirrorTasks::Instance(), false);
     profilingReporter_ = std::make_unique<ProfilingReporter>(mirrorTaskManager_, &ProfilingHandler::GetInstance());
 }
