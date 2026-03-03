6ddf7bf2ed2fcd9e307cac8da174e741aa5ec34f
zhongqin2020 <zhongqin3@huawei.com>
2026-03-03 20:06:09 +0800

    Split huge source and header files to fix cleancode

--- src/platform/hccp/rdma_service/rs_rdma.c
+++ src/platform/hccp/rdma_service/rs_rdma.c
@@ -23,10 +23,12 @@
 #include "securec.h"
 #include "rs.h"
 #include "ra_rs_err.h"
+#include "rs_common_inner.h"
 #include "rs_inner.h"
 #include "rs_rdma_inner.h"
 #include "rs_epoll.h"
 #include "dl_ibverbs_function.h"
+#include "rs_drv_socket.h"
 #include "rs_drv_rdma.h"
 #include "rs_rdma.h"
 
