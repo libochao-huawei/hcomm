937454e54bf0f232f191b051b022cd3280ab52f1
guoziqi2025 <guoziqi2@huawei.com>
2026-03-04 10:10:44 +0800

    ccu find loop eid by rtpEnable

--- test/legacy/ut/framework/interface/hcom/ut_hcom_v2.cc
+++ test/legacy/ut/framework/interface/hcom/ut_hcom_v2.cc
@@ -319,11 +319,10 @@ TEST_F(HcomTest, HcomGetWorkspaceMemSizeV2_func_err)
     u64 memSize = 0;
     HcclResult ret = HcomGetWorkspaceMemSizeV2(opType, count, dataType, group, memSize);
     EXPECT_EQ(ret, HCCL_E_NOT_FOUND);
- 
-    char group1[64];
+
     opType = "null";
-    ret = HcomGetWorkspaceMemSizeV2(opType, count, dataType, group1, memSize);
-    EXPECT_EQ(ret, HCCL_E_PARA);
+    ret = HcomGetWorkspaceMemSizeV2(opType, count, dataType, group, memSize);
+    EXPECT_EQ(ret, HCCL_E_NOT_FOUND);
 }
  
 TEST_F(HcomTest, HcomSetWorkspaceResource_V2_func)
