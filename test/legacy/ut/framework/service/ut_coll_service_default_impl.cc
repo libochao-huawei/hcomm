7f1aeddcea8c56637b14852e0cc6712cd8ebd6aa
linyixin4 <linyixin4@huawei.com>
2026-03-05 17:25:56 +0800

    dfx plus

--- test/legacy/ut/framework/service/ut_coll_service_default_impl.cc
+++ test/legacy/ut/framework/service/ut_coll_service_default_impl.cc
@@ -694,6 +694,7 @@ TEST_F(CollServiceDefaultImplTest, test_load_with_offload_mode)
 
     CollServiceDefaultImpl service(&comm);
     CollOperator op;
+    service.AddOpCounterMems();
     op.inputMem = DevBuffer::Create(0x100, 1);
     op.outputMem = DevBuffer::Create(0x100, 1);
     op.scratchMem = DevBuffer::Create(0x100, 1);
@@ -881,6 +882,7 @@ TEST_F(CollServiceDefaultImplTest, test_load_with_offload_mode_with_task)
 
     CollServiceDefaultImpl service(&comm);
     CollOperator op;
+    service.AddOpCounterMems();
     op.inputMem = DevBuffer::Create(0x100, 1);
     op.outputMem = DevBuffer::Create(0x100, 1);
     op.scratchMem = DevBuffer::Create(0x100, 1);
