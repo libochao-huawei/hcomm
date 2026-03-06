65ad4a2ac0dcafb806c6ef78d5c826ed0fb329d6
weixin_58244yso <suimingcong@huawei.com>
2026-03-06 15:56:29 +0800

    Op_profiling

--- src/framework/next/coll_comms/dfx/hcclCommOp.h
+++ src/framework/next/coll_comms/dfx/hcclCommOp.h
@@ -16,37 +16,42 @@
 #include <unordered_map>
 #include "buffer.h"
 #include "common.h"
+#include "op_mode.h"
+#include "task_info.h"
 
 namespace hccl {
 using RankId = u32;
 
 class HcclDfxOpInfo {
 public:
-    std::string  opTag;
-    RankId       myRank; //hcomm可填
-    Hccl::OpMode       opMode;
-    HcclCMDType  opType = HcclCMDType::HCCL_CMD_INVALID;
-    HcclReduceOp reduceOp = HcclReduceOp::HCCL_REDUCE_RESERVED;
-    HcclDataType dataType;
-    HcclDataType outputType;
-    u64          dataCount{0};
-    u32          root = INVALID_VALUE_RANKID;
-    u32          numBlocksLimit{0};
-    bool         staticAddr{false};
-    bool         staticShape{false};
+    //DfxOpInfo_base
+    std::string         tag_;
+    Hccl::AlgType       algType_;
+    u32                 index_{0};
+    u64                 beginTime_{0};
+    u64                 endTime_{0};
+    //CollOperator
+    std::string         opTag;
+    bool                staticAddr{false};
+    bool                staticShape{false};
+    RankId              myRank;
+    //baseCollOperator
+    Hccl::OpMode        opMode;
+    HcclCMDType         opType = HcclCMDType::HCCL_CMD_INVALID;
+    HcclReduceOp        reduceOp = HcclReduceOp::HCCL_REDUCE_RESERVED;
+    HcclDataType        dataType;
+    HcclDataType        outputType;
+    u64                 dataCount{0};
+    u32                 root = INVALID_VALUE_RANKID;
+    u32                 numBlocksLimit{0};
     std::shared_ptr<Hccl::Buffer> inputMem{nullptr};
     std::shared_ptr<Hccl::Buffer> outputMem{nullptr};
     std::shared_ptr<Hccl::Buffer> scratchMem{nullptr};
-    std::string  tag_;
-    Hccl::AlgType      algType;
-    u32          index_{0};
-    u64          beginTime_;
-    u64          endTime_;
-    u32          mainStreamId_; 
-    u32          notifyId; //host wait device notifyId
+    //task_exception
+    u32          notifyId{0}; //host wait device notifyId
     union {
         struct {
-            u64 dataCount;
+            u64 dataCount{0};
             HcclDataType dataType;
             HcclDataType dataOutputType;
         } dataDes;
@@ -58,8 +63,8 @@ public:
         struct {
             HcclDataType sendType;
             HcclDataType recvType;
-            u64 sendCount;
-            u64 recvCount;
+            u64 sendCount{0};
+            u64 recvCount{0};
         } all2AllDataDes;
         struct {
             HcclDataType sendType;
@@ -76,7 +81,7 @@ public:
         } all2AllVCDataDes;
         struct {
             HcclSendRecvItem* sendRecvItemsPtr;
-            u32 itemNum;
+            u32 itemNum{0};
         } batchSendRecvDataDes;
     };
 
@@ -85,15 +90,14 @@ public:
     {
         return Hccl::StringFormat(
             "DfxOpInfo: [tag:[%s], algType:[%u], index:[%u], beginTime:[%llu], endTime:[%llu]",
-             tag_.c_str(), algType, index_, beginTime_, endTime_);
+             tag_.c_str(), algType_, index_, beginTime_, endTime_);
     }
 };
 
 void SetCollopDataDes(Hccl::CollOperator& collOp, const HcclDfxOpInfo& dfxOpInfo);
 std::shared_ptr<Hccl::Buffer> CreateBufferShared(const Hccl::Buffer* buffer);
 std::shared_ptr<Hccl::DfxOpInfo> ConvertToDfxOpInfo(const HcclDfxOpInfo& dfxOpInfo);
-int32_t HcommThreadRegisterDfx(ThreadHandle thread, std::function<HcclResult(u32, u32, const Hccl::TaskParam&, u64)> callback);
-
+HcclResult HcommThreadRegisterDfx(ThreadHandle thread, std::function<HcclResult(u32, u32, const Hccl::TaskParam&, u64)> callback);
 
 }
 #endif
