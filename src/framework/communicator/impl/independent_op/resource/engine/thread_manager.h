1052861f837e88e231a18c16e9af834d255cdaeb
linzy_hccl <linzhiyang5@huawei.com>
2026-03-06 10:00:54 +0800

    channel复用

--- src/framework/communicator/impl/independent_op/resource/engine/thread_manager.h
+++ src/framework/communicator/impl/independent_op/resource/engine/thread_manager.h
@@ -28,6 +28,8 @@ public:
     ~ThreadMgr() = default;
     HcclResult HcclThreadAcquire(CommEngine engine, uint32_t threadNum,
         uint32_t notifyNumPerThread, ThreadHandle *threads, std::vector<uint32_t> &threadId);
+    HcclResult HcclThreadAcquireV2(CommEngine engine, uint32_t threadNum,
+        uint32_t notifyNumPerThread, ThreadHandle *threads, std::vector<uint32_t> &threadId);
     HcclResult HcclThreadAcquireWithStream(CommEngine engine,
         rtStream_t stream, uint32_t notifyNum, ThreadHandle *thread);
     HcclResult HcclGetNotifyNumInThread(ThreadHandle thread, uint32_t *notifyNum);
@@ -36,7 +38,11 @@ public:
     u32 GetNotifyNumPerThread() const { return notifyNumPerThread_; }
 
 private:
-
+    uint64_t GetMaxNotifyTotal();
+    HcclResult CheckNotifyNum(CommEngine engine, uint32_t threadNum, uint32_t notifyNumPerThread);
+    HcclResult CheckThreadNum(CommEngine engine, uint32_t threadNum, uint32_t notifyNumPerThread);
+    HcclResult SupplementNotify(CommEngine engine, uint32_t notifyNumPerThread);
+    HcclResult SupplementThread(CommEngine engine, uint32_t supplementThreadNum, uint32_t notifyNumPerThread);
     HcclResult ThreadExportToCommEngineCpu(uint32_t threadNum, const ThreadHandle *threads, ThreadHandle *exportedThreads);
     HcclResult ThreadExportToCommEngineAicpu(uint32_t threadNum, const ThreadHandle *threads, CommEngine dstCommEngine, ThreadHandle *exportedThreads);
     HcclResult GetExportedThread(const ThreadHandle threadHandle, CommEngine commEngine, Thread *&exportedThread, std::shared_ptr<Thread> &threadOut);
@@ -51,6 +57,9 @@ private:
 
     std::mutex mainThreadMutex_;
     std::map<rtStream_t, std::shared_ptr<Thread>> mainThread_;
+
+    std::mutex engineToThreadMutex_;
+    std::map<CommEngine, std::vector<std::shared_ptr<Thread>>> engineToThreadsMap_;
 
     std::mutex threadMapMutex_;
     std::unordered_map<ThreadHandle, ThreadHandle> threadHandleOthersToCpu_; // 其他引擎上的ThreadHandle与CPU_TS上的ThreadHandle的映射
