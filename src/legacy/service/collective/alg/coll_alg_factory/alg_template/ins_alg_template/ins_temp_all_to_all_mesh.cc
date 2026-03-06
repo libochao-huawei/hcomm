b6328d5d86db01a5f61ff1bd34c6deb405e384f7
gcw_dFpssWuk <wanghaixu5@h-partners.com>
2026-03-06 10:08:53 +0800

    2026.3 code check

--- src/legacy/service/collective/alg/coll_alg_factory/alg_template/ins_alg_template/ins_temp_all_to_all_mesh.cc
+++ src/legacy/service/collective/alg/coll_alg_factory/alg_template/ins_alg_template/ins_temp_all_to_all_mesh.cc
@@ -348,6 +348,10 @@ HcclResult InsTempAlltoAllMesh::Run(const TempFuncs &tempFuncs, const RankSliceI
 {
     (void) tempFuncs;
     (void) sliceInfoVec;
+    if (tempInsQues.size() == 0) {
+        HCCL_ERROR("[CcuTempAlltoAllMesh1D] tempInsQues.size() is zero.");
+        return HcclResult::HCCL_E_PARA;
+    }
     dmaMode_ = DmaMode::PUT;
     buffInfo_ = buffInfo;
     u32 totalStep = CalcStepNum();
@@ -355,11 +359,6 @@ HcclResult InsTempAlltoAllMesh::Run(const TempFuncs &tempFuncs, const RankSliceI
     std::unordered_map<u32, UsrData> sendSliceInfoMap;
     std::unordered_map<u32, UsrData> recvSliceInfoMap;
     CHK_RET(CalcSendRecvAllSliceInfo(sendSliceInfoMap, recvSliceInfoMap));
-    if (tempInsQues.size() == 0) {
-        HCCL_ERROR("[CcuTempAlltoAllMesh1D] tempInsQues.size() is zero.");
-        return HcclResult::HCCL_E_PARA;
-    }
-
     std::vector<InsQuePtr> localCopyQues;
     if (tempInsQues.size() > 1) {
         localCopyQues.push_back(tempInsQues[0]);
