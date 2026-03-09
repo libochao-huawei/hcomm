5b27e7eb674c7eddb155c10a9d81cc7c5da6f14e
cywwwwww <chenyuwei14@huawei.com>
2026-03-09 18:35:21 +0800

    blockgroup

--- src/algorithm/base/alg_aiv_template/aiv_crossnode_91093_base.h
+++ src/algorithm/base/alg_aiv_template/aiv_crossnode_91093_base.h
@@ -397,7 +397,7 @@ __aicore__ inline void AivCrossNode91093Base::InitDeter(GM_ADDR buffOut0, GM_ADD
     usedBlockNum_ = numBlocks;
     numBlocks_ = numBlocks;
     pingpongOffset = 0;
-    blockGroup_ = rankSize_;
+    blockGroup_ = numBlocks_;
     commAddr_ = buffOut1;
 
     InitSetCheckClearArgsTensor();
