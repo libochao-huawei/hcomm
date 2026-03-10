2a4771b962048824beb5c5af357b2b9ee1808d0a
maxinyuan1 <maxinyuan2@h-partners.com>
2026-03-10 20:28:42 +0800

    alter License

--- test/ut/depends/include/base/context_builder/context_holder.h
+++ test/ut/depends/include/base/context_builder/context_holder.h
@@ -1,11 +1,12 @@
-/* Copyright (c) 2025 Huawei Technologies Co., Ltd.
- * This file is a part of the CANN Open Software.
- * Licensed under CANN Open Software License Agreement Version 1.0 (the "License").
- * Please refer to the License for details. You may not use this file except in compliance with the License.
+/**
+ * Copyright (c) 2025 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for the details. You may not use this file except in compliance with the License.
  * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
  * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
- * See LICENSE in the root of the software repository for the full text of the License.
- * ===================================================================================================================*/
+ * See the License in the root of the software repository for the full text of the License.
+ */
 
 #ifndef METADEF_INC_EXTERNAL_BASE_CONTEXT_BUILDER_CONTEXT_HOLDER_H_
 #define METADEF_INC_EXTERNAL_BASE_CONTEXT_BUILDER_CONTEXT_HOLDER_H_
