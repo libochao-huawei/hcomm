b47480b3f2c5bde840e6767433d696b2c50cd191
xz00xz <xiongzhe5@huawei.com>
2026-03-10 11:59:56 +0800

    HCCP support nda api

---
+++ src/platform/hccp/rdma_service/dl_nda_function.h
@@ -0,0 +1,40 @@
+/**
+ * Copyright (c) 2026 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+#ifndef DL_NDA_FUNCTION_H
+#define DL_NDA_FUNCTION_H
+
+#include <infiniband/verbs.h>
+#include "ibv_extend.h"
+#include "hccp_dl.h"
+
+#if defined(HNS_ROCE_LLT) || defined(DEFINE_HNS_LLT)
+#define STATIC
+#else
+#define STATIC static
+#endif
+
+struct RsNdaOps {
+    struct ibv_context_extend *(*rsNdaIbvOpenExtend)(struct ibv_context *context);
+    int (*rsNdaIbvCloseExtend)(struct ibv_context_extend *context);
+    struct ibv_qp_extend *(*rsNdaCreateQpExtend)(struct ibv_context_extend *context,
+        struct ibv_qp_init_attr_extend *qp_init_attr);
+    struct ibv_cq_extend *(*rsNdaCreateCqExtend)(struct ibv_context_extend *context,
+        struct ibv_cq_init_attr_extend *cq_init_attr);
+    int (*rsNdaIbvDestroyQpExtend)(struct ibv_context_extend *context, struct ibv_qp_extend *qp_extend);
+    int (*rsNdaIbvDestroyCqExtend)(struct ibv_context_extend *context, struct ibv_cq_extend *cq_extend);
+};
+
+int RsNdaApiInit(void);
+void RsNdaApiDeinit(void);
+struct ibv_context_extend *RsNdaIbvOpenExtend(struct ibv_context *context);
+int RsNdaIbvCloseExtend(struct ibv_context_extend *context);
+
+#endif // DL_NDA_FUNCTION_H
