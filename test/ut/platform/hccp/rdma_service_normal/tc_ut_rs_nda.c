9ad150bb25e09db08420c8eef3afa407a77f3f73
xz00xz <xiongzhe5@huawei.com>
2026-03-12 21:39:19 +0800

    HCCP support RaNdaQpCreate

---
+++ test/ut/platform/hccp/rdma_service_normal/tc_ut_rs_nda.c
@@ -0,0 +1,46 @@
+/**
+ * Copyright (c) 2026 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include "ut_dispatch.h"
+#include "rs_common_inner.h"
+#include "rs_inner.h"
+#include "rs_rdma.h"
+#include "rs_nda.h"
+#include "tc_ut_rs_nda.h"
+
+extern int RsQueryRdevCb(unsigned int phyId, unsigned int rdevIndex, struct RsRdevCb **rdevCb);
+
+int RsQueryRdevCbNdaStub(unsigned int phyId, unsigned int rdevIndex, struct RsRdevCb **rdevCb)
+{
+    static struct RsRdevCb ndaRdevCb = {0};
+
+    *rdevCb = &ndaRdevCb;
+    return 0;
+}
+
+void TcRsNdaGetDirectFlag()
+{
+    unsigned int rdevIndex = 0;
+    unsigned int phyId = 0;
+    int directFlag = 0;
+    int ret = 0;
+
+    ret = RsNdaGetDirectFlag(RS_MAX_DEV_NUM, rdevIndex, &directFlag);
+    EXPECT_INT_EQ(ret, -EINVAL);
+
+    mocker_clean();
+    mocker_invoke(RsQueryRdevCb, RsQueryRdevCbNdaStub, 1);
+    ret = RsNdaGetDirectFlag(phyId, rdevIndex, &directFlag);
+    EXPECT_INT_EQ(ret, 0);
+    mocker_clean();
+}
