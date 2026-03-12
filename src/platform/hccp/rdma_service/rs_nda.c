04bde244f7a91280c04d54f169e2910864ac741a
xz00xz <xiongzhe5@huawei.com>
2026-03-12 14:20:54 +0800

    HCCP support RaNdaGetDirectFlag

---
+++ src/platform/hccp/rdma_service/rs_nda.c
@@ -0,0 +1,27 @@
+/**
+ * Copyright (c) 2026 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+#include "hccp_nda.h"
+#include "rs_inner.h"
+#include "rs.h"
+#include "rs_nda.h"
+
+RS_ATTRI_VISI_DEF int RsNdaGetDirectFlag(unsigned int phyId, unsigned int rdevIndex, int *directFlag)
+{
+    struct RsRdevCb *rdevCb = NULL;
+    int ret = 0;
+
+    ret = RsQueryRdevCb(phyId, rdevIndex, &rdevCb);
+    CHK_PRT_RETURN(ret != 0, hccp_err("RsQueryRdevCb phyId:%u rdev_index:%u ret:%d", phyId, rdevIndex, ret), ret);
+
+    rdevCb->directFlag = (rdevCb->deviceAttr.vendor_id == RS_VENDOR_ID_19E5) ? DIRECT_FLAG_UB : DIRECT_FLAG_PCIE;
+    *directFlag = rdevCb->directFlag;
+    return ret;
+}
