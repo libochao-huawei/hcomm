92c3030ae0910c1977cdcfa4b5efc35575262ae9
xz00xz <xiongzhe5@huawei.com>
2026-03-12 21:39:19 +0800

    HCCP support RaNdaQpCreate

--- src/platform/hccp/common/dl_hal_function.h
+++ src/platform/hccp/common/dl_hal_function.h
@@ -107,6 +107,8 @@ struct DlHalOps {
     drvError_t (*dlHalMemRegUbSegment)(uint32_t devId, uint64_t va, uint64_t size);
     drvError_t (*dlHalMemUnRegUbSegment)(uint32_t devid, uint64_t va);
     DVresult (*dlDrvMemGetAttribute)(DVdeviceptr vptr, struct DVattribute *attr);
+    drvError_t (*dlHalHostRegister)(void *srcPtr, UINT64 size, UINT32 flag, UINT32 devId, void **dstPtr);
+    drvError_t (*dlHalHostUnregister)(void *src_ptr, UINT32 devid);
 };
 
 int DlHalInit(void);
@@ -191,5 +193,7 @@ int DlHalResAddrUnmapV2(unsigned int devId, struct res_map_info_in *resInfoIn);
 int DlHalMemRegUbSegment(uint32_t devId, uint64_t va, uint64_t size);
 int DlHalMemUnRegUbSegment(uint32_t devId, uint64_t va);
 int DlDrvMemGetAttribute(DVdeviceptr vptr, struct DVattribute *attr);
+int DlHalHostRegister(void *srcPtr, UINT64 size, UINT32 flag, UINT32 devId, void **dstPtr);
+int DlHalHostUnRegister(void *srcPtr, UINT32 devId);
 
 #endif  // __DL_HAL_FUNCTION_H__
