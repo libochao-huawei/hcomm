938dbd264f85f3b5018950a8a01694e3c7490176
yan_yu_a123 <wangjingcong@h-partners.com>
2026-03-12 20:44:00 +0800

    common function

--- src/legacy/framework/service/coll_service_base.cc
+++ src/legacy/framework/service/coll_service_base.cc
@@ -364,7 +364,7 @@ void CollServiceBase::SaveMirrorDfxOpInfo()
 
     dfxOpInfo->op_ = *comm->GetCurrentCollOperator();
     dfxOpInfo->tag_ = OpTypeToString(dfxOpInfo->op_.opType);
-    dfxOpInfo->StrToAlg(comm->GetCurAlgName());
+    dfxOpInfo->algType_ = AlgType::MESH;
     dfxOpInfo->index_ = comm->GetIdIndex();
     dfxOpInfo->comm_ = comm;
     dfxOpInfo->beginTime_ = DlProfFunction::GetInstance().dlMsprofSysCycleTime();
