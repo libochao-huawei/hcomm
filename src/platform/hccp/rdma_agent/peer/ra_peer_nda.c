e523867200ffab0361e0b7c565667dee4b0d01bf
xz00xz <xiongzhe5@huawei.com>
2026-03-12 21:39:19 +0800

    HCCP support RaNdaQpCreate

--- src/platform/hccp/rdma_agent/peer/ra_peer_nda.c
+++ src/platform/hccp/rdma_agent/peer/ra_peer_nda.c
@@ -12,7 +12,7 @@
 #include <errno.h>
 #include "securec.h"
 #include "ra_comm.h"
-#include "rs.h"
+#include "rs_nda.h"
 #include "ra_peer.h"
 #include "ra_peer_nda.h"
 
@@ -28,5 +28,63 @@ int RaPeerNdaGetDirectFlag(struct RaRdmaHandle *rdmaHandle, int *directFlag)
     if (ret != 0) {
         hccp_err("[get][directFlag]RsNdaGetDirectFlag failed ret:%d", ret);
     }
+    return ret;
+}
+
+int RaPeerNdaQpCreate(struct RaRdmaHandle *rdmaHandle, struct NdaQpInitAttr *attr, struct NdaQpInfo *info,
+    void **qpHandle)
+{
+    unsigned int phyId = rdmaHandle->rdevInfo.phyId;
+    struct RaQpHandle *qpPeer = NULL;
+    struct RsQpResp qpResp = {0};
+    int ret = 0;
+
+    qpPeer = (struct RaQpHandle *)calloc(1, sizeof(struct RaQpHandle));
+    CHK_PRT_RETURN(qpPeer == NULL, hccp_err("[create][RaNdaQp]qpPeer calloc failed"), -ENOMEM);
+
+    RaPeerMutexLock(phyId);
+    RsSetCtx(phyId);
+    ret = RsNdaGetDirectFlag(phyId, rdmaHandle->rdevIndex, &qpPeer->directFlag);
+    if (ret != 0) {
+        hccp_err("[create][RaNdaQp]RsNdaGetDirectFlag failed ret:%d", ret);
+        RaPeerMutexUnlock(phyId);
+        goto free_qp_handle;
+    }
+    ret = RsNdaQpCreate(phyId, rdmaHandle->rdevIndex, attr, info, &qpResp);
+    RaPeerMutexUnlock(phyId);
+    if (ret != 0) {
+        hccp_err("[create][RaNdaQp]RsNdaQpCreate failed ret:%d", ret);
+        goto free_qp_handle;
+    }
+    qpPeer->phyId = phyId;
+    qpPeer->qpn = qpResp.qpn;
+    qpPeer->psn = qpResp.psn;
+    qpPeer->gidIdx = qpResp.gidIdx;
+    qpPeer->rdevIndex = rdmaHandle->rdevIndex;
+    qpPeer->rdmaHandle = rdmaHandle;
+    qpPeer->rdmaOps = rdmaHandle->rdmaOps;
+
+    *qpHandle = qpPeer;
+    return ret;
+
+free_qp_handle:
+    free(qpPeer);
+    qpPeer = NULL;
+    return ret;
+}
+
+int RaPeerNdaQpDestroy(struct RaQpHandle *qpPeer)
+{
+    int ret = 0;
+
+    RaPeerMutexLock(qpPeer->phyId);
+    RsSetCtx(qpPeer->phyId);
+    ret = RsNdaQpDestroy(qpPeer->phyId, qpPeer->rdevIndex, qpPeer->qpn);
+    if (ret != 0) {
+        hccp_err("[destroy][RaNdaQp]RsNdaQpDestroy failed ret:%d", ret);
+    }
+    RaPeerMutexUnlock(qpPeer->phyId);
+    free(qpPeer);
+    qpPeer = NULL;
     return ret;
 }
