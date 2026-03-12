70bcc3aade26b2642c0c6aa5c1db0a1f684d3022
yuechenyao_huawei <yuechenyao@h-partners.com>
2026-03-12 11:56:42 +0800

    adapt batch delete jettys

--- src/legacy/unified_platform/resource/ccu_transport/ccu_connection.cpp
+++ src/legacy/unified_platform/resource/ccu_transport/ccu_connection.cpp
@@ -481,22 +481,33 @@ int32_t CcuConnection::GetDevLogicId() const
     return devLogicId;
 }
 
-std::vector<ConnJettyInfo> CcuConnection::GetJettyInfo()
+std::vector<ConnJettyInfo> CcuConnection::GetDeleteJettyInfo()
 {
-    std::vector<ConnJettyInfo> connJettyInfos;
+    std::vector<ConnJettyInfo> connDeleteJettyInfos;
     ConnJettyInfo jettyInfo;
-    for (size_t i = 0; i < jettyNum; i++) {
-        if (ccuJettys_[i] != nullptr) {
-            ccuJettys_[i]->GetJettyInfo(jettyInfo);
+    for (auto &ccuJetty : ccuJettys_) {
+        if (ccuJetty != nullptr) {
+            ccuJetty->GetJettyInfo(jettyInfo);
             jettyInfo.rdmaHandle = rdmaHandle;
-            if (importJettyCtxs[i].outParam.handle != 0) {
-                jettyInfo.remoteJetty = importJettyCtxs[i].outParam.handle;
-                importJettyCtxs[i].outParam.handle = 0;
-            }
-            connJettyInfos.push_back(jettyInfo);
+            connDeleteJettyInfos.push_back(jettyInfo);
         }
     }
     return connJettyInfos;
+}
+
+std::vector<ConnJettyInfo> CcuConnection::GetUnimportJettyInfo()
+{
+    std::vector<ConnJettyInfo> connUnimportJettyInfos;
+    ConnJettyInfo jettyInfo;
+    for (auto &item : importJettyCtxs) {
+        if (item.outParam.handle != 0) {
+            jettyInfo.remoteJetty = item.outParam.handle;
+            jettyInfo.rdmaHandle = rdmaHandle;
+            item.outParam.handle = 0;
+            connUnimportJettyInfos.push_back(jettyInfo);
+        }
+    }
+    return connUnimportJettyInfos;
 }
 
 void CcuConnection::Clean()
