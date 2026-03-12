2c9d2ef46fe7bd8e44a37ef189bca23b6dd63fb2
xz00xz <xiongzhe5@huawei.com>
2026-03-12 21:39:19 +0800

    HCCP support RaNdaQpCreate

--- test/ut/platform/hccp/rdma_service_normal/stub/nda_api.c
+++ test/ut/platform/hccp/rdma_service_normal/stub/nda_api.c
@@ -56,12 +56,33 @@ struct ibv_qp_extend *ibv_create_qp_extend(struct ibv_context_extend *context,
     struct ibv_qp_init_attr_extend *qp_init_attr)
 {
     struct ibv_qp_extend *qp_extend = NULL;
+	static unsigned int qpn = 0;
+	struct ibv_qp *qp;
+
+	qp = malloc(sizeof(struct ibv_qp));
+	if (NULL == qp) {
+		return NULL;
+	}
+	memset(qp, 0, sizeof(struct ibv_qp));
+
+	qp->pd = qp_init_attr->pd;
+	qp->state = IBV_QPS_RESET;
+	qp->qp_type = qp_init_attr->attr.qp_type;
+	qp->send_cq = qp_init_attr->attr.send_cq;
+	qp->recv_cq = qp_init_attr->attr.recv_cq;
+	qp->context = qp_init_attr->pd->context;
+
+	qp->qp_num = qpn;
+	qpn++;
 
     qp_extend = malloc(sizeof(struct ibv_qp_extend));
     if (NULL == qp_extend) {
+        free(qp);
+        qp = NULL;
         return NULL;
     }
     memset(qp_extend, 0, sizeof(struct ibv_qp_extend));
+    qp_extend->qp = qp;
 
     return qp_extend;
 }
@@ -97,7 +118,12 @@ struct ibv_srq_extend *ibv_create_srq_extend(struct ibv_context_extend *context,
 int ibv_destroy_qp_extend(struct ibv_context_extend *context, struct ibv_qp_extend *qp_extend)
 {
     if (qp_extend != NULL) {
+        if (qp_extend->qp != NULL) {
+            free(qp_extend->qp);
+            qp_extend->qp = NULL;
+        }
         free(qp_extend);
+        qp_extend = NULL;
     }
 
     return 0;
