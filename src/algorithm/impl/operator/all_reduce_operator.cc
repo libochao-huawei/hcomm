827c8073627fa772130e1431638992c133a3c224
zeng_hao <zenghao56@h-partners.com>
2026-03-12 16:19:40 +0800

    clean code

--- src/algorithm/impl/operator/all_reduce_operator.cc
+++ src/algorithm/impl/operator/all_reduce_operator.cc
@@ -728,9 +728,6 @@ HcclResult AllReduceOperator::SelectAlgfor91093(const OpParam& param, std::strin
 
         CHK_PRT_RET(superPodNum_ != 1, HCCL_ERROR("multi superpod [%u] not supported", superPodNum_), HCCL_E_NOT_SUPPORT);
 
-        CHK_PRT_RET(userRankSize_ == 1, HCCL_ERROR("[HcclCommunicator][ExecOp] opType[AllReduce] currently do not select aiv mode, "
-            "aiv only not support, please ensure rankNum is greater than one"), HCCL_E_NOT_SUPPORT);
-
         CHK_PRT_RET(multiModuleDiffDeviceNumMode_, HCCL_ERROR("multiModuleDiffDeviceNumMode [%d] not supported", multiModuleDiffDeviceNumMode_), HCCL_E_NOT_SUPPORT);
         return HCCL_E_NOT_SUPPORT;
     }
