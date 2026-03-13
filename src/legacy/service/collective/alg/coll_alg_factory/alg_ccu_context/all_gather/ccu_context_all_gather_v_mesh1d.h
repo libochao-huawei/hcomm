b890720f49b5eb1cef9c1fc23802914cc2556574
gcw_dFpssWuk <wanghaixu5@h-partners.com>
2026-03-13 10:54:58 +0800

    重复代码修改

--- src/legacy/service/collective/alg/coll_alg_factory/alg_ccu_context/all_gather/ccu_context_all_gather_v_mesh1d.h
+++ src/legacy/service/collective/alg/coll_alg_factory/alg_ccu_context/all_gather/ccu_context_all_gather_v_mesh1d.h
@@ -8,8 +8,8 @@
  * See LICENSE in the root of the software repository for the full text of the License.
  */
 
-#ifndef HCCLV2_CCU_CONTEXT_REDUCE_MESH_2D_ONE_SHOT_H_
-#define HCCLV2_CCU_CONTEXT_REDUCE_MESH_2D_ONE_SHOT_H_
+#ifndef HCCLV2_CCU_CONTEXT_ALL_GATHER_V_MESH_1D_H_
+#define HCCLV2_CCU_CONTEXT_ALL_GATHER_V_MESH_1D_H_
 
 #include <vector>
 #include <ios>
@@ -19,29 +19,27 @@
 
 namespace Hccl {
 
-class CcuContextReduceMesh2DOneShot : public CcuContext {
+class CcuContextAllGatherVMesh1D : public CcuContext {
 public:
-    CcuContextReduceMesh2DOneShot(const CcuCtxArg &arg, const std::vector<CcuTransport*> &transports,
+    CcuContextAllGatherVMesh1D(const CcuCtxArg &arg, const std::vector<CcuTransport*> &transports,
                               const CcuTransportGroup &group);
-    ~CcuContextReduceMesh2DOneShot() override {}
+    ~CcuContextAllGatherVMesh1D() override {}
 
     void Algorithm() override;
     std::vector<uint64_t> GeneArgs(const CcuTaskArg &arg) override;
-
+    void LoadArgs();
+    void InitResources();
+    void PreSync();
+    void PostSync();
+    void DoGroupBroadcast();
 private:
-    uint64_t rankSize{0};
-    uint32_t rankId;
-    uint32_t rootId; // 当rankid == rootid时，为root节点 则跳过write操作
-    DataType dataType;
-    DataType outputDataType;
-    ReduceOp reduceOp;
-    std::vector<CcuRep::Variable> input_;
+    uint64_t rankSize_{0};
+    uint32_t rankId_{0};
+    CcuRep::Variable input_;
     std::vector<CcuRep::Variable> output_;
     std::vector<CcuRep::Variable> token_;
-    CcuRep::Variable offSet_;
-    CcuRep::Variable slicesize_;
+    CcuRep::Variable mySliceOffSet_;
     GroupOpSize groupOpSize_;
 };
-} // namespace Hccl
-
-#endif // HCCLV2_CCU_CONTEXT_REDUCE_MESH_2D_ONE_SHOT_H_
+}// namespace Hccl
+#endif // HCCLV2_CCU_CONTEXT_ALL_GATHER_V_MESH_1D_H_
