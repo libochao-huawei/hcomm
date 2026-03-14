f6fd8807d85144eb4fd476966d2253594e90cf8d
xz00xz <xiongzhe5@huawei.com>
2026-03-14 19:09:11 +0800

    HCCP support tp peer

--- src/platform/hccp/inc/network/hccp_common.h
+++ src/platform/hccp/inc/network/hccp_common.h
@@ -129,6 +129,23 @@ union HccpGid {
 
 /**
  * @ingroup libinit
+ * rdma/ub gid/eid
+ */
+union HccpEid {
+    uint8_t raw[16U]; /* Network Order */
+    struct {
+        uint64_t reserved; /* If IPv4 mapped to IPv6, == 0 */
+        uint32_t prefix;   /* If IPv4 mapped to IPv6, == 0x0000ffff */
+        uint32_t addr;     /* If IPv4 mapped to IPv6, == IPv4 addr */
+    } in4;
+    struct {
+        uint64_t subnetPrefix;
+        uint64_t interfaceId;
+    } in6;
+};
+
+/**
+ * @ingroup libinit
  * info need of rdma_agent
  */
 struct RaInfo {
