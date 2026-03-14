449ca8934631cecda376afc9e34ad620b8547435
one_random <shibingchen@huawei.com>
2026-03-14 19:18:12 +0800

    feat(ccu): ccu new c style interfaces

---
+++ pkg_inc/hcomm/ccu_new/ccu_variable.hpp
@@ -0,0 +1,58 @@
+/**
+ * Copyright (c) 2026 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+#ifndef CCU_VARIABLE_HPP
+#define CCU_VARIABLE_HPP
+
+#include <cstdint>
+
+#include "ccu_types.h"
+#include "ccu_data_utils.hpp"
+#include "ccu_data_api_impl.h"
+
+class CcuVariable {
+public:
+    explicit CcuVariable() {}
+
+    CcuVariable(const CcuVariable& other) {
+        this->handle = other.handle;
+    }
+
+    void operator=(CcuVariable&& other) {
+        this->handle = other.handle;
+    }
+
+    void operator=(uint64_t immediate) const {
+        auto ret = CcuVariableAssignImpl(this->handle, immediate);
+        if (ret != CcuResult::CCU_SUCCESS) {
+            throw "todo: failed";
+        }
+    }
+
+    void operator=(CcuArithmeticOperator<CcuVariable, CcuVariable> op) const {
+        auto ret = CcuVariableAddVarToVarImpl(this->handle, op.lhs.handle, op.rhs.handle);
+        if (ret != CcuResult::CCU_SUCCESS) {
+            throw "todo: failed";
+        }
+    }
+
+    CcuArithmeticOperator<CcuVariable, CcuVariable> operator+(const CcuVariable &that) const {
+        return CcuArithmeticOperator<CcuVariable, CcuVariable>(*this, that, CcuArithmeticOperatorType::ADDITION);
+    }
+
+    CcuVariableHandle handle{0};
+};
+
+template <> void CcuArithmeticOperator<CcuVariable, CcuVariable>::Check() const
+{
+    // 放开CcuVariable与CcuVariable的运算拦截
+}
+
+#endif // CCU_VARIABLE_HPP
