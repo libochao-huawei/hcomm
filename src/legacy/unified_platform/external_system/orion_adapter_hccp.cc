b30c1e5c5110a0b9ef4f974dd7a2f7b06f03911c
liguicheng8888 <liguicheng7@huawei.com>
2026-03-14 11:07:52 +0800

    Wrong IP Core Dump

--- src/legacy/unified_platform/external_system/orion_adapter_hccp.cc
+++ src/legacy/unified_platform/external_system/orion_adapter_hccp.cc
@@ -1040,19 +1040,26 @@ RdmaHandle HrtRaUbCtxInit(const HrtRaUbCtxInitParam &in)
     ctxInfo.phyId       = in.phyId;
     ctxInfo.ub.eidIndex = 0;
     HCCL_INFO("[HrtRaUbCtxInit] use eid[%s]", in.addr.Describe().c_str());
-    s32 sRet = memcpy_s(ctxInfo.ub.eid.raw, sizeof(ctxInfo.ub.eid.raw), in.addr.GetEid().raw, sizeof(in.addr.GetEid().raw));
+    printf("LGC:00001!");
+    auto raw = in.addr.GetEid().raw;
+    printf("LGC:00002!");
+    s32 sRet = memcpy_s(ctxInfo.ub.eid.raw, sizeof(ctxInfo.ub.eid.raw), raw, sizeof(raw));
+    printf("LGC:00003!");
     if (sRet != EOK) {
         MACRO_THROW(InternalException, StringFormat("[HrtRaUbCtxInit]memcpy_s failed. sRet[%d]", sRet));
     }
 
     RdmaHandle handle;
+    printf("LGC:00004!");
     s32        ret = RaCtxInit(&initCfg, &ctxInfo, &handle);
+    printf("LGC:00005!");
     if (ret != 0) {
         string msg = StringFormat(
             "[Init][RaUbCtx]errNo[0x%016llx] ub ctx init fail, mode[%d], phyId[%u], addr[%s], ret[%d]",
             HCCL_ERROR_CODE(HcclResult::HCCL_E_NETWORK), in.mode, in.phyId, in.addr.GetIpStr().c_str(), ret);
         MACRO_THROW(NetworkApiException, msg);
     }
+    printf("LGC:00006!");
     return handle;
 }
 
