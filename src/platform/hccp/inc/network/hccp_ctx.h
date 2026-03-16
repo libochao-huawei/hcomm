b7b12f94386f28d62d12792835ea08b06cf7c0f0
gaoxiong17 <gaoxiong17@huawei.com>
2026-03-16 20:25:24 +0800

    reduce oversized header file

--- src/platform/hccp/inc/network/hccp_ctx.h
+++ src/platform/hccp/inc/network/hccp_ctx.h
@@ -12,6 +12,7 @@
 #define HCCP_CTX_H
 
 #include "hccp_common.h"
+#include "hccp_data_plane.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -533,107 +534,6 @@ struct QpImportInfoT {
     struct QpImportInfo out;
 };
 
-struct WrSgeList {
-    uint64_t addr;
-    uint32_t len;
-    void *lmemHandle;
-};
-
-struct WrNotifyInfo {
-    uint64_t notifyData; /**< notify data */
-    uint64_t notifyAddr; /**< remote notify addr */
-    void *notifyHandle; /**< remote notify handle */
-};
-
-struct WrReduceInfo {
-    bool reduceEn;
-    uint8_t reduceOpcode;
-    uint8_t reduceDataType;
-};
-
-enum RaUbOpcode {
-    RA_UB_OPC_WRITE               = 0x00,
-    RA_UB_OPC_WRITE_NOTIFY        = 0x02,
-    RA_UB_OPC_READ                = 0x10,
-    RA_UB_OPC_NOP                 = 0x51,
-    RA_UB_OPC_LAST
-};
-
-union JfsWrFlag {
-    struct {
-        uint32_t placeOrder : 2;       /* 0: There is no order with other WR.
-                                           1: relax order.
-                                           2: strong order.
-                                           3: reserve. see urma_order_type_t */
-        uint32_t compOrder : 1;        /* 0: There is no completion order with other WR.
-                                           1: Completion order with previous WR. */
-        uint32_t fence : 1;             /* 0: There is no fence.
-                                           1: Fence with previous read and atomic WR. */
-        uint32_t solicitedEnable : 1;  /* 0: not solicited.
-                                           1: Solicited. */
-        uint32_t completeEnable : 1;   /* 0: DO not Generate CR for this WR.
-                                           1: Generate CR for this WR after the WR is completed. */
-        uint32_t inlineFlag : 1;       /* 0: Nodata.
-                                           1: Inline data. */
-        uint32_t reserved : 25;
-    } bs;
-    uint32_t value;
-};
-
-struct SendWrData {
-    struct WrSgeList *sges;
-    uint32_t numSge; /**< size of segs, not exceeds to MAX_SGE_NUM */
-
-    uint8_t *inlineData;
-    uint32_t inlineSize; /**< size of inline_data, see struct dev_base_attr */
-
-    uint64_t remoteAddr;
-    void *rmemHandle;
-
-    union {
-        struct {
-            uint64_t wrId;
-            enum RaWrOpcode opcode;
-            unsigned int flags; /**< reference to ra_send_flags */
-            struct WrAuxInfo aux; /**< aux info */
-        } rdma;
-
-        struct {
-            uint64_t userCtx;
-            enum RaUbOpcode opcode; /**< refer to urma_opcode_t */
-            union JfsWrFlag flags; /**< refer to urma_jfs_wr_flag_t */
-            void *remQpHandle; /**< resv for RM use */
-            struct WrNotifyInfo notifyInfo; /**< required for opcode RA_UB_OPC_WRITE_NOTIFY */
-            struct WrReduceInfo reduceInfo; /**<reduce is enabled when reduce_en is set to true */
-        } ub;
-    };
-
-    uint32_t immData;
-
-    uint32_t resv[16U];
-};
-
-struct UbPostInfo {
-    uint16_t funcId : 7;
-    uint16_t dieId : 1;
-    uint16_t rsv : 8;
-    uint16_t jettyId;
-    // doorbell value
-    uint16_t piVal;
-    // direct wqe
-    uint8_t dwqe[128U];
-    uint16_t dwqeSize; /**< size of dwqe calc by piVal, 64 or 128 */
-};
-
-struct SendWrResp {
-    union {
-        struct WqeInfoT wqeTmp; /**< wqe template info used for V80 offload */
-        struct DbInfo db; /**< doorbell info used for V71 and V80 opbase */
-        struct UbPostInfo doorbellInfo; /**< doorbell info used for UB */
-        uint8_t resv[384U]; /**< resv for write value doorbell info */
-    };
-};
-
 #define CUSTOM_CHAN_DATA_MAX_SIZE 2048
 
 struct CustomChanInfoIn {
@@ -989,21 +889,6 @@ HCCP_ATTRI_VISI_DEF int RaCtxQpBind(void *qpHandle, void *remQpHandle);
  * @retval #non-zero Failure
 */
 HCCP_ATTRI_VISI_DEF int RaCtxQpUnbind(void *qpHandle);
-
-/**
- * @ingroup libudma
- * @brief batch post send wr
- * @param qp_handle [IN] qp handle
- * @param send_wr_data [IN] send wr data
- * @param op_resp [IN/OUT] send wr resp
- * @param num [IN] size of wr_list & op_resp
- * @param complete_num [OUT] number of wr been post send successfully
- * @see ra_ctx_qp_create
- * @retval #zero Success
- * @retval #non-zero Failure
-*/
-HCCP_ATTRI_VISI_DEF int RaBatchSendWr(void *qpHandle, struct SendWrData wrList[], struct SendWrResp opResp[],
-    unsigned int num, unsigned int *completeNum);
 
 /**
  * @ingroup libudma
