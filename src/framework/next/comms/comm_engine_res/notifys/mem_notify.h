6b5c4dbd9110d7f98c68567dc5718ec06dc2b817
zwlStateflow <zhengwenliang@huawei.com>
2026-03-16 21:18:59 +0800

    fix mem_notify(PCIe/UB/Others)

--- src/framework/next/comms/comm_engine_res/notifys/mem_notify.h
+++ src/framework/next/comms/comm_engine_res/notifys/mem_notify.h
@@ -34,11 +34,7 @@ public:
         if (!isInit_) {
             return;
         }
-        s32 devId = 0;
-        hrtGetDevice(&devId);
-        int64_t connectType = 0;
-        hrtHalGetDeviceInfo(devId, MODULE_TYPE_SYSTEM, INFO_TYPE_HD_CONNECT_TYPE, &connectType); // ?
-        if (connectType == HOST_DEVICE_CONNECT_TYPE_PCIE) { // PCIe
+        if (connectType_ == HOST_DEVICE_CONNECT_TYPE_PCIE) { // PCIe
             // 释放notify
             if (notifyDeviceVa_) {
                 aclrtFree(&notifyDeviceVa_); // device
@@ -46,11 +42,11 @@ public:
             // 对齐
             uint64_t va = reinterpret_cast<uint64_t>(notifyDeviceVa_);
             uint64_t registerVa = (va + (4096ULL - 1ULL)) & ~(4096ULL - 1ULL);
-            int32_t ret = halSvmUnregister(devId, registerVa, sizeof(uint8_t), SVM_REGISTER_FLAG_WITH_ACCESS_VA);
+            int32_t ret = halSvmUnregister(devId_, registerVa, sizeof(uint8_t), SVM_REGISTER_FLAG_WITH_ACCESS_VA);
             if (ret != 0) {
-                HCCL_ERROR("halSvmUnregister failed, ret = %d", ret);
+                HCCL_ERROR("[MemNotify][%s] halSvmUnregister failed, ret = %d", __func__, ret);
             }
-        } else {
+        } else if (connectType_ == HOST_DEVICE_CONNECT_TYPE_UB) {
             if (notifyHostVa_) {
                 (void)aclrtHostUnregister(notifyHostVa_);
                 free(notifyHostVa_);
@@ -62,14 +58,14 @@ public:
         return HCCL_SUCCESS;
     };
     HcclResult Wait() {
+        if (!isInit_) {
+            HCCL_ERROR("[MemNotify][%s] not init", __func__);
+            return HCCL_E_INTERNAL;
+        }
         // 等待notify被写入（flag从0变为非0）
-        s32 devId = 0;
-        CHK_RET(hrtGetDevice(&devId));
-        int64_t connectType = 0;
-        hrtHalGetDeviceInfo(devId, MODULE_TYPE_SYSTEM, INFO_TYPE_HD_CONNECT_TYPE, &connectType);
         auto timeout = std::chrono::milliseconds(WAIT_TIMEOUT_MS);
         auto startTime = std::chrono::steady_clock::now();
-        if (connectType == HOST_DEVICE_CONNECT_TYPE_PCIE) { // PCIe
+        if (connectType_ == HOST_DEVICE_CONNECT_TYPE_PCIE) { // PCIe
             // 轮询notifyDeviceVa_地址，等待被写入
             while (true) {
                 uint8_t flag;
@@ -91,11 +87,11 @@ public:
                 }
 
                 if ((std::chrono::steady_clock::now() - startTime) >= timeout) {
-                    HCCL_ERROR("[HostCpuRoceChannel][%s] call ibv_poll_cq timeout.", __func__);
+                    HCCL_ERROR("[MemNotify][%s] call ibv_poll_cq timeout.", __func__);
                     return HCCL_E_TIMEOUT;
                 }
             }
-        } else {
+        } else if (connectType_ == HOST_DEVICE_CONNECT_TYPE_UB) {
             // 轮询notifyHostVa_地址，等待被写入
             while (true) {
                 uint8_t flag = *reinterpret_cast<uint8_t*>(notifyHostVa_);
@@ -106,19 +102,20 @@ public:
                 }
 
                 if ((std::chrono::steady_clock::now() - startTime) >= timeout) {
-                    HCCL_ERROR("[HostCpuRoceChannel][%s] call ibv_poll_cq timeout.", __func__);
+                    HCCL_ERROR("[MemNotify][%s] call ibv_poll_cq timeout.", __func__);
                     return HCCL_E_TIMEOUT;
                 }
             }
+        } else {
+            HCCL_ERROR("[MemNotify][%s] not support connectType [%ld]", __func__, connectType_);
+            return HCCL_E_NOT_SUPPORT;
         }
         return HCCL_SUCCESS;
     };
     HcclResult Alloc() {
-        s32 devId = 0;
-        CHK_RET(hrtGetDevice(&devId));
-        int64_t connectType = 0;
-        hrtHalGetDeviceInfo(devId, MODULE_TYPE_SYSTEM, INFO_TYPE_HD_CONNECT_TYPE, &connectType); // ?
-        if (connectType == HOST_DEVICE_CONNECT_TYPE_PCIE) { // PCIe
+        CHK_RET(hrtGetDevice(&devId_));
+        hrtHalGetDeviceInfo(devId_, MODULE_TYPE_SYSTEM, INFO_TYPE_HD_CONNECT_TYPE, &connectType_); // ?
+        if (connectType_ == HOST_DEVICE_CONNECT_TYPE_PCIE) { // PCIe
             // 申请notify
             aclError mallocRet = aclrtMalloc(&notifyDeviceVa_, sizeof(uint8_t) + 4096ULL, ACL_MEM_MALLOC_HUGE_ONLY);
             if (mallocRet != ACL_SUCCESS) {
@@ -128,27 +125,30 @@ public:
             uint64_t va = reinterpret_cast<uint64_t>(notifyDeviceVa_);
             uint64_t registerVa = (va + (4096ULL - 1ULL)) & ~(4096ULL - 1ULL);
             uint64_t accessVa;
-            int32_t ret = halSvmRegister(devId, registerVa, sizeof(uint8_t), SVM_REGISTER_FLAG_WITH_ACCESS_VA, &accessVa);
+            int32_t ret = halSvmRegister(devId_, registerVa, sizeof(uint8_t), SVM_REGISTER_FLAG_WITH_ACCESS_VA, &accessVa);
             if (ret != 0) {
-                HCCL_ERROR("halSvmRegister failed, ret = %d", ret);
+                HCCL_ERROR("[MemNotify][%s] halSvmRegister failed, ret = %d", __func__, ret);
                 aclrtFree(&notifyDeviceVa_);
                 return HCCL_E_INTERNAL;
             }
             notifyHostVa_ = reinterpret_cast<void*>(accessVa);
-        } else {
+        } else if (connectType_ == HOST_DEVICE_CONNECT_TYPE_UB) {
             // 申请host内存
             notifyHostVa_ = malloc(sizeof(uint8_t));
             if (!notifyHostVa_) {
-                HCCL_ERROR("malloc for notifyHostVa_ failed");
+                HCCL_ERROR("[MemNotify][%s] malloc for notifyHostVa_ failed", __func__);
                 return HCCL_E_RUNTIME;
             }
             aclError aclRet = aclrtHostRegister(notifyHostVa_, sizeof(uint8_t), ACL_HOST_REGISTER_MAPPED, &notifyDeviceVa_);
             if (aclRet != ACL_SUCCESS) {
-                HCCL_ERROR("aclrtHostRegister failed, ret = %d", aclRet);
+                HCCL_ERROR("[MemNotify][%s] aclrtHostRegister failed, ret = %d", __func__, aclRet);
                 free(notifyHostVa_);
                 notifyHostVa_ = nullptr;
                 return HCCL_E_INTERNAL;
             }
+        } else {
+            HCCL_ERROR("[MemNotify][%s] not support connectType [%ld]", __func__, connectType_);
+            return HCCL_E_NOT_SUPPORT;
         }
         isInit_ = true;
         return HCCL_SUCCESS;
@@ -158,6 +158,8 @@ public:
     };
 private:
     bool isInit_ = false;
+    s32 devId_ = 0;
+    int64_t connectType_ = 0;
     void* notifyHostVa_{};
     void* notifyDeviceVa_{};
 };
