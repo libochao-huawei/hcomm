2567e831453ad5c80d86c6fddf4df3635d19b549
one_random <shibingchen@huawei.com>
2026-03-16 20:29:32 +0800

    bugfix(ccu): fix compile bugs

--- pkg_inc/hcomm/ccu_new/ccu_control_api.h
+++ pkg_inc/hcomm/ccu_new/ccu_control_api.h
@@ -1,13 +1,5 @@
 /**
-<<<<<<< HEAD
-<<<<<<< HEAD
- * Copyright (c) 2026 Huawei Technologies Co., Ltd.
-=======
  * Copyright (c) 2025 Huawei Technologies Co., Ltd.
->>>>>>> 1e05e06b (bugfix(ccu): fix compile bugs)
-=======
- * Copyright (c) 2026 Huawei Technologies Co., Ltd.
->>>>>>> 17cd52f8 (bugfix(ccu): ccu c style fix llt example)
  * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
  * CANN Open Software License Agreement Version 2.0 (the "License").
  * Please refer to the License for details. You may not use this file except in compliance with the License.
@@ -16,38 +8,15 @@
  * See LICENSE in the root of the software repository for the full text of the License.
  */
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-#ifndef CCU_CONTROL_API_H
-#define CCU_CONTROL_API_H
-
-#include "hccl_types.h"
-#include "ccu_types.h"
-#include "hccl_res.h"
-<<<<<<< HEAD
-=======
 #ifndef HCCL_CCU_CONTROL_API_H
 #define HCCL_CCU_CONTROL_API_H
-=======
-#ifndef CCU_CONTROL_API_H
-#define CCU_CONTROL_API_H
->>>>>>> 17cd52f8 (bugfix(ccu): ccu c style fix llt example)
 
 #include "ccu_types.h"
->>>>>>> 1e05e06b (bugfix(ccu): fix compile bugs)
-=======
->>>>>>> 67098c8a (feat(ccu): kernel launch)
 
 #ifdef __cplusplus
 extern "C" {
 #endif // __cplusplus
 
-<<<<<<< HEAD
-extern HcclResult HcclCommQueryCcuIns(HcclComm comm,
-    CcuInsHandle *insHandles, uint32_t *insNum);
-
-=======
->>>>>>> 1e05e06b (bugfix(ccu): fix compile bugs)
 extern CcuResult HcommCcuKernelRegisterStart(CcuInsHandle insHandle);
 
 extern CcuResult HcommCcuKernelRegister(CcuInsHandle insHandle,
@@ -56,28 +25,8 @@ extern CcuResult HcommCcuKernelRegister(CcuInsHandle insHandle,
 
 extern CcuResult HcommCcuKernelRegisterEnd(CcuInsHandle insHandle);
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-extern CcuResult HcommCcuKernelLaunch(ThreadHandle threadHandle,
-    CcuKernelHandle kernelHandle, void *taskArgs, uint32_t argSize);
-
-=======
->>>>>>> 1e05e06b (bugfix(ccu): fix compile bugs)
-=======
-extern CcuResult HcommCcuKernelLaunch(ThreadHandle threadHandle,
-    CcuKernelHandle kernelHandle, void *taskArgs, uint32_t argSize);
-
->>>>>>> 67098c8a (feat(ccu): kernel launch)
 #ifdef __cplusplus
 }
 #endif // __cplusplus
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-#endif // CCU_CONTROL_API_H
-=======
 #endif
->>>>>>> 1e05e06b (bugfix(ccu): fix compile bugs)
-=======
-#endif // CCU_CONTROL_API_H
->>>>>>> 17cd52f8 (bugfix(ccu): ccu c style fix llt example)
