6b1b976bf742adb3a183dc4145f6dbc03b70b1cd
chengjing43 <chengjing43@huawei.com>
2026-03-16 14:43:00 +0800

    support nda create cq

--- src/platform/hccp/common/dl_hal_function.c
+++ src/platform/hccp/common/dl_hal_function.c
@@ -204,6 +204,12 @@ static void DlHalApiInit(void)
     return;
 }
 
+gHalOps.dlHalHostRegister = (drvError_t (*)(void *srcPtr, UINT64 size, UINT32 flag, UINT32 devId, void **dstPtr))
+    AscendHalDlsym(gHalApiHandle, "halHostRegister");
+
+gHalOps.dlHalHostUnregister = (drvError_t (*)(void *src_ptr, UINT32 devid))
+    AscendHalDlsym(gHalApiHandle, "halHostUnregister");
+
 void DlHalDeinit(void)
 {
     pthread_mutex_lock(&gHalApiLock);
@@ -642,4 +648,18 @@ int DlDrvMemGetAttribute(DVdeviceptr vptr, struct DVattribute *attr)
     DL_API_IS_NULL_CHECK(gHalApiHandle, gHalOps.dlDrvMemGetAttribute, "dlDrvMemGetAttribute");
 
     return gHalOps.dlDrvMemGetAttribute(vptr, attr);
+}
+
+int DlHalHostRegister(void *srcPtr, UINT64 size, UINT32 flag, UINT32 devId, void **dstPtr)
+{
+    DL_API_IS_NULL_CHECK(gHalApiHandle, gHalOps.dlHalHostRegister, "dlHalHostRegister");
+
+    return gHalOps.dlHalHostRegister(srcPtr, size, flag, devId, dstPtr);
+}
+
+int DlHalHostUnRegister(void *srcPtr, UINT32 devId)
+{
+    DL_API_IS_NULL_CHECK(gHalApiHandle, gHalOps.dlHalHostUnregister, "dlHalHostUnregister");
+
+    return gHalOps.dlHalHostUnregister(srcPtr, devId);
 }
