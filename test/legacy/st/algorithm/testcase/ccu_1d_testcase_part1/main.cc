de6e3a4a3fecb7ce7a36c1a8558100b423b5fd98
ZhanPhil <zhanhaifeng1@huawei.com>
2026-03-17 14:22:10 +0800

    legacy checker testcase
    Co-authored-by: ZhanPhil<zhanhaifeng1@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !977 merge master into master
    
    legacy checker testcase
    
    Created-by: ZhanPhil
    Commit-by: ZhanPhil
    Merged-by: cann-robot
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #123-->
    <!-- 如果这个PR不涉及Issue，可填写"NA"。-->
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于构造对应xx测试用例、二级冒烟、算子泛化等。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    
    
    See merge request: cann/hcomm!977

--- test/legacy/st/algorithm/testcase/ccu_1d_testcase_part1/main.cc
+++ test/legacy/st/algorithm/testcase/ccu_1d_testcase_part1/main.cc
@@ -11,7 +11,7 @@
 
 GTEST_API_ int main(int argc, char **argv) {
     // testcase调试代码，只跑特定的用例
-    testing::GTEST_FLAG(filter) = "AllReduceCCUTest.allreduce_ccu_case_test_6rank_mem2mem";
+    // testing::GTEST_FLAG(filter) = "AllReduceCCUTest.allreduce_ccu_case_test_6rank_mem2mem";
     testing::InitGoogleTest(&argc, argv);
     // 用来控制各个LLT任务的topoPath.json的名字，不能和其他任务的名字一样，可能会冲突
     setenv("TOPO_PATH_NAME", "ccu_1d_testcase", 1);
