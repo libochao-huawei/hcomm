b8245a7749ded7edd3d506e0a2e34ee03240800f
chenyia <chenyi163@huawei.com>
2026-03-17 19:26:04 +0800

    optimize aiv compile time

---
+++ src/algorithm/base/alg_aiv_template/reduce_scatter_v/aiv_reduce_scatter_v_op.h
@@ -0,0 +1,35 @@
+/**
+ * Copyright (c) 2025 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+#ifndef AIV_REDUCE_SCATTER_V_OP_H
+#define AIV_REDUCE_SCATTER_V_OP_H
+
+#include "aiv_communication_base.h"
+
+#include "aiv_reduce_scatter_v_910b_smalldata.h"
+#include "aiv_reduce_scatter_v_910b_middata.h"
+#include "aiv_reduce_scatter_v_910b_bigdata.h"
+
+#define AIV_REDUCE_SCATTER_V_KERNEL_BATCH_DEF(type) \
+extern "C" __global__ __aicore__ void aiv_reduce_scatter_v_##type(EXTERN_KERNEL_ARGS_DEF) { \
+    if (extraArgs.maxCount * sizeof(type) > AIV_REDUCE_SCATTER_V_MID_SIZE) { \
+        return aiv_reduce_scatter_v_910b_bigdata<type>(EXTERN_KERNEL_ARGS_CALL); \
+    } else if (extraArgs.maxCount * sizeof(type) > UB_MAX_DATA_SIZE) { \
+        return aiv_reduce_scatter_v_910b_middata<type>(EXTERN_KERNEL_ARGS_CALL); \
+    } else  { \
+        return aiv_reduce_scatter_v_910b_smalldata<type>(EXTERN_KERNEL_ARGS_CALL); \
+    } \
+} \
+EXPORT_AIV_META_INFO(aiv_reduce_scatter_v_##type)
+
+// 定义算子各数据类型Kernel入口
+AIV_ATOMIC_DATA_TYPE_DEF(AIV_REDUCE_SCATTER_V_KERNEL_BATCH_DEF);
+
+#endif  /* AIV_REDUCE_SCATTER_V_OP_H */
