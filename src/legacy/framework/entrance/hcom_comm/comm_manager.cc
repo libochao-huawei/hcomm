1ca6eb07b6a5a4ecd43c47bab04164c7d32c0cee
renyumin <renyumin1@huawei.com>
2026-03-17 19:32:54 +0800

    update: 更新文件 comm_manager.cc add CHK
    Signed-off-by: renyumin <renyumin1@huawei.com>

--- src/legacy/framework/entrance/hcom_comm/comm_manager.cc
+++ src/legacy/framework/entrance/hcom_comm/comm_manager.cc
@@ -471,6 +471,7 @@ HcclResult HcomGetRankSizeV2(const char *group, u32 *rankSize)
 
 HcclResult HcomGetCommV2(void **commV2)
 {
+    CHK_PTR_NULL(commV2);
     HcclCommInfoV2 &hcomCommInfoV2 = GetCommInfoV2();
     CHK_PTR_NULL(hcomCommInfoV2.pComm);
     *commV2 = static_cast<void *>(hcomCommInfoV2.pComm.get());
