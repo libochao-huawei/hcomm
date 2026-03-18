55a375aa02404ffd4723b742ee9bffd053deaa30
lhdlhdlhd1234569 <lihaodong19@h-partners.com>
2026-03-18 09:23:15 +0800

    update

--- test/legacy/ut/framework/service/ut_coll_service_ai_cpu_impl.cc
+++ test/legacy/ut/framework/service/ut_coll_service_ai_cpu_impl.cc
@@ -516,6 +516,7 @@ TEST_F(CollServiceAiCpuImplTest, Ut_RegisterCclBuffer_When_Normal_Expect_Success
 TEST_F(CollServiceAiCpuImplTest, Ut_Resume_When_Normal_Expect_Success)
 {
     MOCKER(HrtGetStreamId).stubs().will(returnValue(static_cast<s32>(0)));
+    MOCKER(HrtNotifyRecord).stubs();
     CommunicatorImpl comm;
     comm.InitNotifyManager();
     comm.InitSocketManager();
