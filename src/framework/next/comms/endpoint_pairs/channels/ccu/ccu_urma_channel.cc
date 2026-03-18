05c63962d8e39d1bfa264ad7b578539334291b68
renyumin <renyumin1@huawei.com>
2026-03-18 14:09:22 +0800

    update: 更新文件 ccu_urma_channel.cc delete check memHandleNum
    Signed-off-by: renyumin <renyumin1@huawei.com>

--- src/framework/next/comms/endpoint_pairs/channels/ccu/ccu_urma_channel.cc
+++ src/framework/next/comms/endpoint_pairs/channels/ccu/ccu_urma_channel.cc
@@ -35,10 +35,6 @@ CcuUrmaChannel::CcuUrmaChannel(const EndpointHandle locEndpointHandle,
 HcclResult BuildBufferInfos(void **memHandles, uint32_t memHandleNum,
     std::vector<CcuTransport::CclBufferInfo> &bufferInfos)
 {
-    if (memHandleNum == 0) {
-        HCCL_ERROR("[BuildBufferInfos] memHandleNum is 0.");
-        return HCCL_E_PARA;
-    }
     for (uint32_t i = 0; i < memHandleNum; ++i) {
         auto *locRmaBuffer = reinterpret_cast<Hccl::LocalUbRmaBuffer *>(memHandles[i]);
         CHK_PTR_NULL(locRmaBuffer);
