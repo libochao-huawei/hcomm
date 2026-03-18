17cd52f80c69662bf27c581cc50b62ed06061ff9
one_random <shibingchen@huawei.com>
2026-03-18 18:47:39 +0800

    bugfix(ccu): ccu c style fix llt example

--- src/framework/next/comms/ccu/ccu_kernel/ccu_kernel_mgr.h
+++ src/framework/next/comms/ccu/ccu_kernel/ccu_kernel_mgr.h
@@ -68,7 +68,7 @@ private:
 private:
     bool initializedFlag_{false};
     int32_t devLogicId_{-1};
-    std::mutex kernelMapMutex_;
+    std::mutex kernelMapMutex_{};
     CcuKernelHandle kernelId_ = 0;
     std::unordered_map<CcuKernelHandle, std::unique_ptr<CcuKernel>> kernelMap_{};
     void *instructionLoadDevMem_{nullptr};
