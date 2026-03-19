e9fbae2ae3e9c95c96e1fe5b707df1424b315ce8
renyumin <renyumin1@huawei.com>
2026-03-19 19:44:52 +0800

    ut

--- src/framework/next/comms/endpoint_pairs/channels/ccu/ccu_urma_channel.h
+++ src/framework/next/comms/endpoint_pairs/channels/ccu/ccu_urma_channel.h
@@ -57,5 +57,7 @@ private:
     std::string memTag_{"HcclBuffer"};
 };
 
+HcclResult BuildBufferInfos(void **memHandles, uint32_t memHandleNum,
+    std::vector<CcuTransport::CclBufferInfo> &bufferInfos);
 }  // namespace hcomm
 #endif  // CCU_URMA_CHANNEL_H
