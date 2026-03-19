71890272620fb9500e1026a4493e7573c1a4a65e
laodazhao <laodazhao@huawei.com>
2026-03-19 17:03:38 +0800

    fix aclgraph callback core

--- test/ut/framework/cluster_maintenance/aclgraph/ut_aclgraph_callback.cc
+++ test/ut/framework/cluster_maintenance/aclgraph/ut_aclgraph_callback.cc
@@ -12,6 +12,7 @@
 #include <mockcpp/mockcpp.hpp>
 #include <memory>
 #include <cstring>
+#include <unordered_set>
 
 #include "hccl/base.h"
 #include "hccl/hccl_types.h"
@@ -20,9 +21,19 @@
 #include "comm_config_pub.h"
 #include "common.h"
 #include "hccl_communicator.h"
+
+#define private public
 #include "aclgraph_callback.h"
+#undef private
 
 using namespace hccl;
+
+// Mock 函数用于 rtModelGetId
+rtError_t rtModelGetIdMock(rtModel_t model, uint32_t *modelId)
+{
+    *modelId = 1; // 返回一个固定的 mock modelId
+    return RT_ERROR_NONE;
+}
 
 class AclgraphCallbackTest : public testing::Test {
 protected:
@@ -35,7 +46,11 @@ protected:
     }
 
     void TearDown() override {
-        GlobalMockObject::verify();
+        // 清理单例状态，避免测试用例之间相互影响
+        std::lock_guard<std::mutex> lock(AclgraphCallback::GetInstance().resMutex_);
+        AclgraphCallback::GetInstance().captureResMap_.clear();
+        AclgraphCallback::GetInstance().captureCallbackParamMap_.clear();
+        GlobalMockObject::reset();
     }
 
     std::shared_ptr<hccl::hcclComm> comm;
@@ -51,6 +66,10 @@ TEST_F(AclgraphCallbackTest, ut_InsertNewTagToCaptureResMap_When_Capture_Expect_
     .stubs()
     .with(any(), outBoundP(&captureStatus, sizeof(captureStatus)), outBoundP(&pmockModel, sizeof(pmockModel)))
     .will(returnValue(0));
+
+    MOCKER(rtModelGetId)
+    .stubs()
+    .will(invoke(rtModelGetIdMock));
 
     std::string newTag = "tag";
     OpParam opParam;
@@ -74,5 +93,76 @@ TEST_F(AclgraphCallbackTest, ut_CleanCaptureRes_When_modelId_Invalid_Expect_Fail
 
     HcclResult ret = AclgraphCallback::GetInstance().CleanCaptureRes(modelId);
     EXPECT_EQ(ret, HCCL_E_NOT_FOUND);
+    GlobalMockObject::verify();
+}
+
+TEST_F(AclgraphCallbackTest, ut_CleanCaptureRes_By_Communicator_With_Multiple_Comms_Expect_Correct_Comm_Removed)
+{
+    int mockModel = 0;
+    void *pmockModel = &mockModel;
+    aclmdlRICaptureStatus captureStatus = aclmdlRICaptureStatus::ACL_MODEL_RI_CAPTURE_STATUS_ACTIVE;
+    MOCKER(aclmdlRICaptureGetInfo)
+    .stubs()
+    .with(any(), outBoundP(&captureStatus, sizeof(captureStatus)), outBoundP(&pmockModel, sizeof(pmockModel)))
+    .will(returnValue(0));
+
+    MOCKER(aclmdlRIDestroyRegisterCallback)
+    .stubs()
+    .will(returnValue(0));
+
+    MOCKER(rtModelGetId)
+    .stubs()
+    .will(invoke(rtModelGetIdMock));
+
+    std::string newTag = "tag";
+    OpParam opParam;
+
+    // 创建两个不同的 communicator
+    HcclCommunicator communicator1;
+    HcclCommunicator communicator2;
+
+    // 插入两个 communicator 到 captureResMap_
+    HcclResult ret1 = AclgraphCallback::GetInstance().InsertNewTagToCaptureResMap(&communicator1, newTag, opParam);
+    EXPECT_EQ(ret1, HCCL_SUCCESS);
+    HcclResult ret2 = AclgraphCallback::GetInstance().InsertNewTagToCaptureResMap(&communicator2, newTag, opParam);
+    EXPECT_EQ(ret2, HCCL_SUCCESS);
+
+    printf("[TEST] insert success\n");
+
+    // 验证插入成功
+    bool foundComm1Before = false;
+    bool foundComm2Before = false;
+    for (const auto &modelEntry : AclgraphCallback::GetInstance().captureResMap_) {
+        if (modelEntry.second.find(&communicator1) != modelEntry.second.end()) {
+            foundComm1Before = true;
+        }
+        if (modelEntry.second.find(&communicator2) != modelEntry.second.end()) {
+            foundComm2Before = true;
+        }
+    }
+    EXPECT_TRUE(foundComm1Before) << "Communicator1 should be found in captureResMap_ before CleanCaptureRes";
+    EXPECT_TRUE(foundComm2Before) << "Communicator2 should be found in captureResMap_ before CleanCaptureRes";
+
+    printf("[TEST] check insert success\n");
+
+    // 只删除 communicator1
+    AclgraphCallback::GetInstance().CleanCaptureRes(&communicator1);
+
+    printf("[TEST] delete success\n");
+    // 验证：communicator1 应该被删除，communicator2 应该仍然存在
+    bool foundComm1After = false;
+    bool foundComm2After = false;
+    for (const auto &modelEntry : AclgraphCallback::GetInstance().captureResMap_) {
+        if (modelEntry.second.find(&communicator1) != modelEntry.second.end()) {
+            foundComm1After = true;
+        }
+        if (modelEntry.second.find(&communicator2) != modelEntry.second.end()) {
+            foundComm2After = true;
+        }
+    }
+    EXPECT_FALSE(foundComm1After) << "Communicator1 should NOT be found in captureResMap_ after CleanCaptureRes";
+    EXPECT_TRUE(foundComm2After) << "Communicator2 should still be found in captureResMap_ after CleanCaptureRes";
+    printf("[TEST] delete check success\n");
+
     GlobalMockObject::verify();
 }
