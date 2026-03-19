6db8f6378657c30d758bcc3eb9ad39af8797834c
yan_yu_a123 <wangjingcong@h-partners.com>
2026-03-19 15:48:30 +0800

    打印截断问题

--- src/legacy/framework/ccu/ccu_manager/ccu_transport_manager.cpp
+++ src/legacy/framework/ccu/ccu_manager/ccu_transport_manager.cpp
@@ -199,7 +199,13 @@ void CcuTransportMgr::DumpNotReadyTransports(vector<std::pair<CcuTransport*, Lin
 {
     HCCL_ERROR("Dump ccu timeout transport info, transport size[%u]", transports.size());
     for (auto transIter = transports.begin(); transIter != transports.end(); ++transIter) {
-        HCCL_ERROR("CcuTransport[%s]", (*transIter).first->Describe().c_str());
+        string allStr=(*transIter).first->Describe().c_str();
+        size_t pos = allStr.find("Socket");
+        if(pos!=string::npos){
+            HCCL_ERROR("CcuTransport[%s]", allStr.substr(0,pos));
+            allStr=allStr.substr(pos);
+        }
+        HCCL_ERROR("CcuTransport[%s]", allStr);
         HCCL_ERROR("LinkData[%s]", (*transIter).second.Describe().c_str());
     }
 }
