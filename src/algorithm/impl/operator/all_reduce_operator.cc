62442dae06614f19b340fac1f28e8db0dbac5f3a
g00852163 <guoren1@huawei.com>
2026-03-20 11:16:24 +0800

    hcomm: change AllReduce deter algorithm impl.

--- src/algorithm/impl/operator/all_reduce_operator.cc
+++ src/algorithm/impl/operator/all_reduce_operator.cc
@@ -617,18 +617,18 @@ HcclResult AllReduceOperator::SelectAlgfor91093(const OpParam& param, std::strin
                     && (!retryEnable_)
                     && !multiModuleDiffDeviceNumMode_;
     
+    if (isSupportAivDeter) {
+        algName = "AllReduceMeshAivFor91093Executor";
+        HCCL_INFO("[SelectAlgfor91093] allreduce SelectAlgfor91093 algName [%s].", algName.c_str());
+        return HCCL_SUCCESS;
+    }
+
     if (IsNeedStrictMode(param)) {
         CHK_PRT_RET(!CheckStrictCondition(param), 
             HCCL_ERROR("[AllReduceOperator][SelectAlgfor91093] not support DETERMINISTIC_STRICT mode."),
             HCCL_E_NOT_SUPPORT);
 
         algName = "AllReduceOrderPreservedFor91093Executor";
-        HCCL_INFO("[SelectAlgfor91093] allreduce SelectAlgfor91093 algName [%s].", algName.c_str());
-        return HCCL_SUCCESS;
-    }
-
-    if (isSupportAivDeter) {
-        algName = "AllReduceMeshAivFor91093Executor";
         HCCL_INFO("[SelectAlgfor91093] allreduce SelectAlgfor91093 algName [%s].", algName.c_str());
         return HCCL_SUCCESS;
     }
