15e37b2d426a51ebefc50910e98b9d417f65c353
acjr0011 <chenjiarong6@huawei.com>
2026-03-21 09:45:51 +0800

    1、aclgrpah no fastlunch in ccu. 2、aclgraph profiling need cache

--- src/legacy/framework/communicator/communicator_impl.h
+++ src/legacy/framework/communicator/communicator_impl.h
@@ -592,7 +592,6 @@ private:
     HcclResult GetTilingAccelerator(void *mc2Tiling, AcceleratorState& acceleratorState) const;
 
     // AICPU场景aclgraph专用
-    bool IsOpSupportZeroCopyAlg(const CollOpParams &opParams, const rtStream_t stream) const;
     HcclResult OffloadResourcePre(std::string &opTag, const CollOpParams &opParams);
 };
 } // namespace Hccl
