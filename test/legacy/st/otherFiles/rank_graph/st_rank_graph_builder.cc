224823fa17c588c155fa536207a822a7f5e50da0
WujingChen <chenwujing@h-partners.com>
2026-03-23 11:53:04 +0800

    check ranktable ip address

--- test/legacy/st/otherFiles/rank_graph/st_rank_graph_builder.cc
+++ test/legacy/st/otherFiles/rank_graph/st_rank_graph_builder.cc
@@ -43,6 +43,7 @@ protected:
  
     virtual void SetUp() {
         std::cout << "A Test case in RankGraphBuilderTest SetUP" << std::endl;
+        MOCKER_CPP(&RankTableInfo::CheckAddrs).stubs().with().will(ignoreReturnValue());
     }
  
     virtual void TearDown() {
