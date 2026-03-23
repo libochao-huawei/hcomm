e34a0a7daae9cb5582f54e1104334e2475324df6
WujingChen <chenwujing@h-partners.com>
2026-03-23 11:53:04 +0800

    check ranktable ip address

--- src/legacy/framework/entrance/hcom_comm/comm_manager.cc
+++ src/legacy/framework/entrance/hcom_comm/comm_manager.cc
@@ -92,8 +92,8 @@ HcclResult CallSingletons()
         }
         
         // 不同通信域初始化方式时序不同，hdc manager 重复 init 内部会跳过
-        HccpHdcManager::GetInstance();
-        HccpPeerManager::GetInstance(); // host网卡需要拉起peer模式hccp
+        HccpHdcManager::GetInstance().Init(deviceLogicId);
+        HccpPeerManager::GetInstance().Init(deviceLogicId); // host网卡需要拉起peer模式hccp
         HccpTlvHdcManager::GetInstance();
         RdmaHandleManager::GetInstance();
         InnerNetDevManager::GetInstance();
