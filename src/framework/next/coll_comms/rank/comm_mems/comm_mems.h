74b16a0a9c9b0b5d0b48c0eb0a9a25f8245e8711
renyumin <renyumin1@huawei.com>
2026-03-24 20:07:56 +0800

    ut

--- src/framework/next/coll_comms/rank/comm_mems/comm_mems.h
+++ src/framework/next/coll_comms/rank/comm_mems/comm_mems.h
@@ -82,8 +82,8 @@ public:
     HcclResult CommUnregMem(const std::string& tag, const void* rawHandle);
     HcclResult GetTagMemoryHandles(void** memHandles, uint32_t memHandleNum, std::vector<HcclMem> &mem, 
         std::vector<std::string> &memTag);
-    HcclResult SetMemHandles(HcommChannelDesc &hcommDesc, std::vector<MemHandle> &memHandleVec,
-        std::vector<CommMemHandle> &commMemHandles) const;
+    HcclResult SetMemHandles(HcommChannelDesc &hcommDesc, const std::vector<MemHandle> &memHandleVec,
+        std::vector<std::unique_ptr<CommMemHandle>> &commMemHandles) const;
 
 private:
     uint64_t bufferSize_{};
