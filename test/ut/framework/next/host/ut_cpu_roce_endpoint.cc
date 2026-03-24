194437b69001f1a332eb1da10af06b7b02f586e5
wengzhuohao <wengzhuohao@h-partners.com>
2026-03-24 16:38:08 +0800

    增加ut测试

--- test/ut/framework/next/host/ut_cpu_roce_endpoint.cc
+++ test/ut/framework/next/host/ut_cpu_roce_endpoint.cc
@@ -69,7 +69,7 @@ TEST_F(CpuRoceEndpointTest, Ut_When_wrongIp_EXPECT_Return_128003)
     endpointDesc.commAddr.addr = localIp.GetBinaryAddress().addr;
     endpointDesc.loc.locType = ENDPOINT_LOC_TYPE_DEVICE;
     void* endpointHandle{nullptr};
-    MOCKER(&Hccl::RdmaHandleManager::GetByIp).stubs().will(Throw(Hccl::NetworkApiException));
+    MOCKER(&Hccl::RdmaHandleManager::GetByIp).stubs().will(Throw(Hccl::NetworkApiException("error")));
     HcclResult ret = HcommEndpointCreate(&endpointDesc, &endpointHandle);
     EXPECT_EQ(ret, 128003);
 }
