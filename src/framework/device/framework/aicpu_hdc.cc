521c9241fc748b8f38a918f3e87848dd7bc510bb
gcw_TwqkoH55 <shengsiyuan1@huawei.com>
2026-03-26 10:16:10 +0800

    support partial opretry for AICPU in A3

--- src/framework/device/framework/aicpu_hdc.cc
+++ src/framework/device/framework/aicpu_hdc.cc
@@ -58,6 +58,23 @@ HcclResult AicpuHdc::SetOpExecStatus(std::shared_ptr<HDCommunicate> d2hTransfer,
     return d2hTransfer->Put(0, sizeof(status.opId) + sizeof(status.execStatus), reinterpret_cast<uint8_t *>(&status));
 }
 
+HcclResult AicpuHdc::SetOpExecStatus(std::shared_ptr<HDCommunicate> d2hTransfer, KfcStatus state,
+    KfcError errorCode, u32 retryCount, bool isEnablePartialOpRetry)
+{
+    if (!d2hTransfer) {
+        return HCCL_SUCCESS;
+    }
+    KfcExecStatus status;
+    status.execStatus.kfcStatus = state;
+    status.execStatus.kfcError = errorCode;
+    status.execStatus.retryInfo.retryCount = retryCount;
+    status.execStatus.retryInfo.isEnablePartialOpRetry = isEnablePartialOpRetry;
+    HCCL_INFO("SetOpExecStatus: state[%u] errorCode[%u] retryCount[%u] isEnablePartialOpRetry[%d].",
+        state, errorCode, retryCount, isEnablePartialOpRetry);
+    return d2hTransfer->Put(sizeof(status.opId), sizeof(status.execStatus),
+        reinterpret_cast<uint8_t *>(&status.execStatus));
+}
+
 HcclResult AicpuHdc::GetOpExecCtrlTargetOp(std::shared_ptr<HDCommunicate> h2dTransfer, HcclOpIdentifier &opId)
 {
     if (!h2dTransfer) {
