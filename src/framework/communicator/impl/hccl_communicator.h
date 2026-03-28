f00943837cd956e1e4b982812a284ad5c4d9753a
wangkl <wangkailang1@huawei.com>
2026-03-28 18:07:49 +0800

    fix cleancode

--- src/framework/communicator/impl/hccl_communicator.h
+++ src/framework/communicator/impl/hccl_communicator.h
@@ -605,14 +605,18 @@ private:
     HcclResult SwitchNic(uint32_t nRanks, uint32_t *ranks, bool *useBackup,
         std::shared_ptr<HDCommunicate> &controlH2D, std::shared_ptr<HDCommunicate> &statusD2H);
     HcclResult SaveRankInfoHasLinked(const AlgResourceRequest& resRequest);
-    HcclResult RecordOpPara(HcclCMDType opType, OpParam &opParam);
+    HcclResult RecordOpPara(HcclCMDType opType, const OpParam &opParam);
     HcclResult SaveTopoDesc(std::string &identifier);
 
     HcclResult SetAicpuUnfoldFlag();
     bool GetAicpuUnfoldFlag();
 
-    HcclResult ReAllocScratchMemForAlltoall(HcclCMDType opType, OpParam &opParam,
+    HcclResult ReAllocScratchMemForAlltoall(HcclCMDType opType, const OpParam &opParam,
         AlgResourceRequest &resRequest, AlgResourceResponse &algResResponse);
+
+    HcclResult HandleExistAlgResource(const std::string& newTag, const std::string& algName,
+        HcclCMDType opType, const OpParam& opParam, bool selectAivAlg, bool aicpuUnfoldModeFor910B,
+        bool needRecreateAlltoallComm);
     u32 deviceNumPerServer_;
     HcclDispatcher dispatcher_; // dispatcher放到最后析构
     DispatcherCtxPtr dispatcherCtx_{nullptr};
