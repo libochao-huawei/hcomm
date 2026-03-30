be304147f47faa0fdc11879c4ae622fa810b31bb
suchengliu <liujiyuan6@huawei.com>
2026-03-30 15:59:19 +0800

    nda control

---
+++ src/framework/next/comms/endpoint_pairs/channels/aicpu/device/aicpu_ts_roce_channel_lite.cc
@@ -0,0 +1,244 @@
+/**
+ * Copyright (c) 2025 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+#include "aicpu_ts_roce_channel_lite.h"
+
+ namespace Hccl {
+
+AicpuTsRoceChannelLite::AicpuTsRoceChannelLite(std::vector<char> &uniqueId)
+{
+    BinaryStream binaryStream(uniqueId);
+    binaryStream >> notifyNum_;
+    binaryStream >> bufferNum_;
+    binaryStream >> connNum_;
+ 
+    std::vector<char> locNotifyUniqueIds;
+    binaryStream >> locNotifyUniqueIds;
+    ParseLocNotifyVec(locNotifyUniqueIds);
+
+    std::vector<char> rmtNotifyUniqueIds;
+    binaryStream >> rmtNotifyUniqueIds;
+    ParseRmtNotifyVec(rmtNotifyUniqueIds);
+
+    std::vector<char> notifyValueBufferUniqueIds;
+    binaryStream >> notifyValueBufferUniqueIds;
+    ParseNotifyValueBuffer(notifyValueBufferUniqueIds);
+ 
+    std::vector<char> locBufferUniqueIds;
+    binaryStream >> locBufferUniqueIds;
+    ParseLocBufferVec(locBufferUniqueIds);
+ 
+    std::vector<char> rmtBufferUniqueIds;
+    binaryStream >> rmtBufferUniqueIds;
+    ParseRmtBufferVec(rmtBufferUniqueIds);
+
+    std::vector<char> connUniqueIds;
+    binaryStream >> connUniqueIds;
+    ParseConnVec(connUniqueIds);
+}
+
+AicpuTsRoceChannelLite::~AicpuTsRoceChannelLite() {}
+
+void AicpuTsRoceChannelLite::ParseLocNotifyVec(std::vector<char> &data)
+{
+    if (notifyNum_ == 0) {
+        HCCL_WARNING("[AicpuTsRoceChannelLite::%s] notifyNum is 0", __func__);
+        return;
+    }
+
+    u32 notifySizePerDto = data.size() / notifyNum_;
+
+    for (u32 idx = 0; idx < notifyNum_; idx++) {
+        auto              start = data.begin() + idx * notifySizePerDto;
+        auto              end   = start + notifySizePerDto;
+        std::vector<char> dto(start, end);
+        localNotifies_.push_back(std::make_unique<NotifyLite>(dto));
+        HCCL_INFO("locNotify idx=%u, %s", idx, localNotifies_.back()->Describe().c_str());
+    }
+}
+
+void AicpuTsRoceChannelLite::ParseRmtNotifyVec(std::vector<char> &data)
+{
+    if (notifyNum_ == 0) {
+        HCCL_WARNING("[AicpuTsRoceChannelLite::%s] notifyNum is 0", __func__);
+        return;
+    }
+
+    u32 rmtBufferSizePerDto = data.size() / notifyNum_;
+    HCCL_INFO("[AicpuTsRoceChannelLite::%s] Parse remote notify num=%u, sizePerDto=%u",
+        __func__, notifyNum_, rmtBufferSizePerDto);
+
+    BinaryStream binaryStream(data);
+    remoteNotifies_.clear();
+    u64 addr;
+    u64 size;
+    u32 rkey;
+    for (u32 idx = 0; idx < notifyNum_; idx++) {
+        binaryStream >> addr;
+        binaryStream >> size;
+        binaryStream >> rkey;
+        RmtRmaBufferLite rdmaBufLite(addr, size, rkey);
+        HCCL_INFO("idx=%u, %s", idx, rdmaBufLite.Describe().c_str());
+        remoteNotifies_.emplace_back(rdmaBufLite);
+    }
+}
+
+void AicpuTsRoceChannelLite::ParseNotifyValueBuffer(std::vector<char> &data)
+{
+    HCCL_INFO("[AicpuTsRoceChannelLite::%s] Parse notify value buffer",  __func__);
+
+    BinaryStream binaryStream(data);
+    u64 addr;
+    u64 size;
+    u32 lkey;
+    binaryStream >> addr;
+    binaryStream >> size;
+    binaryStream >> lkey;
+    notifyValueBuffer_ = std::make_unique<RmaBufferLite>(addr, size, lkey);
+
+}
+
+void AicpuTsRoceChannelLite::ParseLocBufferVec(std::vector<char> &data)
+{
+    if (bufferNum_ == 0) {
+        HCCL_WARNING("[AicpuTsRoceChannelLite::%s] bufferNum is 0", __func__);
+        return;
+    }
+
+    u32 locBufferSizePerDto = data.size() / bufferNum_;
+    HCCL_INFO("[AicpuTsRoceChannelLite::%s] Parse local buffer num=%u, sizePerDto=%u",
+        __func__, bufferNum_, locBufferSizePerDto);
+
+    BinaryStream binaryStream(data);
+    locBufferVec_.clear();
+    u64 addr;
+    u64 size;
+    u32 lkey;
+    for (u32 idx = 0; idx < bufferNum_; idx++) {
+        binaryStream >> addr;
+        binaryStream >> size;
+        binaryStream >> lkey;
+        RmaBufferLite rdmaBufLite(addr, size, lkey);
+        HCCL_INFO("idx=%u, %s", idx, rdmaBufLite.Describe().c_str());
+        locBufferVec_.emplace_back(rdmaBufLite);
+    }
+}
+
+void AicpuTsRoceChannelLite::ParseRmtBufferVec(std::vector<char> &data)
+{
+    if (bufferNum_ == 0) {
+        HCCL_WARNING("[AicpuTsRoceChannelLite::%s] bufferNum is 0", __func__);
+        return;
+    }
+
+    u32 rmtBufferSizePerDto = data.size() / bufferNum_;
+    HCCL_INFO("[AicpuTsRoceChannelLite::%s] Parse remote buffer num=%u, sizePerDto=%u",
+        __func__, bufferNum_, rmtBufferSizePerDto);
+
+    BinaryStream binaryStream(data);
+    rmtBufferVec_.clear();
+    u64 addr;
+    u64 size;
+    u32 rkey;
+    for (u32 idx = 0; idx < bufferNum_; idx++) {
+        binaryStream >> addr;
+        binaryStream >> size;
+        binaryStream >> rkey;
+        RmtRmaBufferLite rdmaBufLite(addr, size, rkey);
+        HCCL_INFO("idx=%u, %s", idx, rdmaBufLite.Describe().c_str());
+        rmtBufferVec_.emplace_back(rdmaBufLite);
+    }
+}
+
+void AicpuTsRoceChannelLite::ParseConnVec(std::vector<char> &data)
+{
+    if (connNum_ == 0) {
+        HCCL_WARNING("[AicpuTsRoceChannelLite::%s] connNum is 0", __func__);
+        return;
+    }
+
+    u32 connSizePerDto = data.size() / connNum_;
+    HCCL_INFO("[AicpuTsRoceChannelLite::%s] Parse conn num=%u, sizePerDto=%u",
+        __func__, connNum_, connSizePerDto);
+    for (u32 idx = 0; idx < connNum_; idx++) {
+        auto              start = data.begin() + idx * connSizePerDto;
+        auto              end   = start + connSizePerDto;
+        std::vector<char> connUniqueId(start, end);
+        connUniqueIdVec_.emplace_back(connUniqueId);
+        // ljy:不确定rdnaConn的key，暂不用rdmaConnLiteMgr管理
+        std::unique_ptr<RdmaConnLite> connLite;
+        connLite = std::make_unique<RdmaConnLite>(connUniqueId);
+        connVec_.emplace_back(std::move(connLite));
+        HCCL_INFO("[AicpuTsRoceChannelLite::%s] idx=%u, %s", __func__, idx, connLite->Describe().c_str());
+    }
+}
+
+RmaBufSliceLite AicpuTsRoceChannelLite::GetRmaBufSlicelite(const RmaBufferLite &lite) const
+{
+    // rdma conn lite 不关心tokenId , tokenId 设定为0
+    return RmaBufSliceLite(lite.GetAddr(), lite.GetSize(), lite.GetLkey(), 0);
+}
+
+RmtRmaBufSliceLite AicpuTsRoceChannelLite::GetRmtRmaBufSliceLite(const RmtRmaBufferLite &lite) const
+{
+    // rdma conn lite 不关心tokenId , tokenId 设定为0
+    return RmtRmaBufSliceLite(lite.GetAddr(), lite.GetSize(), lite.GetRkey(), 0, 0);
+}
+
+RmtRmaBufSliceLite AicpuTsRoceChannelLite::GetRmtNotifySliceLite(u32 index) const
+{
+    auto &lite = remoteNotifies_[index];
+    // rdma conn lite 不关心tokenId , tokenId 设定为0
+    return RmtRmaBufSliceLite(lite.GetAddr(), lite.GetSize(), lite.GetRkey(), 0, 0);
+}
+
+std::string AicpuTsRoceChannelLite::Describe() const
+{
+    std::string desc = "AicpuTsRoceChannelLite[";
+
+    u32 idx = 0;
+    desc += "localNotifies=[";
+    for (auto &it : localNotifies_) {
+        desc += StringFormat("idx=%u, %u;", idx, it->Describe().c_str());
+        idx++;
+    }
+
+    idx = 0;
+    desc += "], remoteNotifies=[";
+    for (auto &it : remoteNotifies_) {
+        desc += StringFormat("idx=%u, %u;", idx, it.Describe().c_str());
+        idx++;
+    }
+
+    idx = 0;
+    desc += "], locBufferVec=[";
+    for (auto &it : locBufferVec_) {
+        desc += StringFormat("idx=%u, %s;", idx, it.Describe().c_str());
+        idx++;
+    }
+
+    idx = 0;
+    desc += "], rmtBufferVec=[";
+    for (auto &it : rmtBufferVec_) {
+        desc += StringFormat("idx=%u, %s;", idx, it.Describe().c_str());
+        idx++;
+    }
+
+    idx = 0;
+    desc += "], connVec=[";
+    for (auto &it : connVec_) {
+        desc += StringFormat("idx=%u, %s;", idx, it->Describe().c_str());
+        idx++;
+    }
+
+    desc += "]]";
+    return desc;
+}
+}
---
+++ src/framework/next/comms/endpoint_pairs/channels/aicpu/device/dev_rdma_conn_lite.h
@@ -0,0 +1,58 @@
+/**
+ * Copyright (c) 2025 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+#ifndef HCCLV2_RDMA_CONN_LITE_H_
+#define HCCLV2_RDMA_CONN_LITE_H_
+
+#include "binary_stream.h"
+#include "exception_util.h"
+#include "not_support_exception.h"
+#include "log.h"
+
+struct RdmaSqContextLite {
+    uint32_t qpn;
+    uint64_t sqVa;
+    uint32_t wqeSize;
+    uint32_t depth;
+    uint64_t headAddr;
+    uint64_t tailAddr;
+    uint8_t sl;
+    uint64_t dbVa;
+    int8_t dbMode; // 0-hw/1-sw
+};
+
+struct RdmaCqContextLite{
+    uint32_t cqn;
+    uint64_t cqVa;
+    uint32_t cqeSize;
+    uint32_t cqDepth;
+    uint64_t headAddr;
+    uint64_t tailAddr;
+    uint64_t dbVa;
+    int8_t dbMode; // 0-hw/1-sw
+};
+
+namespace Hccl {
+class RdmaConnLite {
+public:
+    explicit RdmaConnLite(std::vector<char>& uniqueId);
+    ~RdmaConnLite();
+
+    std::string Describe();
+
+private:
+    uint32_t            dmaMode_{0};
+    RdmaSqContextLite   sqContext{};
+    RdmaCqContextLite   cqContext{};
+};
+
+} // namespace Hccl
+
+#endif
