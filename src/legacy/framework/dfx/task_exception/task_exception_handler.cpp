56b282f2d6166feb062000a5a15a54903a6f7daf
linyixin4 <linyixin4@huawei.com>
2026-03-30 11:57:57 +0800

    aicpu dfx

--- src/legacy/framework/dfx/task_exception/task_exception_handler.cpp
+++ src/legacy/framework/dfx/task_exception/task_exception_handler.cpp
@@ -352,13 +352,8 @@ void TaskExceptionHandler::ProcessException(rtExceptionInfo_t* exceptionInfo, co
     HCCL_ERROR("[TaskExceptionHandler][%s]Task run failed, groupRank information is %s.", __func__,
         GetGroupRankInfo(taskInfo).c_str());
     auto count = GetOpCounter(taskInfo);
-<<<<<<< HEAD
  	HCCL_ERROR("[TaskExceptionHandler][%s]Task run failed, headOpCounter[%u] tailOpCounter[%u] opIndex[%u].", __func__, static_cast<u32>(count.first), static_cast<u32>(count.second), taskInfo.dfxOpInfo_->opIndex_);
     HCCL_ERROR("[TaskExceptionHandler][%s]Task run failed, opData information is %s.", __func__, taskInfo.GetOpInfo().c_str());
-=======
-    HCCL_ERROR("[TaskExceptionHandler]Task run failed, headOpCounter[%u] tailOpCounter[%u] opIndex[%u].", static_cast<u32>(count.first), static_cast<u32>(count.second), taskInfo.dfxOpInfo_->opIndex_);
-    HCCL_ERROR("[TaskExceptionHandler]Task run failed, opData information is %s.", taskInfo.GetOpInfo().c_str());
->>>>>>> a85552b1... opindex fix
 }
 
 void TaskExceptionHandler::PrintTaskContextInfo(uint32_t deviceId, uint32_t streamId, uint32_t taskId)
