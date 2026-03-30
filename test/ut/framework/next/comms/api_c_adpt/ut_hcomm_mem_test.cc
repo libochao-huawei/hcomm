0626470dfb5a8cc96d9fb066e20462da682462db
yanzhenghang <625612333@qq.com>
2026-03-30 10:30:38 +0800

    fix ut

--- test/ut/framework/next/comms/api_c_adpt/ut_hcomm_mem_test.cc
+++ test/ut/framework/next/comms/api_c_adpt/ut_hcomm_mem_test.cc
@@ -29,7 +29,7 @@ TEST_F(TestHcommMem, Ut_TestHcommMemReg_When_InvalidHandle_Return_HCCL_E_NOT_FOU
     void* memHandle = nullptr;
 
     EndpointHandle invalidHandle = reinterpret_cast<EndpointHandle>(0xFFFFFFFFFFFFFFFF);
-    HcclResult ret = HcommMemReg(invalidHandle, "test_mem", mem, &memHandle);
+    HcommResult ret = HcommMemReg(invalidHandle, "test_mem", mem, &memHandle);
     EXPECT_EQ(ret, HCCL_E_NOT_FOUND);
 
     free(mem.addr);
@@ -42,7 +42,7 @@ TEST_F(TestHcommMem, Ut_TestHcommMemReg_When_MemHandleNullptr_Return_HCCL_E_PTR)
     mem.size = 1024;
     mem.type = HCCL_MEM_TYPE_HOST;
 
-    HcclResult ret = HcommMemReg(nullptr, "test_mem", mem, nullptr);
+    HcommResult ret = HcommMemReg(nullptr, "test_mem", mem, nullptr);
     EXPECT_EQ(ret, HCCL_E_PTR);
 
     free(mem.addr);
@@ -51,7 +51,7 @@ TEST_F(TestHcommMem, Ut_TestHcommMemReg_When_MemHandleNullptr_Return_HCCL_E_PTR)
 TEST_F(TestHcommMem, Ut_TestHcommMemUnreg_When_InvalidHandle_Return_HCCL_E_PTR)
 {
     EndpointHandle invalidHandle = reinterpret_cast<EndpointHandle>(0xFFFFFFFFFFFFFFFF);
-    HcclResult ret = HcommMemUnreg(invalidHandle, nullptr);
+    HcommResult ret = HcommMemUnreg(invalidHandle, nullptr);
     EXPECT_EQ(ret, HCCL_E_PTR);
 }
 
@@ -59,31 +59,31 @@ TEST_F(TestHcommMem, Ut_TestHcommMemExport_When_InvalidMemHandle_Return_HCCL_E_P
 {
     void* memDesc = nullptr;
     uint32_t memDescLen = 0;
-    HcclResult ret = HcommMemExport(nullptr, nullptr, &memDesc, &memDescLen);
+    HcommResult ret = HcommMemExport(nullptr, nullptr, &memDesc, &memDescLen);
     EXPECT_EQ(ret, HCCL_E_PTR);
 }
 
 TEST_F(TestHcommMem, Ut_TestHcommMemExport_When_OutputNullptr_Return_HCCL_E_PTR)
 {
-    HcclResult ret = HcommMemExport(nullptr, this, nullptr, nullptr);
+    HcommResult ret = HcommMemExport(nullptr, this, nullptr, nullptr);
     EXPECT_EQ(ret, HCCL_E_PTR);
 }
 
 TEST_F(TestHcommMem, Ut_TestHcommMemImport_When_InvalidDesc_Return_HCCL_E_PTR)
 {
     HcommMem outMem;
-    HcclResult ret = HcommMemImport(nullptr, nullptr, 0, &outMem);
+    HcommResult ret = HcommMemImport(nullptr, nullptr, 0, &outMem);
     EXPECT_EQ(ret, HCCL_E_PTR);
 }
 
 TEST_F(TestHcommMem, Ut_TestHcommMemUnimport_When_InvalidParams_Return_HCCL_E_PTR)
 {
-    HcclResult ret = HcommMemUnimport(nullptr, nullptr, 0);
+    HcommResult ret = HcommMemUnimport(nullptr, nullptr, 0);
     EXPECT_EQ(ret, HCCL_E_PTR);
 }
 
 TEST_F(TestHcommMem, Ut_TestHcommMemGetAllMemHandles_When_Nullptr_Return_HCCL_E_PTR)
 {
-    HcclResult ret = HcommMemGetAllMemHandles(nullptr, nullptr, nullptr);
+    HcommResult ret = HcommMemGetAllMemHandles(nullptr, nullptr, nullptr);
     EXPECT_EQ(ret, HCCL_E_PTR);
 }
