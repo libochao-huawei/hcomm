1442fab64458f33d2ec1ca7fd80aafcb987e8efa
tianmingL <liutianming2@h-partners.com>
2026-03-31 10:14:21 +0800

    fix build.sh

--- build.sh
+++ build.sh
@@ -337,11 +337,11 @@ function run_ut() {
   if [[ "X$ENABLE_UT" = "Xon" ]]; then
     local ut_dir="${BUILD_DIR}/test"
     echo "ut_dir = ${ut_dir}"
-    python3 ${CURRENT_DIR}/run_ut.py --test-dir ${ut_dir}
-    if [ $? -ne 0 ]; then
-      echo "[ERROR] run_ut.py 执行失败"
-      exit 1
-    fi
+    find "$ut_dir" -type f -executable | while read -r ut_exec; do
+        filename=$(basename "$ut_exec")
+        echo "Executing: $filename"
+        ${ut_exec}
+    done
   else
     echo "Unit tests is not enabled, sh build.sh with parameter -u or --ut to enable it"
   fi
