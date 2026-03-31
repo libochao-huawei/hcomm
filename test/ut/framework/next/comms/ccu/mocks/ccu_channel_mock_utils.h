4f4d10373d33bb67f742ee9b67cfc1dd35799433
one_random <shibingchen@huawei.com>
2026-03-31 17:40:19 +0800

    bugfix(ccu): up-to-date

--- test/ut/framework/next/comms/ccu/mocks/ccu_channel_mock_utils.h
+++ test/ut/framework/next/comms/ccu/mocks/ccu_channel_mock_utils.h
@@ -94,16 +94,16 @@ std::unique_ptr<Hccl::LocalUbRmaBuffer> MockUbRmaBuffer()
     return make_unique<Hccl::LocalUbRmaBuffer>(localBuffer);
 }
 
-CommMemInfo MockCommMemInfo(Hccl::LocalUbRmaBuffer *bufferPtr)
+CommMemHandle MockCommMemHandle(Hccl::LocalUbRmaBuffer *bufferPtr)
 {
-    CommMemInfo memInfo{};
-    auto &mem = memInfo.mem;
-    mem.type = CommMemType::COMM_MEM_TYPE_DEVICE;
-    mem.addr = reinterpret_cast<void*>(bufferPtr->GetAddr());
-    mem.size = bufferPtr->GetSize();
+    CommMemHandle memHandle{};
+    memHandle.addr = reinterpret_cast<void*>(bufferPtr->GetAddr());
+    memHandle.size = bufferPtr->GetSize();
+    memHandle.memType = CommMemType::COMM_MEM_TYPE_DEVICE;
+    memHandle.bufferHandle = static_cast<void *>(bufferPtr);
+    memHandle.memTag = "FakeCommBuffer";
 
-    memInfo.bufferHandle = static_cast<void *>(bufferPtr);
-    return memInfo;
+    return memHandle;
 }
 
 HcclResult MockCcuConnectionImportJettys(hcomm::CcuConnection *This)
@@ -136,9 +136,6 @@ HcclResult MockCcuTransportGetRmtXn(hcomm::CcuTransport *This, const uint32_t in
 void MockCcuChannelGetRes()
 {
     MOCKER_CPP(&hcomm::CcuConnection::ImportJetty).stubs().will(invoke(MockCcuConnectionImportJettys));
-    MOCKER_CPP(&hcomm::CcuTransport::RecvDataSize).stubs().will(returnValue(HcclResult::HCCL_SUCCESS));
-    MOCKER_CPP(&hcomm::CcuTransport::RecvConnAndTransInfo).stubs().will(returnValue(HcclResult::HCCL_SUCCESS));
-    MOCKER_CPP(&hcomm::CcuTransport::RecvDataProcess).stubs().will(returnValue(HcclResult::HCCL_SUCCESS));
     MOCKER_CPP(&hcomm::CcuTransport::CheckFinish).stubs().will(returnValue(HcclResult::HCCL_SUCCESS));
     MOCKER_CPP(&hcomm::CcuTransport::GetRmtCkeByIndex).stubs().will(invoke(MockCcuTransportGetRmtCke));
     MOCKER_CPP(&hcomm::CcuTransport::GetRmtXnByIndex).stubs().will(invoke(MockCcuTransportGetRmtXn));
