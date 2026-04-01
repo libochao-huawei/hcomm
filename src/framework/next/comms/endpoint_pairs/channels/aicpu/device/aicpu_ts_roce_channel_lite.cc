8032af9deb40798aa96fee053f5bc4146ec4fdf5
zhu-wangyi <zhuwangyi@huawei.com>
2026-04-01 10:08:47 +0800

    nda data api

--- src/framework/next/comms/endpoint_pairs/channels/aicpu/device/aicpu_ts_roce_channel_lite.cc
+++ src/framework/next/comms/endpoint_pairs/channels/aicpu/device/aicpu_ts_roce_channel_lite.cc
@@ -241,4 +241,47 @@ std::string AicpuTsRoceChannelLite::Describe() const
     desc += "]]";
     return desc;
 }
+
+// 下发Rtsq sqe, 敲DB
+void AicpuTsRoceChannelLite::BuildRdmaDbSendTask(const StreamLite &stream, u64 remoteAddr, u64 dbValue)
+{
+    stream.GetRtsq()->RdmaDbSend(remoteAddr, dbValue);
+}
+
+// 下发Rtsq sqe, NotifyWait
+void AicpuTsRoceChannelLite::BuildNotifyWaitTask(const StreamLite &stream, u32 notifyId)
+{
+    stream.GetRtsq()->NotifyWait(notifyId);
+}
+
+void AicpuTsRoceChannelLite::Write(const RmaBufferLite &loc, const RmtRmaBufferLite &rmt, const StreamLite &stream) {
+    // Post Wqe && return dbValue
+    u64 dbAddr = 0;
+    u64 dbValue = 0;
+    connVec_[0]->Write(
+        GetRmaBufSlicelite(loc), GetRmtRmaBufSliceLite(rmt), dbAddr, dbValue);
+
+    // Ring Doorbell
+    BuildRdmaDbSendTask(stream, dbAddr, dbValue);
+}
+
+void AicpuTsRoceChannelLite::WriteWithNotify(const RmaBufferLite &loc, const RmtRmaBufferLite &rmt, const uint32_t remoteNotifyIdx, const StreamLite &stream) {
+    // Post Wqe && return dbValue
+    u64 dbAddr = 0;
+    u64 dbValue = 0;
+
+    // TODO 此处localNotify获取接口未添加？
+    connVec_[0]->WriteWithNotify(
+        GetRmaBufSlicelite(loc), GetRmtRmaBufSliceLite(rmt),
+        GetRmaBufSlicelite(loc), GetRmtNotifySliceLite(remoteNotifyIdx), dbAddr, dbValue);
+
+    // Ring Doorbell
+    BuildRdmaDbSendTask(stream, dbAddr, dbValue);
+}
+
+void AicpuTsRoceChannelLite::NotifyWait(const uint32_t index, const StreamLite &stream) {
+    auto notifyId = localNotifies_[index]->GetId();
+    BuildNotifyWaitTask(stream, notifyId);
+}
+
 }
