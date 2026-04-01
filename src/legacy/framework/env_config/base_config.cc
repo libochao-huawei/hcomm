adf970541fdaa304216cdf073bdcc72bebdfdc78
BigGitMan <huangteng13@huawei.com>
2026-04-01 11:13:17 +0800

    deleteDetour

--- src/legacy/framework/env_config/base_config.cc
+++ src/legacy/framework/env_config/base_config.cc
@@ -217,15 +217,4 @@ const DfsConfig &EnvLogConfig::GetDfsConfig() const
     return dfsConfig.Get();
 }
 
-// EnvDetourConfig
-
-void EnvDetourConfig::Parse()
-{
-}
-
-HcclDetourType EnvDetourConfig::GetDetourType() const
-{
-    return HcclDetourType::HCCL_DETOUR_DISABLE;
-}
-
 } // namespace Hccl
