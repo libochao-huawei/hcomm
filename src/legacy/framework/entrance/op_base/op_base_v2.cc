7d4f575dde2a15ebb33a57a987db34ba6dfc0a17
Helmar <zhiweihai@huawei.com>
2026-04-03 00:47:09 +0800

    update: 更新文件 op_base_v2.cc
    Signed-off-by: Helmar <zhiweihai@huawei.com>

--- src/legacy/framework/entrance/op_base/op_base_v2.cc
+++ src/legacy/framework/entrance/op_base/op_base_v2.cc
@@ -2342,6 +2342,9 @@ HcclResult HcclBatchSendRecvV2(HcclSendRecvItem *sendRecvInfo, uint32_t itemNum,
     CHK_PRT_RET(itemNum == 0, HCCL_WARNING("[BatchSendRecv] taskList itemNum is zero."), HCCL_SUCCESS);
     CHK_RET(GetStreamCaptureInfo(stream, rtModel, isCapture));
 
+    /* 根据ranksize校验相关入参 */
+    u32 rankSize = 0;
+    CHK_RET(communicator->GetRankSize(&rankSize));
     for (uint32_t i = 0; i < itemNum; i++) {
         // 支持数据量为0的场景，buf为空的跳过
         if ((sendRecvInfo + i)->buf == nullptr) {
@@ -2350,8 +2353,8 @@ HcclResult HcclBatchSendRecvV2(HcclSendRecvItem *sendRecvInfo, uint32_t itemNum,
         // 校验DataType和count，与其他算子保持校验统一性
         CHK_RET(HcomCheckDataTypeV2((sendRecvInfo + i)->dataType));
         CHK_RET(HcomCheckCountV2((sendRecvInfo + i)->count));
+        CHK_RET(HcomCheckUserRankV2(rankSize, (sendRecvInfo + i)->remoteRank));
     }
-    CHK_RET(HcomCheckDataTypeV2(sendRecvInfo->dataType));
 
     /* 记录接口交互信息日志 */
     char stackLogBufferV2[LOG_TMPBUF_SIZE];
