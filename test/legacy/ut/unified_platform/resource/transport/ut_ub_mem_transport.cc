13bb3bb16b34cec5225350f5d1be5953f9c93781
renyumin <renyumin1@huawei.com>
2026-04-02 20:30:01 +0800

    ChannelReuse

--- test/legacy/ut/unified_platform/resource/transport/ut_ub_mem_transport.cc
+++ test/legacy/ut/unified_platform/resource/transport/ut_ub_mem_transport.cc
@@ -836,7 +836,7 @@ TEST_F(UbMemTransportTest, ut_UbMemTransport_UpdateMemInfo_When_Normal_Expect_Re
     EXPECT_EQ(rmtBuffer1->GetMemTag(), "buffer1");
 }
 
-TEST_F(UbMemTransportTest, ut_UbMemTransport_UpdateMemInfo_When_Timeout_Expect_ReturnIsHCCL_E_INTERNAL)
+TEST_F(UbMemTransportTest, ut_UbMemTransport_UpdateMemInfo_When_SocketTimeout_Expect_ReturnIsHCCL_E_INTERNAL)
 {
     BaseMemTransport::CommonLocRes    locRes;
     BaseMemTransport::Attribution     attr;
@@ -862,4 +862,21 @@ TEST_F(UbMemTransportTest, ut_UbMemTransport_UpdateMemInfo_When_Timeout_Expect_R
 
     HcclResult ret = transport.UpdateMemInfo(bufferVecTemp);
     EXPECT_EQ(ret, HCCL_E_INTERNAL);
+}
+
+TEST_F(UbMemTransportTest, ut_UbMemTransport_UpdateMemInfo_When_bufferNumIs0_Expect_ReturnIsHCCL_SUCCESS)
+{
+    BaseMemTransport::CommonLocRes    locRes;
+    BaseMemTransport::Attribution     attr;
+    BaseMemTransport::LocCntNotifyRes locCntRes;
+    LinkData                          link(BasePortType(PortDeploymentType::DEV_NET), 0, 1, 0, 1);
+    void                             *rdmaHandle = (void *)0x100;
+    IpAddress                         ipAddress("1.0.0.0");
+    Socket fakeSocket(nullptr, ipAddress, 100, ipAddress, "tag", SocketRole::SERVER, NicType::DEVICE_NIC_TYPE);
+
+    UbMemTransport transport(locRes, attr, link, fakeSocket, rdmaHandle, locCntRes, isRecvFirst);
+    std::vector<LocalRmaBuffer *> bufferVecTemp{};
+
+    HcclResult ret = transport.UpdateMemInfo(bufferVecTemp);
+    EXPECT_EQ(ret, HCCL_SUCCESS);
 }
