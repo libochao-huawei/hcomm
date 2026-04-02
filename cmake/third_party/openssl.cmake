08dabf32e339f4336ec7b253060109de6d7efbdf
xz00xz <xiongzhe5@huawei.com>
2026-04-02 21:38:52 +0800

    hccp fix ut

--- cmake/third_party/openssl.cmake
+++ cmake/third_party/openssl.cmake
@@ -35,7 +35,7 @@ set(OPENSSL_INCLUDE_DIR
 # 查找目录下是否已经安装，避免重复编译安装
 message(STATUS "[ThirdParty] OPENSSL_INSTALL_PATH=${OPENSSL_INSTALL_PATH}")
 find_path(CRYPTO_INCLUDE
-    NAMES crypto/x509.h
+    NAMES openssl/x509.h
     PATH_SUFFIXES include
     NO_CMAKE_SYSTEM_PATH
     NO_CMAKE_FIND_ROOT_PATH
