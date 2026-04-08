714949a28bf2fe939a2bbb3f8edd18f528d9acf9
zwlStateflow <zhengwenliang@huawei.com>
2026-04-08 15:57:18 +0800

    dpu stream core

--- src/legacy/framework/communicator/communicator_impl.cc
+++ src/legacy/framework/communicator/communicator_impl.cc
@@ -2401,11 +2401,12 @@ MirrorTaskManager &CommunicatorImpl::GetMirrorTaskManager() const
 CommunicatorImpl::~CommunicatorImpl()
 {
     HCCL_INFO("[~CommunicatorImpl] start CommunicatorImpl destroy, commId[%s]", id.c_str());
+    (void)DestroyDpuKernelResource();
+    g_taskServiceMap.erase(id);
+
     (void)NotifyAicpuDestroyComm();
     ccuDrvHandle = nullptr;
 
-    (void)DestroyDpuKernelResource();
-    g_taskServiceMap.erase(id);
     DeInitPreResource();
     HCCL_RUN_INFO("[~CommunicatorImpl] cclBuffer free, commId[%s] ", id.c_str());
 }
@@ -2423,7 +2424,7 @@ HcclResult CommunicatorImpl::DestroyDpuKernelResource()
         return HCCL_SUCCESS;
     }
 
-    CHK_RET(WaitDpuKernelThreadTerminate());
+    (void)WaitDpuKernelThreadTerminate();
 
     // 切换回 dpu ctx
     if (ACL_SUCCESS != aclrtSetCurrentContext(dpuContext)) {
@@ -2434,12 +2435,14 @@ HcclResult CommunicatorImpl::DestroyDpuKernelResource()
     HCCL_INFO("Destroy Stream");
     if (aclrtDestroyStreamForce(dpuStream) != ACL_SUCCESS) {
         HCCL_ERROR("Destroy Stream Failed");
+        (void)aclrtSetCurrentContext(npuContext);
         return HCCL_E_RUNTIME;
     }
     // reset DPU kernel 线程
     HCCL_INFO("Start to reset DPU device");
     if (HrtResetXpuDevice(TEMP_DEV_TYPE_DPU, 0) != HCCL_SUCCESS) {
         HCCL_ERROR("ResetXpuDevice Failed");
+        (void)aclrtSetCurrentContext(npuContext);
         return HCCL_E_RUNTIME;
     }
     // 切回 npu ctx
@@ -2469,7 +2472,7 @@ HcclResult CommunicatorImpl::WaitDpuKernelThreadTerminate()
         return HCCL_E_RUNTIME;
     }
     HcclUs        startTime                   = std::chrono::steady_clock::now();
-    constexpr u32 waitTransportReadyTimeoutMs = 200 * 1000; // 定义最大等待200秒
+    constexpr u32 waitTransportReadyTimeoutMs = 500 * 1000; // 定义最大等待500秒
     auto          timeout                     = std::chrono::milliseconds(waitTransportReadyTimeoutMs);
     do {
         if (std::chrono::steady_clock::now() - startTime >= timeout) {
