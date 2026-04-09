4fe1ec0c340f999b373d636da545f4d030b96671
ting010 <ting010@noreply.gitcode.com>
2026-04-09 04:50:50 -0400

    Mask support assignment operation

--- pkg_inc/hcomm/ccu_new/ccu_event.hpp
+++ pkg_inc/hcomm/ccu_new/ccu_event.hpp
@@ -8,43 +8,60 @@
  * See LICENSE in the root of the software repository for the full text of the License.
  */
 
-#ifndef CCU_EVENT_HPP
-#define CCU_EVENT_HPP
-
-#include <cstdint>
-#include <type_traits>
-
-#include "ccu_types.h"
-
-class CcuEvent final {
-public:
-    explicit CcuEvent() {}
-
-    CcuEvent(const CcuEvent& other) {
-        this->handle = other.handle;
-    }
-
-    void operator=(CcuEvent&& other) {
-        this->handle = other.handle;
-    }
-
-    void setMask(uint32_t mask) const;
-
-    CcuEventHandle handle{0};
-};
-
-static_assert(std::is_standard_layout<CcuEvent>::value,
-    "CcuEvent must be standard layout for .so ABI stability");
-static_assert(sizeof(CcuEvent) == sizeof(CcuEventHandle),
-    "CcuEvent layout changed - will break .so ABI!");
-
-extern "C" CcuResult CcuSetEventMask(CcuEvent event, uint32_t mask);
-
-inline void CcuEvent::setMask(uint32_t mask) const {
-    auto ret = CcuSetEventMask(*this, mask);
-    if (ret != CcuResult::CCU_SUCCESS) {
-        throw "todo: failed";
-    }
-}
-
-#endif // CCU_EVENT_HPP
+ #ifndef CCU_EVENT_HPP
+ #define CCU_EVENT_HPP
+ 
+ #include <cstdint>
+ #include <type_traits>
+ 
+ #include "ccu_types.h"
+ 
+ class CcuEvent;
+ 
+ class CcuEventMask {
+ public:
+     explicit CcuEventMask(CcuEventHandle* owner) : ownerHandle_(owner) {}
+     void operator=(uint32_t newMask) const;
+ private:
+     CcuEventHandle* ownerHandle_;
+ };
+ 
+ class CcuEvent final {
+ public:
+     explicit CcuEvent() : mask(&handle) {}
+ 
+     CcuEvent(const CcuEvent& other) : handle(other.handle), mask(&handle) {}
+ 
+     void operator=(CcuEvent&& other) {
+         this->handle = other.handle;
+     }
+ 
+     void setMask(uint32_t m) const;
+ 
+     CcuEventHandle handle{0};
+     CcuEventMask mask;
+ };
+ 
+ static_assert(std::is_standard_layout<CcuEvent>::value,
+     "CcuEvent must be standard layout for .so ABI stability");
+ 
+ extern "C" CcuResult CcuSetEventMask(CcuEvent event, uint32_t mask);
+ 
+ inline void CcuEvent::setMask(uint32_t m) const {
+     auto ret = CcuSetEventMask(*this, m);
+     if (ret != CcuResult::CCU_SUCCESS) {
+         throw "todo: failed";
+     }
+ }
+ 
+ inline void CcuEventMask::operator=(uint32_t newMask) const {
+     CcuEvent tmp;
+     tmp.handle = *ownerHandle_;
+     auto ret = CcuSetEventMask(tmp, newMask);
+     if (ret != CcuResult::CCU_SUCCESS) {
+         throw "todo: failed";
+     }
+ }
+ 
+ #endif // CCU_EVENT_HPP
+ 
