61117c239156a08d01b59202a53a2af63cf26741
leironghao1 <leironghao1@h-partners.com>
2026-04-10 15:32:34 +0800

    token快恢bug修复

--- test/ut/device/ut_aicpuu_one_side_service.cc
+++ test/ut/device/ut_aicpuu_one_side_service.cc
@@ -39,7 +39,6 @@ protected:
     }
     virtual void SetUp()
     {
-        // 清空全局 services_ 注册表，确保每个用例独立
         HcclOneSideServiceAicpu::services_.clear();
         std::cout << "A Test SetUp" << std::endl;
     }
@@ -58,32 +57,49 @@ TEST_F(OneSideServiceDeviceUT, CleanStreamFunc_WhenDisabled_ExecutesClean)
     service->execStreamEnable_ = false;
     service->devId_ = 0;
 
-    // 构造 execStream_ 的 streamInfo
     HcclComStreamInfo streamInfo{};
     streamInfo.sqId = 1;
     streamInfo.actualStreamId = 100;
     service->execStream_ = Stream(streamInfo);
 
-    // 构造 SqCqeContext 用于 UpdateSqStatus
     SqCqeContext sqCqeContext{};
     service->execStream_.sqeContext_ = &sqCqeContext.sqContext;
 
-    // Mock ConfigSqStatusByType 返回 SUCCESS
     MOCKER(ConfigSqStatusByType)
         .stubs()
         .will(returnValue(HCCL_SUCCESS));
 
-    // Mock Stream::ClearLocalBuff 返回 SUCCESS
     MOCKER_CPP(&Stream::ClearLocalBuff)
         .stubs()
         .will(returnValue(HCCL_SUCCESS));
 
-    // Mock QuerySqStatusByType 返回 SUCCESS
     MOCKER(QuerySqStatusByType)
         .stubs()
         .will(returnValue(HCCL_SUCCESS));
 
     HcclResult ret = service->CleanStreamFunc();
     EXPECT_EQ(ret, HCCL_SUCCESS);
-    EXPECT_TRUE(service->execStreamEnable_);  // 标志恢复为 true
+    EXPECT_TRUE(service->execStreamEnable_);
+}
+
+TEST_F(OneSideServiceDeviceUT, CleanAllStreamFunc_OneServiceFails_ReturnsError)
+{
+    auto service = std::make_shared<HcclOneSideServiceAicpu>();
+    service->identifier_ = "fail_tag";
+    service->execStreamEnable_ = false;
+    service->devId_ = 0;
+
+    HcclComStreamInfo streamInfo{};
+    streamInfo.sqId = 1;
+    streamInfo.actualStreamId = 100;
+    service->execStream_ = Stream(streamInfo);
+
+    HcclOneSideServiceAicpu::services_["fail_tag"] = service;
+
+    MOCKER(ConfigSqStatusByType)
+        .stubs()
+        .will(returnValue(HCCL_E_INTERNAL));
+
+    HcclResult ret = HcclOneSideServiceAicpu::CleanAllStreamFunc();
+    EXPECT_NE(ret, HCCL_SUCCESS);
 }
