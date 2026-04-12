f82f64e4a7e0336680d9313072f1c3873cd8c417
ren-mengguang <renmengguang@h-partners.com>
2026-04-12 15:42:02 +0800

    ut 2

---
+++ test/ut/framework/op_base_api/ut_HcomGetCommHandleByGroup_API_test.cc
@@ -0,0 +1,64 @@
+/**
+ * Copyright (c) 2025 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+#include "hccl_api_base_test.h"
+
+class HcomGetCommHandleByGroupTest : public BaseInit {
+public:
+    void SetUp() override {
+        BaseInit::SetUp();
+        UT_USE_1SERVER_1RANK_AS_DEFAULT;
+    }
+    
+    void TearDown() override {
+        BaseInit::TearDown();
+        GlobalMockObject::verify();
+    }
+protected:
+    HcclComm commHandle = nullptr;
+};
+
+TEST_F(HcomGetCommHandleByGroupTest, Ut_HcomGetCommHandleByGroup_When_Normal_Expect_ReturnIsHCCL_SUCCESS)
+{
+    UT_COMM_CREATE_DEFAULT(comm);
+    
+    char group[] = "hccl_world_group";
+    HcclResult ret = HcomGetCommHandleByGroup(group, &commHandle);
+    EXPECT_EQ(ret, HCCL_SUCCESS);
+    EXPECT_NE(commHandle, nullptr);
+}
+
+TEST_F(HcomGetCommHandleByGroupTest, Ut_HcomGetCommHandleByGroup_When_GroupIsNull_Expect_ReturnIsHCCL_E_PTR)
+{
+    UT_COMM_CREATE_DEFAULT(comm);
+    
+    HcclComm commHandle = nullptr;
+    HcclResult ret = HcomGetCommHandleByGroup(nullptr, &commHandle);
+    EXPECT_EQ(ret, HCCL_E_PTR);
+}
+
+TEST_F(HcomGetCommHandleByGroupTest, Ut_HcomGetCommHandleByGroup_When_CommHandleIsNull_Expect_ReturnIsHCCL_E_PTR)
+{
+    UT_COMM_CREATE_DEFAULT(comm);
+    
+    char group[] = "hccl_world_group";
+    HcclResult ret = HcomGetCommHandleByGroup(group, nullptr);
+    EXPECT_EQ(ret, HCCL_E_PTR);
+}
+
+TEST_F(HcomGetCommHandleByGroupTest, Ut_HcomGetCommHandleByGroup_When_GroupNotExist_Expect_ReturnIsHCCL_E_NOT_FOUND)
+{
+    UT_COMM_CREATE_DEFAULT(comm);
+    
+    char group[] = "non_existent_group";
+    HcclComm commHandle = nullptr;
+    HcclResult ret = HcomGetCommHandleByGroup(group, &commHandle);
+    EXPECT_EQ(ret, HCCL_E_NOT_FOUND);
+}
