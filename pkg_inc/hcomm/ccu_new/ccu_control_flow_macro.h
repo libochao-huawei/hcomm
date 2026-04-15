71a8c429e75d0c375807dcd694c670ada2932b37
one_random <shibingchen@huawei.com>
2026-04-15 10:22:28 +0800

    feat(ccu): update interfaces

--- pkg_inc/hcomm/ccu_new/ccu_control_flow_macro.h
+++ pkg_inc/hcomm/ccu_new/ccu_control_flow_macro.h
@@ -1,65 +1,19 @@
 /**
- * Copyright (c) 2026 Huawei Technologies Co., Ltd.
- * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
- * CANN Open Software License Agreement Version 2.0 (the "License").
- * Please refer to the License for details. You may not use this file except in compliance with the License.
- * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
- * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
- * See LICENSE in the root of the software repository for the full text of the License.
- */
-
-#ifndef CCU_DATA_API_H
-#define CCU_DATA_API_H
-
-#include <stdint.h>
-
-#include "hccl_types.h"
-#include "hcomm_primitives.h"
-
-#include "ccu_types.h"
-#include "ccu_data_resource.h"
-
-#ifdef __cplusplus
-#include "ccu_loop_macro.h"
-class CcuVariable;
-#endif
-
-#ifdef __cplusplus
-extern "C" {
-#endif // __cplusplus
-
-/**
- * @defgroup ccu数据面编程接口
- * @{
+ * Copyright (c) 2026 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
  */
 
-/**
- * @brief 本地同步操作
- * @param[in] thread 线程句柄
- * @return HcclResult 执行结果状态码
- * @note todo
- * @warning
- */
+#ifndef CCU_CONTROL_FLOW_MACRO_H
+#define CCU_CONTROL_FLOW_MACRO_H
 
-
-//支持从SQE加载参数
-
-extern CcuResult CcuIfBegin(CcuVariable *var, uint64_t immediate,
-    CcuConditionType condType, const char *label);
-
-extern CcuResult CcuIfElse(const char *label);
-
-extern CcuResult CcuIfEnd(const char *label);
-
-extern CcuResult CcuWhileBegin(CcuVariable *var, uint64_t immediate,
-    CcuConditionType condType, const char *label);
-
-extern CcuResult CcuWhileEnd(const char *label);
-
-extern CcuResult CcuDoWhileBegin(const char *label);
-
-extern CcuResult CcuDoWhileEnd(CcuVariable *var, uint64_t immediate,
-    CcuConditionType condType, const char *label);
+#include "ccu_variable.hpp"
+#include "ccu_if_label_stack.hpp"
+#include "ccu_data_api_impl.h"
 
 /* ---------------------------------------------------------------------------
  * Macro wrappers for structured control flow
@@ -120,8 +74,8 @@ extern CcuResult CcuDoWhileEnd(CcuVariable *var, uint64_t immediate,
              *uid##_p = &uid##_ce;                                          \
          uid##_p != nullptr;                                                \
          uid##_p = nullptr)                                                 \
-    for (int uid##_rc = (int)CcuWhileBegin(uid##_ce.var, uid##_ce.imm,      \
-                 uid##_ce.cond, CCU_STRINGIFY(uid)),                         \
+    for (int uid##_rc = (int)CcuWhileBegin(uid##_ce.var->handle,            \
+                 uid##_ce.imm, uid##_ce.cond, CCU_STRINGIFY(uid)),          \
              uid##_done = 0;                                                \
          uid##_rc == (int)CCU_SUCCESS && !uid##_done;                       \
          uid##_done = 1,                                                    \
@@ -146,8 +100,8 @@ extern CcuResult CcuDoWhileEnd(CcuVariable *var, uint64_t immediate,
          uid##_p != nullptr;                                                \
          uid##_p = nullptr)                                                 \
     for (int uid##_push = (CcuIfLabelStack::Push(CCU_STRINGIFY(uid)), 0),   \
-             uid##_rc = (int)CcuIfBegin(uid##_ce.var, uid##_ce.imm,         \
-                 uid##_ce.cond, CCU_STRINGIFY(uid));                        \
+             uid##_rc = (int)CcuIfBegin(uid##_ce.var->handle,               \
+                 uid##_ce.imm, uid##_ce.cond, CCU_STRINGIFY(uid));          \
          uid##_rc == (int)CCU_SUCCESS && uid##_push == 0;                   \
          uid##_push = 1)
 
@@ -186,8 +140,8 @@ extern CcuResult CcuDoWhileEnd(CcuVariable *var, uint64_t immediate,
              *uid##_p = &uid##_ce;                                          \
          uid##_p != nullptr;                                                \
          uid##_p = nullptr)                                                 \
-    for (int uid##_rc = (int)CcuIfBegin(uid##_ce.var, uid##_ce.imm,         \
-                 uid##_ce.cond, CCU_STRINGIFY(uid)),                         \
+    for (int uid##_rc = (int)CcuIfBegin(uid##_ce.var->handle,               \
+                 uid##_ce.imm, uid##_ce.cond, CCU_STRINGIFY(uid)),          \
              uid##_done = 0;                                                \
          uid##_rc == (int)CCU_SUCCESS && !uid##_done;                       \
          uid##_done = 1,                                                    \
@@ -214,103 +168,7 @@ extern CcuResult CcuDoWhileEnd(CcuVariable *var, uint64_t immediate,
              uid##_done = 0;                                                \
          uid##_rc == (int)CCU_SUCCESS && !uid##_done;                       \
          uid##_done = 1,                                                    \
-             uid##_rc = (int)CcuDoWhileEnd(uid##_ce.var, uid##_ce.imm,      \
-                 uid##_ce.cond, CCU_STRINGIFY(uid)))
+             uid##_rc = (int)CcuDoWhileEnd(uid##_ce.var->handle,            \
+                 uid##_ce.imm, uid##_ce.cond, CCU_STRINGIFY(uid)))
 
-
-
-// LocalAddr / RemoteAddr 创建
-extern CcuResult CcuLocalAddrCreate(CcuLocalAddr* localAddr);
-extern CcuResult CcuRemoteAddrCreate(CcuRemoteAddr* remoteAddr);
-
-// LocalAddr → Buffer（读入 Buffer）
-extern CcuResult CcuLocalCopyHBMToBuffer(
-    CcuBuffer dstBuffer, CcuLocalAddr src,
-    CcuVariable len, CcuEvent event);
-// Buffer → LocalAddr
-extern CcuResult CcuLocalCopyBufferToHBM(
-    CcuLocalAddr dst, CcuBuffer srcBuffer,
-    CcuVariable len, CcuEvent event);
-// LocalAddr → LocalAddr
-extern CcuResult CcuLocalCopyHBMToHBM(
-    CcuLocalAddr dst, CcuLocalAddr src,
-    CcuVariable len, CcuEvent event);
-/*========== 本地 Reduce ==========*/
-
-// LocalAddr → LocalAddr Reduce
-extern CcuResult CcuLocalHBMReduce(
-    CcuLocalAddr dst, CcuLocalAddr src,
-    CcuVariable len, HcclDataType dataType,
-    HcclReduceOp opType, CcuEvent event);
-
-// 多 Buffer Reduce
-extern CcuResult CcuLocalBufferReduce(
-    CcuBuffer* buffers, uint32_t count,
-    HcclDataType dataType, HcclDataType outputDataType,
-    HcclReduceOp opType,
-    CcuVariable len, CcuEvent event);
-
-/*========== 远端数据传输操作 ==========*/
-extern CcuResult CcuReadHBMToHBM(
-    ChannelHandle channel, CcuLocalAddr local, CcuRemoteAddr remote,
-    CcuVariable len, CcuEvent event);
-extern CcuResult CcuReadHBMToBuffer(
-    ChannelHandle channel, CcuBuffer local, CcuRemoteAddr remote,
-    CcuVariable len, CcuEvent event);
-extern CcuResult CcuReadHBMToHBMReduce(
-    ChannelHandle channel, CcuLocalAddr local, CcuRemoteAddr remote,
-    CcuVariable len, HcclDataType dataType,
-    HcclReduceOp opType, CcuEvent event);
-extern CcuResult CcuWriteHBMToHBM(
-    ChannelHandle channel, CcuRemoteAddr remote,CcuLocalAddr local, 
-    CcuVariable len, CcuEvent event);
-extern CcuResult CcuWriteBufferToHBM(
-    ChannelHandle channel, CcuRemoteAddr remote, CcuBuffer local,
-    CcuVariable len, CcuEvent event);
-extern CcuResult CcuWriteHBMToHBMReduce(
-    ChannelHandle channel, CcuRemoteAddr remote, CcuLocalAddr local,
-    CcuVariable len, HcclDataType dataType,
-    HcclReduceOp opType, CcuEvent event);
-
-
-/**
- * @brief 远端同步操作
- * @param[in] channel 链路句柄
- * 
- * @return HcclResult 执行结果状态码
- * @note x
- * @warning
- */
-// extern CcuResult CcuNotifyRecord(ChannelHandle channel, uint32_t remoteNotifyIdx, uint32_t mask);
-
-/**
- * @brief 远端同步操作
- * @param[in] channel 链路句柄
- * 
- * @return HcclResult 执行结果状态码
- * @note x
- * @warning
- */
-// extern CcuResult CcuNotifyWait(ChannelHandle channel, uint32_t localNotifyIdx, uint32_t mask);
-
-
-// Loop
-extern CcuResult CcuLoopCreate(CcuLoopHandle *loop);
-extern CcuResult _CcuLoopBodyEnter(CcuLoopHandle loop);
-extern CcuResult _CcuLoopBodyExit(CcuLoopHandle loop);
-extern CcuResult CcuLoopSetParam(CcuLoopHandle loop,
-    CcuVariable *formalParam, CcuVariable *actualParam);
-extern CcuResult CcuLoopGroupCreate(CcuLoopGroupHandle *group,
-    const CcuLoopGroupConfig *config);
-extern CcuResult CcuLoopGroupCreateFromVar(CcuLoopGroupHandle *group,
-    CcuVariable *parallelVar, CcuVariable *offsetVar);
-extern CcuResult CcuLoopGroupAddLoop(CcuLoopGroupHandle group,
-    CcuLoopHandle loop, const CcuLoopConfig *config, bool isUnroll);
-extern CcuResult CcuLoopGroupAddLoopFromVar(CcuLoopGroupHandle group,
-    CcuLoopHandle loop, CcuVariable *loopParamVar, bool isUnroll);
-
-#ifdef __cplusplus
-}
-#endif // __cplusplus
-
-#endif // CCU_DATA_API_H
+#endif // CCU_CONTROL_FLOW_MACRO_H
