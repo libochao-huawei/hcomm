25730a17d91e610ea1f57ccaaef59db35f956936
one_random <shibingchen@huawei.com>
2026-04-16 14:56:24 +0800

    bugfix: avoid legacy only fail

--- src/framework/communicator/hccl_comm_host.cc
+++ src/framework/communicator/hccl_comm_host.cc
@@ -334,11 +334,14 @@ namespace hccl
             __func__, collComm_->GetCommId().c_str(), commAicpuParam_.deviceLogicId, commAicpuParam_.devicePhyId,
             commAicpuParam_.deviceType, commAicpuParam_.userRank, commAicpuParam_.userRankSize);
         
-        // 当前需要支持coll comm与legacy comm混跑，coll comm确定加速模式后，需要设置comm加速模式
-        const auto opExpansionMode = GetCollCommOpExpansionMode(collComm_.get());
-        auto *commImplV2 = static_cast<Hccl::HcclCommunicator *>(commV2);
-        constexpr bool isCcuMsAvailable = false; // 禁止legacy通信域使用ms模式，避免抢占过多coll comm ccu可用资源
-        CHK_RET(commImplV2->SetAccelerator(opExpansionMode, false));
+        const char *indOp = getenv("HCCL_INDEPENDENT_OP");
+        if (indOp != nullptr && strcmp(indOp, "") != 0) {
+            // 当前需要支持coll comm与legacy comm混跑，coll comm确定加速模式后，需要设置comm加速模式
+            const auto opExpansionMode = GetCollCommOpExpansionMode(collComm_.get());
+            auto *commImplV2 = static_cast<Hccl::HcclCommunicator *>(commV2);
+            constexpr bool isCcuMsAvailable = false; // 禁止legacy通信域使用ms模式，避免抢占过多coll comm ccu可用资源
+            CHK_RET(commImplV2->SetAccelerator(opExpansionMode, false));
+        }
 
         return HCCL_SUCCESS;
     }
