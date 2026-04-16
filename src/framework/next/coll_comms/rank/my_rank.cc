2b811afb473058f84c9fae75563a752a3834cf6b
HafenW <wanglizhi19@huawei.com>
2026-04-16 20:44:53 +0800

    support ub comm log

--- src/framework/next/coll_comms/rank/my_rank.cc
+++ src/framework/next/coll_comms/rank/my_rank.cc
@@ -41,6 +41,48 @@ HcommChannelDesc ChannelDescHccl2Hcomm(const HcclChannelDesc &hcclDesc)
     return hcommDesc;
 }
 
+/* 公共模块函数返回值定义，跟业务层同步 */
+const std::unordered_map<CommProtocol, std::string> HCOM_COMM_PROTOCOL_STR_MAP = {
+    {COMM_PROTOCOL_RESERVED, "RESERVED"},
+    {COMM_PROTOCOL_HCCS, "HCCS"},
+    {COMM_PROTOCOL_ROCE, "ROCE"},
+    {COMM_PROTOCOL_PCIE, "PCIE"},
+    {COMM_PROTOCOL_SIO, "SIO"},
+    {COMM_PROTOCOL_UBC_CTP, "UBC_CTP"},
+    {COMM_PROTOCOL_UBC_TP, "UBC_TP"},
+    {COMM_PROTOCOL_UB_MEM, "UB_MEM"}
+};
+
+inline std::string GetCommProtocolEnumStr(CommProtocol protocol)
+{
+    auto iter = HCOM_COMM_PROTOCOL_STR_MAP.find(protocol);
+    if (iter == HCOM_COMM_PROTOCOL_STR_MAP.end()) {
+        return "CommProtocol(" + std::to_string(protocol) + ")";
+    } else {
+        return iter->second;
+    }
+}
+
+const std::unordered_map<CommEngine, std::string> HCOM_COMM_ENGINE_STR_MAP = {
+    {COMM_ENGINE_RESERVED, "RESERVED"},
+    {COMM_ENGINE_CPU, "CPU"},
+    {COMM_ENGINE_CPU_TS, "CPU_TS"},
+    {COMM_ENGINE_AICPU, "AICPU"},
+    {COMM_ENGINE_AICPU_TS, "AICPU_TS"},
+    {COMM_ENGINE_AIV, "AIV"},
+    {COMM_ENGINE_CCU, "CCU"}
+};
+
+inline std::string GetCommEngineEnumStr(CommEngine engine)
+{
+    auto iter = HCOM_COMM_ENGINE_STR_MAP.find(engine);
+    if (iter == HCOM_COMM_ENGINE_STR_MAP.end()) {
+        return "CommEngine(" + std::to_string(engine) + ")";
+    } else {
+        return iter->second;
+    }
+}
+
 } // namespace MyRankUtils
 
 namespace hccl {
@@ -424,9 +466,9 @@ HcclResult MyRank::CreateChannels(CommEngine engine, const std::string &commTag,
         HCCL_RUN_INFO("create channel info:channel handle[0x%llx] comm tag[%s] protocol[%s]"
             " local rank[%u] local dev phyid[%u] remote rank[%u] remote dev phyid[%u] engine[%s]",
             hostChannelHandleList[i], commTag.c_str(),
-            GetCommProtocolEnumStr(channelDescs[i].localEndpoint.protocol).c_str(),
+            MyRankUtils::GetCommProtocolEnumStr(channelDescs[i].localEndpoint.protocol).c_str(),
             rankId_, channelDescs[i].localEndpoint.loc.device.devPhyId, remoteRank,
-            channelDescs[i].remoteEndpoint.loc.device.devPhyId, GetCommEngineEnumStr(engine).c_str());
+            channelDescs[i].remoteEndpoint.loc.device.devPhyId, MyRankUtils::GetCommEngineEnumStr(engine).c_str());
     }
 
     if (engine == COMM_ENGINE_AICPU || engine == COMM_ENGINE_AICPU_TS) {
