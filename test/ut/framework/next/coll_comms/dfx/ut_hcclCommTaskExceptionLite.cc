ed0b8d2a12b4870b0440b44a62957b68a8098d5d
rockethcgs <luobinyan1@huawei.com>
2026-04-17 19:45:22 +0800

    fix aicpu errcode switch

---
+++ test/ut/framework/next/coll_comms/dfx/ut_hcclCommTaskExceptionLite.cc
@@ -0,0 +1,66 @@
+/**
+ * Copyright (c) 2026 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+#include "gtest/gtest.h"
+#include "mockcpp/mokc.h"
+#include <mockcpp/mockcpp.hpp>
+#include "hccl_comm_pub.h"
+#include "hcclCommTaskExceptionLite.h"
+#include "task_scheduler_error.h"
+
+using namespace hccl;
+using namespace hcomm;
+
+constexpr u32 RT_UB_LOCAL_OPERATIOINERR = 0x2;
+constexpr u32 RT_UB_REMOTE_OPERATIOINERR = 0x3;
+constexpr u32 RT_UB_LINK_FAILEDERR = 0x5;
+
+class hcclCommTaskExceptionLiteTest : public testing::Test
+{
+protected:
+    virtual void SetUp() override {}
+
+    virtual void TearDown() override
+    {
+        GlobalMockObject::verify();
+    }
+};
+
+TEST_F(hcclCommTaskExceptionLiteTest, Ut_SwitchUBCqeErrCodeToTsErrCode_When_Normal_Expect_ReturnIsCorrect)
+{
+    HcclCommTaskExceptionLite::GetInstance().Init(0);
+    uint16_t ret = HcclCommTaskExceptionLite::GetInstance().SwitchUBCqeErrCodeToTsErrCode(RT_UB_LOCAL_OPERATIOINERR);
+    EXPECT_EQ(ret, TS_ERROR_HCCL_OP_UB_DDRC_FAILED);
+    
+    ret = HcclCommTaskExceptionLite::GetInstance().SwitchUBCqeErrCodeToTsErrCode(RT_UB_REMOTE_OPERATIOINERR);
+    EXPECT_EQ(ret, TS_ERROR_HCCL_OP_UB_POISON_FAILED);
+
+    ret = HcclCommTaskExceptionLite::GetInstance().SwitchUBCqeErrCodeToTsErrCode(RT_UB_LINK_FAILEDERR);
+    EXPECT_EQ(ret, TS_ERROR_HCCL_OP_UB_LINK_FAILED);
+
+    ret = HcclCommTaskExceptionLite::GetInstance().SwitchUBCqeErrCodeToTsErrCode(static_cast<u32>(123));
+    EXPECT_EQ(ret, TS_ERROR_HCCL_OTHER_ERROR);
+}
+
+TEST_F(hcclCommTaskExceptionLiteTest, Ut_SwitchSdmaCqeErrCodeToTsErrCode_When_Normal_Expect_ReturnIsCorrect)
+{
+    HcclCommTaskExceptionLite::GetInstance().Init(0);
+    uint16_t ret = HcclCommTaskExceptionLite::GetInstance().SwitchSdmaCqeErrCodeToTsErrCode(RT_SDMA_COMPERR);
+    EXPECT_EQ(ret, TS_ERROR_SDMA_LINK_ERROR);
+    
+    ret = HcclCommTaskExceptionLite::GetInstance().SwitchSdmaCqeErrCodeToTsErrCode(RT_SDMA_COMPDATAERR);
+    EXPECT_EQ(ret, TS_ERROR_SDMA_POISON_ERROR);
+
+    ret = HcclCommTaskExceptionLite::GetInstance().SwitchSdmaCqeErrCodeToTsErrCode(RT_SDMA_DATAERR);
+    EXPECT_EQ(ret, TS_ERROR_SDMA_DDRC_ERROR);
+
+    ret = HcclCommTaskExceptionLite::GetInstance().SwitchSdmaCqeErrCodeToTsErrCode(static_cast<u32>(123));
+    EXPECT_EQ(ret, TS_ERROR_HCCL_OTHER_ERROR);
+}
