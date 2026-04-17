a3f54eec9ac6d43ef67d4c2372b1b95e9128248c
yevolin2023 <yeshengyu3@h-partners.com>
2026-04-17 16:03:39 +0800

    fix aiv build

--- src/algorithm/base/alg_aiv_template/aiv_crossnode_91093_base.h
+++ src/algorithm/base/alg_aiv_template/aiv_crossnode_91093_base.h
@@ -66,6 +66,14 @@ int32_t tag = args->tag; uint32_t numBlocks = args->numBlocks; \
 bool isOpBase = args->isOpBase; int32_t step = args->step; \
 uint32_t deterministic = args->deterministic
 
+// sk 绑定函数 A3
+#define SuperKernelBindA3(kernel_name) \
+extern "C" __sk__ void kernel_name##_1(SK_BIND_FUNC_ARGS_A3); \
+extern "C" __sk__ void kernel_name##_2(SK_BIND_FUNC_ARGS_A3); \
+extern "C" __sk__ void kernel_name##_3(SK_BIND_FUNC_ARGS_A3); \
+extern "C" __sk__ void kernel_name##_4(SK_BIND_FUNC_ARGS_A3); \
+SK_BIND(kernel_name, 0, kernel_name##_1, kernel_name##_2, kernel_name##_3, kernel_name##_4)
+
 // A3 sk 导出函数
 #define SK_BIND_FUNC_DEF_A3(kernel_name, postfix) \
 extern "C" __sk__ void kernel_name##_##postfix(SK_BIND_FUNC_ARGS_A3) \
