47a7bf3653f7512258e2c515fd5ca6dd8b2e3b1b
yuncliu <lyc1990@qq.com>
2026-04-18 19:49:10 +0800

    适配驱动接口变化

--- src/legacy/framework/topo/topo_addr_info/src/hal.c
+++ src/legacy/framework/topo/topo_addr_info/src/hal.c
@@ -122,6 +122,9 @@ int load_dcmi()
     dcmiv2_get_device_pcie_info = hal_dlsym(dcmi, "dcmiv2_get_device_pcie_info");
     dcmiv2_get_device_info = hal_dlsym(dcmi, "dcmiv2_get_device_info");
     get_logicid_from_phyid = hal_dlsym(dcmi, "dcmiv2_get_dev_id_from_chip_phyid");
+    if (get_logicid_from_phyid == NULL) {
+        get_logicid_from_phyid = hal_dlsym(dcmi, "dcmiv2_get_dev_id_phy_chip_phy_id");
+    }
 
     if ((dcmi_init == NULL)
      || (dcmiv2_get_urma_device_cnt == NULL)
