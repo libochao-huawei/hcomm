494916290c8693ee4cf01472afffd9b8b36d58fd
guoziqi2025 <guoziqi2@huawei.com>
2026-04-18 16:27:40 +0800

    fix error log in rootinfo connect timeout

--- src/legacy/unified_platform/resource/socket/socket.h
+++ src/legacy/unified_platform/resource/socket/socket.h
@@ -90,7 +90,7 @@ public:
 
     string Describe()
     {
-        return StringFormat("Socket[role=%s, localIp=%s, listenPort=0x%x, remoteIp=%s, tag=%s, nicType=%s]",
+        return StringFormat("Socket[role=%s, localIp=%s, listenPort=%u, remoteIp=%s, tag=%s, nicType=%s]",
                             role.Describe().c_str(), localIp.Describe().c_str(), listenPort,
                             remoteIp.Describe().c_str(), tag.c_str(), nicType.Describe().c_str());
     }
