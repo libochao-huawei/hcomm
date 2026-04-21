30070f5139b44e9f2994c92c610f77248fec07f0
ouyangcaike <ouyangcaike@h-partners.com>
2026-04-21 09:29:45 +0800

    add ut

--- test/ut/framework/communicator/impl/symmetric_memory/ut_hccl_communicator_host.cc
+++ test/ut/framework/communicator/impl/symmetric_memory/ut_hccl_communicator_host.cc
@@ -243,3 +243,63 @@ TEST_F(HcclCommunicatorHostTest, Ut_SetDynamicTilingData_When_A2GroupSendRecv_Ex
     EXPECT_EQ(hcclCommunicator->isGroupMode_, true);
     EXPECT_EQ(hcclCommunicator->userRankSize_, 2);
 }
+
+TEST_F(HcclCommunicatorHostTest, Ut_HcclGetAlgExecParam_When_Normal_Expect_ReturnHCCL_SUCCESS) {
+    std::unique_ptr<HcclCommunicator> hcclCommunicator(new (std::nothrow) HcclCommunicator());
+    hcclCommunicator->rankInfoList_.resize(2);
+    hcclCommunicator->realUserRank_ = 0;
+    hcclCommunicator->deviceType_ = DevType::DEV_TYPE_910_93;
+    hcclCommunicator->userRankSize_ = 2;
+    hcclCommunicator->userRank_ = 0;
+    hcclCommunicator->pimpl = new CommunicatorImpl();
+    hcclCommunicator->pimpl->myRank = 0;
+    hcclCommunicator->pimpl->rankSize = 2;
+
+    HcclResult ret = hcclCommunicator->InitSymmetricMemory();
+    EXPECT_EQ(ret, HCCL_SUCCESS);
+
+    MOCKER_CPP(&hrtMalloc).stubs().will(invoke(invocation {
+        void** ptr, u64 size, rtMemType_t type) -> HcclResult {
+        *ptr = malloc(size);
+        return HCCL_SUCCESS;
+    }));
+
+    MOCKER_CPP(&hrtMemSyncCopy).stubs().will(returnValue(HCCL_SUCCESS));
+
+    HcclResult initParaRet = hcclCommunicator->InitPara();
+    if (initParaRet != HCCL_SUCCESS) {
+        free(hcclCommunicator->notifyPool_.get());
+        free(hcclCommunicator->workSpaceRes_.get());
+        delete hcclCommunicator->pimpl;
+        EXPECT_EQ(initParaRet, HCCL_SUCCESS);
+        return;
+    }
+
+    std::string tag = "testTag";
+    HcclCMDType opType = HcclCMDType::HCCL_CMD_ALLREDUCE;
+    u64 count = 1024;
+    void* inputPtr = malloc(1024);
+    void* outputPtr = malloc(1024);
+    bool clearEnable = true;
+    HcclDataType dataType = HCCL_DATA_TYPE_INT8;
+    HcclReduceOp op = HCCL_REDUCE_SUM;
+    void* commContext = nullptr;
+    u64 len = 0;
+    u32 aivCoreLimit = 2;
+
+    ret = hcclCommunicator->HcclGetAlgExecParam(tag, opType, count, inputPtr, outputPtr,
+        clearEnable, dataType, op, commContext, len, aivCoreLimit);
+
+    free(inputPtr);
+    free(outputPtr);
+    free(hcclCommunicator->notifyPool_.get());
+    free(hcclCommunicator->workSpaceRes_.get());
+    delete hcclCommunicator->pimpl;
+
+    EXPECT_EQ(ret, HCCL_SUCCESS);
+    EXPECT_NE(commContext, nullptr);
+    EXPECT_EQ(len, sizeof(AivSuperKernelArgs));
+
+    free(commContext);
+    GlobalMockObject::verify();
+}
