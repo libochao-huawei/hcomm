515800ef398b8d1515c709a65d04e19e4ece43d6
wenp1213 <wenpeng26@huawei.com>
2026-04-21 16:34:07 +0800

    set host stream mode

--- src/platform/resource/stream/stream.cc
+++ src/platform/resource/stream/stream.cc
@@ -80,18 +80,16 @@ Stream::Stream(const StreamType streamType, bool isMainStream)
         HCCL_ERROR("[%s][%s]Construct stream by stream type failed, errNo[0x%016llx] rtStreamCreate error, ret[%d]",
             LOG_KEYWORDS_INIT_GROUP.c_str(), LOG_KEYWORDS_RESOURCE.c_str(), HCCL_ERROR_CODE(HCCL_E_RUNTIME), ret);
     }
-#if (!defined (HCCD)) && (!defined (CCL_KERNEL_AICPU))
     bool isSupportV2 = false;
     hrtGetHcclV2Support(&isSupportV2);
     if (isSupportV2) {
         constexpr u32 streamMode = 1; // 配置流失败模式为遇错即停
-        HcclResult setModeRet = hrtStreamSetMode(rtStream, streamMode);
+        HcclResult setModeRet = hrtStreamSetMode(stream_, streamMode);
         if (setModeRet != HCCL_SUCCESS) {
-            HCCL_RUN_WARNING("[Stream][SetMode]Failed to set stream mode, errNo[0x%016llx], ret[%d]",
+            HCCL_ERROR("[Stream][SetMode]Failed to set stream mode, errNo[0x%016llx], ret[%d]",
                 HCCL_ERROR_CODE(setModeRet), setModeRet);
         }
     }
-#endif
 }
 
 Stream::Stream(const rtStream_t rtStream, bool isMainStream)
