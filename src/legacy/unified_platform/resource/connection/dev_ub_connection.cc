235c0a784f196d86978062bc8213dca5b1032ccb
HafenW <wanglizhi19@huawei.com>
2026-04-21 11:58:26 +0800

    support ub comm log

--- src/legacy/unified_platform/resource/connection/dev_ub_connection.cc
+++ src/legacy/unified_platform/resource/connection/dev_ub_connection.cc
@@ -813,7 +813,7 @@ HcclResult DevUbConnection::Describe(std::string &dfxMsg) const
 {
     uint16_t udpSport = 0xFFFF; // 无法获取实际的udpSport，使用0xFFFF表示未知
     if (tpProtocol == TpProtocol::TP) {
-        uint32_t attrBitmap = 0;
+        uint32_t attrBitmap = 8192;
         struct TpAttr tpAttr {0};
         u32 devicePhyId = HrtGetDevicePhyIdByIndex(devLogicId);
         CHK_RET(HrtRaCtxGetTpAttr(devicePhyId, rdmaHandle, tpInfo.tpHandle, attrBitmap, tpAttr));
