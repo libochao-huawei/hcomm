3630558999a1fe20ee362655d12a71bf91761831
Innoecho <sunzhepeng@huawei.com>
2026-04-21 10:47:10 +0800

    完善HcommChannelNotifyWaitOnThread支持timeout配置
    Co-authored-by: Innoecho<sunzhepeng@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !1726 merge add_channel_timeout into master
    
    完善HcommChannelNotifyWaitOnThread支持timeout配置
    
    Created-by: Innoecho
    Commit-by: Innoecho
    Merged-by: cann-robot
    Description: ## 描述
    feat: 新增WaitWithTimeout接口支持超时等待
    
    问题描述:
    - HcommChannelNotifyWaitOnThread中timeOut参数被忽略，未实际生效
    - 导致等待操作无法实现超时控制
    
    解决方案:
    - BaseTransportLiteImpl新增WaitWithTimeout虚函数
    - P2PTransportLiteImpl和UbTransportLiteImpl实现WaitWithTimeout方法
    - Wait方法内部调用WaitWithTimeout（使用默认超时配置hcclExecTimeout）
    - HcommChannelNotifyWaitOnThread调用WaitWithTimeout传入timeOut参数
    - 使用stream.GetRtsq()->NotifyWait替代BuildNotifyWaitTask实现超时等待
    
    影响范围:
    - BaseTransportLiteImpl: 新增虚函数WaitWithTimeout
    - P2PTransportLiteImpl: Wait行为变更为调用WaitWithTimeout
    - UbTransportLiteImpl: Wait行为变更为调用WaitWithTimeout
    - HcommChannelNotifyWaitOnThread: timeOut参数正式生效
    
    新增UT测试用例:
    - ut_p2p_transport_lite_impl_wait_with_timeout: P2PTransportLiteImpl超时等待功能测试
    - ut_ub_transport_lite_impl_wait_with_timeout: UbTransportLiteImpl超时等待功能测试
    - ut_aicpu_ts_HcommChannelNotifyWaitOnThread_WithTimeout: HcommChannelNotifyWaitOnThread超时参数传递测试
    
    ## 关联的Issue
    NA
    
    ## 测试
    UT & RDV
    
    ## 文档更新
    NA
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [x] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    
    
    See merge request: cann/hcomm!1726

---
+++ test/legacy/ut/unified_platform/resource/transport/ut_p2p_transport_lite_impl_wait_with_timeout.cc
@@ -0,0 +1,171 @@
+/**
+ * Copyright (c) 2025 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+#include "gtest/gtest.h"
+#include <mockcpp/mockcpp.hpp>
+#include <mockcpp/MockObject.h>
+#define private public
+#include "orion_adapter_rts.h"
+#include "p2p_transport_lite_impl.h"
+#include "stream_lite.h"
+#include "rtsq_a5.h"
+#include "ascend_hal.h"
+#include "drv_api_exception.h"
+#undef private
+
+using namespace Hccl;
+
+class P2PTransportLiteImplWaitWithTimeoutTest : public testing::Test {
+protected:
+    static void SetUpTestCase()
+    {
+        std::cout << "P2PTransportLiteImplWaitWithTimeoutTest tests set up." << std::endl;
+    }
+
+    static void TearDownTestCase()
+    {
+        std::cout << "P2PTransportLiteImplWaitWithTimeoutTest tests tear down." << std::endl;
+    }
+
+    virtual void SetUp()
+    {
+        MOCKER(HrtGetDeviceType).stubs().will(returnValue((DevType)DevType::DEV_TYPE_910A2));
+        MOCKER_CPP(&RtsqBase::QuerySqBaseAddr).stubs().with(any()).will(returnValue(reinterpret_cast<u64>(&mockSq)));
+        MOCKER_CPP(&RtsqBase::QuerySqStatusByType).stubs().with(any()).will(returnValue(static_cast<u32>(0)));
+        MOCKER_CPP(&RtsqBase::ConfigSqStatusByType).stubs();
+        std::cout << "A Test case in P2PTransportLiteImplWaitWithTimeoutTest SetUp" << std::endl;
+    }
+
+    virtual void TearDown()
+    {
+        std::cout << "A Test case in P2PTransportLiteImplWaitWithTimeoutTest TearDown" << std::endl;
+        GlobalMockObject::verify();
+    }
+
+    std::vector<char> GetNotifyUniqueId(u32 notifyId, u32 devPhyId)
+    {
+        BinaryStream binaryStream;
+        binaryStream << notifyId;
+        binaryStream << devPhyId;
+        std::vector<char> result;
+        binaryStream.Dump(result);
+        return result;
+    }
+
+    u8  mockSq[64];
+};
+
+TEST_F(P2PTransportLiteImplWaitWithTimeoutTest, P2PTransportLiteImpl_WaitWithTimeout_Normal_Success)
+{
+    u32 fakeStreamId = 1;
+    u32 fakeSqId     = 1;
+    u32 fakeDevPhyId = 1;
+
+    BinaryStream liteBinaryStream;
+    liteBinaryStream << fakeStreamId;
+    liteBinaryStream << fakeSqId;
+    liteBinaryStream << fakeDevPhyId;
+    std::vector<char> uniqueId{};
+    liteBinaryStream.Dump(uniqueId);
+
+    StreamLite stream(uniqueId);
+    RtsqA5     rtsq(fakeDevPhyId, fakeStreamId, fakeSqId);
+    stream.rtsq = std::make_unique<RtsqA5>(rtsq);
+
+    std::vector<char> notifyUniqueId = GetNotifyUniqueId(0, fakeDevPhyId);
+    BinaryStream binaryStream;
+    u32 notifyNum = 2;
+    u32 bufferNum = 2;
+    u32 connNum = 1;
+    binaryStream << notifyNum;
+    binaryStream << bufferNum;
+    binaryStream << connNum;
+    binaryStream << notifyUniqueId;
+    std::vector<char> p2pUniqueId;
+    binaryStream.Dump(p2pUniqueId);
+
+    auto callback = [](u32 streamId, u32 taskId, const Hccl::TaskParam &taskParam) { (void)streamId; (void)taskId; (void)taskParam; };
+    P2PTransportLiteImpl transportLite(p2pUniqueId, callback);
+
+    u32 timeout = 1800;
+    EXPECT_NO_THROW(transportLite.WaitWithTimeout(0, stream, timeout));
+}
+
+TEST_F(P2PTransportLiteImplWaitWithTimeoutTest, P2PTransportLiteImpl_WaitWithTimeout_ZeroTimeout_Success)
+{
+    u32 fakeStreamId = 1;
+    u32 fakeSqId     = 1;
+    u32 fakeDevPhyId = 1;
+
+    BinaryStream liteBinaryStream;
+    liteBinaryStream << fakeStreamId;
+    liteBinaryStream << fakeSqId;
+    liteBinaryStream << fakeDevPhyId;
+    std::vector<char> uniqueId{};
+    liteBinaryStream.Dump(uniqueId);
+
+    StreamLite stream(uniqueId);
+    RtsqA5     rtsq(fakeDevPhyId, fakeStreamId, fakeSqId);
+    stream.rtsq = std::make_unique<RtsqA5>(rtsq);
+
+    std::vector<char> notifyUniqueId = GetNotifyUniqueId(0, fakeDevPhyId);
+    BinaryStream binaryStream;
+    u32 notifyNum = 2;
+    u32 bufferNum = 2;
+    u32 connNum = 1;
+    binaryStream << notifyNum;
+    binaryStream << bufferNum;
+    binaryStream << connNum;
+    binaryStream << notifyUniqueId;
+    std::vector<char> p2pUniqueId;
+    binaryStream.Dump(p2pUniqueId);
+
+    auto callback = [](u32 streamId, u32 taskId, const Hccl::TaskParam &taskParam) { (void)streamId; (void)taskId; (void)taskParam; };
+    P2PTransportLiteImpl transportLite(p2pUniqueId, callback);
+
+    u32 timeout = 0;
+    EXPECT_NO_THROW(transportLite.WaitWithTimeout(0, stream, timeout));
+}
+
+TEST_F(P2PTransportLiteImplWaitWithTimeoutTest, P2PTransportLiteImpl_WaitWithTimeout_MaxTimeout_Success)
+{
+    u32 fakeStreamId = 1;
+    u32 fakeSqId     = 1;
+    u32 fakeDevPhyId = 1;
+
+    BinaryStream liteBinaryStream;
+    liteBinaryStream << fakeStreamId;
+    liteBinaryStream << fakeSqId;
+    liteBinaryStream << fakeDevPhyId;
+    std::vector<char> uniqueId{};
+    liteBinaryStream.Dump(uniqueId);
+
+    StreamLite stream(uniqueId);
+    RtsqA5     rtsq(fakeDevPhyId, fakeStreamId, fakeSqId);
+    stream.rtsq = std::make_unique<RtsqA5>(rtsq);
+
+    std::vector<char> notifyUniqueId = GetNotifyUniqueId(0, fakeDevPhyId);
+    BinaryStream binaryStream;
+    u32 notifyNum = 2;
+    u32 bufferNum = 2;
+    u32 connNum = 1;
+    binaryStream << notifyNum;
+    binaryStream << bufferNum;
+    binaryStream << connNum;
+    binaryStream << notifyUniqueId;
+    std::vector<char> p2pUniqueId;
+    binaryStream.Dump(p2pUniqueId);
+
+    auto callback = [](u32 streamId, u32 taskId, const Hccl::TaskParam &taskParam) { (void)streamId; (void)taskId; (void)taskParam; };
+    P2PTransportLiteImpl transportLite(p2pUniqueId, callback);
+
+    u32 timeout = 0xFFFFFFFF;
+    EXPECT_NO_THROW(transportLite.WaitWithTimeout(0, stream, timeout));
+}
