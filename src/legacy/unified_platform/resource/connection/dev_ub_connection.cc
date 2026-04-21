b0458290e8915f784d73a44f1da17f9ed7f5879f
HafenW <wanglizhi19@huawei.com>
2026-04-21 20:42:23 +0800

    support ub comm log

--- src/legacy/unified_platform/resource/connection/dev_ub_connection.cc
+++ src/legacy/unified_platform/resource/connection/dev_ub_connection.cc
@@ -812,13 +812,13 @@ string DevUbConnection::Describe() const
 HcclResult DevUbConnection::Describe(std::string &dfxMsg) const
 {
     uint16_t udpSport = 0xFFFF; // 无法获取实际的udpSport，使用0xFFFF表示未知
-    // if (tpProtocol == TpProtocol::TP) {
-    //     uint32_t attrBitmap = 8192;
-    //     struct TpAttr tpAttr {0};
-    //     u32 devicePhyId = HrtGetDevicePhyIdByIndex(devLogicId);
-    //     CHK_RET(HrtRaCtxGetTpAttr(devicePhyId, rdmaHandle, tpInfo.tpHandle, attrBitmap, tpAttr));
-    //     udpSport = tpAttr.dataUdpSrcport;
-    // }
+    if (tpProtocol == TpProtocol::TP) {
+        uint32_t attrBitmap = 8192;
+        struct TpAttr tpAttr {0};
+        u32 devicePhyId = HrtGetDevicePhyIdByIndex(devLogicId);
+        CHK_RET(HrtRaCtxGetTpAttr(devicePhyId, rdmaHandle, tpInfo.tpHandle, attrBitmap, tpAttr));
+        udpSport = tpAttr.dataUdpSrcport;
+    }
     udpSport = udpSport & 0xFF;
 
     std::string dfxStr = StringFormat("chip id[%u] die id[%u] func id[%u] jetty id[%u] "
