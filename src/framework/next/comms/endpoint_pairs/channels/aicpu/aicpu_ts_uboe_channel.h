0c6b3a0a68396b64bd1b08d13d7c14ba3998553c
jankin_weng <wengjingchong@huawei.com>
2026-04-23 10:09:01 +0800

    code review update

--- src/framework/next/comms/endpoint_pairs/channels/aicpu/aicpu_ts_uboe_channel.h
+++ src/framework/next/comms/endpoint_pairs/channels/aicpu/aicpu_ts_uboe_channel.h
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2025 Huawei Technologies Co., Ltd.
+ * Copyright (c) 2026 Huawei Technologies Co., Ltd.
  * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
  * CANN Open Software License Agreement Version 2.0 (the "License").
  * Please refer to the License for details. You may not use this file except in compliance with the License.
@@ -10,8 +10,8 @@
 #ifndef AICPU_TS_UBOE_CHANNEL_H
 #define AICPU_TS_UBOE_CHANNEL_H
 
-#include "../channel.h"
-#include "../../sockets/socket_mgr.h"
+#include "channel.h"
+#include "socket_mgr.h"
 
 // Orion
 #include "../../../../../../legacy/unified_platform/resource/socket/socket.h"
