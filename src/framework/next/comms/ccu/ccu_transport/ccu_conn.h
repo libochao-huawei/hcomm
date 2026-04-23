e94ed6274a0d19c19bc1c38f2f1a69e96d2fd37c
HafenW <wanglizhi19@huawei.com>
2026-04-23 14:21:43 +0800

    support ub comm log

--- src/framework/next/comms/ccu/ccu_transport/ccu_conn.h
+++ src/framework/next/comms/ccu/ccu_transport/ccu_conn.h
@@ -78,7 +78,6 @@ private:
     HcclResult    ReleaseConnRes();
     HcclResult    ReturnErrorStatus(const std::string &funcName);
     std::string   Describe();
-    HcclResult    testSetTpAttr();
 
 private:
     CcuConnStatus status_{CcuConnStatus::CONN_INVALID};
