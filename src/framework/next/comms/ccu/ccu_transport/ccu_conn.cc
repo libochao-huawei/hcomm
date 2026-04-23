928d9a6ab0920380e671bbfd0a8a40d017247cd6
HafenW <wanglizhi19@huawei.com>
2026-04-23 10:51:19 +0800

    support ub comm log

--- src/framework/next/comms/ccu/ccu_transport/ccu_conn.cc
+++ src/framework/next/comms/ccu/ccu_transport/ccu_conn.cc
@@ -546,7 +546,7 @@ HcclResult CcuConnection::testSetTpAttr()
     uint32_t attrBitmap = 0x1FFFF;
     struct TpAttr tpAttr {0};
     tpAttr.dataUdpSrcport = 128;
-    CHK_RET(HrtRaSetTpAttrAsync(ctxHandle_, tpInfo_.tpHandle, attrBitmap, tpAttr, reqHandles_[0]));
+    CHK_RET(Hccl::HrtRaSetTpAttrAsync(ctxHandle_, tpInfo_.tpHandle, attrBitmap, tpAttr, reqHandles_[0]));
     return HCCL_SUCCESS;
 }
 
