5651466a30e44658d2d1f92609bff327ea1ddc57
jankin_weng <wengjingchong@huawei.com>
2026-04-23 10:09:01 +0800

    code review update

--- src/framework/next/comms/endpoint_pairs/channels/aicpu/aicpu_ts_uboe_channel.cc
+++ src/framework/next/comms/endpoint_pairs/channels/aicpu/aicpu_ts_uboe_channel.cc
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2025 Huawei Technologies Co., Ltd.
+ * Copyright (c) 2026 Huawei Technologies Co., Ltd.
  * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
  * CANN Open Software License Agreement Version 2.0 (the "License").
  * Please refer to the License for details. You may not use this file except in compliance with the License.
@@ -9,7 +9,7 @@
  */
 
 #include "aicpu_ts_uboe_channel.h"
-#include "../../../endpoints/endpoint.h"
+#include "endpoint.h"
 #include "orion_adpt_utils.h"
 #include "hcomm_c_adpt.h"
 #include "exception_handler.h"
