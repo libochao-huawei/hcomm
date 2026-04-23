229b553336bc508d0187c471e1928f5f868f671a
HafenW <wanglizhi19@huawei.com>
2026-04-23 15:59:13 +0800

    support ub comm log

--- src/framework/next/comms/ccu/ccu_transport/ccu_conn.cc
+++ src/framework/next/comms/ccu/ccu_transport/ccu_conn.cc
@@ -513,6 +513,7 @@ HcclResult CcuConnection::Describe(std::string &dfxMsg)
 {
     uint16_t udpSport = 0xFFFF; // 无法获取实际的udpSport，使用0xFFFF表示未知
     if (tpProtocol_ == TpProtocol::RTP) {
+        uint32_t attrBitmap = 8192;
         struct TpAttr tpAttr {0};
         uint32_t attrBitmap = 1 << 13; // 13对应dataUdpSrcport
         EXCEPTION_HANDLE_BEGIN
@@ -528,7 +529,6 @@ HcclResult CcuConnection::Describe(std::string &dfxMsg)
         EXCEPTION_HANDLE_END
         udpSport = tpAttr.dataUdpSrcport;
     }
-    HCCL_INFO("[CcuConnection::%s] Get tp attr success, udpSport[%u]", __func__, udpSport);
     udpSport = udpSport & 0xFF;
 
     std::ostringstream oss;
@@ -548,8 +548,8 @@ HcclResult CcuConnection::Describe(std::string &dfxMsg)
     Hccl::Eid rmtEid = rmtAddr.GetReverseEid();
 
     std::string dfxStr = Hccl::StringFormat("chip id[%u] die id[%u] func_id[%u] jetty id[%s] "
-        "local %s remote %s udp sport[%u] attrBitMap[%u]",
-        devLogicId_, dieId_, funcId_, jettyIds.c_str(), locEid.Describe().c_str(), rmtEid.Describe().c_str(), udpSport, attrBitmap);
+        "local %s remote %s udp sport[%u]",
+        devLogicId_, dieId_, funcId_, jettyIds.c_str(), locEid.Describe().c_str(), rmtEid.Describe().c_str(), udpSport);
     dfxMsg += dfxStr;
     HCCL_INFO("[CcuConnection::%s] %s", __func__, dfxStr.c_str());
     return HcclResult::HCCL_SUCCESS;
