3c5c1334e0250c49fd75fedc119fc9cec68e7330
Leewis <liwei945@huawei.com>
2026-04-23 20:38:43 +0800

    add ut

--- test/ut/device/ut_executor_tracer.cc
+++ test/ut/device/ut_executor_tracer.cc
@@ -12,188 +12,15 @@ using namespace dfx_tracer;
 class ExecutorTracerTest : public ::testing::Test {
 protected:
     void SetUp() override {
-        // 初始化测试环境
     }
 
     void TearDown() override {
-        // 清理测试环境
+        GlobalMockObject::verify();
     }
 };
 
-// Mock AicpuHcclProcess 类的静态方法
-MOCKER(AicpuHcclProcess::AicpuGetCommMutex)
-    .stubs()
-    .will(returnValueByRef(mockCommMutex));
-
-MOCKER(AicpuHcclProcess::AicpuGetCommAll)
-    .stubs()
-    .will(doAnswer(Return(0)));
-
-MOCKER(AicpuHcclProcess::AicpuDestoryCommbyGroup)
-    .stubs()
-    .will(doNothing());
-
-// Mock AicpuExecutorTracer 类的静态方法
-MOCKER(AicpuExecutorTracer::HandleBackGround)
-    .stubs()
-    .will(doNothing());
-
-MOCKER(AicpuExecutorTracer::StopLaunchCommandHandle)
-    .stubs()
-    .will(doNothing());
-
-MOCKER(AicpuExecutorTracer::KfcCommandHandle)
-    .stubs()
-    .will(doNothing());
-
-MOCKER(AicpuExecutorTracer::HandleCqeStatus)
-    .stubs()
-    .will(doNothing());
-
-MOCKER(AicpuExecutorTracer::StopKfcThread)
-    .stubs()
-    .will(doNothing());
-
-// Mock HcclOneSideServiceAicpu 类的静态方法
-MOCKER(hccl::HcclOneSideServiceAicpu::isAllDestroy)
-    .stubs()
-    .will(returnValue(true));
-
-MOCKER(hccl::HcclOneSideServiceAicpu::HandleErrCqe)
-    .stubs()
-    .will(doNothing());
-
-// Mock CannErrorReporter 类的方法
-MOCKER(dfx::CannErrorReporter::GetInstance)
-    .stubs()
-    .will(returnValue(dfx::CannErrorReporter::GetInstance()));
-
-MOCKER(dfx::CannErrorReporter::UpdateSensorNode)
-    .stubs()
-    .will(returnValue(HCCL_SUCCESS));
-
-MOCKER(dfx::CannErrorReporter::Clear)
-    .stubs()
-    .will(returnValue(HCCL_SUCCESS));
-
-// 测试 TaskMonitor 函数
-TEST_F(ExecutorTracerTest, Ut_TaskMonitor) {
-    // 执行测试
-    ExecutorTracer::TaskMonitor();
-    // 验证测试通过
-    EXPECT_TRUE(true);
-}
-
-// 测试 HandleCqeStatusInComm 函数
-TEST_F(ExecutorTracerTest, Ut_HandleCqeStatusInComm) {
-    // 执行测试
-    ExecutorTracer::HandleCqeStatusInComm();
-    // 验证测试通过
-    EXPECT_TRUE(true);
-}
-
-// 测试 HandleReportStatusInComm 函数
-TEST_F(ExecutorTracerTest, Ut_HandleReportStatusInComm) {
-    // 执行测试
-    ExecutorTracer::HandleReportStatusInComm();
-    // 验证测试通过
-    EXPECT_TRUE(true);
-}
-
-// 测试 StopBackGround 函数
-TEST_F(ExecutorTracerTest, Ut_StopBackGround) {
-    // 创建模拟上下文
-    AicpuComContext mockCtx;
-    mockCtx.commOpenStatus = false;
-
-    bool isNotStop = false;
-
-    // 执行测试
-    ExecutorTracer::StopBackGround(&mockCtx, isNotStop);
-    // 验证测试通过
-    EXPECT_TRUE(true);
-}
-
-// 测试 HandleBackGround 函数
-TEST_F(ExecutorTracerTest, Ut_HandleBackGround) {
-    // 创建模拟上下文
-    AicpuComContext mockCtx;
-
-    // 执行测试
-    ExecutorTracer::HandleBackGround(&mockCtx);
-    // 验证测试通过
-    EXPECT_TRUE(true);
-}
-
-// 测试 StopBackGroundDfx 函数
-TEST_F(ExecutorTracerTest, Ut_StopBackGroundDfx) {
-    // 创建模拟上下文
-    AicpuComContext mockCtx;
-
-    // 执行测试
-    ExecutorTracer::StopBackGroundDfx(&mockCtx);
-    // 验证测试通过
-    EXPECT_TRUE(true);
-}
-
-// 测试 StopLaunchCommandHandle 函数
-TEST_F(ExecutorTracerTest, Ut_StopLaunchCommandHandle) {
-    // 创建模拟上下文
-    AicpuComContext mockCtx;
-
-    // 执行测试
-    ExecutorTracer::StopLaunchCommandHandle(&mockCtx);
-    // 验证测试通过
-    EXPECT_TRUE(true);
-}
-
-// 测试 KfcCommandHandle 函数
-TEST_F(ExecutorTracerTest, Ut_KfcCommandHandle) {
-    // 创建模拟上下文
-    AicpuComContext mockCtx;
-
-    // 执行测试
-    ExecutorTracer::KfcCommandHandle(&mockCtx);
-    // 验证测试通过
-    EXPECT_TRUE(true);
-}
-
-// 测试 HandleSwitchNic 函数
-TEST_F(ExecutorTracerTest, Ut_HandleSwitchNic) {
-    // 创建模拟上下文
-    AicpuComContext mockCtx;
-
-    // 执行测试
-    ExecutorTracer::HandleSwitchNic(&mockCtx);
-    // 验证测试通过
-    EXPECT_TRUE(true);
-}
-
-// 测试 HandleResumeChangeLink 函数
-TEST_F(ExecutorTracerTest, Ut_HandleResumeChangeLink) {
-    // 创建模拟上下文
-    AicpuComContext mockCtx;
-
-    // 执行测试
-    ExecutorTracer::HandleResumeChangeLink(&mockCtx);
-    // 验证测试通过
-    EXPECT_TRUE(true);
-}
-
-// 测试 HandleCqeStatus 函数
-TEST_F(ExecutorTracerTest, Ut_HandleCqeStatus) {
-    // 创建模拟上下文
-    AicpuComContext mockCtx;
-
-    // 执行测试
-    ExecutorTracer::HandleCqeStatus(&mockCtx);
-    // 验证测试通过
-    EXPECT_TRUE(true);
-}
-
-// 测试 SetCqeQueryInput 函数
-TEST_F(ExecutorTracerTest, Ut_SetCqeQueryInput) {
-    // 创建测试数据
+TEST_F(ExecutorTracerTest, Ut_SetCqeQueryInput)
+{
     uint32_t devId = 0;
     HcclComStreamInfo streamInfo;
     streamInfo.actualStreamId = 1;
@@ -201,15 +28,10 @@ TEST_F(ExecutorTracerTest, Ut_SetCqeQueryInput) {
     streamInfo.logicCqId = 3;
     CqeQueryInput cqeQueryInput;
 
-    // 执行测试
     ExecutorTracer::SetCqeQueryInput(devId, streamInfo, cqeQueryInput);
-    // 验证结果
     EXPECT_EQ(cqeQueryInput.devId, devId);
     EXPECT_EQ(cqeQueryInput.streamId, streamInfo.actualStreamId);
     EXPECT_EQ(cqeQueryInput.sqId, streamInfo.sqId);
     EXPECT_EQ(cqeQueryInput.cqId, streamInfo.logicCqId);
     EXPECT_EQ(cqeQueryInput.type, static_cast<uint32_t>(DRV_LOGIC_TYPE));
 }
-
-// 全局变量
-ReadWriteLockBase mockCommMutex;
