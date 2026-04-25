b192179a28cb8e60a280796f692f4c5a81460d1d
leironghao1 <leironghao1@h-partners.com>
2026-04-25 11:01:02 +0800

    rank间一致性校验

--- src/framework/communicator/impl/hccl_communicator.h
+++ src/framework/communicator/impl/hccl_communicator.h
@@ -475,6 +475,10 @@ public:
     HcclResult DeregisterWindow(HcclCommSymWindow winHandle);
     HcclResult InitSymmetricMemory();
     HcclResult GetCommSymWin(void* ptr, size_t size, HcclCommSymWindow *winHandle, size_t *offset);
+
+    TransportManager GetTransportManager();
+    TransportManager GetIndptOpTransportManager();
+    
 private:
 
     bool IsEnableRoce();
@@ -618,9 +622,6 @@ private:
     HcclResult HandleExistAlgResource(const std::string& newTag, const std::string& algName,
         HcclCMDType opType, const OpParam& opParam, std::unique_ptr<CollAlgOperator>& algOperator,
         bool selectAivAlg, bool aicpuUnfoldModeFor910B, bool needRecreateAlltoallComm);
-
-    TransportManager GetTransportManager();
-    TransportManager GetIndptOpTransportManager();
     
     u32 deviceNumPerServer_;
     HcclDispatcher dispatcher_; // dispatcher放到最后析构
