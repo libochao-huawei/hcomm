000af19ce020f51853bea3df0dd4d32ca0c15163
HafenW <wanglizhi19@huawei.com>
2026-04-27 14:03:55 +0800

    support ub comm log

--- src/legacy/unified_platform/resource/connection/dev_ub_connection.cc
+++ src/legacy/unified_platform/resource/connection/dev_ub_connection.cc
@@ -812,22 +812,18 @@ string DevUbConnection::Describe() const
 HcclResult DevUbConnection::Describe(std::string &dfxMsg)
 {
     uint16_t udpSport = 0xFFFF; // 无法获取实际的udpSport，使用0xFFFF表示未知
-    uint8_t retryTimesInit = 0;
-    uint8_t at = 0;
     if (tpProtocol == TpProtocol::TP) {
-        uint32_t attrBitmap = 8195;
+        uint32_t attrBitmap = 8192;
         struct TpAttr tpAttr {0};
         u32 devicePhyId = HrtGetDevicePhyIdByIndex(devLogicId);
         CHK_RET(HrtRaGetTpAttrAsync(devicePhyId, rdmaHandle, tpInfo.tpHandle, attrBitmap, tpAttr, reqHandle));
         udpSport = tpAttr.dataUdpSrcport;
-        retryTimesInit = tpAttr.retryTimesInit;
-        at = tpAttr.at;
     }
     udpSport = udpSport & 0xFF;
 
     std::string dfxStr = StringFormat("chip id[%u] die id[%u] func id[%u] jetty id[%u] "
-        "local %s remote %s udp sport[%u] retryTimesInit[%u] at[%u]",
-        devLogicId, dieId, funcId, jettyId, locEid.Describe().c_str(), rmtEid.Describe().c_str(), udpSport, retryTimesInit, at);
+        "local %s remote %s udp sport[%u]",
+        devLogicId, dieId, funcId, jettyId, locEid.Describe().c_str(), rmtEid.Describe().c_str(), udpSport);
     dfxMsg += dfxStr;
     HCCL_INFO("[DevUbConnection::%s] %s", __func__, dfxStr.c_str());
     return HCCL_SUCCESS;
