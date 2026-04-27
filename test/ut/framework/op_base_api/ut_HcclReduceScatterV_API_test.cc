53cf0190a6d073db6605938b9e3cf1f2db0672c5
chengjing43 <chengjing43@huawei.com>
2026-04-27 17:10:16 +0800

    fix possible null pointer dereference

--- test/ut/framework/op_base_api/ut_HcclReduceScatterV_API_test.cc
+++ test/ut/framework/op_base_api/ut_HcclReduceScatterV_API_test.cc
@@ -270,3 +270,25 @@ TEST_F(HcclReduceScatterVTest, Ut_HcclReduceScatterV_When_2Server4Rank_Expect_Re
 
     UT_UNSET_SENDBUFV_RECVBUF_COMM_STREAM(comm, stream);
 }
+
+TEST_F(HcclReduceScatterVTest, Ut_HcclReduceScatterV_When_GroupModeSuccess_Expect_ReturnIsHCCL_SUCCESS)
+{
+    MOCKER(taskAppend)
+        .stubs()
+        .with(any(), any())
+        .will(returnValue(HCCL_SUCCESS));
+    UT_SET_SENDBUFV_RECVBUF_COUNT(HCCL_COM_DATA_SIZE,
+        1, HCCL_COM_DATA_SIZE,
+        1, 0,
+        HCCL_COM_DATA_SIZE,
+        HCCL_COM_DATA_SIZE);
+    UT_COMM_CREATE_DEFAULT(comm);
+    UT_STREAM_CREATE_DEFAULT(stream);
+    hcclGroupDepth = 1;
+
+    HcclResult ret = HcclReduceScatterVInner(sendBuf, sendCounts, sendDispls, recvBuf, recvCount, HCCL_DATA_TYPE_INT8, HCCL_REDUCE_SUM, comm, stream);
+    EXPECT_EQ(ret, HCCL_SUCCESS);
+
+    hcclGroupDepth = 0;
+    UT_UNSET_SENDBUFV_RECVBUF_COMM_STREAM_WITHSTREAMSYNCHRONIZEFIRST(comm, stream);
+}
