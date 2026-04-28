139be763f916c5123f0fd2a0be3fdfd59cb95513
guoziqi2025 <guoziqi2@huawei.com>
2026-04-28 15:02:41 +0800

    [fix]add fallback ut
    Co-authored-by: guoziqi2025<guoziqi2@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !1967 merge master into master
    
    [fix]add fallback ut
    
    Created-by: guoziqi2025
    Commit-by: guoziqi2025
    Merged-by: cann-robot
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    加回ccu切新流程临时下掉的ut
    
    ## 变更类型
    请选择本次引入的变更类型：
    <!--  [x] 表示选中 -->
    - [x] 🐛 Bug修复
    - [ ] ✨ 新特性
    - [ ] 🚀 性能优化
    - [ ] 📝 文档更新
    - [ ] 📋 其他，请描述：
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。-->
    <!-- 如果这个PR不涉及Issue，可填写"NA"。-->
    NA
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于构造对应xx测试用例、二级冒烟、算子泛化等。-->
    已完成的测试用例和场景：
    1. ut编译
    
    由于只改ut，无需RDV
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 合入检查
    <!-- 在正式合入前，请做好必要的代码测试，用例补充，软件代码风格检查等。提高合入效率。-->
    <!--  [x] 表示选中 -->
    - [x] 🧐 已经详细阅读了贡献指南（CONTRIBUTING.md），并遵守了其中的所有规定，包括但不限于commit message的格式、无效commit的合并等
    - [x] 🔍 邀请 committer评论`/lgtm`前的必要检查
        - [x] 🏷️ 标题中使用了合适的类型标签（如：`[feat]`, `[fix]`）
        - [x] 📄 代码修改内容已简要描述，相关文档已更新
        - [x] 📝 代码注释已更新，代码遵循项目整体代码风格
        - [x] 🧪 代码UT测试已更新，覆盖率已达标
        - [x] 🔬 验证方法已更新到"测试"部分
        - [x] 🛠️ 代码已通过静态分析工具检查，无错误
        - [x] 👥 代码检视/code review/同行评议和必要的代码串讲，确保代码质量
        - [x] ✅ 代码检视意见已处理或答复，无未处理的检视意见
    - [x] 🚀 预约 前冒烟 用例前的必要检查
        - [x] ✔️ 代码已有committer的`/lgtm` 和 模块committer的`/lgtm`评论
        - [x] 🔧 代码已通过compile，编译无错误，无告警
        - [x] 🖥️ 代码已通过基本功能本地测试或者在线测试，确保基本功能正常
    - [x] 🎯 预约 approver评论`/approve`，正式合入前的必要检查
        - [x] 📊 前冒烟 用例已全量通过
        - [x] 📦 新增功能已同步补充基本功能测试用例到前冒烟里
    
    See merge request: cann/hcomm!1967

--- test/ut/framework/resource/ut_HcclOpResParam_size_check.cc
+++ test/ut/framework/resource/ut_HcclOpResParam_size_check.cc
@@ -22,59 +22,58 @@ TEST_F(HcclHcclOpResParamStructTest, TestHcclOpResParamSize)
     EXPECT_EQ(sizeof(HcclOpResParam), 2123112u);
 }
 
-// 支持ccu切换开源流程临时下掉，需要处理因设备号扩容导致的偏移量变更
-// TEST_F(HcclHcclOpResParamStructTest, TestHcclOpResParamFieldOffsets)
-// {
-//     EXPECT_EQ(offsetof(HcclOpResParam, mc2WorkSpace), 0u);
-//     EXPECT_EQ(offsetof(HcclOpResParam, localUsrRankId), offsetof(HcclOpResParam, mc2WorkSpace) + 16u);
-//     EXPECT_EQ(offsetof(HcclOpResParam, rankSize), offsetof(HcclOpResParam, localUsrRankId) + 4u);
-//     EXPECT_EQ(offsetof(HcclOpResParam, winSize), offsetof(HcclOpResParam, rankSize) + 4u);
-//     EXPECT_EQ(offsetof(HcclOpResParam, localWindowsIn), offsetof(HcclOpResParam, winSize) + 8u);
-//     EXPECT_EQ(offsetof(HcclOpResParam, localWindowsOut), offsetof(HcclOpResParam, localWindowsIn) + 8u);
-//     EXPECT_EQ(offsetof(HcclOpResParam, hcomId), offsetof(HcclOpResParam, localWindowsOut) + 8u);
-//     EXPECT_EQ(offsetof(HcclOpResParam, winExpSize), offsetof(HcclOpResParam, hcomId) + 128u);
-//     EXPECT_EQ(offsetof(HcclOpResParam, localWindowsExp), offsetof(HcclOpResParam, winExpSize) + 8u);
-//     EXPECT_EQ(offsetof(HcclOpResParam, rWinStart), offsetof(HcclOpResParam, localWindowsExp) + 8u);
-//     EXPECT_EQ(offsetof(HcclOpResParam, rWinOffset), offsetof(HcclOpResParam, rWinStart) + 4u);
-//     EXPECT_EQ(offsetof(HcclOpResParam, version), offsetof(HcclOpResParam, rWinOffset) + 4u);
-//     EXPECT_EQ(offsetof(HcclOpResParam, reservedStrcut), offsetof(HcclOpResParam, version) + 8u);
-//     EXPECT_EQ(offsetof(HcclOpResParam, topoInfo), offsetof(HcclOpResParam, reservedStrcut) + 2472u);
-//     EXPECT_EQ(offsetof(HcclOpResParam, config), offsetof(HcclOpResParam, topoInfo) + 160u);
-//     EXPECT_EQ(offsetof(HcclOpResParam, hostStateInfo), offsetof(HcclOpResParam, config) + 48u);
-//     EXPECT_EQ(offsetof(HcclOpResParam, aicpuStateInfo), offsetof(HcclOpResParam, hostStateInfo) + 8u);
-//     EXPECT_EQ(offsetof(HcclOpResParam, lockAddr), offsetof(HcclOpResParam, aicpuStateInfo) + 8u);
-//     EXPECT_EQ(offsetof(HcclOpResParam, rsv), offsetof(HcclOpResParam, lockAddr) + 8u);
-//     EXPECT_EQ(offsetof(HcclOpResParam, notifysize), offsetof(HcclOpResParam, rsv) + 64u);
-//     EXPECT_EQ(offsetof(HcclOpResParam, remoteResNum), offsetof(HcclOpResParam, notifysize) + 4u);
-//     EXPECT_EQ(offsetof(HcclOpResParam, remoteRes), offsetof(HcclOpResParam, remoteResNum) + 4u);
-//     EXPECT_EQ(offsetof(HcclOpResParam, kfcControlTransferH2DParams), offsetof(HcclOpResParam, remoteRes) + 2097152u);
-//     EXPECT_EQ(offsetof(HcclOpResParam, kfcStatusTransferD2HParams), offsetof(HcclOpResParam, kfcControlTransferH2DParams) + 40u);
-//     EXPECT_EQ(offsetof(HcclOpResParam, tinyMem), offsetof(HcclOpResParam, kfcStatusTransferD2HParams) + 40u);
-//     EXPECT_EQ(offsetof(HcclOpResParam, tinyMemSize), offsetof(HcclOpResParam, tinyMem) + 8u);
-//     EXPECT_EQ(offsetof(HcclOpResParam, zeroCopyHeadPtr), offsetof(HcclOpResParam, tinyMemSize) + 8u);
-//     EXPECT_EQ(offsetof(HcclOpResParam, zeroCopyTailPtr), offsetof(HcclOpResParam, zeroCopyHeadPtr) + 8u);
-//     EXPECT_EQ(offsetof(HcclOpResParam, zeroCopyRingBuffer), offsetof(HcclOpResParam, zeroCopyTailPtr) + 8u);
-//     EXPECT_EQ(offsetof(HcclOpResParam, zeroCopyIpcPtrs), offsetof(HcclOpResParam, zeroCopyRingBuffer) + 8u);
-//     EXPECT_EQ(offsetof(HcclOpResParam, zeroCopyDevicePhyId), offsetof(HcclOpResParam, zeroCopyIpcPtrs) + 256u);
-//     EXPECT_EQ(offsetof(HcclOpResParam, utraceStatusFlag), offsetof(HcclOpResParam, zeroCopyDevicePhyId) + 128u);
-//     EXPECT_EQ(offsetof(HcclOpResParam, opCounterInfo), offsetof(HcclOpResParam, utraceStatusFlag) + 8u);
-//     EXPECT_EQ(offsetof(HcclOpResParam, hierarchicalAlgInfo), offsetof(HcclOpResParam, opCounterInfo) + 32u);
-//     EXPECT_EQ(offsetof(HcclOpResParam, localRes), offsetof(HcclOpResParam, hierarchicalAlgInfo) + 32u);
-//     EXPECT_EQ(offsetof(HcclOpResParam, debugConfig), offsetof(HcclOpResParam, localRes) + 3464u);
-//     EXPECT_EQ(offsetof(HcclOpResParam, aicpuCustomParamAddr), offsetof(HcclOpResParam, debugConfig) + 8u);
-//     EXPECT_EQ(offsetof(HcclOpResParam, aicpuCustomParamSize), offsetof(HcclOpResParam, aicpuCustomParamAddr) + 8u);
-//     EXPECT_EQ(offsetof(HcclOpResParam, userMemRes), offsetof(HcclOpResParam, aicpuCustomParamSize) + 8u);
-//     EXPECT_EQ(offsetof(HcclOpResParam, userMemType), offsetof(HcclOpResParam, userMemRes) + 18432u);
-//     EXPECT_EQ(offsetof(HcclOpResParam, aicpuOrderStreamParam), offsetof(HcclOpResParam, userMemType) + 8u);
-//     EXPECT_EQ(offsetof(HcclOpResParam, aicpuOrderNotifyAddr), offsetof(HcclOpResParam, aicpuOrderStreamParam) + 32u);
-//     EXPECT_EQ(offsetof(HcclOpResParam, aicpuOrderNotifySize), offsetof(HcclOpResParam, aicpuOrderNotifyAddr) + 8u);
-//     EXPECT_EQ(offsetof(HcclOpResParam, multiSuperPodDiffDeviceNumMode), offsetof(HcclOpResParam, aicpuOrderNotifySize) + 8u);
-//     EXPECT_EQ(offsetof(HcclOpResParam, isARSDoubleRing), offsetof(HcclOpResParam, multiSuperPodDiffDeviceNumMode) + 4u);
-//     EXPECT_EQ(offsetof(HcclOpResParam, opEntry), offsetof(HcclOpResParam, isARSDoubleRing) + 1u);
-//     EXPECT_EQ(offsetof(HcclOpResParam, hcclSdmaQos), offsetof(HcclOpResParam, opEntry) + 3u);
-//     EXPECT_EQ(offsetof(HcclOpResParam, sizeOfAiRMAInfo), offsetof(HcclOpResParam, hcclSdmaQos) + 8u);
-//     EXPECT_EQ(offsetof(HcclOpResParam, aiRMAInfo), offsetof(HcclOpResParam, sizeOfAiRMAInfo) + 8u);
-// }
+TEST_F(HcclHcclOpResParamStructTest, TestHcclOpResParamFieldOffsets)
+{
+    EXPECT_EQ(offsetof(HcclOpResParam, mc2WorkSpace), 0u);
+    EXPECT_EQ(offsetof(HcclOpResParam, localUsrRankId), offsetof(HcclOpResParam, mc2WorkSpace) + 16u);
+    EXPECT_EQ(offsetof(HcclOpResParam, rankSize), offsetof(HcclOpResParam, localUsrRankId) + 4u);
+    EXPECT_EQ(offsetof(HcclOpResParam, winSize), offsetof(HcclOpResParam, rankSize) + 4u);
+    EXPECT_EQ(offsetof(HcclOpResParam, localWindowsIn), offsetof(HcclOpResParam, winSize) + 8u);
+    EXPECT_EQ(offsetof(HcclOpResParam, localWindowsOut), offsetof(HcclOpResParam, localWindowsIn) + 8u);
+    EXPECT_EQ(offsetof(HcclOpResParam, hcomId), offsetof(HcclOpResParam, localWindowsOut) + 8u);
+    EXPECT_EQ(offsetof(HcclOpResParam, winExpSize), offsetof(HcclOpResParam, hcomId) + 128u);
+    EXPECT_EQ(offsetof(HcclOpResParam, localWindowsExp), offsetof(HcclOpResParam, winExpSize) + 8u);
+    EXPECT_EQ(offsetof(HcclOpResParam, rWinStart), offsetof(HcclOpResParam, localWindowsExp) + 8u);
+    EXPECT_EQ(offsetof(HcclOpResParam, rWinOffset), offsetof(HcclOpResParam, rWinStart) + 4u);
+    EXPECT_EQ(offsetof(HcclOpResParam, version), offsetof(HcclOpResParam, rWinOffset) + 4u);
+    EXPECT_EQ(offsetof(HcclOpResParam, reservedStrcut), offsetof(HcclOpResParam, version) + 8u);
+    EXPECT_EQ(offsetof(HcclOpResParam, topoInfo), offsetof(HcclOpResParam, reservedStrcut) + 2472u);
+    EXPECT_EQ(offsetof(HcclOpResParam, config), offsetof(HcclOpResParam, topoInfo) + 160u);
+    EXPECT_EQ(offsetof(HcclOpResParam, hostStateInfo), offsetof(HcclOpResParam, config) + 48u);
+    EXPECT_EQ(offsetof(HcclOpResParam, aicpuStateInfo), offsetof(HcclOpResParam, hostStateInfo) + 8u);
+    EXPECT_EQ(offsetof(HcclOpResParam, lockAddr), offsetof(HcclOpResParam, aicpuStateInfo) + 8u);
+    EXPECT_EQ(offsetof(HcclOpResParam, rsv), offsetof(HcclOpResParam, lockAddr) + 8u);
+    EXPECT_EQ(offsetof(HcclOpResParam, notifysize), offsetof(HcclOpResParam, rsv) + 64u);
+    EXPECT_EQ(offsetof(HcclOpResParam, remoteResNum), offsetof(HcclOpResParam, notifysize) + 4u);
+    EXPECT_EQ(offsetof(HcclOpResParam, remoteRes), offsetof(HcclOpResParam, remoteResNum) + 4u);
+    EXPECT_EQ(offsetof(HcclOpResParam, kfcControlTransferH2DParams), offsetof(HcclOpResParam, remoteRes) + 2097152u);
+    EXPECT_EQ(offsetof(HcclOpResParam, kfcStatusTransferD2HParams), offsetof(HcclOpResParam, kfcControlTransferH2DParams) + 40u);
+    EXPECT_EQ(offsetof(HcclOpResParam, tinyMem), offsetof(HcclOpResParam, kfcStatusTransferD2HParams) + 40u);
+    EXPECT_EQ(offsetof(HcclOpResParam, tinyMemSize), offsetof(HcclOpResParam, tinyMem) + 8u);
+    EXPECT_EQ(offsetof(HcclOpResParam, zeroCopyHeadPtr), offsetof(HcclOpResParam, tinyMemSize) + 8u);
+    EXPECT_EQ(offsetof(HcclOpResParam, zeroCopyTailPtr), offsetof(HcclOpResParam, zeroCopyHeadPtr) + 8u);
+    EXPECT_EQ(offsetof(HcclOpResParam, zeroCopyRingBuffer), offsetof(HcclOpResParam, zeroCopyTailPtr) + 8u);
+    EXPECT_EQ(offsetof(HcclOpResParam, zeroCopyIpcPtrs), offsetof(HcclOpResParam, zeroCopyRingBuffer) + 8u);
+    EXPECT_EQ(offsetof(HcclOpResParam, zeroCopyDevicePhyId), offsetof(HcclOpResParam, zeroCopyIpcPtrs) + 520u);
+    EXPECT_EQ(offsetof(HcclOpResParam, utraceStatusFlag), offsetof(HcclOpResParam, zeroCopyDevicePhyId) + 260u);
+    EXPECT_EQ(offsetof(HcclOpResParam, opCounterInfo), offsetof(HcclOpResParam, utraceStatusFlag) + 4u);
+    EXPECT_EQ(offsetof(HcclOpResParam, hierarchicalAlgInfo), offsetof(HcclOpResParam, opCounterInfo) + 32u);
+    EXPECT_EQ(offsetof(HcclOpResParam, localRes), offsetof(HcclOpResParam, hierarchicalAlgInfo) + 32u);
+    EXPECT_EQ(offsetof(HcclOpResParam, debugConfig), offsetof(HcclOpResParam, localRes) + 3464u);
+    EXPECT_EQ(offsetof(HcclOpResParam, aicpuCustomParamAddr), offsetof(HcclOpResParam, debugConfig) + 8u);
+    EXPECT_EQ(offsetof(HcclOpResParam, aicpuCustomParamSize), offsetof(HcclOpResParam, aicpuCustomParamAddr) + 8u);
+    EXPECT_EQ(offsetof(HcclOpResParam, userMemRes), offsetof(HcclOpResParam, aicpuCustomParamSize) + 8u);
+    EXPECT_EQ(offsetof(HcclOpResParam, userMemType), offsetof(HcclOpResParam, userMemRes) + 18432u);
+    EXPECT_EQ(offsetof(HcclOpResParam, aicpuOrderStreamParam), offsetof(HcclOpResParam, userMemType) + 8u);
+    EXPECT_EQ(offsetof(HcclOpResParam, aicpuOrderNotifyAddr), offsetof(HcclOpResParam, aicpuOrderStreamParam) + 32u);
+    EXPECT_EQ(offsetof(HcclOpResParam, aicpuOrderNotifySize), offsetof(HcclOpResParam, aicpuOrderNotifyAddr) + 8u);
+    EXPECT_EQ(offsetof(HcclOpResParam, multiSuperPodDiffDeviceNumMode), offsetof(HcclOpResParam, aicpuOrderNotifySize) + 8u);
+    EXPECT_EQ(offsetof(HcclOpResParam, isARSDoubleRing), offsetof(HcclOpResParam, multiSuperPodDiffDeviceNumMode) + 4u);
+    EXPECT_EQ(offsetof(HcclOpResParam, opEntry), offsetof(HcclOpResParam, isARSDoubleRing) + 1u);
+    EXPECT_EQ(offsetof(HcclOpResParam, hcclSdmaQos), offsetof(HcclOpResParam, opEntry) + 3u);
+    EXPECT_EQ(offsetof(HcclOpResParam, sizeOfAiRMAInfo), offsetof(HcclOpResParam, hcclSdmaQos) + 8u);
+    EXPECT_EQ(offsetof(HcclOpResParam, aiRMAInfo), offsetof(HcclOpResParam, sizeOfAiRMAInfo) + 8u);
+}
 
 TEST_F(HcclHcclOpResParamStructTest, TestHcclOpResParamFieldSizes)
 {
