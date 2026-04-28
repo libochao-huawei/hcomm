be4bd88e068aa51a87dbc8d2f821c5a76dc69080
rockethcgs <luobinyan1@huawei.com>
2026-04-28 11:53:10 +0800

    fix safety issue

--- test/ut/framework/next/comms/api_c_adpt/ut_hcomm_engine_ctx_test.cc
+++ test/ut/framework/next/comms/api_c_adpt/ut_hcomm_engine_ctx_test.cc
@@ -20,6 +20,20 @@ public:
     }
 };
 
+void MockMallocAndMemset()
+{
+    void* mallocAddr = malloc(1024);
+    ASSERT_NE(mallocAddr, nullptr);
+
+    MOCKER(::malloc)
+        .stubs()
+        .will(returnValue(mallocAddr));
+
+    MOCKER(::memset_s)
+        .stubs()
+        .will(returnValue(1));
+}
+
 TEST_F(TestHcommEngineCtx, Ut_TestHcommEngineCtxCreate_When_CPUEngine_Return_HCCL_Success)
 {
     void* ctx = nullptr;
@@ -28,6 +42,15 @@ TEST_F(TestHcommEngineCtx, Ut_TestHcommEngineCtxCreate_When_CPUEngine_Return_HCC
     EXPECT_NE(ctx, nullptr);
 
     (void)HcommEngineCtxDestroy(COMM_ENGINE_CPU, ctx);
+}
+
+TEST_F(TestHcommEngineCtx, Ut_TestHcommEngineCtxCreate_When_CPUAndMemsetFailed_Return_HCCL_E_INTERNAL)
+{
+    void* ctx = nullptr;
+    MockMallocAndMemset();
+
+    HcommResult ret = HcommEngineCtxCreate(COMM_ENGINE_CPU, 1024, &ctx);
+    EXPECT_EQ(ret, HCCL_E_INTERNAL);
 }
 
 TEST_F(TestHcommEngineCtx, Ut_TestHcommEngineCtxCreate_When_SizeZero_Return_HCCL_Success)
