c39016ff3ad5a41ec620aafaecf2332736db489b
yangshaohua <yangshaohua7@huawei.com>
2026-04-30 18:35:08 +0800

    add ffts failed dfx info

--- src/common/debug/profiling/task_profiling.cc
+++ src/common/debug/profiling/task_profiling.cc
@@ -316,7 +316,7 @@ HcclResult TaskProfiling::Save(u32 &streamID, u32 &taskID, TaskType &taskType, c
     return HCCL_SUCCESS;
 }
 
-HcclResult TaskProfiling::Save(u32 captureStreamID, u32 streamID, u32 taskID)
+HcclResult TaskProfiling::Save(u32 captureStreamID, u32 streamID, u32 taskID, const void *descBuf, size_t descBufLen)
 {
     return HCCL_SUCCESS;
 }
@@ -379,7 +379,7 @@ HcclResult TaskProfiling::Save(u32 streamID, u32 taskID, const TaskParaAiv &para
     return Save(streamID, streamID, taskID, paraAiv);
 }
 
-HcclResult TaskProfiling::Save(u32 &streamID, u32 &taskID)
+HcclResult TaskProfiling::Save(u32 &streamID, u32 &taskID, const void *descBuf, size_t descBufLen)
 {
     return HCCL_SUCCESS;
 }
