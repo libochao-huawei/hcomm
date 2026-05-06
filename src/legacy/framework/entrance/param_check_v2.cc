8b226c05d5142094d9857e6b082dc21d85983334
howell_why <wuhaoyu6@h-partners.com>
2026-05-06 11:25:15 +0800

    engine
    Co-authored-by: howell_why<wuhaoyu6@h-partners.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !2012 merge master into master
    
    engine
    
    Created-by: howell_why
    Commit-by: howell_why
    Merged-by: cann-robot
    Description: ## 描述
    [fix]engine打印为枚举值，修改为字符串
    
    ## 变更类型
    请选择本次引入的变更类型：
    <!--  [x] 表示选中 -->
    - [x] 🐛 Bug修复
    - [ ] ✨ 新特性
    - [ ] 🚀 性能优化
    - [ ] 📝 文档更新
    - [ ] 📋 其他，请描述：
    
    ## 关联的Issue
    NA
    
    ## 测试
    RDV
    已完成的测试用例和场景：
    1. rdv用例
    2. 部分daily用例
    
    补充的UT用例：
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 合入检查
    <!-- 在正式合入前，请做好必要的代码测试，用例补充，软件代码风格检查等。提高合入效率。-->
    <!--  [x] 表示选中 -->
    - [x] 🧐 已经详细阅读了贡献指南（CONTRIBUTING.md），并遵守了其中的所有规定，包括但不限于commit message的格式、无效commit的合并等
    - [x] 🔍 邀请 committer评论`/lgtm`前的必要检查
        - [x] 🏷️ 标题中使用了合适的类型标签（如：`[feat]`, `[fix]`）
        - [x] 📄 代码修改内容已简要描述，相关文档已更新
        - [x] 📝 代码注释已更新，代码遵循项目整体代码风格
        - [x] 🧪 代码UT测试已更新，覆盖率已达标
        - [x] 🔬 验证方法已更新到"测试"部分
        - [x] 🛠️ 代码已通过静态分析工具检查，无错误
        - [x] 👥 代码检视/code review/同行评议和必要的代码串讲，确保代码质量
        - [x] ✅ 代码检视意见已处理或答复，无未处理的检视意见
    - [x] 🚀 预约 前冒烟 用例前的必要检查
        - [x] ✔️ 代码已有committer的`/lgtm` 和 模块committer的`/lgtm`评论
        - [x] 🔧 代码已通过compile，编译无错误，无告警
        - [x] 🖥️ 代码已通过基本功能本地测试或者在线测试，确保基本功能正常
    - [x] 🎯 预约 approver评论`/approve`，正式合入前的必要检查
        - [x] 📊 前冒烟 用例已全量通过
        - [x] 📦 新增功能已同步补充基本功能测试用例到前冒烟里
    
    See merge request: cann/hcomm!2012

--- src/legacy/framework/entrance/param_check_v2.cc
+++ src/legacy/framework/entrance/param_check_v2.cc
@@ -453,7 +453,7 @@ HcclResult HcomLoadRankTableFileV2(const char *clusterInfo, std::string &rankTab
     if (realpath(clusterInfo, resolvedPath) == nullptr) {
         std::string rankTablePath(clusterInfo);
         RPT_INPUT_ERR(true, "EI0004", std::vector<std::string>({"error_reason", "ranktable_path"}), 
-            std::vector<std::string>({rankTablePath, "The rankTable file path is not a valid real path or the permission is insufficient."}));
+            std::vector<std::string>({rankTablePath, "The rankTable file path does not exist, the permission is insufficient, or the JSON format is incorrect."}));
         HCCL_ERROR("RanktableRealPath: %s is not a valid real path", clusterInfo);
         return HCCL_E_PARA;
     }
