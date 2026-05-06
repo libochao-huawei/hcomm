2a5b357217ce74fab8f60a944815c37a8e3064a8
rockethcgs <luobinyan1@huawei.com>
2026-05-06 14:22:08 +0800

    fix aicpu errcode switch

--- src/framework/next/coll_comms/dfx/taskException/aicpu/hcclCommTaskExceptionLite.cc
+++ src/framework/next/coll_comms/dfx/taskException/aicpu/hcclCommTaskExceptionLite.cc
@@ -296,11 +296,12 @@ HcclResult HcclCommTaskExceptionLite::SendTaskExceptionByMBox(const u32 notifyId
     aicpuSqe.ts_id = static_cast<uint8_t>(tsId);
     aicpuSqe.u.aicpu_record.fault_task_id = 0xffffffff;
 
-    const uint8_t ubErrorType = 1; // ub类型为1
-    if (exceptionInfo.errorType == ubErrorType) {
+    if (exceptionInfo.sqeType == static_cast<uint8_t>(SqeType950::UBDMA_SQE)) {
         aicpuSqe.u.aicpu_record.ret_code = SwitchUBCqeErrCodeToTsErrCode(exceptionInfo.errorCode & 0xFF);
-    } else {
+    } else if (exceptionInfo.sqeType == static_cast<uint8_t>(SqeType950::SDMA_SQE)) {
         aicpuSqe.u.aicpu_record.ret_code = SwitchSdmaCqeErrCodeToTsErrCode(exceptionInfo.errorCode);
+    } else {
+        aicpuSqe.u.aicpu_record.ret_code = TS_ERROR_HCCL_OTHER_ERROR;
     }
 
     struct event_summary event;
