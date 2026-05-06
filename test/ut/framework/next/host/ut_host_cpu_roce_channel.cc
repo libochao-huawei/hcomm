10f07d5b9e9fc6c9a0ab97c17e3dd1d44f64ea7e
lhdlhdlhd1234569 <lihaodong19@h-partners.com>
2026-05-06 17:00:06 +0800

    update

--- test/ut/framework/next/host/ut_host_cpu_roce_channel.cc
+++ test/ut/framework/next/host/ut_host_cpu_roce_channel.cc
@@ -793,7 +793,7 @@ TEST_F(HostCpuRoceChannelTest, Ut_ReportWcStatusError_When_Normal_Expect_HCCL_E_
     SetupSuccessfulConnectionMocks();
     auto impl_ = CreateInitAndConnect();
     impl_->localDpuNotifyIds_ = {0};
-    impl_->remoteDpuNotifyIds_ = {0}
+    impl_->remoteDpuNotifyIds_ = {0};
 
     HcclResult ret = impl_->ReportWcStatusError(IBV_WC_WR_FLUSH_ERR);
     EXPECT_EQ(ret, HCCL_E_NETWORK);
@@ -805,7 +805,7 @@ TEST_F(HostCpuRoceChannelTest, Ut_ReportWcStatusError_When_VariousStatuses_Expec
     SetupSuccessfulConnectionMocks();
     auto impl_ = CreateInitAndConnect();
     impl_->localDpuNotifyIds_ = {0};
-    impl_->remoteDpuNotifyIds_ = {0}
+    impl_->remoteDpuNotifyIds_ = {0};
 
     std::vector<enum ibv_wc_status> errorStatuses = {
         IBV_WC_WR_FLUSH_ERR,
