454ac1625ae85ba7cb4c6fa10a5d16bc71e8fe0a
guoziqi2025 <guoziqi2@huawei.com>
2026-05-08 15:40:35 +0800

    [fix]fix coredump when aiv config error
    Co-authored-by: guoziqi2025<guoziqi2@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !2040 merge master into master
    
    [fix]fix coredump when aiv config error
    
    Created-by: guoziqi2025
    Commit-by: guoziqi2025
    Merged-by: cann-robot
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    解决环境ubmemory配置异常时，下发aiv模式的算子coredump
    在RmtBufferUnpackProc里失败会抛出异常，但现在代码捕获后未检查返回值，需要改为判断返回值异常时返回Hccl::TransportStatus::INVALID，在外面ChannelGetStatus检查并报错终止流程
    
    ## 变更类型
    请选择本次引入的变更类型：
    <!--  [x] 表示选中 -->
    - [x] 🐛 Bug修复
    - [ ] ✨ 新特性
    - [ ] 🚀 性能优化
    - [ ] 📝 文档更新
    - [ ] 📋 其他，请描述：
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。-->
    <!-- 如果这个PR不涉及Issue，可填写"NA"。-->
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于构造对应xx测试用例、二级冒烟、算子泛化等。-->
    已完成的测试用例和场景：
    1. 打桩HrtIpcOpenMemory抛异常，期望报错终止流程，不coredump
    
    补充的UT用例：
    ut_aiv_ub_mem_transport.cc
    测试RmtBufferUnpackProc抛异常时报错，返回baseStatus_=INVALID
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 合入检查
    <!-- 在正式合入前，请做好必要的代码测试，用例补充，软件代码风格检查等。提高合入效率。-->
    <!--  [x] 表示选中 -->
    - [x] 🧐 已经详细阅读了贡献指南（CONTRIBUTING.md），并遵守了其中的所有规定，包括但不限于commit message的格式、无效commit的合并等
    - [x] 🔍 邀请 committer评论`/lgtm`前的必要检查
        - [x] 🏷️ 标题中使用了合适的类型标签（如：`[feat]`, `[fix]`）
        - [x] 📄 代码修改内容已简要描述，相关文档已更新
        - [x] 📝 代码注释已更新，代码遵循项目整体代码风格
        - [x] 🧪 代码UT测试已更新，覆盖率已达标
        - [x] 🔬 验证方法已更新到"测试"部分
        - [x] 🛠️ 代码已通过静态分析工具检查，无错误
        - [x] 👥 代码检视/code review/同行评议和必要的代码串讲，确保代码质量
        - [x] ✅ 代码检视意见已处理或答复，无未处理的检视意见
    - [x] 🚀 预约 前冒烟 用例前的必要检查
        - [x] ✔️ 代码已有committer的`/lgtm` 和 模块committer的`/lgtm`评论
        - [x] 🔧 代码已通过compile，编译无错误，无告警
        - [x] 🖥️ 代码已通过基本功能本地测试或者在线测试，确保基本功能正常
    - [ ] 🎯 预约 approver评论`/approve`，正式合入前的必要检查
        - [ ] 📊 前冒烟 用例已全量通过
        - [ ] 📦 新增功能已同步补充基本功能测试用例到前冒烟里
    
    See merge request: cann/hcomm!2040

---
+++ test/ut/framework/next/comms/endpoint_pairs/channels/aiv/ut_aiv_ub_mem_transport.cc
@@ -0,0 +1,96 @@
+#include <iostream>
+#include "gtest/gtest.h"
+#include "mockcpp/mokc.h"
+#include <mockcpp/mockcpp.hpp>
+#define private public
+#include "next/comms/endpoint_pairs/channels/aiv/aiv_ub_mem_transport.h"
+#include "runtime_api_exception.h"
+#include "socket_exception.h"
+
+using namespace hcomm;
+
+class AivUbMemTransportTest : public testing::Test {
+protected:
+    static void SetUpTestCase()
+    {
+        GlobalMockObject::verify();
+        std::cout << "AivUbMemTransportTest tests set up." << std::endl;
+    }
+
+    static void TearDownTestCase()
+    {
+        GlobalMockObject::verify();
+        std::cout << "AivUbMemTransportTest tests tear down." << std::endl;
+    }
+
+    virtual void SetUp()
+    {
+        GlobalMockObject::verify();
+        std::cout << "A Test case in AivUbMemTransportTest SetUP" << std::endl;
+    }
+
+    virtual void TearDown()
+    {
+        GlobalMockObject::verify();
+        std::cout << "A Test case in AivUbMemTransportTest TearDown" << std::endl;
+    }
+};
+
+TEST_F(AivUbMemTransportTest, St_GetStatus_When_SOCKET_OK_Expect_Success)
+{
+    Hccl::Socket *fakeSocket = reinterpret_cast<Hccl::Socket *>(0x1);
+    HcommChannelDesc desc{};
+    AivUbMemTransport aivTransport(fakeSocket, desc);
+
+    aivTransport.baseStatus_ = Hccl::TransportStatus::SOCKET_OK;
+    aivTransport.aivUbStatus_ = hcomm::AivUbMemTransport::AivUbMemTransportStatus::SOCKET_OK;
+    
+    MOCKER_CPP(&AivUbMemTransport::RmtBufferUnpackProc).stubs();
+
+    EXPECT_EQ(aivTransport.GetStatus(), Hccl::TransportStatus::SOCKET_OK);
+    EXPECT_EQ(aivTransport.aivUbStatus_, hcomm::AivUbMemTransport::AivUbMemTransportStatus::SEND_DATA_SIZE);
+
+    EXPECT_EQ(aivTransport.GetStatus(), Hccl::TransportStatus::SOCKET_OK);
+    EXPECT_EQ(aivTransport.aivUbStatus_, hcomm::AivUbMemTransport::AivUbMemTransportStatus::RECV_DATA_SIZE);
+
+    EXPECT_EQ(aivTransport.GetStatus(), Hccl::TransportStatus::SOCKET_OK);
+    EXPECT_EQ(aivTransport.aivUbStatus_, hcomm::AivUbMemTransport::AivUbMemTransportStatus::SEND_MEM_INFO);
+
+    EXPECT_EQ(aivTransport.GetStatus(), Hccl::TransportStatus::SOCKET_OK);
+    EXPECT_EQ(aivTransport.aivUbStatus_, hcomm::AivUbMemTransport::AivUbMemTransportStatus::RECV_MEM_INFO);
+
+    EXPECT_EQ(aivTransport.GetStatus(), Hccl::TransportStatus::SOCKET_OK);
+    EXPECT_EQ(aivTransport.aivUbStatus_, hcomm::AivUbMemTransport::AivUbMemTransportStatus::RECV_MEM_FIN);
+
+    EXPECT_EQ(aivTransport.GetStatus(), Hccl::TransportStatus::READY);
+    EXPECT_EQ(aivTransport.aivUbStatus_, hcomm::AivUbMemTransport::AivUbMemTransportStatus::READY);
+}
+
+TEST_F(AivUbMemTransportTest, Ut_GetStatus_When_RecvData_Fail_Expect_Status_Invalid)
+{
+    Hccl::Socket *fakeSocket = reinterpret_cast<Hccl::Socket *>(0x1);
+    HcommChannelDesc desc{};
+    AivUbMemTransport aivTransport(fakeSocket, desc);
+
+    aivTransport.baseStatus_ = Hccl::TransportStatus::SOCKET_OK;
+    aivTransport.aivUbStatus_ = hcomm::AivUbMemTransport::AivUbMemTransportStatus::RECV_MEM_INFO;
+
+    MOCKER_CPP(&AivUbMemTransport::RmtBufferUnpackProc).stubs().will(throws(Hccl::RuntimeApiException("test_fail")));
+
+    EXPECT_EQ(aivTransport.GetStatus(), Hccl::TransportStatus::INVALID);
+    EXPECT_EQ(aivTransport.aivUbStatus_, hcomm::AivUbMemTransport::AivUbMemTransportStatus::RECV_MEM_FIN);
+}
+
+TEST_F(AivUbMemTransportTest, Ut_GetStatus_When_SocketReady_Fail_Expect_Status_Invalid)
+{
+    Hccl::Socket *fakeSocket = reinterpret_cast<Hccl::Socket *>(0x1);
+    HcommChannelDesc desc{};
+    AivUbMemTransport aivTransport(fakeSocket, desc);
+
+    aivTransport.baseStatus_ = Hccl::TransportStatus::SOCKET_OK;
+    aivTransport.aivUbStatus_ = hcomm::AivUbMemTransport::AivUbMemTransportStatus::RECV_MEM_INFO;
+
+    MOCKER_CPP(&Hccl::Socket::GetAsyncStatus).stubs().will(throws(Hccl::SocketException("test_fail")));
+
+    EXPECT_EQ(aivTransport.GetStatus(), Hccl::TransportStatus::INVALID);
+}
