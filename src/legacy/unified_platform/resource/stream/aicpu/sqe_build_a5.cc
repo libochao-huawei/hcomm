66f9aa8d9900bf25efc9ed33a1629cf25779e3b4
liuhaoyu35 <liuhaoyu35@huawei.com>
2026-05-08 14:50:44 +0800

    test

--- src/legacy/unified_platform/resource/stream/aicpu/sqe_build_a5.cc
+++ src/legacy/unified_platform/resource/stream/aicpu/sqe_build_a5.cc
@@ -81,6 +81,7 @@ void ConstructNop(Rt91095StarsCondOpNop_t &nop)
 
 void BuildA5SqeCCoreNotifyWait(u32 streamId, u32 taskId, u64 waitAddr, u64 actAddr, bool last, uint8_t * const sqeIn)
 {
+    FUNCTION_TRACE;
     Rt91095StarsCCoreSqeNotifyWait* sqe = (Rt91095StarsCCoreSqeNotifyWait *)sqeIn;
     sqe->header.type = static_cast<uint8_t>(Rt91095StarsSqeType::RT_91095_SQE_TYPE_COND);
     sqe->header.rtStreamId = static_cast<uint16_t>(taskId);
@@ -129,6 +130,7 @@ void BuildA5SqeCCoreNotifyWait(u32 streamId, u32 taskId, u64 waitAddr, u64 actAd
 
 void BuildA5SqeCCoreNotifyRecord(u32 streamId, u32 taskId, u64 writeAddr, u64 valueAddr, uint8_t * const sqeIn)
 {
+    FUNCTION_TRACE;
     Rt91095StarsCCoreSqeNotifyRecord* sqe = (Rt91095StarsCCoreSqeNotifyRecord *)sqeIn;
     sqe->header.type = static_cast<uint8_t>(Rt91095StarsSqeType::RT_91095_SQE_TYPE_COND);
     sqe->header.rtStreamId = static_cast<uint16_t>(taskId);
