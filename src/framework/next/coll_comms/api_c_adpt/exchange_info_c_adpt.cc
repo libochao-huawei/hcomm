2412f5b9c64c303dd94a8d2a5be64ff48d4fc18e
leironghao1 <leironghao1@h-partners.com>
2026-05-08 17:13:44 +0800

    rank间一致性校验向算子包提供交换信息能力

---
+++ src/framework/next/coll_comms/api_c_adpt/exchange_info_c_adpt.cc
@@ -0,0 +1,38 @@
+/**
+ * Copyright (c) 2025 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+#include "hccl_comm_pub.h"
+#include "hccl/hccl_res_expt.h"
+
+using namespace hccl;
+
+HcclResult HcclCommAddExchangeInfo(HcclComm comm, void* data, uint32_t length)
+{
+    CHK_PTR_NULL(comm);
+    CHK_PTR_NULL(data);
+    CHK_PRT_RET(length == 0, HCCL_ERROR("[HcclCommAddExchangeInfo] length is 0."), HCCL_E_PARA);
+    hccl::hcclComm *hcclComm = static_cast<hccl::hcclComm *>(comm);
+    return hcclComm->AddExchangeInfo(data, length);
+}
+
+HcclResult HcclCommGetExchangeInfo(HcclComm comm, uint32_t remoteRank, void* data, uint32_t &length)
+{
+    CHK_PTR_NULL(comm);
+    CHK_PTR_NULL(data);
+    hccl::hcclComm *hcclComm = static_cast<hccl::hcclComm *>(comm);
+    return hcclComm->GetExchangeInfo(remoteRank, data, length);
+}
+
+HcclResult HcclCommResetExchangeInfo(HcclComm comm)
+{
+    CHK_PTR_NULL(comm);
+    hccl::hcclComm *hcclComm = static_cast<hccl::hcclComm *>(comm);
+    return hcclComm->ResetExchangeInfo();
+}
