c95c7c67d42c8aca9d986d62a8394d0dceb968a2
xiekunpeng <xiekunpeng1@huawei.com>
2026-05-08 05:02:30 -0400

    修复loadArg参数个数大于13的问题，LocalAddr/RemoteAddr支持直接赋值，增加StoreVar接口

--- pkg_inc/hcomm/ccu_new/ccu_local_addr.hpp
+++ pkg_inc/hcomm/ccu_new/ccu_local_addr.hpp
@@ -17,7 +17,10 @@ public:
         this->addr.handle = other.addr.handle;
         this->token.handle = other.token.handle;
     }
-
+    void operator=(const LocalAddr& other) {
+        this->addr = other.addr;     
+        this->token = other.token;   
+    }
     void operator=(LocalAddr&& other) {
         this->handle = other.handle;
         this->addr.handle = other.addr.handle;
