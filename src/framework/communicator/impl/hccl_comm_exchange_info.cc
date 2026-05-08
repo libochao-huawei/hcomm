ada95a4ec6a15e879c4eb9540afa536fb95ff9a2
leironghao1 <leironghao1@h-partners.com>
2026-05-08 17:13:44 +0800

    rank间一致性校验向算子包提供交换信息能力

---
+++ src/framework/communicator/impl/hccl_comm_exchange_info.cc
@@ -0,0 +1,104 @@
+/**
+ * Copyright (c) 2025 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+#include "hccl_comm_pub.h"
+
+namespace hccl {
+HcclResult hcclComm::AddExchangeInfo(void* data, uint32_t length)
+{
+    CHK_PTR_NULL(data);
+
+    std::lock_guard<std::mutex> lock(exchangeInfoMutex_);
+    CHK_PRT_RET(exchangeInfoReady_, 
+        HCCL_ERROR("[AddExchangeInfo] exchangeInfo already exists, consume it first via HcclChannelAcquire."), 
+        HCCL_E_PARA);
+
+    exchangeInfoBuf_.resize(length);
+    s32 sRet = memcpy_s(exchangeInfoBuf_.data(), length, data, length);
+    CHK_PRT_RET(sRet != EOK, 
+        HCCL_ERROR("[AddExchangeInfo] memcpy_s failed, ret[%d]", sRet), HCCL_E_MEMORY);
+    exchangeInfoLen_ = length;
+    exchangeInfoReady_ = true;
+    HCCL_INFO("[AddExchangeInfo] success, length[%u].", length);
+    return HCCL_SUCCESS;
+}
+
+HcclResult hcclComm::GetExchangeInfo(uint32_t remoteRank, void* data, uint32_t &length)
+{
+    CHK_PTR_NULL(data);
+
+    std::lock_guard<std::mutex> lock(exchangeInfoMutex_);
+    auto iter = remoteExchangeInfoMap_.find(remoteRank);
+    if (iter == remoteExchangeInfoMap_.end()) {
+        length = 0;
+        return HCCL_SUCCESS;
+    }
+    length = iter->second.length;
+    
+    s32 sRet = memcpy_s(data, iter->second.length, iter->second.data.data(), iter->second.length);
+    CHK_PRT_RET(sRet != EOK, 
+        HCCL_ERROR("[GetExchangeInfo] memcpy_s failed, ret[%d]", sRet), HCCL_E_MEMORY);
+
+    // 读后清除
+    remoteExchangeInfoMap_.erase(iter);
+    HCCL_INFO("[GetExchangeInfo] success, remoteRank[%u], length[%u].", remoteRank, length);
+    return HCCL_SUCCESS;
+}
+
+HcclResult hcclComm::StoreRemoteExchangeInfo(uint32_t remoteRank, const std::vector<u8>& data)
+{
+    std::lock_guard<std::mutex> lock(exchangeInfoMutex_);
+    ExchangeInfoEntry infoEntry;
+    infoEntry.data = data;
+    infoEntry.length = static_cast<uint32_t>(data.size());
+    remoteExchangeInfoMap_[remoteRank] = std::move(infoEntry);
+    HCCL_INFO("[StoreRemoteExchangeInfo] success, remoteRank[%u], length[%u].", remoteRank, infoEntry.length);
+    return HCCL_SUCCESS;
+}
+
+HcclResult hcclComm::ResetExchangeInfo()
+{
+    std::lock_guard<std::mutex> lock(exchangeInfoMutex_);
+    exchangeInfoReady_ = false;
+    exchangeInfoBuf_.clear();
+    exchangeInfoLen_ = 0;
+    HCCL_INFO("[ResetExchangeInfo] exchange info state cleared.");
+    return HCCL_SUCCESS;
+}
+
+bool hcclComm::IsExchangeInfoReady() const
+{
+    return exchangeInfoReady_;
+}
+
+const std::vector<u8>& hcclComm::GetExchangeInfoBuf() const
+{
+    return exchangeInfoBuf_;
+}
+
+uint32_t hcclComm::GetExchangeInfoLen() const
+{
+    return exchangeInfoLen_;
+}
+
+bool hcclComm::IsNewRemoteRank(uint32_t remoteRank) const
+{
+    return checkedRemoteRanks_.find(remoteRank) == checkedRemoteRanks_.end();
+}
+
+void hcclComm::MarkRemoteRankChecked(uint32_t remoteRank)
+{
+    std::lock_guard<std::mutex> lock(exchangeInfoMutex_);
+    checkedRemoteRanks_.insert(remoteRank);
+    HCCL_DEBUG("[MarkRemoteRankChecked] remoteRank[%u] marked as checked.", remoteRank);
+}
+
+}
+
--- src/framework/next/coll_comms/api_c_adpt/coll_comm_res_c_adpt.cc
+++ src/framework/next/coll_comms/api_c_adpt/coll_comm_res_c_adpt.cc
@@ -272,14 +272,13 @@ HcclResult HcclChannelAcquire(HcclComm comm, CommEngine engine,
                 __func__, hcclComm, hcclComm->GetIdentifier().c_str(), opExpansionMode, engine);
             return HcclResult::HCCL_E_PARA;
         }
-
         if (engine != CommEngine::COMM_ENGINE_CPU) { // host dpu场景暂不支持cluster monitor
             ret = RegisterToClusterMonitor(comm);
             CHK_PRT_RET(ret != HCCL_SUCCESS,
                 HCCL_ERROR("RegisterToClusterMonitor failed. group[%s], engine[%d], channelNum[%llu], ret[%d]", hcclComm->GetIdentifier().c_str(), engine, channelNum, ret), ret);
         }
 
-        ret = myRank->CreateChannels(engine, commTag, channelDescFinals.data(), channelNum, channels);
+        ret = myRank->CreateChannels(engine, commTag, channelDescFinals.data(), channelNum, channels, hcclComm);
         CHK_PRT_RET((ret == HCCL_E_AGAIN || ret == HCCL_E_UNAVAIL),
             HCCL_WARNING("CreateChannels group[%s], engine[%d] ret[%d]", commTag.c_str(), engine, ret), ret);
         CHK_PRT_RET(ret != HCCL_SUCCESS,
@@ -568,8 +567,23 @@ HcclResult HcclCcuKernelLaunch(HcclComm comm, const ThreadHandle threadHandle,
     }
     std::vector<hcomm::CcuProfilingInfo> allCcuProfilingInfo;
     CHK_RET(kernel->GetCcuProfilingInfo(*ccuTaskArgs, allCcuProfilingInfo));
-    Hccl::TaskParam taskParam = {};
-    taskParam.taskType = Hccl::TaskParamType::TASK_CCU;
+    Hccl::TaskParam taskParam = {
+        .taskType  = Hccl::TaskParamType::TASK_CCU,
+        .beginTime = 0,
+        .endTime   = 0,
+        .isMaster = false,
+        .taskPara  = {
+            .Ccu = {
+                .dieId         = 0,
+                .missionId     = 0,
+                .execMissionId = 0,
+                .instrId       = 0,
+                .costumArgs    = {},
+                .executeId     = 0
+            }
+        },
+        .ccuDetailInfo  = nullptr
+    };
     CHK_RET(LaunchCcuTasks(ccuParams, streamPtr, taskParam));
     CHK_RET(HcclReportCcuProfilingInfo(threadHandle, kernelHandle, allCcuProfilingInfo.data(), allCcuProfilingInfo.size(),
                                         comm, taskParam, rtsThread->GetMaster()));
