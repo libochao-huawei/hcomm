800a7099c7ce8834e69278575b39f9c72380c18b
p_ch <pengchenghao1@huawei.com>
2026-05-09 09:35:24 +0800

    feat: change threshold to 4MB

--- src/algorithm/impl/operator/reduce_scatter_operator.cc
+++ src/algorithm/impl/operator/reduce_scatter_operator.cc
@@ -525,7 +525,7 @@ HcclResult ReduceScatterOperator::SelectAlgfor91093(const OpParam& param, std::s
                isSupportInlineReduce &&
                (topoType_ == TopoType::TOPO_TYPE_NP_SINGLE_RING ||
                 topoType_ == TopoType::TOPO_TYPE_NP_DOUBLE_RING) &&
-               maxStablePipelineBlockSize >= HCCL_SMALL_COUNT_1_MB) {
+               maxStablePipelineBlockSize >= HCCL_SMALL_COUNT_4_MB) {
         algName = "ReduceScatterPipelineFor91093Executor";
     } else {
         if (topoType_ == TopoType::TOPO_TYPE_NP_SINGLE_RING) {
