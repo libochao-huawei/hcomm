3126177e936fedb18e0ef0cb66aaa52f6c76dee4
renyumin <renyumin1@huawei.com>
2026-05-09 11:05:31 +0800

    merge

--- src/framework/next/comms/endpoint_pairs/channels/channel_process.cc
+++ src/framework/next/comms/endpoint_pairs/channels/channel_process.cc
@@ -136,7 +136,7 @@ HcclResult ChannelProcess::ChannelUpdateMemInfo(HcommMemHandle *memHandles, uint
         }
         channel = itC->second.get();
     }
-    // UpdateMemInfo需要rank间交互，若在锁内执行会导致其他rank被锁拦住
+    // UpdateMemInfo需要rank间交互，若在锁内执行会导致单进程多线程场景其他rank被锁拦住
     CHK_RET(channel->UpdateMemInfo(memHandles, memHandleNum));
     EXCEPTION_HANDLE_END
     return HCCL_SUCCESS;
