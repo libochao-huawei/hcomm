06a22e2bfd2ea5c06d57977168b7861821458c89
lhdlhdlhd1234569 <lihaodong19@h-partners.com>
2026-05-09 18:49:20 +0800

    update
    Co-authored-by: lhdlhdlhd1234569<lihaodong19@h-partners.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !1735 merge master into master
    
    update
    
    Created-by: lhdlhdlhd1234569
    Commit-by: lhdlhdlhd1234569
    Merged-by: cann-robot
    Description: ## 描述
    支持HCCL ERROR MESSAGE EI0012-EI0015、EI0017、EI0018错误码打屏显示
    
    ## 关联的Issue
    NA
    
    ## 测试
    RDV pass
    
    ## 文档更新
    NA
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [x] 其他，请描述：
    
    
    
    
    See merge request: cann/hcomm!1735

--- src/legacy/framework/topo/rank_info_detect/rank_info_dispatcher.cc
+++ src/legacy/framework/topo/rank_info_detect/rank_info_dispatcher.cc
@@ -23,6 +23,7 @@
 #include "env_config.h"
 #include "hccp_common.h"
 #include "network_api_exception.h"
+#include "adapter_error_manager_pub.h"
 
 namespace Hccl {
 
@@ -205,8 +206,15 @@ void RankInfoDispather::ProcessSend()
         }
 
         //循环超时
-        CHK_PRT_THROW(((std::chrono::steady_clock::now() - startTime) >= timeout), 
-                        HCCL_ERROR("[RankInfoDispather::%s] epoll_wait timeout.", __func__), TimeoutException, "epoll_wait timeout");
+        if ((std::chrono::steady_clock::now() - startTime) >= timeout) {
+            HCCL_ERROR("[RankInfoDispather::%s] epoll_wait timeout, timeout[%lld s].", __func__,
+                static_cast<long long>(timeout.count()));
+            RPT_INPUT_ERR(true, "EI0015", std::vector<std::string>({"error_reason"}),
+                std::vector<std::string>({StringFormat("Receiving message from the root node timed out "
+                    "Timeout was set to %lld seconds. Expected to send to %u nodes, completed %u nodes.",
+                    static_cast<long long>(timeout.count()), rankNum_, sendDoneCount_.load())}));
+            THROW<TimeoutException>("epoll_wait timeout");
+        }
         
         // 等待epoll事件
         s32 epollTimeout = lastEpollWaitFlag ? LAST_EPOLL_TIMEOUT_MS : EPOLL_TIMEOUT_MS;
