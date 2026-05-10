94649d7951e277fd525b3db05fc4edccbffcb10c
littlemons <yaowenjie7@huawei.com>
2026-05-11 00:20:40 +0800

    ut

--- test/ut/framework/next/host/ut_host_cpu_roce_channel.cc
+++ test/ut/framework/next/host/ut_host_cpu_roce_channel.cc
@@ -162,6 +162,7 @@ protected:
         channelDesc.memHandles = &memHandle_;
         channelDesc.memHandleNum = 1;
         channelDesc.notifyNum = notifyNum;
+        channelDesc.roceAttr.queueNum = 1;
         auto impl = std::make_unique<hcomm::HostCpuRoceChannel>(endpointHandle, channelDesc);
         EXPECT_EQ(impl->Init(), HCCL_SUCCESS);
         hcomm::ChannelStatus status = impl->GetStatus();
@@ -659,6 +660,7 @@ TEST_F(HostCpuRoceChannelTest, Ut_When_HostCpuRoceChannel_Pack_And_Unpack_Expect
     MOCKER_CPP(&HostCpuRoceChannel::ExchangeCapability).stubs().will(returnValue(HCCL_SUCCESS));
     MOCKER_CPP(&HostCpuRoceChannel::ExchangeData).stubs().will(returnValue(HCCL_SUCCESS));
     MOCKER_CPP(&HostCpuRoceChannel::IbvPostRecv).stubs().will(returnValue(HCCL_SUCCESS));
+    MOCKER(RaDestroyCompChannel).stubs().with(any(), any()).will(returnValue(0));
     // construct
     void *memHandle = static_cast<void *>(localRdmaRmaBuffer.get());
     channelDesc.memHandles = &memHandle;
@@ -682,8 +684,8 @@ TEST_F(HostCpuRoceChannelTest, Ut_When_HostCpuRoceChannel_Pack_And_Unpack_Expect
     EXPECT_EQ(impl_->rdmaStatus_, HostCpuRoceChannel::RdmaStatus::DATA_EXCHANGE);
     EXPECT_EQ(status, ChannelStatus::SOCKET_OK);
     status = impl_->GetStatus();
-    EXPECT_EQ(impl_->rdmaStatus_, HostCpuRoceChannel::RdmaStatus::QP_MODIFIED);
-    EXPECT_EQ(status, ChannelStatus::SOCKET_OK);
+    EXPECT_EQ(impl_->rdmaStatus_, HostCpuRoceChannel::RdmaStatus::CONN_OK);
+    EXPECT_EQ(status, ChannelStatus::READY);
     struct QpAttr localQpAttr;
     localQpAttr.qpn = 0;
     localQpAttr.udpSport = 1;
