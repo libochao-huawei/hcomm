3a9363fcbce87b28c6de3e0f68957c7cc8a17666
andy_git <liaomeifeng@huawei.com>
2026-05-11 17:40:01 +0800

    feat:add hccs endpoint
    Co-authored-by: andy_git<liaomeifeng@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !1404 merge one_side_service into master
    
    feat:add hccs endpoint
    
    Created-by: andy_git
    Commit-by: andy_git
    Merged-by: cann-robot
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    A2A3使用endpoint实现hccs的部分单边通信能力
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。-->
    <!-- 如果这个PR不涉及Issue，可填写"NA"。-->
    https://gitcode.com/cann/hcomm/issues/220
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于构造对应xx测试用例、二级冒烟、算子泛化等。-->
    1.ut/st设计
    针对endpoint管理，设计AiCpuTsHccsEndpointTest，主要测试endpoint的创建，内存注册，去注册等相关逻辑的验证
    针对channel管理，设计TestHcommHccsChannel，主要测试channel的创建，包括内部依赖的endpoint对的创建，channel对的创建，内存的注册，导出，导入等
    
    2.冒烟设计
    联合hixl的修改，设计d2d, h2d(仅针对A3)的全流程验证
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [x] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    
    
    See merge request: cann/hcomm!1404

--- src/framework/next/comms/api_c_adpt/hcomm_c_adpt.cc
+++ src/framework/next/comms/api_c_adpt/hcomm_c_adpt.cc
@@ -194,6 +194,7 @@ HcommResult HcommEndpointGet(EndpointHandle endpointHandle, void **endpoint)  //
         __func__, endpointHandle), HCCL_E_NOT_FOUND);
 
     *endpoint = static_cast<void *>(it);
+    HCCL_INFO("[%s] START. endpointHandle[0x%llx] endpoint[0x%llx].",__func__, endpointHandle, endpoint);
     return HCCL_SUCCESS;
 }
 
@@ -229,12 +230,15 @@ HcommResult HcommEndpointCreate(const EndpointDesc *endpoint, EndpointHandle *en
     EXECEPTION_CATCH(g_EndpointMap.AddEndpoint(handle, std::move(endpointPtr)), return HCCL_E_INTERNAL);
     *endpointHandle = handle;
 
+    HCCL_INFO("[%s] endpointDesc.protocol [%d] and endpointDesc.loc.locType [%d] create endpointHandle [%p] done.", 
+            __func__, endpoint->protocol, endpoint->loc.locType, handle);
     EXCEPTION_HANDLE_END
     return HCCL_SUCCESS;
 }
 
 HcommResult HcommEndpointDestroy(EndpointHandle endpointHandle)
 {
+    HCCL_INFO("[%s] START. endpointHandle[0x%llx].",__func__, endpointHandle);
     auto ret = g_EndpointMap.RemoveEndpoint(endpointHandle);
     CHK_PRT_RET(ret == false, HCCL_ERROR("[%s] endpoint not found, endpointHandle[0x%llx]",
         __func__, endpointHandle), HCCL_E_NOT_FOUND);
@@ -270,6 +274,7 @@ HcommResult HcommMemReg(EndpointHandle endpointHandle, const char *memTag, const
     EXCEPTION_HANDLE_BEGIN
     CHK_PTR_NULL(mem);
     CHK_PTR_NULL(memHandle);
+    HCCL_INFO("[%s] START. endpointHandle[0x%llx].",__func__, endpointHandle);
     auto endpoint = g_EndpointMap.GetEndpoint(endpointHandle);
     CHK_PRT_RET(endpoint == nullptr, HCCL_ERROR("[%s] endpoint not found, endpointHandle[0x%llx]",
         __func__, endpointHandle), HCCL_E_NOT_FOUND);
@@ -282,6 +287,7 @@ HcommResult HcommMemUnreg(EndpointHandle endpointHandle, HcommMemHandle memHandl
 {
     CHK_PTR_NULL(memHandle);
     EXCEPTION_HANDLE_BEGIN
+    HCCL_INFO("[%s] START. endpointHandle[0x%llx].",__func__, endpointHandle);
     auto endpoint = g_EndpointMap.GetEndpoint(endpointHandle);
     CHK_PRT_RET(endpoint == nullptr, HCCL_ERROR("[%s] endpoint not found, endpointHandle[0x%llx]",
         __func__, endpointHandle), HCCL_E_NOT_FOUND);
@@ -296,6 +302,7 @@ HcommResult HcommMemExport(EndpointHandle endpointHandle, HcommMemHandle memHand
     CHK_PTR_NULL(memHandle);
     CHK_PTR_NULL(memDesc);
     CHK_PTR_NULL(memDescLen);
+    HCCL_INFO("[%s] START. endpointHandle[0x%llx].",__func__, endpointHandle);
     auto endpoint = g_EndpointMap.GetEndpoint(endpointHandle);
     CHK_PRT_RET(endpoint == nullptr, HCCL_ERROR("[%s] endpoint not found, endpointHandle[0x%llx]",
         __func__, endpointHandle), HCCL_E_NOT_FOUND);
@@ -307,6 +314,7 @@ HcommResult HcommMemImport(EndpointHandle endpointHandle, const void *memDesc, u
 {
     CHK_PTR_NULL(memDesc);
     CHK_PTR_NULL(outMem);
+    HCCL_INFO("[%s] START. endpointHandle[0x%llx].",__func__, endpointHandle);
     auto endpoint = g_EndpointMap.GetEndpoint(endpointHandle);
     CHK_PRT_RET(endpoint == nullptr, HCCL_ERROR("[%s] endpoint not found, endpointHandle[0x%llx]",
         __func__, endpointHandle), HCCL_E_NOT_FOUND);
@@ -320,6 +328,7 @@ HcommResult HcommMemImport(EndpointHandle endpointHandle, const void *memDesc, u
 HcommResult HcommMemUnimport(EndpointHandle endpointHandle, const void *memDesc, uint32_t descLen)
 {
     CHK_PTR_NULL(memDesc);
+    HCCL_INFO("[%s] START. endpointHandle[0x%llx].",__func__, endpointHandle);
     auto endpoint = g_EndpointMap.GetEndpoint(endpointHandle);
     CHK_PRT_RET(endpoint == nullptr, HCCL_ERROR("[%s] endpoint not found, endpointHandle[0x%llx]",
         __func__, endpointHandle), HCCL_E_NOT_FOUND);
@@ -330,7 +339,13 @@ HcommResult HcommMemUnimport(EndpointHandle endpointHandle, const void *memDesc,
 /* 暂未实现 */
 HcommResult HcommMemGrant(EndpointHandle endpointHandle, const HcommMemGrantInfo *remoteGrantInfo)
 {
-    return HCCL_E_NOT_SUPPORT;
+    CHK_PTR_NULL(remoteGrantInfo);
+    HCCL_INFO("[%s] START. endpointHandle[0x%llx].",__func__, endpointHandle);
+    auto endpoint = g_EndpointMap.GetEndpoint(endpointHandle);
+    CHK_PRT_RET(endpoint == nullptr, HCCL_ERROR("[%s] endpoint not found, endpointHandle[0x%llx]",
+        __func__, endpointHandle), HCCL_E_NOT_FOUND);
+    CHK_RET(endpoint->MemoryGrant(remoteGrantInfo));
+    return HCCL_SUCCESS;
 }
 
 /* 暂未实现 */
