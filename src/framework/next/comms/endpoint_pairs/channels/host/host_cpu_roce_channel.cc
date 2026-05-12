a1ee7b9eb19f1585c4520157bb2f105c580ec1c2
hblnb <huangbolin3@huawei.com>
2026-05-12 21:29:13 +0800

    解决重复创建channel时，只能创建一个socket导致后续channel创建失败的问题

--- src/framework/next/comms/endpoint_pairs/channels/host/host_cpu_roce_channel.cc
+++ src/framework/next/comms/endpoint_pairs/channels/host/host_cpu_roce_channel.cc
@@ -72,6 +72,11 @@ HostCpuRoceChannel::~HostCpuRoceChannel() {
     if (ret != HCCL_SUCCESS) {
         HCCL_ERROR("[HostCpuRoceChannel::~HostCpuRoceChannel] exception occurred, HcclResult=[%d]", ret);
     }
+
+    if (socket_ != nullptr) {
+        SocketMgr::GetInstance().PutSocket(socketConfig_, socket_);
+        socket_ = nullptr;
+    }
 }
 
 HcclResult HostCpuRoceChannel::ParseInputParam()
@@ -115,8 +120,6 @@ HcclResult HostCpuRoceChannel::ParseInputParam()
         }
     }
 
-    EXECEPTION_CATCH(socketMgr_ = std::make_unique<SocketMgr>(), return HCCL_E_PTR);
-
     auto* localCpuRoceEpPtr = dynamic_cast<CpuRoceEndpoint *>(localEpPtr);
     if (localCpuRoceEpPtr == nullptr) {
         HCCL_ERROR("[HostCpuRoceChannel][%s] endpointHandle_ is not CpuRoceEndpoint.", __func__);
@@ -151,6 +154,7 @@ HcclResult HostCpuRoceChannel::StartListen()
 HcclResult HostCpuRoceChannel::BuildSocket()
 {
     if (socket_ != nullptr) {
+        HCCL_INFO("[HostCpuRoceChannel::%s] socket ptr is not NULL, return success", __func__);
         return HCCL_SUCCESS;
     }
     HCCL_INFO("[HostCpuRoceChannel::%s] socket ptr is NULL, rebuild Socket", __func__);
@@ -165,7 +169,7 @@ HcclResult HostCpuRoceChannel::BuildSocket()
     }
     std::string socketTag = "AUTOMATIC_SOCKET_TAG";
     Hccl::SocketConfig socketConfig = Hccl::SocketConfig(linkData, port, socketTag);
-    CHK_RET(socketMgr_->GetSocket(socketConfig, socket_));
+    CHK_RET(SocketMgr::GetInstance().GetSocket(socketConfig, socket_));
     HCCL_INFO("[HostCpuRoceChannel::%s] SUCCESS. port[%u].", __func__, port);
     return HCCL_SUCCESS;
 }
@@ -227,6 +231,21 @@ ChannelStatus HostCpuRoceChannel::GetStatus()
     return status;
 }
 
+HcclResult HostCpuRoceChannel::ProcessStatus()
+{
+    switch (channelStatus_) {
+        case ChannelStatus::READY:
+            if (socket_ != nullptr) {
+                SocketMgr::GetInstance().PutSocket(socketConfig_, socket_);
+            }
+            return HCCL_SUCCESS;
+        case ChannelStatus::SOCKET_TIMEOUT:
+            return HCCL_E_ROCE_CONNECT;
+        default:
+            return HCCL_E_AGAIN;
+    }
+}
+
 HcclResult HostCpuRoceChannel::GetStatus(ChannelStatus &status) {
     switch (rdmaStatus_) {
         case RdmaStatus::INIT:
@@ -272,14 +291,7 @@ HcclResult HostCpuRoceChannel::GetStatus(ChannelStatus &status) {
     }
 
     status = channelStatus_;
-    switch (channelStatus_) {
-        case ChannelStatus::READY:
-            return HCCL_SUCCESS;
-        case ChannelStatus::SOCKET_TIMEOUT:
-            return HCCL_E_ROCE_CONNECT;
-        default:
-            return HCCL_E_AGAIN;
-    }
+    return ProcessStatus();
 }
 
 HcclResult HostCpuRoceChannel::CheckSocketStatus() {
@@ -577,7 +589,11 @@ std::string HostCpuRoceChannel::Describe() const
         msg += ", ";
     }
     msg += Hccl::StringFormat("], rdmaHandle: %p, %s, ", rdmaHandle_, channelStatus_.Describe().c_str());
-    msg += socket_->Describe();
+
+    if (socket_ != nullptr) {
+        msg += socket_->Describe();
+    }
+    
     msg += ", ";
     // msg += attr_.Describe();
     return msg;
@@ -1445,8 +1461,10 @@ HcclResult HostCpuRoceChannel::ConnectSingleQpHybrid(std::function<bool()> needS
 {
     auto qpInfo = connections_[0]->GetQpInfo();
 
+    CHK_RET(SocketMgr::GetInstance().GetSocket(*socketConfig_, socket_));
     CHK_RET(HrtRaQpConnectAsync(qpInfo.qpHandle, socket_->GetFdHandle(), needStop));
-
+    SocketMgr::GetInstance().PutSocket(socketConfig_, socket_);
+    
     // 查询QP建链是否成功
     s32 qpStatus = 0;
     s32 raRet = 0;
