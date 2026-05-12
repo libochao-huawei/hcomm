ade0fcc37c8eadd35c2f9795b19034005f693ab9
hblnb <huangbolin3@huawei.com>
2026-05-12 21:29:13 +0800

    解决重复创建channel时，只能创建一个socket导致后续channel创建失败的问题

--- src/framework/next/comms/endpoint_pairs/sockets/socket_process.h
+++ src/framework/next/comms/endpoint_pairs/sockets/socket_process.h
@@ -48,7 +48,6 @@ private:
     std::map<std::pair<Hccl::PortData, u32>, std::unique_ptr<Hccl::Socket>> serverSocketMap_{};
     std::unordered_map<std::string, std::pair<Hccl::Socket *, u32>> tag2socketMap_{};
     std::unordered_map<Hccl::Socket *, std::string> socket2TagMap_{};
-    std::unique_ptr<SocketMgr> socketMgr_{};
     std::mutex mutex_;
     u32 devicePhyId_{0};
 };
