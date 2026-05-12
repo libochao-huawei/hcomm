96f104046ec5520e064ec65ede1bf8387c5ca3cb
lhdlhdlhd1234569 <lihaodong19@h-partners.com>
2026-05-12 09:20:49 +0800

    update

--- src/framework/next/coll_comms/dfx/taskException/host/hcclCommTaskException.cc
+++ src/framework/next/coll_comms/dfx/taskException/host/hcclCommTaskException.cc
@@ -83,8 +83,8 @@ void TaskExceptionHostManager::RegisterGetAicpuTaskExceptionCallBack(s32 streamI
 
 void TaskExceptionHostManager::UnregisterGetAicpuTaskExceptionCallBack(s32 streamId, u32 deviceLogicId)
 {
-    lock_guard<mutex> lock(g_communicatorCallbackMapMutexV2);
-    auto& deviceMap = g_communicatorCallbackMapV2[deviceLogicId];
+    lock_guard<mutex> lock(Hccl::g_communicatorCallbackMapMutexV2);
+    auto& deviceMap = Hccl::g_communicatorCallbackMapV2[deviceLogicId];
     auto it = deviceMap.find(streamId);
     if (it != deviceMap.end()) {
         deviceMap.erase(it);
