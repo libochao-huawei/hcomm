1d62ab4fbefde1151779918e69426a4b85ded341
wenp1213 <wenpeng26@huawei.com>
2026-05-12 15:41:57 +0800

    Debug AICPU batch mode in large data size

--- src/legacy/interface/aicpu_ts_thread_interface.cc
+++ src/legacy/interface/aicpu_ts_thread_interface.cc
@@ -78,6 +78,18 @@ void IAicpuTsThread::LaunchTask() const
     return;
 }
 
+void IAicpuTsThread::TryLaunchTask() const
+{
+    HCCL_INFO("[IAicpuTsThread::%s] TryLaunch Task at Stream id [%u]", __func__,
+        static_cast<StreamLite *>(streamLiteVoidPtr_)->GetId());
+
+    RtsqBase *rtsqA5 = static_cast<StreamLite *>(streamLiteVoidPtr_)->GetRtsq();
+    if (rtsqA5 != nullptr) {
+        rtsqA5->TryLaunchTask();
+    }
+    return;
+}
+
 HcclResult IAicpuTsThread::NotifyWait(uint32_t notifyId) const
 {
     return NotifyWait(notifyId, GetKernelExecTimeoutFromEnvConfig());
