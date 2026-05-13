e77e43613413a7afc027c23f6c76f83854f56b6a
zhanghedong <zhanghedong@huawei.com>
2026-05-13 10:44:56 +0800

    socket冲突问题

--- include/hcomm_res.h
+++ include/hcomm_res.h
@@ -42,6 +42,8 @@ extern HcommResult HcommThreadAlloc(CommEngine engine, uint32_t threadNum, const
 
 extern HcommResult HcommThreadFree(const ThreadHandle *threads, uint32_t threadNum);
 
+extern HcommResult HcommEndpointGetListenPort(EndpointHandle endpointHandle, uint32_t *port);
+
 #ifdef __cplusplus
 }
 #endif // __cplusplus
