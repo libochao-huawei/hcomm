f7f700a362edaa389adf6c5e5ed68ff9a69ed196
leironghao1 <leironghao1@h-partners.com>
2026-05-13 16:36:04 +0800

    rank间一致性校验向算子包提供交换信息能力

--- src/pub_inc/hccl_common.h
+++ src/pub_inc/hccl_common.h
@@ -117,6 +117,7 @@ constexpr uint32_t HCCL_QOS_LEVEL_1_LIMIT = 2;
 constexpr uint32_t HCCL_QOS_LEVEL_2_LIMIT = 4;
 constexpr uint32_t HCCL_QOS_LEVEL_3_LIMIT = 7;
 
+constexpr uint32_t HCCL_EXCHANGE_INFO_LEN = 4096;  // HCCL一致性校验交换信息最大长度
 /* error message相关 */
     /* EI0004 */
     const std::string RANKTABLE_PARSE_ERROR_REASON =
