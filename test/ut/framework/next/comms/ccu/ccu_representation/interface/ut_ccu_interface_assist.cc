3b77a23575f328de201ac7b8be0c3df523bec428
gcw_xljEvKEv <zenglingshun@h-partners.com>
2026-05-13 09:53:38 +0800

    修改

---
+++ test/ut/framework/next/comms/ccu/ccu_representation/interface/ut_ccu_interface_assist.cc
@@ -0,0 +1,43 @@
+#include "gtest/gtest.h"
+#include <mockcpp/mockcpp.hpp>
+
+
+#include "../../../../../../../pkg_inc/hcomm/ccu/ccu_rep_block_v1.h"
+#include "ccu_kernel.h"
+#include "exception_util.h"
+#include "ccu_api_exception.h"
+
+#define privite public
+
+using namespace hcomm;
+
+class CcuResBatchAllocatorTest : public testing::Test {
+protected:
+    static void SetUpTestCase() {
+        mockcpp::GlobalMockObject::verify();
+        mockcpp::GlobalMockObject::reset();
+    }
+    static void TearDownTestCase() {
+        mockcpp::GlobalMockObject::verify();
+        mockcpp::GlobalMockObject::reset();
+    }
+    void SetUp() override {
+        mockcpp::GlobalMockObject::verify();
+        mockcpp::GlobalMockObject::reset();
+    }
+    void TearDown() override {
+        mockcpp::GlobalMockObject::verify();
+        mockcpp::GlobalMockObject::reset();
+    }
+
+};
+
+TEST_F(CcuInterfaceAssistTest, Ut_AppendToContext_NullContext_ShouldThrowException)
+{
+    std::shared_ptr<CcuRep::CcuRepBase> rep = std::make_shared<CcuRep::CcuRepBlock>("test_rep");
+    
+    EXPECT_THROW(
+        AppendToContext(nullptr,rep),
+        Hccl::CcuApiException
+    );
+}
