af685a74a8f81023af6d6bd92f9fbd06aaeaa991
zwlStateflow <zhengwenliang@huawei.com>
2026-05-13 09:26:35 +0800

    adapt A2 / A5

--- src/legacy/framework/communicator/hostdpu/task_service.h
+++ src/legacy/framework/communicator/hostdpu/task_service.h
@@ -40,7 +40,7 @@ using ProfCallbackTemplate = std::function<HcclResult(const TaskParam&, uint64_t
 class TaskService {
 public:
     TaskService() = default;
-    TaskService(void* deviceMem, int32_t deviceMemSize, void* hostMem, int32_t hostMemSize);
+    TaskService(void* deviceMem, int32_t deviceMemSize, void* hostMem, int32_t hostMemSize, bool isHBM);
     HcclResult TaskRun();
     HcclResult TaskRegister(std::string taskType, CallbackTemplate callback);
     HcclResult TaskUnRegister(std::string taskType);
@@ -49,6 +49,7 @@ private:
     HcclResult WriteFlag(uint8_t *flagPtr, uint8_t newFlag) const;
     HcclResult ReadFlag(uint8_t *ctrlHdr, uint64_t hdrLen, uint8_t &flag) const;
     HcclResult ReadTaskType(uint8_t *ctrlHdr, uint64_t hdrLen, uint8_t *srcTaskTypePtr, std::string &taskTypeStr) const;
+    HcclResult CopyTaskDataToHost(uint8_t *ctrlHdr, uint64_t hdrLen, uint8_t *srcPtr, uint64_t dataLen);
     HcclResult ExecuteTask(uint8_t *ctrlHdr, uint64_t hdrLen, uint8_t *srcPtr, std::string taskTypeStr);
     HcclResult SynchronizeControlInfo(uint8_t *ctrlHdr, uint64_t hdrLen);
     HcclResult ProcessTaskOk(uint8_t *ctrlHdr, uint64_t hdrLen, uint8_t *srcFlagPtr, uint8_t *srcTaskTypePtr);
@@ -61,6 +62,7 @@ private:
     int32_t leftSize_{0}; // npu2dpuMem_中除去控制信息后剩余的可用空间大小
     void       *hostMem_{nullptr};
     int32_t hostMemSize_{0};
+    bool isHBM_{false};
 };
 } // namespace Hccl
 
