b46a6dd27b755a4cf995c97ac883215bb8cc461a
zhanghedong <zhanghedong@huawei.com>
2026-05-13 10:44:56 +0800

    socket冲突问题

--- src/framework/next/comms/endpoints/cpu_roce_endpoint.cc
+++ src/framework/next/comms/endpoints/cpu_roce_endpoint.cc
@@ -29,6 +29,15 @@ CpuRoceEndpoint::CpuRoceEndpoint(const EndpointDesc &endpointDesc)
 {
 }
 
+CpuRoceEndpoint::~CpuRoceEndpoint() noexcept
+{
+    std::lock_guard<std::mutex> lock(portMutex_);
+    if (dynamicPort_ != HCCL_INVALID_PORT) {
+        ServerSocketStopListen(dynamicPort_);
+    }
+    dynamicPort_ = HCCL_INVALID_PORT;
+}
+
 HcclResult CpuRoceEndpoint::Init()
 {
     HCCL_INFO("[%s] localEndpoint protocol[%d]", __func__, endpointDesc_.protocol);
@@ -75,8 +84,8 @@ HcclResult CpuRoceEndpoint::ServerSocketListen(const uint32_t port)
     HCCL_INFO("[CpuRoceEndpoint::%s] devicePhyId[%u] ipAddress[%s]",
         __func__, devPhyId, ipAddr.Describe().c_str());
 
-    CHK_RET(ServerSocketManager::GetInstance().ServerSocketStartListen(localPort, Hccl::NicType::HOST_NIC_TYPE, devPhyId, port));
-
+    uint32_t requestPort = port;
+    CHK_RET(ServerSocketManager::GetInstance().ServerSocketStartListen(localPort, Hccl::NicType::HOST_NIC_TYPE, devPhyId, &requestPort));
     return HCCL_SUCCESS;
 }
 
@@ -94,6 +103,41 @@ HcclResult CpuRoceEndpoint::ServerSocketStopListen(const uint32_t port)
     Hccl::PortData localPort = Hccl::PortData(devPhyId, type, 0, ipAddr);
     CHK_RET(ServerSocketManager::GetInstance().ServerSocketStopListen(localPort, Hccl::NicType::HOST_NIC_TYPE, port));
 
+    return HCCL_SUCCESS;
+}
+
+HcclResult CpuRoceEndpoint::ServerSocketGetListenPort(uint32_t *port)
+{
+    std::lock_guard<std::mutex> lock(portMutex_);
+    CHK_PTR_NULL(port);
+    s32 devId = 0;
+    CHK_RET(hrtGetDevice(&devId));
+    u32 devPhyId = 0;
+    CHK_RET(hrtGetDevicePhyIdByIndex(devId, devPhyId));
+
+    Hccl::IpAddress ipAddr{};
+    CHK_RET(CommAddrToIpAddress(endpointDesc_.commAddr, ipAddr));
+
+    Hccl::DevNetPortType type = Hccl::DevNetPortType(Hccl::ConnectProtoType::RDMA);
+    Hccl::PortData localPort = Hccl::PortData(devPhyId, type, 0, ipAddr);
+
+    HCCL_INFO("[CpuRoceEndpoint::%s] devicePhyId[%u] ipAddress[%s]",
+        __func__, devPhyId, ipAddr.Describe().c_str());
+
+    // 已有监听端口则直接返回
+    if (dynamicPort_ != HCCL_INVALID_PORT) {
+        *port = dynamicPort_;
+        HCCL_INFO("[CpuRoceEndpoint::%s] already listening, return existing port[%u]", __func__, dynamicPort_);
+        return HCCL_SUCCESS; 
+    }
+    uint32_t requestPort = 0;
+    CHK_RET(ServerSocketManager::GetInstance().ServerSocketStartListen(localPort, Hccl::NicType::HOST_NIC_TYPE, devPhyId, &requestPort));
+    if (requestPort == 0 || requestPort == HCCL_INVALID_PORT) {
+        HCCL_ERROR("[CpuRoceEndpoint::%s] get listen port failed, port is invalid", __func__);
+        return HCCL_E_NETWORK;
+    }
+    dynamicPort_ = requestPort;
+    *port = dynamicPort_;
     return HCCL_SUCCESS;
 }
 
