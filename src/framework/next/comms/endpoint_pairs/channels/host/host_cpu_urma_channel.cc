e8d365d2f0dffe914e0a3c549f8d61fefd502dec
zhanghedong <zhanghedong@huawei.com>
2026-05-13 10:44:56 +0800

    socket冲突问题

--- src/framework/next/comms/endpoint_pairs/channels/host/host_cpu_urma_channel.cc
+++ src/framework/next/comms/endpoint_pairs/channels/host/host_cpu_urma_channel.cc
@@ -97,10 +97,18 @@ HcclResult HostCpuUrmaChannel::BuildSocket()
         port = DEFAULT_LISTENING_PORT;
         HCCL_INFO("[HostCpuUrmaChannel::%s] channelDesc port is 0, use default port [%u]", __func__, port);
     }
+    
     std::string socketTag = "AUTOMATIC_SOCKET_TAG";
-    bool noRankId = true;
-    Hccl::SocketConfig socketConfig = Hccl::SocketConfig(linkData, socketTag, noRankId);
-    CHK_RET(socketMgr_->GetSocket(socketConfig, socket_));
+    if (channelDesc_.role != HCOMM_SOCKET_ROLE_RESERVED) {
+        bool isServer = (channelDesc_.role == HCOMM_SOCKET_ROLE_SERVER);
+        Hccl::SocketConfig socketConfig = Hccl::SocketConfig(linkData, port, socketTag, isServer);
+        CHK_RET(socketMgr_->GetSocket(socketConfig, socket_));
+    } else {
+        bool noRankId = true;
+        Hccl::SocketConfig socketConfig = Hccl::SocketConfig(linkData, socketTag, noRankId);
+        CHK_RET(socketMgr_->GetSocket(socketConfig, socket_));
+    }
+
     HCCL_INFO("[HostCpuUrmaChannel::%s] SUCCESS. port[%u].", __func__, port);
     return HCCL_SUCCESS;
 }
@@ -186,7 +194,9 @@ HcclResult HostCpuUrmaChannel::BuildUbMemTransport()
 HcclResult HostCpuUrmaChannel::Init()
 {
     CHK_RET(ParseInputParam());
-    CHK_RET(StartListen());
+    if (channelDesc_.role == HCOMM_SOCKET_ROLE_SERVER) {
+        CHK_RET(StartListen());
+    }
     CHK_RET(BuildSocket());
     CHK_RET(BuildConnection());
     CHK_RET(BuildBuffer());
