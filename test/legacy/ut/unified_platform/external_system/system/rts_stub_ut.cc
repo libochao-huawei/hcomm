53824d576e75a85c3427c288173217e73caee26f
zhengjinwei_y <zhengjinwei2@huawei.com>
2026-05-16 10:33:17 +0800

    fix

--- test/legacy/ut/unified_platform/external_system/system/rts_stub_ut.cc
+++ test/legacy/ut/unified_platform/external_system/system/rts_stub_ut.cc
@@ -591,24 +591,8 @@ aclError aclrtMemP2PMap(void *devPtr, size_t size, int32_t dstDevId, uint64_t fl
 }
 
 namespace aicpu {
-std::mutex g_sqeIdMtx;
-constexpr uint32_t INITAL_SQE_ID = 0x80000000U;
-uint32_t g_sqeId = INITAL_SQE_ID;
 void GetSqeId(const uint32_t num, uint32_t &start, uint32_t &end)
 {
-    std::lock_guard<std::mutex> lk(g_sqeIdMtx);
-    start = g_sqeId;
-    g_sqeId += num;
-    end = g_sqeId;
-    if (start > end) {
-        g_sqeId = INITAL_SQE_ID;
-        start  = g_sqeId;
-        g_sqeId += num;
-        if (start >= end) {
-            g_sqeId = INITAL_SQE_ID;
-            return;
-        }
-    }
     return;
 }
 } // aicpu
