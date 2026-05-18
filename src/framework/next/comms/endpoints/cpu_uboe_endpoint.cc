5603fccb220f90b390ed5937b435ece2e95d34de
Tiasmi <Tiasmi@noreply.gitcode.com>
2026-05-18 10:02:48 +0800

    Uboe 控制面 Endpoint 适配 描述 新增CpuUboeEndpoint，支持Host侧UBoE控制面初始化、Socket监听和基础内存管理转发；同时扩展Endpoint基类的多PORT RdmaHandle保存与查询能力，并新增MULTI_PORT地址类型、日志、比较、hash及地址转换兼容逻辑。保留DEVICE侧UboeEndpoint路径，HOST侧UBoE协议创建CpuUboeEndpoint。

--- src/framework/next/comms/endpoints/cpu_uboe_endpoint.cc
+++ src/framework/next/comms/endpoints/cpu_uboe_endpoint.cc
@@ -53,29 +53,6 @@ HcclResult CpuUboeEndpoint::GetPortAddressByPortIdx(uint8_t idx, Hccl::IpAddress
     s32 sret
         = memcpy_s(inputEid.raw, Hccl::URMA_EID_LEN, endpointDesc_.commAddr.portsAddr.eidList[idx], COMM_ADDR_EID_LEN);
     CHK_PRT_RET(sret != EOK, HCCL_ERROR("[CpuUboeEndpoint::%s] memcpy eid failed, errorno[%d].", __func__, sret),
-        HCCL_E_MEMORY);
-
-    ipAddr = Hccl::IpAddress(inputEid);
-    return HCCL_SUCCESS;
-}
-
-HcclResult CpuUboeEndpoint::GetLinkAddress(Hccl::IpAddress &ipAddr) const
-{
-    if (endpointDesc_.commAddr.type != COMM_ADDR_TYPE_MULTI_PORT) {
-        HCCL_ERROR("[CpuUboeEndpoint::%s] commAddr.type[%d] not support.", __func__, endpointDesc_.commAddr.type);
-        return HCCL_E_NOT_SUPPORT;
-    }
-
-    Hccl::BinaryAddr addr;
-    s32 sret = memcpy_s(
-        &addr, sizeof(Hccl::BinaryAddr), &endpointDesc_.commAddr.portsAddr.linkAddr, sizeof(Hccl::BinaryAddr));
-    CHK_PRT_RET(sret != EOK, HCCL_ERROR("[CpuUboeEndpoint::%s] memcpy linkAddr failed, errorno[%d].", __func__, sret),
-        HCCL_E_MEMORY);
-
-    ipAddr = Hccl::IpAddress(addr, endpointDesc_.commAddr.portsAddr.family);
-    return HCCL_SUCCESS;
-}
-
 HcclResult CpuUboeEndpoint::Init()
 {
     HCCL_INFO("[CpuUboeEndpoint::%s] localEndpoint protocol[%d].", __func__, endpointDesc_.protocol);
