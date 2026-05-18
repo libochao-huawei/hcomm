a09e0d3205ca1aecc4cc167f03a7382109ca28ac
Klayyy <wanglei886@h-partners.com>
2026-05-18 12:19:22 +0800

    ut失败修改

--- test/legacy/ut/unified_platform/external_system/system/hccp_stub_ut.cc
+++ test/legacy/ut/unified_platform/external_system/system/hccp_stub_ut.cc
@@ -750,6 +750,14 @@ int RaGetHccnCfg(struct RaInfo *info, enum HccnCfgKey key, char *value, unsigned
 
 int RaSetTpAttrAsync(void *ctxHandle, uint64_t tpHandle, uint32_t attrBitmap, struct TpAttr *attr, void **reqHandle)
 {
+    static char kStubRaSetTpAttrReq{};
+    (void)ctxHandle;
+    (void)tpHandle;
+    (void)attrBitmap;
+    (void)attr;
+    if (reqHandle != nullptr) {
+        *reqHandle = &kStubRaSetTpAttrReq;
+    }
     return 0;
 }
 
