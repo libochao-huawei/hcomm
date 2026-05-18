07fbc2146c0c7e079239fa182cc92fd4230712e9
richRachel <yinjingcheng@h-partners.com>
2026-05-18 21:44:14 +0800

    Process UB Async Events

--- src/framework/next/comms/api_c_adpt/hcomm_c_adpt.cc
+++ src/framework/next/comms/api_c_adpt/hcomm_c_adpt.cc
@@ -16,6 +16,7 @@
 #include "hcomm_result_defs.h"
 #include "log.h"
 #include "hcomm_c_adpt.h"
+#include "hcom_common.h"
 #include "../endpoints/endpoint.h"
 #include "../endpoint_pairs/channels/channel.h"
 #include "thread.h"
@@ -38,6 +39,7 @@
 #include "param_check_pub.h"
 #include "channel_process.h"
 #include "launch_device.h"
+#include "../../endpoints/dfx/endpoint_monitor.h"//cmakelist加include
 
 
 namespace hcomm {
@@ -230,6 +232,14 @@ HcommResult HcommEndpointCreate(const EndpointDesc *endpoint, EndpointHandle *en
     CHK_PTR_NULL(handle);
     EXECEPTION_CATCH(g_EndpointMap.AddEndpoint(handle, std::move(endpointPtr)), return HCCL_E_INTERNAL);
     *endpointHandle = handle;
+
+    if ((endpoint->protocol == COMM_PROTOCOL_UBC_CTP) || (endpoint->protocol == COMM_PROTOCOL_UBC_TP)) {
+        s32 devLogicIdSigned = HcclGetThreadDeviceId();
+        CHK_PRT_RET(devLogicIdSigned < 0,
+            HCCL_ERROR("[%s] HcclGetThreadDeviceId failed, ret[%d]", __func__, devLogicIdSigned), HCCL_E_INTERNAL);
+        uint32_t devLogicId = static_cast<uint32_t>(devLogicIdSigned);
+        EndpointMonitor::GetInstance(devLogicId).RegisterToEndpointMonitor(devLogicId, handle);
+    }
 
     HCCL_INFO("[%s] endpointDesc.protocol [%d] and endpointDesc.loc.locType [%d] create endpointHandle [%p] done.", 
             __func__, endpoint->protocol, endpoint->loc.locType, handle);
