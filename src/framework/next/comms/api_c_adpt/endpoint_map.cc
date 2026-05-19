593a016d8b435d12e5324352888ca0190bbd8987
zhiweihai_00958272 <zhiweihai@huawei.com>
2026-05-19 18:12:15 +0800

    [feature] process resource manager in V2

--- src/framework/next/comms/api_c_adpt/endpoint_map.cc
+++ src/framework/next/comms/api_c_adpt/endpoint_map.cc
@@ -1,72 +1,134 @@
 /**
- * Copyright (c) 2025 Huawei Technologies Co., Ltd.
- * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
- * CANN Open Software License Agreement Version 2.0 (the "License").
- * Please refer to the License for details. You may not use this file except in compliance with the License.
- * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
- * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
- * See LICENSE in the root of the software repository for the full text of the License.
- */
+ * Copyright (c) 2025 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
 
 #include "endpoint_map.h"
-#include <unordered_map>
-#include <memory>
-#include <mutex>
+#include "base_comm_res_mgr.h"
+#include "sub_resource_mgrs.h"
+#include "hccl_common.h"
 
 namespace hcomm {
 
-static std::unordered_map<EndpointHandle, std::unique_ptr<Endpoint>> g_EndpointMap;
-static std::mutex g_EndpointMapMutex;
+// 从EndpointDesc中获取设备物理ID
+static inline uint32_t GetDevPhyIdFromEndpoint(const EndpointDesc& desc)
+{
+    if (desc.loc.locType == ENDPOINT_LOC_TYPE_DEVICE) {
+        uint32_t devPhyId = desc.loc.device.devPhyId;
+        // 如果设备物理ID超出范围，返回备份设备ID
+        if (devPhyId >= MAX_MODULE_DEVICE_NUM) {
+            HCCL_WARNING("[hcomm][%s] devPhyId[%u] >= MAX_MODULE_DEVICE_NUM[%u], using backup slot",
+                __func__, devPhyId, MAX_MODULE_DEVICE_NUM);
+            return MAX_MODULE_DEVICE_NUM;
+        }
+        return devPhyId;
+    }
+    return MAX_MODULE_DEVICE_NUM;
+}
 
 void HcommEndpointMap::AddEndpoint(EndpointHandle handle, std::unique_ptr<Endpoint> endpoint)
 {
-    std::lock_guard<std::mutex> lock(g_EndpointMapMutex);
-
-    auto it = g_EndpointMap.find(handle);
-    if (it != g_EndpointMap.end()) {
-        it->second = std::move(endpoint);
+    if (endpoint == nullptr) {
+        HCCL_ERROR("[HcommEndpointMap][%s] endpoint is null", __func__);
         return;
     }
 
-    g_EndpointMap.emplace(handle, std::move(endpoint));
-    return;
+    // 从EndpointDesc中获取设备物理ID
+    EndpointDesc desc = endpoint->GetEndpointDesc();
+    uint32_t devPhyId = GetDevPhyIdFromEndpoint(desc);
+
+    BaseCommResMgr& mgr = BaseCommResMgr::Instance();
+    BaseCommRes* baseCommRes = mgr.GetOrCreate(devPhyId);
+    if (baseCommRes == nullptr) {
+        HCCL_ERROR("[HcommEndpointMap][%s] GetOrCreate failed for devPhyId=%u", __func__, devPhyId);
+        return;
+    }
+
+    EndpointsMgr* endpointsMgr = baseCommRes->GetEndpointsMgr();
+    if (endpointsMgr == nullptr) {
+        HCCL_ERROR("[HcommEndpointMap][%s] GetEndpointsMgr failed for devPhyId=%u", __func__, devPhyId);
+        return;
+    }
+
+    endpointsMgr->InstallOwnedEndpoint(handle, std::move(endpoint));
 }
 
 bool HcommEndpointMap::RemoveEndpoint(EndpointHandle handle)
 {
-    std::lock_guard<std::mutex> lock(g_EndpointMapMutex);
+    BaseCommResMgr& mgr = BaseCommResMgr::Instance();
+    Endpoint* endpoint = nullptr;
 
-    auto it = g_EndpointMap.find(handle);
-    if (it != g_EndpointMap.end()) {
-        g_EndpointMap.erase(it);
-        return true;
+    mgr.VisitBaseCommRes([&](BaseCommRes* baseCommRes) {
+        EndpointsMgr* endpointsMgr = baseCommRes->TryGetEndpointsMgr();
+        if (endpointsMgr == nullptr) {
+            return;
+        }
+        Endpoint* ep = endpointsMgr->FindEndpointByHandle(handle);
+        if (ep != nullptr) {
+            endpoint = ep;
+        }
+    });
+
+    if (endpoint == nullptr) {
+        HCCL_WARNING("[HcommEndpointMap][%s] endpoint not found, handle=%p", __func__, handle);
+        return false;
     }
-    return false;
+
+    EndpointDesc desc = endpoint->GetEndpointDesc();
+    uint32_t devPhyId = GetDevPhyIdFromEndpoint(desc);
+
+    BaseCommRes* baseCommRes = mgr.GetBaseCommRes(devPhyId);
+    if (baseCommRes == nullptr) {
+        HCCL_ERROR("[HcommEndpointMap][%s] GetBaseCommRes failed for devPhyId=%u", __func__, devPhyId);
+        return false;
+    }
+
+    EndpointsMgr* endpointsMgr = baseCommRes->GetEndpointsMgr();
+    if (endpointsMgr == nullptr) {
+        HCCL_ERROR("[HcommEndpointMap][%s] GetEndpointsMgr failed for devPhyId=%u", __func__, devPhyId);
+        return false;
+    }
+
+    auto ret = endpointsMgr->RemoveOwnedEndpoint(handle);
+    if (ret != HCCL_SUCCESS) {
+        HCCL_ERROR("[HcommEndpointMap][%s] RemoveOwnedEndpoint failed, handle=%p", __func__, handle);
+        return false;
+    }
+
+    return true;
 }
 
 bool HcommEndpointMap::UpdateEndpoint(
     EndpointHandle handle,
     std::unique_ptr<Endpoint> newEndpoint)
 {
-    std::lock_guard<std::mutex> lock(g_EndpointMapMutex);
-
-    auto it = g_EndpointMap.find(handle);
-    if (it != g_EndpointMap.end()) {
-        it->second = std::move(newEndpoint);
-        return true;
-    }
+    (void)handle;
+    (void)newEndpoint;
     return false;
 }
 
 Endpoint* HcommEndpointMap::GetEndpoint(EndpointHandle handle)
 {
-    std::lock_guard<std::mutex> lock(g_EndpointMapMutex);
+    BaseCommResMgr& mgr = BaseCommResMgr::Instance();
+    Endpoint* endpoint = nullptr;
 
-    auto it = g_EndpointMap.find(handle);
-    if (it != g_EndpointMap.end()) {
-        return it->second.get(); // 返回裸指针，不转移所有权
-    }
-    return nullptr;
+    mgr.VisitBaseCommRes([&](BaseCommRes* baseCommRes) {
+        if (endpoint != nullptr) {
+            return;
+        }
+        EndpointsMgr* endpointsMgr = baseCommRes->TryGetEndpointsMgr();
+        if (endpointsMgr == nullptr) {
+            return;
+        }
+        endpoint = endpointsMgr->FindEndpointByHandle(handle);
+    });
+
+    return endpoint;
 }
 
 } // namespace hcomm
---
+++ src/framework/next/comms/base_comm_res.h
@@ -0,0 +1,102 @@
+/**
+ * Copyright (c) 2025 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+#ifndef BASE_COMM_RES_H
+#define BASE_COMM_RES_H
+
+#include <atomic>
+#include <cstdint>
+#include <functional>
+#include <memory>
+#include <mutex>
+#include <unordered_map>
+#include <vector>
+
+#include "hccl_types.h"
+#include "hcomm_res_defs.h"
+
+namespace hcomm {
+
+class EndpointsMgr;
+class ChannelsMgr;
+class ThreadsMgr;
+class CcuInstancesMgr;
+class HdcProcess;
+
+class BaseCommRes {
+public:
+    enum class State {
+        kReady,
+        kDestroying,
+        kDestroyed
+    };
+
+    struct ResourceCount {
+        size_t endpointCount = 0;
+        size_t channelCount = 0;
+        size_t threadCount = 0;
+        size_t ccuInstanceCount = 0;
+    };
+
+    explicit BaseCommRes(uint32_t devPhyId);
+    ~BaseCommRes();
+
+    EndpointsMgr* GetEndpointsMgr();
+    ChannelsMgr* GetChannelsMgr();
+    ThreadsMgr* GetThreadsMgr();
+    CcuInstancesMgr* GetCcuInstancesMgr();
+    HdcProcess* GetHdcProcess();
+
+    EndpointsMgr* TryGetEndpointsMgr() const { return endpointsMgr_.get(); }
+    ChannelsMgr* TryGetChannelsMgr() const { return channelsMgr_.get(); }
+    ThreadsMgr* TryGetThreadsMgr() const { return threadsMgr_.get(); }
+
+    HcclResult DestroyAll();
+
+    uint32_t GetDevicePhyId() const { return devPhyId_; }
+    State GetState() const { return state_.load(); }
+    bool IsReady() const { return state_.load() == State::kReady; }
+    bool IsDestroyed() const { return state_.load() == State::kDestroyed; }
+
+    ResourceCount GetResourceCount() const;
+
+private:
+    friend class BaseCommResMgr;
+    friend class std::unique_ptr<BaseCommRes>;
+
+    HcclResult DestroyInOrder();
+    HcclResult LazyInitEndpointsMgr();
+    HcclResult LazyInitChannelsMgr();
+    HcclResult LazyInitThreadsMgr();
+    HcclResult LazyInitCcuInstancesMgr();
+    HcclResult LazyInitHdcProcess();
+    bool TransitionState(State from, State to);
+
+private:
+    uint32_t devPhyId_;
+    std::atomic<State> state_{State::kReady};
+
+    std::unique_ptr<EndpointsMgr> endpointsMgr_;
+    std::unique_ptr<ChannelsMgr> channelsMgr_;
+    std::unique_ptr<ThreadsMgr> threadsMgr_;
+    std::unique_ptr<CcuInstancesMgr> ccuInstancesMgr_;
+    std::unique_ptr<HdcProcess> hdcProcess_;
+
+    std::mutex mutex_;
+
+    std::atomic<size_t> endpointCount_{0};
+    std::atomic<size_t> channelCount_{0};
+    std::atomic<size_t> threadCount_{0};
+    std::atomic<size_t> ccuInstanceCount_{0};
+};
+
+} // namespace hcomm
+
+#endif // BASE_COMM_RES_H
