52f9cc46e7ffd867151ac40711e887da85761e71
ouyangcaike <ouyangcaike@h-partners.com>
2026-05-19 16:48:46 +0800

    build: 统一通过链接目标管理外部头文件依赖
    移除硬编码的ASCEND_CANN_PACKAGE_PATH头文件包含路径，改用链接预定义的headers目标来获取头文件路径，优化构建依赖管理，适配开放构建流程

--- cmake/fetch_cann_cmake.cmake
+++ cmake/fetch_cann_cmake.cmake
@@ -15,12 +15,12 @@ if(NOT PROJECT_SOURCE_DIR)
     else()
         include(FetchContent)
 
-        set(CANN_CMAKE_TAG "master-010")
+        set(CANN_CMAKE_TAG "master-012")
         if(CANN_3RD_LIB_PATH AND EXISTS "${CANN_3RD_LIB_PATH}/cmake-${CANN_CMAKE_TAG}.tar.gz")
             FetchContent_Declare(
                 cann-cmake
                 URL "${CANN_3RD_LIB_PATH}/cmake-${CANN_CMAKE_TAG}.tar.gz"
-                URL_HASH SHA256=bde4fef0ac12c66191bc712c9cc10dab4dc3cc1e478c0942e8db18a1d6ee9514
+                URL_HASH SHA256=59032ccbabcb85368cf84546ccbb214cd42a69368eea99faa2a517c7d6100a11
             )
         else()
             FetchContent_Declare(
