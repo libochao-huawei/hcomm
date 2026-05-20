d203b6435bd4a0abdb486efe2029500372c0ff61
ouyangcaike <ouyangcaike@h-partners.com>
2026-05-20 17:36:25 +0800

    tmp: 临时使用cann/cmake的master分支，新增tag后更新为正式版本

--- cmake/fetch_cann_cmake.cmake
+++ cmake/fetch_cann_cmake.cmake
@@ -15,7 +15,7 @@ if(NOT PROJECT_SOURCE_DIR)
     else()
         include(FetchContent)
 
-        set(CANN_CMAKE_TAG "master-013")
+        set(CANN_CMAKE_TAG "master")
         if(CANN_3RD_LIB_PATH AND EXISTS "${CANN_3RD_LIB_PATH}/cmake-${CANN_CMAKE_TAG}.tar.gz")
             FetchContent_Declare(
                 cann-cmake
