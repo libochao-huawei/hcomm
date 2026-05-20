91b3c90138134febfb938f6a6086691f529e3c18
renyumin <renyumin1@huawei.com>
2026-05-20 18:53:15 +0800

    compile

--- src/framework/next/comms/endpoint_pairs/channels/ccu/ccu_urma_channel.h
+++ src/framework/next/comms/endpoint_pairs/channels/ccu/ccu_urma_channel.h
@@ -33,7 +33,7 @@ public:
     ChannelStatus GetStatus() override;
 
     HcclResult GetNotifyNum(uint32_t *notifyNum) const override;
-    HcclResult GetRemoteMems(HcclMem **remoteMem, uint32_t *memNum, char **memTags) override;
+    HcclResult GetRemoteMems(HcclMem **remoteMem, uint32_t *memNum, char ***memTags) override;
     HcclResult UpdateMemInfo(HcommMemHandle *memHandles, uint32_t memHandleNum) override;
 
     virtual HcclResult Clean() override;
