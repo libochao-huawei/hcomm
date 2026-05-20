1a0829cc0f10e3812d7c759a50d0840859f9c7cc
zhiweihai_00958272 <zhiweihai@huawei.com>
2026-05-20 17:51:47 +0800

    [feature] V2单例架构+删除全局变量逻辑+仅考虑新流程中某些单例统一

---
+++ src/framework/next/comms/base_comm_res.cc
@@ -0,0 +1,205 @@
+/**
+ * Copyright (c) 2025 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for the details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+#include "base_comm_res.h"
+#include "hccl_common.h"
+#include "sub_resource_mgrs.h"
+
+namespace hcomm {
+
+BaseCommRes::BaseCommRes(uint32_t devPhyId)
+    : devPhyId_(devPhyId),
+      state_(State::kReady)
+{
+    HCCL_INFO("[BaseCommRes][%s] created for devPhyId=%u", __func__, devPhyId);
+}
+
+BaseCommRes::~BaseCommRes()
+{
+    DestroyAll();
+}
+
+bool BaseCommRes::TransitionState(State from, State to)
+{
+    State expected = from;
+    if (state_.compare_exchange_strong(expected, to)) {
+        HCCL_INFO("[BaseCommRes][%s] state transition %d -> %d for devPhyId=%u",
+            __func__, static_cast<int>(from), static_cast<int>(to), devPhyId_);
+        return true;
+    }
+    HCCL_ERROR("[BaseCommRes][%s] state transition failed: expected %d, actual %d for devPhyId=%u",
+        __func__, static_cast<int>(from), static_cast<int>(state_.load()), devPhyId_);
+    return false;
+}
+
+HcclResult BaseCommRes::DestroyAll()
+{
+    std::lock_guard<std::mutex> lock(mutex_);
+
+    const State current = state_.load();
+    if (current == State::kDestroyed) {
+        return HCCL_SUCCESS;
+    }
+    if (current == State::kDestroying) {
+        return HCCL_E_BUSY;
+    }
+
+    if (!TransitionState(State::kReady, State::kDestroying)) {
+        HCCL_ERROR("[BaseCommRes][%s] failed to enter destroying from ready", __func__);
+        return HCCL_E_BUSY;
+    }
+
+    HcclResult ret = DestroyInOrder();
+
+    if (!TransitionState(State::kDestroying, State::kDestroyed)) {
+        HCCL_ERROR("[BaseCommRes][%s] failed kDestroying -> kDestroyed", __func__);
+        state_.store(State::kDestroyed);
+    }
+    return ret;
+}
+
+HcclResult BaseCommRes::DestroyInOrder()
+{
+    HcclResult finalResult = HCCL_SUCCESS;
+
+    if (channelsMgr_) {
+        auto ret = channelsMgr_->DestroyAll();
+        if (ret != HCCL_SUCCESS) finalResult = ret;
+    }
+
+    if (threadsMgr_) {
+        auto ret = threadsMgr_->DestroyAll();
+        if (ret != HCCL_SUCCESS) finalResult = ret;
+    }
+
+    if (endpointsMgr_) {
+        auto ret = endpointsMgr_->DestroyAllMem();
+        if (ret != HCCL_SUCCESS) finalResult = ret;
+    }
+
+    if (endpointsMgr_) {
+        auto ret = endpointsMgr_->DestroyAll();
+        if (ret != HCCL_SUCCESS) finalResult = ret;
+    }
+
+    return finalResult;
+}
+
+EndpointsMgr* BaseCommRes::GetEndpointsMgr()
+{
+    std::lock_guard<std::mutex> lock(mutex_);
+
+    const State s = state_.load();
+    if (s == State::kDestroyed || s == State::kDestroying) {
+        return nullptr;
+    }
+    if (endpointsMgr_ == nullptr) {
+        auto ret = LazyInitEndpointsMgr();
+        if (ret != HCCL_SUCCESS) {
+            return nullptr;
+        }
+    }
+    return endpointsMgr_.get();
+}
+
+ChannelsMgr* BaseCommRes::GetChannelsMgr()
+{
+    std::lock_guard<std::mutex> lock(mutex_);
+
+    const State s = state_.load();
+    if (s == State::kDestroyed || s == State::kDestroying) {
+        return nullptr;
+    }
+
+    if (channelsMgr_ == nullptr) {
+        auto ret = LazyInitChannelsMgr();
+        if (ret != HCCL_SUCCESS) {
+            return nullptr;
+        }
+    }
+    return channelsMgr_.get();
+}
+
+ThreadsMgr* BaseCommRes::GetThreadsMgr()
+{
+    std::lock_guard<std::mutex> lock(mutex_);
+
+    const State s = state_.load();
+    if (s == State::kDestroyed || s == State::kDestroying) {
+        return nullptr;
+    }
+
+    if (threadsMgr_ == nullptr) {
+        auto ret = LazyInitThreadsMgr();
+        if (ret != HCCL_SUCCESS) {
+            return nullptr;
+        }
+    }
+    return threadsMgr_.get();
+}
+
+HcclResult BaseCommRes::LazyInitEndpointsMgr()
+{
+    if (endpointsMgr_ != nullptr) {
+        return HCCL_SUCCESS;
+    }
+    EXECEPTION_CATCH(endpointsMgr_ = std::make_unique<EndpointsMgr>());
+    if (endpointsMgr_ == nullptr) {
+        HCCL_ERROR("[BaseCommRes][%s] make_unique EndpointsMgr failed", __func__);
+        return HCCL_E_INTERNAL;
+    }
+    HCCL_INFO("[BaseCommRes][%s] EndpointsMgr created for devPhyId=%u", __func__, devPhyId_);
+    return HCCL_SUCCESS;
+}
+
+HcclResult BaseCommRes::LazyInitChannelsMgr()
+{
+    if (channelsMgr_ != nullptr) {
+        return HCCL_SUCCESS;
+    }
+    EXECEPTION_CATCH(channelsMgr_ = std::make_unique<ChannelsMgr>());
+    if (channelsMgr_ == nullptr) {
+        HCCL_ERROR("[BaseCommRes][%s] make_unique ChannelsMgr failed", __func__);
+        return HCCL_E_INTERNAL;
+    }
+    HCCL_INFO("[BaseCommRes][%s] ChannelsMgr created for devPhyId=%u", __func__, devPhyId_);
+    return HCCL_SUCCESS;
+}
+
+HcclResult BaseCommRes::LazyInitThreadsMgr()
+{
+    if (threadsMgr_ != nullptr) {
+        return HCCL_SUCCESS;
+    }
+    EXECEPTION_CATCH(threadsMgr_ = std::make_unique<ThreadsMgr>());
+    if (threadsMgr_ == nullptr) {
+        HCCL_ERROR("[BaseCommRes][%s] make_unique ThreadsMgr failed", __func__);
+        return HCCL_E_INTERNAL;
+    }
+    HCCL_INFO("[BaseCommRes][%s] ThreadsMgr created for devPhyId=%u", __func__, devPhyId_);
+    return HCCL_SUCCESS;
+}
+
+BaseCommRes::ResourceCount BaseCommRes::GetResourceCount() const
+{
+    ResourceCount count;
+    if (endpointsMgr_ != nullptr) {
+        count.endpointCount = endpointsMgr_->GetEndpointCount();
+    }
+    if (channelsMgr_ != nullptr) {
+        count.channelCount = channelsMgr_->GetChannelCount();
+    }
+    if (threadsMgr_ != nullptr) {
+        count.threadCount = threadsMgr_->GetThreadCount();
+    }
+    return count;
+}
+
+} // namespace hcomm
--- src/framework/next/comms/hcomm_res_mgr.cc
+++ src/framework/next/comms/hcomm_res_mgr.cc
@@ -1,15 +1,15 @@
 /**
- * Copyright (c) 2025 Huawei Technologies Co., Ltd.
- * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
- * CANN Open Software License Agreement Version 2.0 (the "License").
- * Please refer to the License for details. You may not use this file except in compliance with the License.
- * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
- * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
- * See LICENSE in the root of the software repository for the full text of the License.
- */
+ * Copyright (c) 2025 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
 
 #include "hcomm_res_mgr.h"
-
+#include "base_comm_res_mgr.h"
 #include "hccl_common.h"
 
 // orion 通用平台层单例
@@ -21,18 +21,17 @@
 #include "socket_handle_manager.h"
 #include "host_socket_handle_manager.h"
 #include "tp_manager.h"
-#include "dfx/endpoint_monitor.h"
 // orion ccu单例
 #include "ccu_component.h"
 #include "../../../legacy/unified_platform/ccu/ccu_device/ccu_res_batch_allocator.h"
 #include "../../../legacy/unified_platform/ccu/ccu_context/ccu_context_mgr_imp.h"
-// 开源开放 ccu单例
-#include "hccp_tlv_hdc_mgr.h"
-#include "tp_mgr.h"
+// 开源开放架构下CCU通信域相关单例
 #include "ccu_comp.h"
+<<<<<<< HEAD
 #include "ccu/ccu_device/ccu_res_batch_allocator.h"
+=======
+>>>>>>> 2c573c6b ([feature] V2单例架构+删除全局变量逻辑+仅考虑新流程中某些单例统一)
 #include "ccu_kernel_mgr.h"
-#include "../endpoint_pairs/sockets/socket_process.h"
 
 namespace hcomm {
 
@@ -45,9 +44,8 @@ HcommResMgr& HcommResMgr::GetInstance(const uint32_t devicePhyId)
         devPhyId = MAX_MODULE_DEVICE_NUM; // 使用备份设备
     }
 
-    // 临时方案：只声明单例对象做生命周期控制，不执行业务动作
-    // 未来需要将各种单例转为该数据结构的成员变量
-    // devicePhyId 目前不影响流程，只是触发静态对象声明
+    // 触发legacy Hccl单例初始化，确保生命周期正确
+    // legacy单例初始化，暂时无法归入统一单例架构，后续再考虑调整
     Hccl::HccpHdcManager::GetInstance();
     Hccl::HccpPeerManager::GetInstance();
     Hccl::HccpTlvHdcManager::GetInstance();
@@ -56,36 +54,40 @@ HcommResMgr& HcommResMgr::GetInstance(const uint32_t devicePhyId)
     Hccl::SocketHandleManager::GetInstance();
     Hccl::HostSocketHandleManager::GetInstance();
     Hccl::TpManager::GetInstance(devicePhyId);
-    EndpointMonitor::GetInstance(devicePhyId); // 用logicId还是phyid
 
     Hccl::CcuComponent::GetInstance(devicePhyId);
     Hccl::CcuResBatchAllocator::GetInstance(devicePhyId);
     Hccl::CtxMgrImp::GetInstance(devicePhyId);
 
-    // 开源开放架构下CCU模式新增类型单例，当前混跑时不使用
-    HccpTlvHdcMgr::GetInstance(devicePhyId);
-    TpMgr::GetInstance(devicePhyId);
-    CcuComponent::GetInstance(devicePhyId);
-    CcuResBatchAllocator::GetInstance(devicePhyId);
-    CcuKernelMgr::GetInstance(devicePhyId);
-    SocketProcess::GetInstance(devicePhyId);
+    // 开源开放架构下CCU通信域相关单例初始化
+    CcuComponent::GetInstance(devPhyId);
+    CcuKernelMgr::GetInstance(devPhyId);
 
     static HcommResMgr hcommResMgrs[MAX_MODULE_DEVICE_NUM + 1];
-    hcommResMgrs[devPhyId].devPhyId_ = devPhyId;
+    HcommResMgr& mgr = hcommResMgrs[devPhyId];
 
-    return hcommResMgrs[devPhyId];
+    {
+        // 写baseCommRes_过程确保线程安全
+        std::lock_guard<std::mutex> lock(mgr.mutex_);
+        if (mgr.baseCommRes_ == nullptr) {
+        BaseCommResMgr& baseCommResMgr = BaseCommResMgr::Instance();
+        mgr.baseCommRes_ = baseCommResMgr.GetOrCreate(devPhyId);
+        if (mgr.baseCommRes_ == nullptr) {
+            HCCL_ERROR("[HcommResMgr][%s] GetOrCreate BaseCommRes failed for devPhyId=%u",
+                __func__, devPhyId);
+        }
+    }
+    }
+
+    return mgr;
 }
 
 HcommResMgr::HcommResMgr()
 {
-    // 临时方案：最小化修改不做处理
-    // 未来需在构造函数中触发各类成员变量声明
 }
 
 HcommResMgr::~HcommResMgr()
 {
-    // 临时方案：最小化修改不做处理
-    // 未来需在析构函数中主动调用各种单例销毁流程，保证销毁时序
 }
 
 } // namespace hcomm
