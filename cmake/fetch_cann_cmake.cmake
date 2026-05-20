24d7e280fa4d6aee7ce540ac09fedd429ca1403e
ouyangcaike <ouyangcaike@h-partners.com>
2026-05-20 17:36:25 +0800

    tmp: 临时使用个人仓，待cann/cmake修改后更新

--- cmake/fetch_cann_cmake.cmake
+++ cmake/fetch_cann_cmake.cmake
@@ -15,7 +15,7 @@ if(NOT PROJECT_SOURCE_DIR)
     else()
         include(FetchContent)
 
-        set(CANN_CMAKE_TAG "master-013")
+        set(CANN_CMAKE_TAG "20260519-update-find-ascend-hal")
         if(CANN_3RD_LIB_PATH AND EXISTS "${CANN_3RD_LIB_PATH}/cmake-${CANN_CMAKE_TAG}.tar.gz")
             FetchContent_Declare(
                 cann-cmake
@@ -25,7 +25,7 @@ if(NOT PROJECT_SOURCE_DIR)
         else()
             FetchContent_Declare(
                 cann-cmake
-                GIT_REPOSITORY https://gitcode.com/cann/cmake.git
+                GIT_REPOSITORY https://gitcode.com/zhengfeiteng/cmake.git
                 GIT_TAG        ${CANN_CMAKE_TAG}
                 GIT_SHALLOW    TRUE
             )
