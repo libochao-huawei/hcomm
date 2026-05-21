927defc45204c5cf186c18c790fc54ce7cf88b72
richRachel <yinjingcheng@h-partners.com>
2026-05-21 16:08:41 +0800

    fix

--- src/framework/next/comms/api_c_adpt/hcomm_c_adpt.cc
+++ src/framework/next/comms/api_c_adpt/hcomm_c_adpt.cc
@@ -42,7 +42,7 @@
 #include "param_check_pub.h"
 #include "channel_process.h"
 #include "launch_device.h"
-#include "../../endpoints/dfx/endpoint_monitor.h" // cmakelist加include
+#include "../../endpoints/dfx/endpoint_monitor.h"
 #include "aiv_urma_channel.h"
 
 namespace hcomm {
