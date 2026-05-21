75659be40eb6f9c4e8f4eb3870f666aa68388dc1
richRachel <yinjingcheng@h-partners.com>
2026-05-21 14:53:25 +0800

    Process UB Async Events

--- src/framework/next/comms/endpoints/endpoint.cc
+++ src/framework/next/comms/endpoints/endpoint.cc
@@ -15,7 +15,8 @@
 #include "ub_mem_endpoint.h"
 #include "uboe_endpoint.h"
 #include "cpu_urma_endpoint.h"
- #include "aicputs_hccs_endpoint.h"
+#include "aicputs_hccs_endpoint.h"
+#include "ra_rs_comm.h"
 
 namespace hcomm{
 static bool IsSupported(const EndpointDesc &endpointDesc)
@@ -89,6 +90,33 @@ if (endpointDesc.protocol == COMM_PROTOCOL_ROCE && endpointDesc.loc.locType == E
         HCCL_ERROR("[%s] failed, endpointDesc.protocol [%d] and endpointDesc.loc.locType [%d] do not match.", 
             __func__, endpointDesc.protocol, endpointDesc.loc.locType);
         return HCCL_E_PARA;
+    }
+    return HCCL_SUCCESS;
+}
+
+HcclResult Endpoint::GetAsyncEventsContext(uint32_t devPhyId, struct AsyncEvent events[], uint32_t &num)
+{
+    uint32_t interfaceVersion{0};
+
+    int ret;
+    // 对RaCtxGetAsyncEvents接口的版本检验
+    ret = RaGetInterfaceVersion(devPhyId, RA_RS_CTX_GET_ASYNC_EVENTS, &interfaceVersion);
+    if (ret != 0) {
+        HCCL_ERROR("[%s] devPhyId[%u] RaGetInterfaceVersion failed, ret[%d]", __func__, devPhyId, ret);
+        return HCCL_E_INTERNAL; 
+    }
+
+    if (interfaceVersion <= 1) {
+        HCCL_ERROR("[%s] devPhyId[%u] version[%u] not support", __func__, devPhyId, interfaceVersion);
+        return HCCL_E_NOT_SUPPORT;
+    }
+
+    CHK_PTR_NULL(ctxHandle_);
+    ret = RaCtxGetAsyncEvents(ctxHandle_, events, &num);
+    if (ret != 0) {
+        HCCL_ERROR("[%s] devPhyId[%u] RaCtxGetAsyncEvents failed, ctxHandle[%p] ret[%d]", __func__, devPhyId,
+            (void *)ctxHandle_, ret);
+        return HCCL_E_INTERNAL;
     }
     return HCCL_SUCCESS;
 }
