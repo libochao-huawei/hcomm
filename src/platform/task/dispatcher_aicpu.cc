5227b07823529076c39c29a57009ab5d5453d49d
zhaozhihui <zhaozhihui3@yeah.net>
2026-05-21 09:21:07 +0800

    [bugfix]回退加锁导致的性能问题

--- src/platform/task/dispatcher_aicpu.cc
+++ src/platform/task/dispatcher_aicpu.cc
@@ -647,16 +647,9 @@ HcclResult DispatcherAiCpu::LaunchTask(Stream &stream, bool isBlockLaunch)
         }
 
         // 当前流无法下发，把其他流都launch一遍，避免等待的其他流没有launch
-        std::vector<std::pair<s32, Stream>> streamList;
-        {
-            std::lock_guard<std::mutex> lock(streamMapMutex_);
-            for (auto it = streamMap_.begin(); it != streamMap_.end(); ++it) {
-                streamList.emplace_back(*it);
-            }
-        }
-        for (auto &item : streamList) {
-            if (item.first != streamInfo.actualStreamId) {
-                CHK_RET(LaunchTask(item.second, false));
+        for (auto it = streamMap_.begin(); it != streamMap_.end(); ++it) {
+            if (it->first != streamInfo.actualStreamId) {
+                CHK_RET(LaunchTask(it->second, false));
             }
         }
 
@@ -862,18 +855,11 @@ HcclResult DispatcherAiCpu::LaunchTasksEx(hccl::Stream &stream, std::vector<Stre
 
 HcclResult DispatcherAiCpu::LaunchAllTasks()
 {
-    std::vector<std::pair<s32, Stream>> streamList;
-    {
-        std::lock_guard<std::mutex> lock(streamMapMutex_);
-        for (auto it = streamMap_.begin(); it != streamMap_.end(); ++it) {
-            streamList.emplace_back(*it);
-        }
-    }
-    for (auto &item : streamList) {
-        HcclResult ret = LaunchTask(item.second, true);
+    for (auto it = streamMap_.begin(); it != streamMap_.end(); ++it) {
+        HcclResult ret = LaunchTask(it->second, true);
         if (ret != HCCL_SUCCESS) {
             HCCL_ERROR("DispatcherAiCpu][LaunchAllTasks] "\
-                "launch task failed, sqid:%u, ret:%u", item.second.sqId(), ret);
+                "launch task failed, sqid:%u, ret:%u", it->second.sqId(), ret);
             return ret;
         }
     }
@@ -1067,16 +1053,9 @@ HcclResult DispatcherAiCpu::WaitRtsq(Stream& stream, const size_t& sqeCount, con
         }
 
         // 当前流无法下发，把其他流都launch一遍，避免等待的其他流没有launch
-        std::vector<std::pair<s32, Stream>> streamList;
-        {
-            std::lock_guard<std::mutex> lock(streamMapMutex_);
-            for (auto it = streamMap_.begin(); it != streamMap_.end(); ++it) {
-                streamList.emplace_back(*it);
-            }
-        }
-        for (auto &item : streamList) {
-            if (item.first != streamInfo.actualStreamId) { // 不是当前stream
-                CHK_RET(LaunchTask(item.second, false)); // 非阻塞launch
+        for (auto it = streamMap_.begin(); it != streamMap_.end(); ++it) {
+            if (it->first != streamInfo.actualStreamId) { // 不是当前stream
+                CHK_RET(LaunchTask(it->second, false)); // 非阻塞launch
             }
         }
 
@@ -1319,7 +1298,6 @@ HcclResult DispatcherAiCpu::AddRetryPreamble(Stream &stream)
 void DispatcherAiCpu::SaveStreamInfo(hccl::Stream &stream)
 {
     const HcclComStreamInfo &streamInfo = stream.GetHcclStreamInfo();
-    std::lock_guard<std::mutex> lock(streamMapMutex_);
     if (streamMap_.find(streamInfo.actualStreamId) == streamMap_.end()) {
         streamMap_.insert({streamInfo.actualStreamId, stream});
         HCCL_INFO("[DispatcherAiCpu][SaveStreamInfo] stream id[%d]", streamInfo.actualStreamId);
