41b8a837a0aaf2800e68164adaea35c657317d05
littlemons <yaowenjie7@huawei.com>
2026-05-25 17:01:21 +0800

    queuenum初始化/打印

--- src/legacy/framework/env_config/base_config.cc
+++ src/legacy/framework/env_config/base_config.cc
@@ -169,7 +169,7 @@ void EnvRdmaConfig::Parse()
         GetRdmaQueueNum());
 
     multiQpThreshold.Parse();
-    HCCL_RUN_INFO("[HCCL_ENV] HCCL_MULTI_QP_THRESHOLD set by %s to [%u]", multiQpThreshold.GetSource(),
+    HCCL_RUN_INFO("[HCCL_ENV] HCCL_MULTI_QP_THRESHOLD set by %s to [%u B]", multiQpThreshold.GetSource(),
         GetRdmaMultiQpThreshold());
 }
 
@@ -209,7 +209,7 @@ u32 EnvRdmaConfig::GetRdmaQueueNum() const
 
 u32 EnvRdmaConfig::GetRdmaMultiQpThreshold() const
 {
-    return multiQpThreshold.Get() * 1024;   // KB 转 B
+    return multiQpThreshold.Get();
 }
 
 // EnvAlgoConfig
