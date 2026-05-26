0f97cbea7263df3e6869fa646b586d00f034faa9
chenliang267 <chenliang318@huawei.com>
2026-05-26 14:53:28 +0800

    bugfix

--- src/framework/communicator/impl/hccl_communicator_host.cc
+++ src/framework/communicator/impl/hccl_communicator_host.cc
@@ -7489,7 +7489,7 @@ namespace hccl
         opTilingData->needIncreLink = opParam.needIncreLink;
         // ACL Graph + Zero Copy 模式下，填充 transport 序列化数据的 device 内存地址
         // ZeroCopy 场景必定是 aicpu 展开，SelectAlg 会在 newTag 末尾追加 _device
-        if (opParam.isCapture && opParam.aclGraphZeroCopyEnable != 0) {
+        if (opParam.isCapture) {
             auto it = transportDeviceMemMap_.find(opParam.tag + "_device_Capture");
             if (it != transportDeviceMemMap_.end()) {
                 opTilingData->transportDeviceMemAddr = reinterpret_cast<u64>(it->second.ptr());
