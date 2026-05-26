87dbcdbea4a28f859ea99e85604c038f4b5558a8
z00940303 <zhangzhuopan@huawei.com>
2026-05-26 17:23:20 +0800

    feat:add A5 symmetric memory

--- src/framework/op_base/src/op_base.cc
+++ src/framework/op_base/src/op_base.cc
@@ -5279,7 +5279,13 @@ HcclResult HcclCommSymWinRegister(HcclComm comm, void* addr, uint64_t size, Hccl
     CHK_PRT_RET(size == 0, HCCL_ERROR("[%s] size is 0, please check size value", __func__), HCCL_E_PARA);
     if (flag == HCCL_COMM_SYM_WINDOW_FLAG_COLL_SYMMETRIC) {
         hccl::hcclComm *hcclComm = static_cast<hccl::hcclComm *>(comm);
-        CHK_RET(hcclComm->RegisterWindow(addr, size, winHandle));
+        if (hcclComm->IsCommunicatorV2()) {
+            hccl::CollComm *collComm = hcclComm->GetCollComm();
+            CHK_PTR_NULL(collComm);
+            CHK_RET(collComm->RegisterWindow(addr, size, winHandle));
+        } else {
+            CHK_RET(hcclComm->RegisterWindow(addr, size, winHandle));
+        }
         HCCL_RUN_INFO("[%s]WindowRegister mem success, group[%s], handle ptr[%p], size[%llu]", __func__,
             hcclComm->GetIdentifier().c_str(), *winHandle, size);
         {
@@ -5310,7 +5316,13 @@ HcclResult HcclCommSymWinDeregister(HcclCommSymWindow winHandle)
     comm = it->second;
     CHK_PTR_NULL(comm);
     hccl::hcclComm *hcclComm = static_cast<hccl::hcclComm *>(comm);
-    CHK_RET(hcclComm->DeregisterWindow(winHandle));
+    if (hcclComm->IsCommunicatorV2()) {
+        hccl::CollComm *collComm = hcclComm->GetCollComm();
+        CHK_PTR_NULL(collComm);
+        CHK_RET(collComm->DeregisterWindow(winHandle));
+    } else {
+        CHK_RET(hcclComm->DeregisterWindow(winHandle));
+    }
     winHandle2comm.erase(it);
     HCCL_RUN_INFO("[%s]WindowDeregister mem success, group[%s]", __func__, hcclComm->GetIdentifier().c_str());
     return HCCL_SUCCESS;
@@ -5326,7 +5338,13 @@ HcclResult HcclCommSymWinGet(HcclComm comm, void *ptr, size_t size, HcclCommSymW
     CHK_PRT_RET(size == 0, HCCL_ERROR("[%s] size is 0, please check size value", __func__), HCCL_E_PARA);
 
     hccl::hcclComm *hcclComm = static_cast<hccl::hcclComm *>(comm);
-    CHK_RET(hcclComm->GetCommSymWin(ptr, size, winHandle, offset));
+    if (hcclComm->IsCommunicatorV2()) {
+        hccl::CollComm *collComm = hcclComm->GetCollComm();
+        CHK_PTR_NULL(collComm);
+        CHK_RET(collComm->GetCommSymWin(ptr, size, winHandle, offset));
+    } else {
+        CHK_RET(hcclComm->GetCommSymWin(ptr, size, winHandle, offset));
+    }
     HCCL_RUN_INFO("[%s]GetCommSymWin success, group[%s], handle ptr[%p], offset[%llu], size[%llu]", __func__,
         hcclComm->GetIdentifier().c_str(), *winHandle, *offset, size);
     return HCCL_SUCCESS;
