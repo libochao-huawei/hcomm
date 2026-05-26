b70c98085ed21370dc295fe6959935cdb0e89c33
renyumin <renyumin1@huawei.com>
2026-05-26 21:12:09 +0800

    ut

--- src/framework/next/coll_comms/rank/my_rank.cc
+++ src/framework/next/coll_comms/rank/my_rank.cc
@@ -714,8 +714,6 @@ HcclResult MyRank::ChannelGetRemoteMems(ChannelHandle channel, CommMem **remoteM
     CHK_PTR_NULL(memTags);
     CHK_PTR_NULL(memNum);
     CHK_RET(static_cast<HcclResult>(HcommChannelGetRemoteMems(channel, remoteMem, memTags, memNum)));
-    // AicpuTsHccsChannel不使用memTag，返回为空，不检查
-    CHK_PTR_NULL(*remoteMem);
     HCCL_INFO("[%s] success. memNum[%u]", __func__, *memNum);
     return HCCL_SUCCESS;
 }
