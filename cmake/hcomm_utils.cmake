566d08d6ffbf37e3cd8f5c9fd64e500473035330
dingzhiqiang <dingzhiqiang3@huawei.com>
2026-05-27 14:56:58 +0800

    hcom-legacy update

--- cmake/hcomm_utils.cmake
+++ cmake/hcomm_utils.cmake
@@ -11,10 +11,10 @@
 unset(hcomm_utils_FOUND CACHE)
 unset(TLS_ADP_LIBRARY CACHE)
 
-set(HCOMM_UTILS_VERSION "9.0.0")
+set(HCOMM_UTILS_VERSION "9.1.0")
 set(HCOMM_UTILS_ARCH "${CMAKE_SYSTEM_PROCESSOR}")
 set(HCOMM_UTILS_FILE "cann-hcomm-utils_${HCOMM_UTILS_VERSION}_linux-${HCOMM_UTILS_ARCH}.tar.gz")
-set(HCOMM_UTILS_URL "https://ascend-cann.obs.cn-north-4.myhuaweicloud.com/CANN/20260330_newest/${HCOMM_UTILS_FILE}")
+set(HCOMM_UTILS_URL "https://ascend-cann.obs.cn-north-4.myhuaweicloud.com/CANN/20260527_newest/${HCOMM_UTILS_FILE}")
 set(HCOMM_UTILS_PKG_PATH ${CANN_3RD_LIB_PATH}/${HCOMM_UTILS_FILE})
 set(HCOMM_UTILS_INSTALL_PATH ${CANN_3RD_LIB_PATH}/hcomm_utils)
 
@@ -77,7 +77,7 @@ elseif(PRODUCT_SIDE STREQUAL "host")
     if(EXISTS ${HCOMM_UTILS_GLOB_PKG})
         set(HCOMM_UTILS_URL_HASH "")    # 忽略版本号，不校验哈希值
     elseif(HCOMM_UTILS_ARCH MATCHES "aarch64|ARM64|arm64")
-        set(HCOMM_UTILS_URL_HASH "SHA256=bf36523b855842f7d391eeaaa23ed7ffa066deb591e531e8ec6fa0c35d422aff")
+        set(HCOMM_UTILS_URL_HASH "SHA256=bbd7b3c3c78c9ad12ba5109ce7b0d4cf37c73c9a3844f3abd601cd4b0db9eeb5")
     else()
         set(HCOMM_UTILS_URL_HASH "SHA256=7b5eea6297b733922b0d6805f02190a6a5fe9f0e2ff969b76375875a7db57746")
     endif()
