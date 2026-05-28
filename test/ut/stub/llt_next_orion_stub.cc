51eefbf985bb3adf092111b9f4795c60bb95020a
acjr0011 <chenjiarong6@huawei.com>
2026-05-28 16:58:27 +0800

    ccu profiling sw change l2 to l1

--- test/ut/stub/llt_next_orion_stub.cc
+++ test/ut/stub/llt_next_orion_stub.cc
@@ -1887,7 +1887,7 @@ void ProfilingHandler::ReportStoragedAdditionInfo()
 {
 }
 
-void ProfilingHandler::StartL2Subscribe()
+void ProfilingHandler::StartCcuSubscribe()
 {
 }
 
@@ -1909,11 +1909,6 @@ bool ProfilingHandler::GetHcclL0State() const
 }
 
 bool ProfilingHandler::GetHcclL1State() const
-{
-    return false;
-}
-
-bool ProfilingHandler::GetHcclL2State() const
 {
     return false;
 }
