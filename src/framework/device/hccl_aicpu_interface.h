7fe9e81634217da2cf7a91bb2980e742d68dd9b3
liujt_ <liujintao28@huawei.com>
2026-05-28 19:45:15 +0800

    add a5 group

--- src/framework/device/hccl_aicpu_interface.h
+++ src/framework/device/hccl_aicpu_interface.h
@@ -16,6 +16,35 @@
 extern "C" {
 __attribute__((visibility("default"))) uint32_t RunAicpuKfcResInitV2(void *args);
 __attribute__((visibility("default"))) uint32_t RunAicpuRpcSrvLaunchV2(void *args);
+__attribute__((visibility("default"))) uint32_t RunAicpuNotifyRecord(void *args);
+__attribute__((visibility("default"))) uint32_t RunAicpuNotifyWait(void *args);
+__attribute__((visibility("default"))) uint32_t HcclP2pLaunchNonGroupSynAicpuKernel(void *args);
+__attribute__((visibility("default"))) uint32_t HcclP2pLaunchGroupAicpuKernel(void *args);
+
+struct ThreadNotifyRecordParam {
+    ThreadHandle thread;
+    ThreadHandle dstThread;
+    uint32_t dstNotifyIdx;
+};
+struct ThreadNotifyWaitParam {
+    ThreadHandle thread;
+    uint32_t notifyIdx;
+    uint32_t timeout;
+};
+
+struct P2pAicpuKernelParam {
+    ThreadNotifyWaitParam waitParam;    // NotifyWait 参数
+    hccl::HcclKernelFuncInfo funcInfo;         // dlopen/func 参数
+    ThreadNotifyRecordParam recordParam; // NotifyRecord 参数
+    void* funcArgs;                      // func执行的参数(OpParam*)
+    ThreadHandle sendRecvStream;         // P2P stream参数
+};
+
+struct P2pGroupAicpuKernelParam {
+    hccl::HcclKernelFuncInfo funcInfo;         // dlopen/func 参数
+    void* funcArgs;                      // func执行的参数(OpParam*)
+    ThreadHandle sendRecvStream;         // P2P stream参数
+};
 }
 
 #endif // __MC2_AICPU_INTERFACE_HPP__
