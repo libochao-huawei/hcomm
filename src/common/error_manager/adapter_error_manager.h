62ec7a0056d17c2526d5872e014c48b8c3a5601f
z30083286 <zenglingshun@h-parters.com>
2026-05-29 16:02:28 +0800

    检视

--- src/common/error_manager/adapter_error_manager.h
+++ src/common/error_manager/adapter_error_manager.h
@@ -1,33 +1,26 @@
-#ifndef HCCLINC_ADAPTER_ERROR_MANAGER_H
-#define HCCLINC_ADAPTER_ERROR_MANAGER_H
+/**
+ * Copyright (c) 2025 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+#ifndef HCCL_INC_ADAPTER_ERROR_MANAGER_H
+#define HCCL_INC_ADAPTER_ERROR_MANAGER_H
 
 #include <string>
 #include "log.h"
 #include "adapter_error_manager_pub.h"
+#include "base/err_msg.h"
+#include "base/err_mgr.h"
 
-// 优先包含新框架头文件
-#if __has_include("base/err_msg.h")
-    #include "base/err_msg.h"
-    #include "base/err_mgr.h"
-    using ErrContext = error_message::ErrorManagerContext;
-#else
-    // 降级到旧定义
-    struct Context {
-        uint64_t work_stream_id;
-        uint64_t reserved[7] = {0};
-    };
-    using ErrContext = struct Context;
-#endif
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+using ErrContext = error_message::ErrorManagerContext;
 
-ErrContext hrtErrMGetErrorContext(void);
-void hrtErrMSetErrorContext(ErrContext error_context);
+ErrContext __attribute__((weak)) hrtErrMGetErrorContext(void);
+void __attribute__((weak)) hrtErrMSetErrorContext(ErrContext error_context);
 
-#ifdef __cplusplus
-}
-#endif
-
-#endif
+#endif  // HCCL_INC_ADAPTER_ERROR_MANAGER_H
