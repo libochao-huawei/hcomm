fc4db65e4829ae7b9d953830b3d5cb0fe4a1dac3
lkk6 <luokangkang1@h-partners.com>
2026-05-29 14:57:22 +0800

    add ccu eid

--- src/framework/next/coll_comms/dfx/taskException/host/ccuTaskException.cc
+++ src/framework/next/coll_comms/dfx/taskException/host/ccuTaskException.cc
@@ -1173,6 +1173,11 @@ HcclResult CcuTaskException::GetCcuJettys(const CcuErrorInfo& errorInfo, std::pa
     CHK_PTR_NULL(endpoint);
     UrmaEndpoint *ccuEndpoint = dynamic_cast<UrmaEndpoint *>(static_cast<Endpoint *>(endpoint));
 
+    // 打印localEid和remoteEid
+    CHK_PTR_NULL(ccuEndpoint);
+    HCCL_ERROR("[GetCcuJettys]localEid[%s], remoteEid[%s]",
+        hcomm::logger::CommAddrLogger::ToString(ccuEndpoint->GetEndpointDesc().commAddr).c_str(),
+        hcomm::logger::CommAddrLogger::ToString(channelImpl->GetChannelDesc().remoteEndpoint.commAddr).c_str());
     // UrmaEndpoint -> CcuChannelCtxPool
     CcuChannelCtxPool *ccuChannelCtxPool = ccuEndpoint->GetCcuChannelCtxPool();
     CHK_PTR_NULL(ccuChannelCtxPool);
