36a68ec7a3a54e283d74c4aa15682e016be03414
Leewis <liwei945@huawei.com>
2026-06-01 14:38:26 +0800

    modify multcode aicpu_communicator.cc & coll_comm_aicpu.cc

--- src/framework/communicator/impl/independent_op/aicpu_init_param.h
+++ src/framework/communicator/impl/independent_op/aicpu_init_param.h
@@ -10,8 +10,10 @@
 #ifndef AICPU_INIT_PARAM_H
 #define AICPU_INIT_PARAM_H
 
+#include <memory>
 #include "hccl_common.h"
 #include "hdc_pub.h"
+#include "log.h"
 
 constexpr uint32_t HCOMID_MAX_SIZE = 128;
 
@@ -33,5 +35,29 @@ struct CommAicpuParam {
     hccl::HDCommunicateParams kfcStatusTransferD2HParams;
     DevAicpuCommConfig commConfig; // 收编通信域配置类变量
 };
+
+namespace hccl {
+
+inline HcclResult InitHDCommunicate(
+    std::shared_ptr<HDCommunicate>& h2dComm,
+    std::shared_ptr<HDCommunicate>& d2hComm,
+    const CommAicpuParam* commAicpuParam)
+{
+    if (commAicpuParam->kfcControlTransferH2DParams.buffLen != 0 && h2dComm == nullptr) {
+        EXCEPTION_CATCH((h2dComm = std::make_shared<HDCommunicate>()), return HCCL_E_PTR);
+        CHK_SMART_PTR_NULL(h2dComm);
+        CHK_RET(h2dComm->InitDevice(commAicpuParam->kfcControlTransferH2DParams));
+    }
+    
+    if (commAicpuParam->kfcStatusTransferD2HParams.buffLen != 0 && d2hComm == nullptr) {
+        EXCEPTION_CATCH((d2hComm = std::make_shared<HDCommunicate>()), return HCCL_E_PTR);
+        CHK_SMART_PTR_NULL(d2hComm);
+        CHK_RET(d2hComm->InitDevice(commAicpuParam->kfcStatusTransferD2HParams));
+    }
+    
+    return HCCL_SUCCESS;
+}
+
+}  // namespace hccl
 
 #endif
