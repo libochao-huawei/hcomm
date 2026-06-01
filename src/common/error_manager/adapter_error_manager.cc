9bcd734f81f446b486d8126e7d2b9fd035ded327
z30083286 <zenglingshun@h-parters.com>
2026-06-01 09:33:50 +0800

    黄区编译

--- src/common/error_manager/adapter_error_manager.cc
+++ src/common/error_manager/adapter_error_manager.cc
@@ -1,12 +1,7 @@
 /**
- * Copyright (c) 2025 Huawei Technologies Co., Ltd.
- * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
- * CANN Open Software License Agreement Version 2.0 (the "License").
- * Please refer to the License for details. You may not use this file except in compliance with the License.
- * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
- * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
- * See LICENSE in the root of the software repository for the full text of the License.
- */
+ * Copyright (c) 2025 Huawei Technologies Co., Ltd.
+ * ... (License header) ...
+ */
 
 #include <string>
 #include "log.h"
@@ -17,10 +12,26 @@
 
 ErrContext hrtErrMGetErrorContext(void)
 {
-    return error_message::GetErrMgrContext();
+    error_message::ErrorManagerContext sdk_ctx = error_message::GetErrMgrContext();
+    
+    ErrContext local_ctx;
+    local_ctx.work_stream_id = sdk_ctx.work_stream_id;
+    // 复制 reserved 数组
+    for (int i = 0; i < 7; ++i) {
+        local_ctx.reserved[i] = sdk_ctx.reserved[i];
+    }
+    
+    return local_ctx;
 }
 
 void hrtErrMSetErrorContext(ErrContext error_context)
 {
-    error_message::SetErrMgrContext(error_context);
+    error_message::ErrorManagerContext sdk_ctx;
+    sdk_ctx.work_stream_id = error_context.work_stream_id;
+    // 复制 reserved 数组
+    for (int i = 0; i < 7; ++i) {
+        sdk_ctx.reserved[i] = error_context.reserved[i];
+    }
+    
+    error_message::SetErrMgrContext(sdk_ctx);
 }
