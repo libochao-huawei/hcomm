8bacebe67508557d317896fe42f6c8f877ea43d5
leironghao1 <leironghao1@h-partners.com>
2026-06-02 19:05:35 +0800

    hcomm参数一致性校验

--- src/coll_communicator/resource_mgr/local/my_rank/exchange_info_mgr.h
+++ src/coll_communicator/resource_mgr/local/my_rank/exchange_info_mgr.h
@@ -29,6 +29,7 @@ public:
         CollCommConfigConsistency &collCommConfigConsistency,
         const std::string &commTag);
     HcclResult CheckHcommInfo(
+        const HcclChannelDesc* channelDescs,
         const std::vector<Hccl::Socket*> &sockets,
         const std::vector<u32> &remoteRanks,
         const std::vector<HcommSocketRole> &roles,
