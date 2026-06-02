1bae71b1472b7f0877e029200d69942547db5a4a
xz00xz <xiongzhe5@huawei.com>
2026-06-02 20:42:56 +0800

    hccp add udevList fix host ub issue

--- src/platform/hccp/rdma_service/rs.c
+++ src/platform/hccp/rdma_service/rs.c
@@ -1689,6 +1689,11 @@ STATIC void RsFreeRdevList(struct rs_cb *rsCb)
     unsigned int phyId = 0;
     int ret;
 
+    if (RsListEmpty(&rsCb->rdevList)) {
+        return;
+    }
+
+    hccp_warn("Rdev list is not empty!");
     ret = rsGetDevIDByLocalDevID(rsCb->chipId, &phyId);
     if (ret != 0) {
         hccp_err("chipId[%u] invalid, ret %d", rsCb->chipId, ret);
@@ -1713,8 +1718,13 @@ STATIC void RsFreeUdevList(struct rs_cb *rsCb)
     struct RsUbDevCb *udevCbNext = NULL;
     int ret;
 
-    RS_LIST_GET_HEAD_ENTRY(udevCbCurr, udevCbNext, &rsCb->rdevList, list, struct RsUbDevCb);
-    for (; (&udevCbCurr->list) != &rsCb->rdevList;
+    if (RsListEmpty(&rsCb->udevList)) {
+        return;
+    }
+
+    hccp_warn("Udev list is not empty!");
+    RS_LIST_GET_HEAD_ENTRY(udevCbCurr, udevCbNext, &rsCb->udevList, list, struct RsUbDevCb);
+    for (; (&udevCbCurr->list) != &rsCb->udevList;
         udevCbCurr = udevCbNext, udevCbNext = list_entry(udevCbNext->list.next, struct RsUbDevCb, list)) {
         ret = RsUbCtxDeinit(udevCbCurr);
         if (ret != 0) {
@@ -1722,27 +1732,6 @@ STATIC void RsFreeUdevList(struct rs_cb *rsCb)
         }
     }
 
-    return;
-}
-
-STATIC void RsFreeDevList(struct rs_cb *rsCb)
-{
-    if (RsListEmpty(&rsCb->rdevList)) {
-        return;
-    }
-
-    hccp_warn("dev list is not empty!");
-    switch (rsCb->protocol) {
-        case PROTOCOL_RDMA:
-            RsFreeRdevList(rsCb);
-            break;
-        case PROTOCOL_UDMA:
-            RsFreeUdevList(rsCb);
-            break;
-        default:
-            hccp_err("protocol[%d] not support", rsCb->protocol);
-            break;
-    }
     return;
 }
 
@@ -1811,7 +1800,8 @@ STATIC void RsDeinitFreeRscb(struct rs_cb *rscb)
     freeifaddrs(rscb->ifaddrList);
     rscb->ifaddrList = NULL;
     RS_PTHREAD_MUTEX_ULOCK(&rscb->mutex);
-    RsFreeDevList(rscb);
+    RsFreeRdevList(rscb);
+    RsFreeUdevList(rscb);
     RsSslFree(rscb);
     RsFreeHeterogTcpFdList(rscb);
 #ifdef CONFIG_TLV
