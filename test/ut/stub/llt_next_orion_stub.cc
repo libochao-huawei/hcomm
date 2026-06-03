e2ff4fbb0b4f729a3f37a47565dffbffee488865
leironghao1 <leironghao1@h-partners.com>
2026-06-03 15:13:18 +0800

    hcomm参数一致性校验

--- test/ut/stub/llt_next_orion_stub.cc
+++ test/ut/stub/llt_next_orion_stub.cc
@@ -114,6 +114,7 @@
 #include "../../../legacy/ascend950/unified_platform/external_system/orion_adapter_hccp.h"
 #include "../../../legacy/ascend950/include/hccl_communicator.h"
 #include "../../../legacy/ascend950/unified_platform/ccu/ccu_microcode/ccu_assist.h"
+#include "../../../legacy/ascend950/framework/entrance/rank_table_crc_bridge.h"
 #include "acl/acl_rt.h"
 
 #include "p2p_transport.h"
