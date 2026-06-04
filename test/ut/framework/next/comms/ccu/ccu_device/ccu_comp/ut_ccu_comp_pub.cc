5f3587dd905526e222b32f3a4816e3ff19a529a5
Klayyy <wanglei7@126.com>
2026-06-04 17:40:51 +0800

    ut有用例失败

--- test/ut/framework/next/comms/ccu/ccu_device/ccu_comp/ut_ccu_comp_pub.cc
+++ test/ut/framework/next/comms/ccu/ccu_device/ccu_comp/ut_ccu_comp_pub.cc
@@ -209,7 +209,9 @@ void PrepareLoopJettyTestFixture(CcuComponent &comp, const uint8_t dieId, const 
     comp.tpAttrInfoMap_[dieId] = tpAttrInfo;
 
     MOCKER_CPP(&Hccl::RdmaHandleManager::GetByIp).stubs().will(returnValue(reinterpret_cast<RdmaHandle>(0x300)));
-    MOCKER_CPP(&Hccl::RdmaHandleManager::GetJfcHandle).stubs().will(returnValue(reinterpret_cast<void *>(0x400)));
+    MOCKER_CPP(&Hccl::RdmaHandleManager::GetJfcHandle)
+        .stubs()
+        .will(returnValue(static_cast<Hccl::JfcHandle>(0x400ULL)));
     MOCKER(HccpUbCreateJetty).stubs().will(invoke(StubHccpUbCreateJettyCaptureQos));
     MOCKER(HccpUbTpImportJetty).stubs().will(invoke(StubHccpUbTpImportJettyOk));
 }
