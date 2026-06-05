3ef3b427057515ba09f83556fd80a716e9575281
ren-mengguang <renmengguang@h-partners.com>
2026-06-05 17:28:49 +0800

    迁移aicpu_ts_sync_data_a_adpt

--- src/base_comm/primitives/api_c_adpt/aicpu_ts_sync_data_a_adpt.cc
+++ src/base_comm/primitives/api_c_adpt/aicpu_ts_sync_data_a_adpt.cc
@@ -7,7 +7,7 @@
  * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
  * See LICENSE in the root of the software repository for the full text of the License.
  */
-#include "hcomm_primitives_inner.h"
+#include "aicpu_ts_sync_data_c_adpt.h"
 
 #include <chrono>
 #include <thread>
