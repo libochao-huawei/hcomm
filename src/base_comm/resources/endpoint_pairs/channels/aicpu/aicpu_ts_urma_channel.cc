f611ebc88d919ebdac2c675b6e38f0c09f39c9d6
hblnb <huangbolin3@huawei.com>
2026-06-05 20:14:24 +0800

    修复N秒快恢 batchsendrecv aicpu模式core问题

--- src/base_comm/resources/endpoint_pairs/channels/aicpu/aicpu_ts_urma_channel.cc
+++ src/base_comm/resources/endpoint_pairs/channels/aicpu/aicpu_ts_urma_channel.cc
@@ -33,6 +33,7 @@ AicpuTsUrmaChannel::~AicpuTsUrmaChannel()
 {
     if (socket_ != nullptr) {
         SocketMgr::GetInstance(devicePhyId_).PutSocket(socketConfig_, socket_);
+        memTransport_->SetSocket(nullptr);
         socket_ = nullptr;
     }
 }
@@ -279,6 +280,11 @@ HcclResult AicpuTsUrmaChannel::GetRemoteMem(HcclMem **remoteMem, uint32_t *memNu
 
 ChannelStatus AicpuTsUrmaChannel::GetStatus()
 {
+    if (socket_ == nullptr && socketConfig_ != nullptr) {
+        SocketMgr::GetInstance(devicePhyId_).GetSocket(*socketConfig_, socket_);
+        printf("[TEST][AicpuTsUrmaChannel][%s] AicpuTsUrmaChannel get socket, socket=%p\n", __func__, socket_);
+        memTransport_->SetSocket(socket_);
+    }
     ChannelStatus out = Channel::TransportStatusToChannelStatus(memTransport_->GetStatus());
 
     if (isFirstPrintChannelInfo_ && out == ChannelStatus::READY) {
@@ -298,6 +304,7 @@ ChannelStatus AicpuTsUrmaChannel::GetStatus()
     
     if (out == ChannelStatus::READY && socket_ != nullptr) {
         SocketMgr::GetInstance(devicePhyId_).PutSocket(socketConfig_, socket_);
+        memTransport_->SetSocket(nullptr);
     }
     return out;
 }
