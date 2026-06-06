9f89874d5e64938f50cd45e6e4ba44d4338be8f7
zhaoyingdong <zhaoyingdong@huawei.com>
2026-06-06 11:09:16 +0800

    upports parallel compilation on the host and device sides

--- cmake/hcomm_utils.cmake
+++ cmake/hcomm_utils.cmake
@@ -8,57 +8,64 @@
 # See LICENSE in the root of the software repository for the full text of the License.
 # -----------------------------------------------------------------------------------------------------------
 
+include_guard(GLOBAL)
+
 unset(hcomm_utils_FOUND CACHE)
 unset(TLS_ADP_LIBRARY CACHE)
+
+if(NOT CANN_3RD_LIB_PATH)
+    set(CANN_3RD_LIB_PATH ${CMAKE_SOURCE_DIR}/third_party)
+endif()
+
+if(PRODUCT_SIDE STREQUAL "device")
+    set(HCOMM_UTILS_INSTALL_PATH ${CANN_3RD_LIB_PATH}/lib_cache/device/hcomm_utils)
+    set(HCOMM_UTILS_PKG_PATH ${CANN_3RD_LIB_PATH}/pkg/device)
+else()
+    set(HCOMM_UTILS_INSTALL_PATH ${CANN_3RD_LIB_PATH}/lib_cache/hcomm_utils)
+    set(HCOMM_UTILS_PKG_PATH ${CANN_3RD_LIB_PATH}/pkg)
+endif()
 
 set(HCOMM_UTILS_VERSION "9.1.0")
 set(HCOMM_UTILS_ARCH "${CMAKE_SYSTEM_PROCESSOR}")
 set(HCOMM_UTILS_FILE "cann-hcomm-utils_${HCOMM_UTILS_VERSION}_linux-${HCOMM_UTILS_ARCH}.tar.gz")
 set(HCOMM_UTILS_URL "https://ascend-cann.obs.cn-north-4.myhuaweicloud.com/CANN/20260527_newest/${HCOMM_UTILS_FILE}")
-set(HCOMM_UTILS_PKG_PATH ${CANN_3RD_LIB_PATH}/${HCOMM_UTILS_FILE})
-set(HCOMM_UTILS_INSTALL_PATH ${CANN_3RD_LIB_PATH}/hcomm_utils)
 
-# 查找目录下是否已经安装，避免重复编译安装
-message(STATUS "[ThirdParty] HCOMM_UTILS_INSTALL_PATH=${HCOMM_UTILS_INSTALL_PATH} PRODUCT_SIDE=${PRODUCT_SIDE}")
 find_library(TLS_ADP_LIBRARY
     NAMES libtls_adp.so
     PATH_SUFFIXES lib
-    NO_CMAKE_SYSTEM_PATH
-    NO_CMAKE_FIND_ROOT_PATH
-    NO_CMAKE_PATH
     PATHS ${HCOMM_UTILS_INSTALL_PATH}/${PRODUCT_SIDE}
+    NO_DEFAULT_PATH
 )
 
-# 是否找到 hcomm_utils 的库文件
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(hcomm_utils
     FOUND_VAR
-    hcomm_utils_FOUND 
+        hcomm_utils_FOUND
     REQUIRED_VARS
-    TLS_ADP_LIBRARY
+        TLS_ADP_LIBRARY
 )
-message(STATUS "[ThirdParty] Found hcomm_utils: ${hcomm_utils_FOUND}")
 
 if(hcomm_utils_FOUND AND NOT FORCE_REBUILD_CANN_3RD)
-    message(STATUS "[ThirdParty] hcomm_utils found in ${HCOMM_UTILS_INSTALL_PATH}, and not force rebuild cann third_party")
-elseif(PRODUCT_SIDE STREQUAL "host")
-    # Host 侧编译时下载 hcomm_utils 包，Device 侧编译时进行复用
+    message(STATUS "[ThirdParty] hcomm_utils found in ${TLS_ADP_LIBRARY}, and not force rebuild cann third_party")
+else()
     file(GLOB HCOMM_UTILS_GLOB_PKG
         ${CANN_3RD_LIB_PATH}/cann-hcomm-utils_*_linux-${HCOMM_UTILS_ARCH}.tar.gz
     )
     if(EXISTS ${HCOMM_UTILS_GLOB_PKG})
         # 离线编译场景，优先使用已下载的包（忽略版本号）
         message(STATUS "[ThirdParty] Found local hcomm_utils package (ignore version): ${HCOMM_UTILS_GLOB_PKG}")
-        set(HCOMM_UTILS_PKG_PATH ${HCOMM_UTILS_GLOB_PKG})
-        set(HCOMM_UTILS_PROJECT_URL ${HCOMM_UTILS_PKG_PATH})
+        set(HCOMM_UTILS_PROJECT_URL ${HCOMM_UTILS_GLOB_PKG})
+        set(HCOMM_UTILS_FULLPATH ${HCOMM_UTILS_GLOB_PKG})
     elseif(EXISTS ${HCOMM_UTILS_PKG_PATH})
         # 离线编译场景，优先使用已下载的包
         message(STATUS "[ThirdParty] Found local hcomm_utils package: ${HCOMM_UTILS_PKG_PATH}")
         set(HCOMM_UTILS_PROJECT_URL ${HCOMM_UTILS_PKG_PATH})
+        set(HCOMM_UTILS_FULLPATH ${HCOMM_UTILS_PKG_PATH})
     else()
         # 下载并解压
         message(STATUS "[ThirdParty] Downloading hcomm_utils from ${HCOMM_UTILS_URL}")
         set(HCOMM_UTILS_PROJECT_URL ${HCOMM_UTILS_URL})
+        set(HCOMM_UTILS_FULLPATH ${HCOMM_UTILS_PKG_PATH}/${HCOMM_UTILS_FILE})
     endif()
 
     if(EXISTS ${HCOMM_UTILS_GLOB_PKG})
@@ -73,42 +80,40 @@ elseif(PRODUCT_SIDE STREQUAL "host")
     ExternalProject_Add(hcomm_utils
         URL ${HCOMM_UTILS_PROJECT_URL}
         URL_HASH ${HCOMM_UTILS_URL_HASH}
-        TLS_VERIFY OFF
-        DOWNLOAD_NO_EXTRACT TRUE
-        DOWNLOAD_NO_PROGRESS TRUE
-        DOWNLOAD_DIR ${CANN_3RD_LIB_PATH}
+        DOWNLOAD_DIR ${HCOMM_UTILS_PKG_PATH}
         SOURCE_DIR ${HCOMM_UTILS_INSTALL_PATH}
-        CONFIGURE_COMMAND tar -xf ${HCOMM_UTILS_PKG_PATH} --overwrite --strip-components=2 -C <SOURCE_DIR>
+        CONFIGURE_COMMAND tar -xf ${HCOMM_UTILS_FULLPATH} --overwrite --strip-components=2 -C <SOURCE_DIR>
         BUILD_COMMAND ""
         INSTALL_COMMAND ""
+        DOWNLOAD_NO_EXTRACT TRUE
+        DOWNLOAD_NO_PROGRESS TRUE
     )
 endif()
 
-if(NOT EXISTS "${HCOMM_UTILS_INSTALL_PATH}/${PRODUCT_SIDE}/include")
+if(NOT EXISTS ${HCOMM_UTILS_INSTALL_PATH}/${PRODUCT_SIDE}/include)
+    file(MAKE_DIRECTORY "${HCOMM_UTILS_INSTALL_PATH}/${PRODUCT_SIDE}/include/kms")
     file(MAKE_DIRECTORY "${HCOMM_UTILS_INSTALL_PATH}/${PRODUCT_SIDE}/include/legacy")
     file(MAKE_DIRECTORY "${HCOMM_UTILS_INSTALL_PATH}/${PRODUCT_SIDE}/include/tls_adp")
-    file(MAKE_DIRECTORY "${HCOMM_UTILS_INSTALL_PATH}/${PRODUCT_SIDE}/include/kms")
 endif()
 
-# 创建 headers 目标
-add_library(hcomm_legacy_headers INTERFACE IMPORTED)
-add_dependencies(hcomm_legacy_headers hcomm_utils)
-set_target_properties(hcomm_legacy_headers PROPERTIES
-    INTERFACE_INCLUDE_DIRECTORIES "${HCOMM_UTILS_INSTALL_PATH}/${PRODUCT_SIDE}/include/legacy"
-)
-
+# 创建导入的目标
 add_library(ascend_kms_headers INTERFACE IMPORTED)
 add_dependencies(ascend_kms_headers hcomm_utils)
 set_target_properties(ascend_kms_headers PROPERTIES
     INTERFACE_INCLUDE_DIRECTORIES "${HCOMM_UTILS_INSTALL_PATH}/${PRODUCT_SIDE}/include/kms"
 )
 
-# 创建链接库目标
 add_library(ascend_kms SHARED IMPORTED)
 add_dependencies(ascend_kms hcomm_utils)
 set_target_properties(ascend_kms PROPERTIES
     INTERFACE_INCLUDE_DIRECTORIES "${HCOMM_UTILS_INSTALL_PATH}/${PRODUCT_SIDE}/include/kms"
     IMPORTED_LOCATION "${HCOMM_UTILS_INSTALL_PATH}/${PRODUCT_SIDE}/lib/libascend_kms.so"
+)
+
+add_library(tls_adp_headers SHARED IMPORTED)
+add_dependencies(tls_adp_headers hcomm_utils)
+set_target_properties(tls_adp_headers PROPERTIES
+    INTERFACE_INCLUDE_DIRECTORIES "${HCOMM_UTILS_INSTALL_PATH}/${PRODUCT_SIDE}/include/tls_adp"
 )
 
 add_library(tls_adp SHARED IMPORTED)
@@ -118,6 +123,12 @@ set_target_properties(tls_adp PROPERTIES
     IMPORTED_LOCATION "${HCOMM_UTILS_INSTALL_PATH}/${PRODUCT_SIDE}/lib/libtls_adp.so"
 )
 
+add_library(hccl_legacy_headers SHARED IMPORTED)
+add_dependencies(hccl_legacy_headers hcomm_utils)
+set_target_properties(hccl_legacy_headers PROPERTIES
+    INTERFACE_INCLUDE_DIRECTORIES "${HCOMM_UTILS_INSTALL_PATH}/${PRODUCT_SIDE}/include/legacy"
+)
+
 if(PRODUCT_SIDE STREQUAL "host")
     add_library(hccl_legacy SHARED IMPORTED)
     add_dependencies(hccl_legacy hcomm_utils)
@@ -125,10 +136,7 @@ if(PRODUCT_SIDE STREQUAL "host")
         INTERFACE_INCLUDE_DIRECTORIES "${HCOMM_UTILS_INSTALL_PATH}/${PRODUCT_SIDE}/include/legacy"
         IMPORTED_LOCATION "${HCOMM_UTILS_INSTALL_PATH}/${PRODUCT_SIDE}/lib/libhccl_legacy.so"
     )
-endif()
 
-# 安装 host 侧需要的库
-if(PRODUCT_SIDE STREQUAL "host")
     set(HCOMM_UTILS_HOST_LIB_FILES
         ${HCOMM_UTILS_INSTALL_PATH}/host/lib/libtls_adp.so
         ${HCOMM_UTILS_INSTALL_PATH}/host/lib/libhccl_legacy.so
