8d8bc312f643aad5342fdc979b8e5186c747b216
ren-mengguang <renmengguang@h-partners.com>
2026-06-08 16:43:43 +0800

    迁移rank graph

--- src/coll_communicator/rank_graphs/rank_graph_v2.h
+++ src/coll_communicator/rank_graphs/rank_graph_v2.h
@@ -7,28 +7,39 @@
  * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
  * See LICENSE in the root of the software repository for the full text of the License.
  */
-#ifndef RANK_GRAPH_V2_H
-#define RANK_GRAPH_V2_H
-
-#include <unordered_map>
-#include "rank_graph.h"
-
+#ifndef HCOMM_RANK_GRAPH_V2_H
+#define HCOMM_RANK_GRAPH_V2_H
+#include "rank_graph_base.h"
+#include "rank_graph_interface.h"
 namespace hccl {
-/**
- * @note 职责：集合通信域内的version为2.0+（91095...）的RankGraph的派生实现类
- */
+
 class RankGraphV2 : public RankGraph {
 public:
-    RankGraphV2(const std::string& rankTable, const std::string& topoFile);
-    ~RankGraphV2() override = default;
+    RankGraphV2();
+    RankGraphV2(void *rankGraphPtr);
+    ~RankGraphV2() override;
 
-    uint32_t GetRankId() const override;
-    uint32_t GetRankSize() const override;
+    HcclResult GetRankId(uint32_t *rank) override;
+    HcclResult GetRankSize(uint32_t *rankSize) override;
+    HcclResult GetLinks(uint32_t netLayer, uint32_t srcRank, uint32_t dstRank, CommLink **linkList, uint32_t *listSize) override;
+    HcclResult GetRankGraphInfo(GraphType type, void **graph, uint32_t *len) override;
+    HcclResult GetNetLayers(uint32_t **netLayers, uint32_t *netLayerNum) override;
+    HcclResult GetInstTopoTypeByNetLayer(uint32_t netLayer, CommTopo *topoType) override;
+    HcclResult GetInstSizeByNetLayer(uint32_t netLayer, uint32_t *rankNum) override;
+    HcclResult GetInstRanksByNetLayer(uint32_t netLayer, uint32_t **rankList, uint32_t *rankNum) override;
+    HcclResult GetInstSizeListByNetLayer(uint32_t netLayer, uint32_t **instSizeList, uint32_t *listSize) override;
+    HcclResult GetDevicePort(const uint32_t rank, uint32_t *devPort) override;
+    HcclResult GetListenPort(const uint32_t rank, uint32_t *listenPort, EndpointLocType locType) override;
+    HcclResult GetTopoInstsByLayer(uint32_t netLayer, uint32_t** topoInsts, uint32_t* topoInstNum);
+    HcclResult GetTopoType(const uint32_t netLayer, const uint32_t topoInstId, CommTopo* topoType);
+    HcclResult GetRanksByTopoInst(const uint32_t netLayer, const uint32_t topoInstId, uint32_t** ranks, uint32_t* rankNum);
+    HcclResult GetEndpointNum(uint32_t netLayer, uint32_t topoInstId, uint32_t *num);
+ 	HcclResult GetEndpointDesc(uint32_t netLayer, uint32_t topoInstId, uint32_t *descNum, EndpointDesc *endpointDesc);
+ 	HcclResult GetEndpointInfo(uint32_t rankId, const EndpointDesc *endPointDesc, EndpointAttr endpointAttr, uint32_t infoLen, void *info);
+    HcclResult GetDeviceId(uint32_t rankId, uint32_t *deviceId);
 
 private:
-    void ParseRankTable(const std::string& rankTable);
-    void ParseTopoFile(const std::string& topoFile);
+    std::unique_ptr<Hccl::IRankGraph> pImpl;
 };
-}
-
-#endif // RANK_GRAPH_V2_H
+}  // namespace hccl
+#endif
