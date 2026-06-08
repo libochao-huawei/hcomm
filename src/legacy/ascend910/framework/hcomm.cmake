ae210de8c8ace1114210f024a8b2ae03164b6b38
jiyuanhao <jiyuanhao1@huawei.com>
2026-06-08 19:40:00 +0800

    revert

--- src/legacy/ascend910/framework/hcomm.cmake
+++ src/legacy/ascend910/framework/hcomm.cmake
@@ -110,6 +110,7 @@ target_include_directories(hcomm PRIVATE
     ${LEGACY_ASCEND950_INCLUDE_LIST}
 
     # 三方件头文件
+    ${JSON_INCLUDE_DIR}
     ${URMA_INCLUDE_DIR}
 )
 
@@ -117,6 +118,11 @@ if(BUILD_OPEN_PROJECT)
     target_compile_definitions(hcomm PRIVATE
         OPEN_BUILD_PROJECT
         LOG_CPP
+    )
+
+    target_include_directories(hcomm PRIVATE
+        # 三方件头文件
+        ${CANN_3RD_LIB_PATH}/hcomm_utils/${PRODUCT_SIDE}/include/legacy
     )
 
     target_link_libraries(hcomm
@@ -129,8 +135,6 @@ if(BUILD_OPEN_PROJECT)
         $<BUILD_INTERFACE:mmpa_headers>
         $<BUILD_INTERFACE:runtime_headers>
         $<BUILD_INTERFACE:rdma_core_headers>
-        $<BUILD_INTERFACE:hccl_legacy_headers>
-        $<BUILD_INTERFACE:json>
         -Wl,--no-as-needed
         c_sec
         unified_dlog
