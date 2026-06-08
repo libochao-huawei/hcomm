5ce9227b4ca804a353a939af6090cceb463eb243
leironghao1 <leironghao1@h-partners.com>
2026-06-08 21:02:12 +0800

    Revert "hcomm参数一致性校验"
    This reverts commit 628d8c54cf454785f535de8fa5cf9b1f0f70b88c.

--- src/legacy/ascend910/framework/communicator/impl/hccl_communicator_host.cc
+++ src/legacy/ascend910/framework/communicator/impl/hccl_communicator_host.cc
@@ -6291,7 +6291,7 @@ namespace hccl
             dataType = param.GetDataType();
         }
 
-        if(GetExternalInconsistentCheckSwitch() == InconsistentCheckMode::ON) {
+        if(GetExternalInconsistentCheckSwitch() == InconsistentCheckMode::ON){
             if (param.opType != HcclCMDType::HCCL_CMD_BATCH_SEND_RECV) {
                 OpInfoDesc opInfo;
                 opInfo.opType = param.opType;
