ddc913200eeb018b042f8455e51d33848630ff5f
ouyangcaike <ouyangcaike@h-partners.com>
2026-06-09 16:22:14 +0800

    build: add -Werror in target_compile_options

--- src/legacy/ascend910/algorithm/hccl_alg.cmake
+++ src/legacy/ascend910/algorithm/hccl_alg.cmake
@@ -14,6 +14,8 @@ add_library(hccl_alg SHARED)
 # 编译选项
 target_compile_options(hccl_alg PRIVATE
     # -Werror
+    -Wno-unused-parameter
+    -Wno-missing-field-initializers
     -fno-strict-aliasing
     $<$<CONFIG:Debug>:-g -O0>
     $<$<CONFIG:Release>:-O3>
